a5bd0e2b25810c6d1df937174a23483978c18f78
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-06-06 12:51:14 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,41 @@
-# write your code here
+# def collect_input(count: int, prefix: str):
+#     input_with_prefix = []
+#
+#     for _ in range(count):
+#         input_with_prefix.append(prefix + input(">>>"))
+#
+#     return input_with_prefix
+import os
+import sys
+from datetime import datetime
+
+
+def create_file_from_input():
+    terminal = sys.argv
+    path = ""
+    for i, command in enumerate(terminal[1:]):
+        if command == "-f":
+            write_content_to_file(terminal[i + 2], path)
+            break
+        elif command != "-d":
+            path += f"/{command}"
+
+
+def write_content_to_file(name_file, dir_path):
+    if dir_path:
+        if not os.path.exists(f"app{dir_path}"):
+            os.makedirs(f"app{dir_path}")
+        dir_path = "app" + dir_path + "/"
+
+    with open(dir_path + name_file, "a") as file:
+        now = datetime.now()
+        date_time = now.strftime("%m-%d-%Y, %H:%M:%S\n")
+        file.write(date_time)
+        while True:
+            new_content = input("Enter content line: ")
+            if new_content == "stop":
+                break
+            file.write(f"{new_content}\n")
+
+
+create_file_from_input()
