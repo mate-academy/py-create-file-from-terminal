580b12eae849c3611b496b9ba042e1c8c15102e3
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-06-06 19:55:36 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -7,27 +7,32 @@ def main():
     filename = ''
     path = ''
     param = sys.argv[1:]
+
     if '-f' in param:
         filename = param[-1]
         param = param[:-2]
+
     if '-d' in param:
         path = "/".join(param[1:])
-        os.makedirs(path)
+        if not os.path.exists(f"{path}"):
+            os.makedirs(path)
 
     if filename:
         name = filename
         if path:
             name = f'{path}/{filename}'
+
         with open(name, 'a') as f:
             now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             f.writelines(now + '\n')
             inp = ''
+            i = 1
             while inp != 'stop':
                 inp = input('input content line: ')
                 if inp == 'stop':
                     break
-                f.writelines(inp + '\n')
+                f.writelines(f'{i} {inp} \n')
+                i += 1
 
 
-if __name__ == "__main__":
-    main()
+main()
