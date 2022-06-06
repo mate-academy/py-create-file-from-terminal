0bd815eb7805e0a74618fe58bf84033c54136a02
Yurii Kindrat <yurkinkek@gmail.com>
2022-06-06 19:33:02 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,45 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def make_directory(path):
+    os.makedirs(path)
+
+
+def file_creator(name):
+    with open(f"{name}", 'a') as f:
+        f.write(datetime.datetime.now().strftime('\n%Y-%m-%d %H:%M:%S\n'))
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                break
+            f.write(f"{content}\n")
+
+
+def create_file():
+
+    parsed_commands = sys.argv
+
+    if "-d" in parsed_commands and "-f" in parsed_commands:
+
+        path = "/".join(parsed_commands[2:-2])
+        file_name = parsed_commands[-1]
+
+        make_directory(path)
+        file_creator(path + "/" + file_name)
+
+    elif "-d" in parsed_commands:
+
+        path = "/".join(parsed_commands[2:])
+
+        make_directory(path)
+
+    elif "-f" in parsed_commands:
+
+        file_name = parsed_commands[-1]
+
+        file_creator(file_name)
+
+
+create_file()
