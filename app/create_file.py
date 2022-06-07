139094e9c318c05decf88a17fadda69c17bdbaca
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-07 23:51:27 +0300

    Fix remarks, change 'w' on 'a+'

--- app/create_file.py
+++ app/create_file.py
@@ -4,22 +4,23 @@ from datetime import datetime
 
 
 def record_file(file_name, path):
-    if not os.path.exists(f"app/{path}"):
+    if not os.path.exists(f"{path}"):
         os.makedirs(f"{path}")
     create_file_from_input(path_and_file_name=f"{path}/{file_name}")
 
 
 def create_file_from_input(path_and_file_name):
 
-    with open(f"app/{path_and_file_name}", "w", encoding="utf8") as file:
+    with open(f"{path_and_file_name}", "a+", encoding="utf8") as file:
 
         datatime_now = datetime.now().strftime("%m-%d-%Y, %H:%M:%S\n")
         file.write(datatime_now)
 
-        line = ""
         i = 1
-        while line != "stop":
+        while True:
             line = input("Enter content line: ")
+            if line == "stop":
+                break
             file.write(f"{i} {line}\n")
             i += 1
 
@@ -27,13 +28,14 @@ def create_file_from_input(path_and_file_name):
 def create_file_from_terminal():
     terminal = sys.argv
     file_name, path = "", ""
+    index_f = -1
     if "-f" in terminal:
         index_f = terminal.index("-f")
         file_name = terminal[index_f + 1]
     if "-d" in terminal:
         index_d = terminal.index("-d")
         path = "/".join(terminal[index_d + 1:index_f])
-
+    print(file_name, path)
     record_file(file_name, path)
 
 
