730726035754e0b4ae804b0afeb448800897eae1
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-07 17:44:13 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,42 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+arguments = sys.argv
+dir_path = os.getcwd()
+
+for i in range(1, len(arguments)):
+
+    if arguments[i - 1] == "-d":
+        dir_path = os.path.join(os.getcwd(), arguments[i])
+        os.mkdir(dir_path)
+
+        for j in range(i + 1, len(arguments)):
+
+            if arguments[j] == "-f":
+                f = open(os.path.join(dir_path, arguments[j + 1]), "w")
+                f.close()
+                break
+
+            dir_path = os.path.join(dir_path, arguments[j])
+            os.mkdir(dir_path)
+
+    elif arguments[i - 1] == "-f":
+
+        if os.path.exists(arguments[i]):
+            with open(arguments[i], "a") as f:
+                line_content = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                while line_content != "stop":
+                    f.writelines(f"{line_content}\n")
+                    line_content = input("Enter content line: ")
+
+        else:
+            with open(os.path.join(dir_path, arguments[i]), "w") as f:
+                line_content = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                while line_content != "stop":
+                    f.writelines(f"{line_content}\n")
+                    line_content = input("Enter content line: ")
+                f.writelines("\n")
