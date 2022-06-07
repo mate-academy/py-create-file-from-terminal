1e431c19d79c5d2e62bfd8af0437f84a4686f89c
Yaroslav Havryliv <ghavryliv@gmail.com>
2022-06-07 17:26:49 +0300

    py-create-file-from-terminal solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,37 @@
-# write your code here
+import datetime
+import os
+import sys
+
+
+def handle_file(file_root):
+    with open(f"{file_root}", "a") as file:
+        counter = 1
+        file.write(
+            f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n'
+        )
+
+        while True:
+            inputted_data = input("Enter content line: ")
+            if inputted_data == "stop":
+                break
+            file.writelines(f"{counter} {inputted_data}\n")
+            counter += 1
+        file.write("\n")
+
+
+def make_dir(dirs):
+    path = os.path.join(*dirs)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+sys_argv = sys.argv
+
+if "-f" in sys_argv and "-d" not in sys_argv:
+    handle_file(f"{sys_argv[-1]}")
+elif "-f" in sys_argv and "-d" in sys_argv:
+    handle_file(f"{make_dir(sys_argv[2:-2])}{sys_argv[-1]}")
+elif "-d" in sys_argv and "-f" not in sys_argv:
+    make_dir(sys_argv[2:])
