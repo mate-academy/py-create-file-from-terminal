4b7973a2340a809f5916150718144a70a7881e91
Yurnerroo <yura.zdanevych@gmail.com>
2022-06-07 17:49:11 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,24 +3,7 @@ import os
 import datetime
 
 
-def control_function():
-    arguments = sys.argv
-
-    if "-d" in arguments and "-f" in arguments:
-        path_list = arguments[2:-2]
-        file_name = arguments[-1]
-        new_path = new_path_creation(path_list)
-        new_dir_file = os.path.join(new_path, file_name)
-        new_file_creation(new_dir_file)
-
-    if "-d" in arguments:
-        new_path_creation(arguments[2:])
-
-    if "-f" in arguments:
-        new_file_creation(arguments[2])
-
-
-def new_path_creation(lst: list) -> str:
+def create_path(lst: list) -> str:
     new_path = os.path.join(*lst)
 
     if not os.path.exists(new_path):
@@ -29,10 +12,10 @@ def new_path_creation(lst: list) -> str:
     return new_path
 
 
-def new_file_creation(file_name: str):
+def create_file(file_name: str):
     with open(file_name, "a") as new_file:
-        timestamp = datetime.datetime.now()
-        current_date = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        current_time = datetime.datetime.now()
+        current_date = current_time.strftime("%Y-%m-%d %H:%M:%S")
         new_file.write(current_date + "\n")
         counter_of_numbers = 1
 
@@ -46,3 +29,20 @@ def new_file_creation(file_name: str):
             else:
                 new_file.write(f"{counter_of_numbers} {content_line}\n")
                 counter_of_numbers += 1
+
+
+def control_function():
+    arguments = sys.argv
+
+    if "-d" in arguments and "-f" in arguments:
+        path_list = arguments[2:-2]
+        file_name = arguments[-1]
+        new_path = create_path(path_list)
+        new_dir_file = os.path.join(new_path, file_name)
+        create_file(new_dir_file)
+
+    if "-d" in arguments:
+        create_path(arguments[2:])
+
+    if "-f" in arguments:
+        create_file(arguments[2])
