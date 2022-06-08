55e00121488b60396a7eb306ae7d0280c1bdc16e
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-08 13:02:59 +0300

    Name of variable 'f' was refactored

--- app/create_file.py
+++ app/create_file.py
@@ -2,7 +2,6 @@ import os
 import sys
 from datetime import datetime
 
-
 arguments = sys.argv
 dir_path = os.getcwd()
 
@@ -15,8 +14,8 @@ for i in range(1, len(arguments)):
         for j in range(i + 1, len(arguments)):
 
             if arguments[j] == "-f":
-                f = open(os.path.join(dir_path, arguments[j + 1]), "w")
-                f.close()
+                file = open(os.path.join(dir_path, arguments[j + 1]), "w")
+                file.close()
                 break
 
             dir_path = os.path.join(dir_path, arguments[j])
@@ -26,20 +25,24 @@ for i in range(1, len(arguments)):
         line_number = 1
 
         if os.path.exists(arguments[i]):
-            with open(arguments[i], "a") as f:
-                line_content = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            with open(arguments[i], "a") as file:
+                date_on_page = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                file.writelines(f"{date_on_page}\n")
+                line_content = input("Enter content line: ")
 
                 while line_content != "stop":
-                    f.writelines(f"{line_number} {line_content}\n")
+                    file.writelines(f"{line_number} {line_content}\n")
                     line_number += 1
                     line_content = input("Enter content line: ")
-
+                file.writelines("\n")
         else:
-            with open(os.path.join(dir_path, arguments[i]), "w") as f:
-                line_content = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            with open(os.path.join(dir_path, arguments[i]), "w") as file:
+                date_on_page = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                file.writelines(f"{date_on_page}\n")
+                line_content = input("Enter content line: ")
 
                 while line_content != "stop":
-                    f.writelines(f"{line_number} {line_content}\n")
+                    file.writelines(f"{line_number} {line_content}\n")
                     line_number += 1
                     line_content = input("Enter content line: ")
-                f.writelines("\n")
+                file.writelines("\n")
