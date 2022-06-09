d3c1039e69061f58c7d78543d06db29847196420
Katya Vasylenko <kettyblack20@gmail.com>
2022-06-09 09:27:46 +0300

    Fixed Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,14 +3,14 @@ import os
 from datetime import datetime
 
 
-if "-d" in sys.argv and "-f" in sys.argv:
-    path = ""
-    for directory in sys.argv[sys.argv.index("-d") + 1: len(sys.argv) - 2]:
-        path += f"{directory}/"
-        if not os.path.isdir(path):
-            os.mkdir(path)
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
 
-    with open(path + sys.argv[-1], "a") as f:
+
+def create_file(file_name):
+    with open(file_name, "a") as f:
         now = datetime.now()
         date_time = now.strftime("%Y-%m-%d %H:%M:%S")
         f.write(date_time)
@@ -22,21 +22,22 @@ if "-d" in sys.argv and "-f" in sys.argv:
             line = input("Enter content line: ")
 
 
-if "-d" in sys.argv and "-f" not in sys.argv:
-    path = ""
-    for directory in sys.argv[sys.argv.index("-d") + 1: len(sys.argv)]:
-        path += f"{directory}/"
-        if not os.path.isdir(path):
-            os.mkdir(path)
+def create_directory_and_file():
+    if "-d" in sys.argv and "-f" in sys.argv:
+        path = ""
+        for directory in sys.argv[sys.argv.index("-d") + 1: len(sys.argv) - 2]:
+            path += f"{directory}/"
+        create_directory(path)
+        create_file(f"{path}/{sys.argv[-1]}")
 
-if "-f" in sys.argv and "-d" not in sys.argv:
-    with open(sys.argv[-1], "a") as f:
-        now = datetime.now()
-        date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-        f.write(date_time)
-        i = 0
-        line = ""
-        while line != "stop":
-            f.write(f"{i} {line} \n")
-            i += 1
-            line = input("Enter content line: ")
+    elif "-d" in sys.argv and "-f" not in sys.argv:
+        path = ""
+        for directory in sys.argv[sys.argv.index("-d") + 1: len(sys.argv) - 2]:
+            path += f"{directory}/"
+        create_directory(path)
+
+    elif "-f" in sys.argv and "-d" not in sys.argv:
+        create_file(sys.argv[-1])
+
+
+create_directory_and_file()
