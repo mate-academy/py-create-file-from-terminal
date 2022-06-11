0b5eeedc811f2bcbbc91699adfd2db1c632e20b0
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-06-11 20:31:47 +0300

    'FixedErrors'

--- app/create_file.py
+++ app/create_file.py
@@ -2,27 +2,30 @@ from datetime import datetime
 import os
 import sys
 
-parent_dir = r"C:\PytyhonLearn\py-create-file-from-terminal\app"
+parent_dir = os.getcwd()
 dir_ = []
 
-time = datetime.now()
-timee = time.strftime("%Y/%m/%d %H:%M:%S")
+time_now = datetime.now()
+time_formated = time_now.strftime("%Y/%m/%d %H:%M:%S")
 
 if "-f" in sys.argv and "-d" in sys.argv:
     copy = sys.argv
     d_index = copy.index("-d")
     f_index = copy.index("-f")
-    dir_ = copy[d_index + 1:f_index]
-    path = '/'.join(dir_)
+    directory = copy[d_index + 1:f_index]
+    directoryy = str(directory)
+    path = '/'.join(directory)
     name = copy[f_index + 1]
-    os.makedirs(path)
-    final_path = path + '/' + name
-    with open(final_path, "a") as f:
-        f.write(str(timee) + "\n")
+    final_path = os.path.join(path + '/' + name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(final_path, "a+") as f:
+        f.write(str(time_formated) + "\n")
         count = 1
         while True:
             content = input('Enter the line content:')
             if content == 'stop':
+                f.write(" \n")
                 break
             f.write(str(count) + " " + content + "\n")
             count += 1
@@ -30,19 +33,20 @@ if "-f" in sys.argv and "-d" in sys.argv:
 elif "-d" in sys.argv:
     copy = sys.argv
     d_index = copy.index("-d")
-    dir_ = copy[d_index + 1:]
-    path = '/'.join(dir_)
+    directory = copy[d_index + 1:]
+    path = '/'.join(directory)
     os.makedirs(path)
 
 elif "-f" in sys.argv:
     name = sys.argv[2]
     current_name = name
-    with open(current_name, 'a') as f:
-        f.write(str(timee) + "\n")
+    with open(current_name, 'a+') as f:
+        f.write(str(time_formated) + "\n")
         count = 1
         while True:
             content = input('Enter the line content:')
             if content == 'stop':
+                f.write("\n")
                 break
             f.write(str(count) + " " + content + "\n")
             count += 1
