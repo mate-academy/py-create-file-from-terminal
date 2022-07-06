54dc21393053f13b3813c2447380c2c5f21d73bc
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-07-06 17:42:36 +0300

    NewVersion

--- app/create_file.py
+++ app/create_file.py
@@ -1,52 +1,72 @@
-from datetime import datetime
+import argparse
 import os
-import sys
 
-parent_dir = os.getcwd()
-dir_ = []
+from datetime import datetime
 
-time_now = datetime.now()
-time_formated = time_now.strftime("%Y/%m/%d %H:%M:%S")
 
-if "-f" in sys.argv and "-d" in sys.argv:
-    copy = sys.argv
-    d_index = copy.index("-d")
-    f_index = copy.index("-f")
-    directory = copy[d_index + 1:f_index]
-    directoryy = str(directory)
-    path = '/'.join(directory)
-    name = copy[f_index + 1]
-    final_path = os.path.join(path + '/' + name)
-    if not os.path.exists(path):
-        os.makedirs(path)
-    with open(final_path, "a+") as f:
-        f.write(str(time_formated) + "\n")
-        count = 1
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+def get_args_from_command() -> tuple:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-d",
+        dest="dirs",
+        type=str,
+        nargs="*",
+        help="List of dirs"
+    )
+    parser.add_argument(
+        "-f",
+        dest="file_name",
+        type=str,
+        nargs=1,
+        help="file name"
+    )
+    args = parser.parse_args()
+
+    return args.dirs, args.file_name
+
+
+def build_and_get_path(dirs: list[str], file_name: list[str]) -> str:
+    path = ""
+
+    if dirs:
+        path = os.path.join(*dirs)
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            print("Folders already exists")
+
+    if file_name:
+        return os.path.join(path, *file_name)
+
+    return ""
+
+
+def file_cli(path: str):
+    with open(path, "a") as file:
+        timestamp = datetime.now().strftime(TIME_FORMAT)
+        file.write(f"{timestamp}\n")
+
+        line_num = 1
         while True:
-            content = input('Enter the line content:')
-            if content == 'stop':
-                f.write(" \n")
+            line = input("Enter content line: ")
+            if line.lower() == "stop":
+                file.write("\n")
                 break
-            f.write(str(count) + " " + content + "\n")
-            count += 1
+            file.write(f"{line_num} {line}\n")
+            line_num += 1
 
-elif "-d" in sys.argv:
-    copy = sys.argv
-    d_index = copy.index("-d")
-    directory = copy[d_index + 1:]
-    path = '/'.join(directory)
-    os.makedirs(path)
 
-elif "-f" in sys.argv:
-    name = sys.argv[2]
-    current_name = name
-    with open(current_name, 'a+') as f:
-        f.write(str(time_formated) + "\n")
-        count = 1
-        while True:
-            content = input('Enter the line content:')
-            if content == 'stop':
-                f.write("\n")
-                break
-            f.write(str(count) + " " + content + "\n")
-            count += 1
+def main():
+    args = get_args_from_command()
+
+    path = build_and_get_path(*args)
+
+    if path:
+        file_cli(path)
+
+
+main()
