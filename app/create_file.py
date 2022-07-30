b1ccc21328de8183dafddeec2f775443ba8b3c79
Misha Hrynko <misha@mate.academy>
2022-07-30 21:59:01 +0300

    i hope that is Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+import sys
+
+from app.append_to_file import append_content
+from app.create_file_with_content import create_file_with_content
+
+
+def create_file():
+    com1, com2 = "-d", "-f"
+    command = sys.argv
+    if com1 in command and com2 in command:
+        path = "/".join(command[command.index(com1) + 1: command.index(com2)])
+        os.makedirs(path)
+        create_file_with_content(command)
+    if com1 in command:
+        path = "/".join(command[(command.index(com1) + 1):])
+        os.makedirs(path)
+    if com2 in command:
+        if os.path.exists(command[-1]):
+            append_content(command)
+        else:
+            create_file_with_content(command)
