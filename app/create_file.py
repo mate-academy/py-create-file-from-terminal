02ffc219da9f4a8c75bab9dcf5ec5dba4eb813ce
AnDy <ansmbox@gmail.com>
2022-08-18 16:38:38 +0300

    Replace code to a function

--- app/create_file.py
+++ app/create_file.py
@@ -3,23 +3,32 @@ import argparse
 import datetime
 
 
-if __name__ == '__main__':
+def create_file_from_terminal(cmd_line: str) -> None:
+    """create_file_from_terminal(cmd_line: str)
+
+    Parse command line and create directories and file.
+    Flags:
+        -d - create directory with given names
+        -f - create file with given name
+    Content of the file should be entered by user from terminal.
+
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", dest="dir_name", nargs="+")
     parser.add_argument("-f", dest="file_name", nargs=1)
-    args = parser.parse_args()
+    args = parser.parse_args(cmd_line.split())
 
     file_path = os.path.curdir
 
     if args.dir_name:
         file_path = f"{os.sep}".join(args.dir_name)
-        os.makedirs(name=file_path, mode=0o777, exist_ok=True)
+        os.makedirs(file_path, 0o777, True)
 
     if args.file_name:
         file_name = os.path.join(file_path, args.file_name[0])
         line_number = 1
 
-        with open(file=file_name, mode="a") as f:
+        with open(file_name, "a") as f:
             line_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             f.write(f"{line_date}\n")
 
