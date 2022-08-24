c8907e8dbb890b9ac9455b144e4bbc0149929a65
Dima Povkh <dimapovkh@gmail.com>
2022-08-25 00:10:39 +0200

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,26 @@
-# write your code here
+from datetime import datetime
+import os
+import sys
+
+
+def create_file():
+    if '-d' in sys.argv:
+        index = 2
+        while index != len(sys.argv) and sys.argv[index] != "-f":
+            os.mkdir(sys.argv[index])
+            os.chdir(sys.argv[index])
+            index += 1
+
+    if '-f' in sys.argv:
+        with open(sys.argv[-1], "a") as f:
+            f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+            line = 1
+            while True:
+                text = input("Enter content line: ")
+                if text == "stop":
+                    break
+                f.write(f"{line} {text} \n")
+                line += 1
+
+
+create_file()
