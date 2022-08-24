1faeac186feac30535f34a0278933ed9f4f6e3e7
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-08-24 13:00:44 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,34 @@
-# write your code here
+import sys
+from os import mkdir
+from time import localtime, strftime
+
+
+def create_file_from_command_line(args):
+    create_dir = False
+    create_file = False
+    directory = ""
+    for arg in args:
+        if arg == "-f":
+            create_file = True
+            create_dir = False
+            continue
+        if arg == '-d':
+            create_dir = True
+            create_file = False
+            continue
+        if create_file:
+            with open(f"{directory}{arg}", "w") as f:
+                print(strftime("%Y-%m-%d %H:%M:%S", localtime()), file=f)
+                while True:
+                    text = input("Enter content line: ")
+                    if text == "stop":
+                        break
+                    print(text, file=f)
+            create_file = False
+        if create_dir:
+            directory += f"{arg}/"
+            mkdir(directory)
+
+
+if __name__ == '__main__':
+    create_file_from_command_line(sys.argv)
