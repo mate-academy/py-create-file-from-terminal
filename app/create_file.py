323d6f145ff301aef416c541ed40b9c5879e1b36
Dima Povkh <dimapovkh@gmail.com>
2022-08-25 10:53:56 +0200

    replace '' -> "" rename f -> file

--- app/create_file.py
+++ app/create_file.py
@@ -11,15 +11,15 @@ def create_file():
             os.chdir(sys.argv[index])
             index += 1
 
-    if '-f' in sys.argv:
-        with open(sys.argv[-1], "a") as f:
-            f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+    if "-f" in sys.argv:
+        with open(sys.argv[-1], "a") as file:
+            file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
             line = 1
             while True:
                 text = input("Enter content line: ")
                 if text == "stop":
                     break
-                f.write(f"{line} {text} \n")
+                file.write(f"{line} {text} \n")
                 line += 1
 
 
