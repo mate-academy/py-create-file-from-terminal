0401f9edab4c184305a37ebfb976d80b2accdbc9
Dmitriy Kostiuk <kostiuk.dmitriy@gmail.com>
2022-08-25 11:56:00 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,45 +3,41 @@ import sys
 import os
 
 
-def create_path(arr):
+def create_path(arr: list, both_flags=False):
     arr_dir = [os.path.dirname(os.path.abspath(__file__))]
-    add = False
-    for arg in arr:
-        if arg == '-d':
-            add = True
-            continue
-        if not add:
-            continue
-        if arg == '-f':
-            break
-        if add:
-            arr_dir.append(arg)
-
+    ind_d = arr.index("-d") + 1
+    ind_f = arr.index("-f") if both_flags else len(arr)
+    dir_list = arr[ind_d: ind_f]
+    for arg in dir_list:
+        arr_dir.append(arg)
     return os.path.join(*arr_dir)
 
 
-if '-d' in sys.argv and '-f' in sys.argv:
-    print("both flags are present. Create directories and file")
-    path = create_path(sys.argv)
-    print(path)
-    os.makedirs(path, exist_ok=True)
-    path = os.path.join(path, sys.argv[-1])
-elif '-d' in sys.argv:
-    print("Only flag '-d' is present. Create directories")
-    path = create_path(sys.argv)
-    os.makedirs(path)
-elif 'if' in sys.argv:
-    print("Only flag '-f' is present. Create file")
-    path = sys.argv[2]
-if '-f' in sys.argv:
-    with open(path, 'at', newline='\n') as f:
-        f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-        num = 1
-        res = ""
-        while True:
-            res = input("Enter content line:")
-            if res == "stop":
-                break
-            f.write(f"{num} {res}\n")
-            num += 1
-        f.close()
+def main():
+    if "-d" in sys.argv and "-f" in sys.argv:
+        path = create_path(sys.argv, True)
+        os.makedirs(path, exist_ok=True)
+        path = os.path.join(path, sys.argv[-1])
+    elif "-d" in sys.argv:
+        path = create_path(sys.argv)
+        os.makedirs(path)
+        return
+    elif "-f" in sys.argv:
+        path = sys.argv[-1]
+    if "-f" in sys.argv:
+        with open(path, 'at', newline='\n') as f:
+            cur_date = datetime.datetime.now()
+            f.write(f"{cur_date.strftime('%Y-%m-%d %H:%M:%S')}\n")
+            num = 1
+            res = ""
+            while True:
+                res = input("Enter content line:")
+                if res == "stop":
+                    break
+                f.write(f"{num} {res}\n")
+                num += 1
+            f.close()
+
+
+if __name__ == "__main__":
+    main()
