bb84ffb3ad2a17d3d16efc98110a4f6c0f6d9623
yaroslav-demchenko <dastex1411@gmail.com>
2022-08-26 17:20:51 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,39 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_file_from_terminal(path):
+    create_dir = False
+    create_file = False
+    for text in path:
+        if text == "-d":
+            create_dir = True
+        if text == "-f":
+            create_file = True
+    if create_dir:
+        path_str = ""
+        index = 0
+        for txt in path:
+            if txt == "-f":
+                index = 0
+            if index == 1:
+                path_str += txt
+                path_str += "/"
+            if txt == "-d":
+                index = 1
+        path_str = path_str[:-1]
+        os.makedirs(path_str)
+    if create_file:
+        file_name = path[-1]
+        with open(file_name, "a") as f:
+            f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            content = input("Enter content line:")
+            while content != "stop":
+                f.write("\n")
+                f.write(content)
+                content = input("Enter content line:")
+
+
+if __name__ == "__main__":
+    create_file_from_terminal(sys.argv)
