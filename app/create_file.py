a8a3e139db9f9283713418fd9f9e0af379d4b3c0
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-09-05 14:09:10 +0300

    Task Complete

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,21 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_file():
+    if "-d" in sys.argv:
+        i = 2
+        while len(sys.argv) != i and sys.argv[i] != "-f":
+            os.mkdir(sys.argv[i])
+            os.chdir(sys.argv[i])
+            i += 1
+    if "-f" in sys.argv:
+        with open(sys.argv[-1], "w") as file:
+            file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+            line = 1
+            new_line = input("Enter content line: ")
+            while new_line != "stop":
+                file.write("Line" + f"{line}" + f"{new_line}")
+                new_line = input("Enter content line: ")
+                line += 1
