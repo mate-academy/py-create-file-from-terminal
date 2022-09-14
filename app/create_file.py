44110a90e3b7de72b984903a4206e8e4c616c4dc
Paul Starodub <amorallex@gmail.com>
2022-09-14 20:38:58 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,33 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+list_command = sys.argv
+if "-d" in list_command and "-f" not in list_command:
+    args = list_command[2:]
+    path_dir = os.path.join(*args)
+    os.makedirs(path_dir)
+
+if "-d" in list_command and "-f" in list_command:
+    args = list_command[2:-2]
+    path_dir = os.path.join(*args)
+    os.makedirs(path_dir)
+    path_file = list_command[-1:][-1]
+    with open(f"{path_dir}/{path_file}", "w") as f:
+        f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        while True:
+            data = input("Enter information: ")
+            if data == "stop":
+                break
+            f.write(f"{data}\n")
+
+if "-d" not in list_command and "-f" in list_command:
+    path_file = list_command[-1:][-1]
+    with open(f"{path_file}", "a") as f:
+        f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        while True:
+            data = input("Enter information: ")
+            if data == "stop":
+                break
+            f.write(f"{data}\n")
