83c19a8f2458a2934e2a500b4492f793192f785f
Paul Starodub <amorallex@gmail.com>
2022-09-15 10:49:44 +0300

    Task

--- app/create_file.py
+++ app/create_file.py
@@ -4,25 +4,30 @@ from datetime import datetime
 
 
 def create_file(path_of_file):
-    with open(path_of_file, "a") as f:
-        f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+    with open(path_of_file, "a") as file:
+        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         while True:
             data = input("Enter information: ")
             if data == "stop":
                 break
-            f.write(f"{data}\n")
+            file.write(f"{data}\n")
 
 
-if "-d" in sys.argv and "-f" not in sys.argv:
-    args = sys.argv[2:]
-    path_dir = os.path.join(*args)
-    os.makedirs(path_dir)
-if "-d" in sys.argv and "-f" in sys.argv:
-    args = sys.argv[2:-2]
-    path_dir = os.path.join(*args)
-    os.makedirs(path_dir)
-    path_file = path_dir + "/" + sys.argv[-1]
-    create_file(path_file)
-if "-d" not in sys.argv and "-f" in sys.argv:
-    path_file = sys.argv[-1]
-    create_file(path_file)
+def get_command(command_string: list):
+    if "-d" in command_string and "-f" not in command_string:
+        args = command_string[2:]
+        path_dir = os.path.join(*args)
+        os.makedirs(path_dir)
+    if "-d" in command_string and "-f" in command_string:
+        args = command_string[2:-2]
+        path_dir = os.path.join(*args)
+        os.makedirs(path_dir)
+        path_file = path_dir + "/" + command_string[-1]
+        create_file(path_file)
+    if "-d" not in command_string and "-f" in command_string:
+        path_file = command_string[-1]
+        create_file(path_file)
+
+
+if __name__ == '__main__':
+    get_command(sys.argv)
