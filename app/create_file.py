2b830fe745dad052933d5a9b7044fe53119181c5
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-09-20 16:07:34 +0300

    Block name == main was added

--- app/create_file.py
+++ app/create_file.py
@@ -2,44 +2,49 @@ from datetime import datetime
 from os import makedirs, path
 from sys import argv
 
-directories = []
-file_name = ""
 
-try:
-    ind = argv.index("-d") + 1
-except ValueError:
-    pass
-else:
-    while ind < len(argv) and argv[ind] != "-f":
-        directories.append(argv[ind])
-        ind += 1
-finally:
+def create_file():
+    directories = []
+    file_name = ""
+
     try:
-        file_name = argv[argv.index("-f") + 1]
+        ind = argv.index("-d") + 1
     except ValueError:
         pass
-
-
-if directories:
-    makedirs("/".join(directories))
-
-if file_name:
-    full_path = path.join("/".join(directories), file_name)
-
-    if path.exists(full_path):
-        file = open(full_path, "a")
-        file.write('\n\n')
     else:
-        file = open(full_path, "w")
+        while ind < len(argv) and argv[ind] != "-f":
+            directories.append(argv[ind])
+            ind += 1
+    finally:
+        try:
+            file_name = argv[argv.index("-f") + 1]
+        except ValueError:
+            pass
 
-    file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
-    inputted_list = []
+    if directories:
+        makedirs("/".join(directories))
 
-    while True:
-        inputted = input("Enter content line:")
-        if inputted == "stop":
-            break
-        inputted_list.append(inputted)
+    if file_name:
+        full_path = path.join("/".join(directories), file_name)
 
-    file.write('\n'.join(inputted_list))
-    file.close()
+        if path.exists(full_path):
+            file = open(full_path, "a")
+            file.write('\n\n')
+        else:
+            file = open(full_path, "w")
+
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+        inputted_list = []
+
+        while True:
+            inputted = input("Enter content line:")
+            if inputted == "stop":
+                break
+            inputted_list.append(inputted)
+
+        file.write('\n'.join(inputted_list))
+        file.close()
+
+
+if __name__ == "__main__":
+    create_file()
