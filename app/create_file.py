14d62f4ed11bf8f2a1681d0f21b86477fbb8434c
Artem <Artemvyhovanec@gmail.com>
2022-09-29 22:01:07 +0300

    small upgrade

--- app/create_file.py
+++ app/create_file.py
@@ -16,14 +16,14 @@ if command_list[1] == "-d":
 
 if "-f" in command_list:
     f_index = command_list.index('-f')
-    with open(f"{os.path.join(directory, command_list[f_index + 1])}",
-              "a") as f:
-        f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+    with open(str(os.path.join(directory, command_list[f_index + 1])),
+              "a") as file:
+        file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         line_number = 1
         while True:
             line = str(input("Enter content line: "))
             if line == "stop":
-                f.write("\n")
+                file.write("\n")
                 break
-            f.write(f"{line_number} {line}\n")
+            file.write(f"{line_number} {line}\n")
             line_number += 1
