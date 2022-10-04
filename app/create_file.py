791c12f0b1b8b878b542e8618184527d1a4c7e2f
Kyryll Katash <kyryllkatash@gmail.com>
2022-10-04 15:20:14 +0300

    Solution for this task

--- app/create_file.py
+++ app/create_file.py
@@ -14,15 +14,16 @@ def file_creation(file_name: str):
 
     if "-f" in file_name:
         path_to_file.append(file_name[file_name.index('-f') + 1])
-        with open(os.path.join(*path_to_file), "a") as f:
-            f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            f.write("\n")
+        with open(os.path.join(*path_to_file), "a") as file_in:
+            file_in.write(datetime.datetime.now().strftime("%Y-%m-%d "
+                                                           "%H:%M:%S"))
+            file_in.write("\n")
             line = 1
             while True:
                 input_txt = input("Enter content line: ")
                 if input_txt.lower() == "stop":
                     return
-                f.write(f"{line} {input_txt} \n")
+                file_in.write(f"{line} {input_txt} \n")
                 line += 1
 
 
