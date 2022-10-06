c08b0a914e82882ea23a8de59f8757feb86d1def
Zakhar Podporinov <zp162162@gmail.com>
2022-10-06 23:51:49 +0300

    Deleted prints and added line numbers

--- app/create_file.py
+++ app/create_file.py
@@ -10,19 +10,19 @@ parser.add_argument("-f", "--file", help="File name")
 
 command = vars(parser.parse_args())
 path = ""
-print(command, type(command), path)
 
 if command["dir"]:
     path = os.path.join(*command["dir"])
     os.makedirs(path)
 
-print(command, type(command), path)
 
 with open(os.path.join(path, command["file"]), "w") as file:
     current_time = datetime.datetime.now()
-    file.write(current_time.strftime("%y-%m-%d %H:%M:%S") + "\n")
+    file.write(current_time.strftime("%y-%m-%d %H:%M:%S\n"))
+    count = 0
     while True:
         text = input("Enter content line: ")
         if text == "stop":
             break
-        file.write(text + "\n")
+        count += 1
+        file.write(f"{count} " + text + "\n")
