d90b7e020d9ef42303a7643c460563cb24e6d8a2
Zakhar Podporinov <zp162162@gmail.com>
2022-10-06 16:32:11 +0300

    Complete!

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,28 @@
-# write your code here
+import argparse
+import os
+import datetime
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d", "--dir", nargs="+", help="Create directory")
+parser.add_argument("-f", "--file", help="File name")
+
+command = vars(parser.parse_args())
+path = ""
+print(command, type(command), path)
+
+if command["dir"]:
+    path = os.path.join(*command["dir"])
+    os.makedirs(path)
+
+print(command, type(command), path)
+
+with open(os.path.join(path, command["file"]), "w") as file:
+    current_time = datetime.datetime.now()
+    file.write(current_time.strftime("%y-%m-%d %H:%M:%S") + "\n")
+    while True:
+        text = input("Enter content line: ")
+        if text == "stop":
+            break
+        file.write(text + "\n")
