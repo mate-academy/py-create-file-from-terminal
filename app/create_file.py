a0a44399bf7008ebb9a493f0ebee72ae5f3847e1
Taras Andruschenko <And999110@gmail.com>
2022-10-06 19:45:22 +0300

    Fix complicated solution by a bit simpler way

--- app/create_file.py
+++ app/create_file.py
@@ -4,27 +4,14 @@ import sys
 from datetime import datetime
 
 
-def get_commands(key: str) -> str:
-    commands = {
-        "-d": None,
-        "-f": None,
-    }
-    for i, char in enumerate(sys.argv[1:]):
-        if char in commands:
-            while i + 1 >= len(sys.argv) or sys.argv[i + 1] in commands:
-                commands[char] = sys.argv[i + 2]
-                i += 1
-    return commands.get(key)
-
-
-def create_path() -> str:
-    path = os.path.join(get_commands("-d"))
+def create_path(path) -> None:
+    path = os.path.join(" ".join(path))
     os.makedirs(path, exist_ok=True)
-    return path
+    os.chdir(path)
 
 
-def create_file() -> None:
-    with open(get_commands("-f"), "a") as new_file:
+def create_file(name: str) -> None:
+    with open(name, "a") as new_file:
         new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
         line = input("Enter content line: ")
         row = 1
@@ -35,18 +22,21 @@ def create_file() -> None:
 
 
 def create_file_from_terminal() -> None:
-    if get_commands("-d") is not None and get_commands("-f") is not None:
-        os.chdir(create_path())
-        create_file()
-    elif get_commands("-d") is not None:
-        create_path()
-    elif get_commands("-f") is not None:
-        create_file()
-    else:
-        print(
-            "Enter the command with flag '-d' - create directory,"
-            "with flag '-f' - create file or with both of them"
-        )
+    command = sys.argv[1::]
+    d_start_index = command.index("-d")
+    f_start_index = command.index("-f")
+    file_name = command[-1]
+    path = command[d_start_index + 1: f_start_index]
+
+    if "-d" in command and "-f" in command:
+        create_path(path)
+        create_file(file_name)
+
+    if "-f" in command and "-d" not in command:
+        create_file(file_name)
+
+    if "-d" in command and "-f" not in command:
+        create_path(path)
 
 
 create_file_from_terminal()
