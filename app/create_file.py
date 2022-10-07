ad280df0365c2c3dbfd902c6187e30007c2f4f8b
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-07 14:44:27 +0300

    Create_file

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,25 @@
-# write your code here
+import argparse
+import os
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d", type=str, nargs="+", default=[])
+parser.add_argument("-f", type=str, default="")
+
+args = parser.parse_args()
+
+dire = args.d
+file_name = args.f
+complete_name = os.path.join("/".join(dire), file_name)
+date = datetime.now().strftime("%Y-%m-%d %I:%M:%S")
+
+if dire:
+    os.makedirs("/".join(dire))
+
+if file_name:
+    with open(complete_name, "a") as f:
+        f.write(f"{date}\n")
+
+        while (line := input("Enter content line: ")) != "stop":
+            f.write(f"{line}\n")
