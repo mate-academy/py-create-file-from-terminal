8b2449ecb947620f27eda8c55e5416776d0d393f
Edik Marushchak <e.moscow138@gmail.com>
2022-10-07 19:10:04 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,28 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+catalog = ""
+command_list = sys.argv
+if command_list[1] == "-d":
+    try:
+        f_command = command_list.index("-f")
+    except ValueError:
+        f_command = None
+    catalog = os.path.join(*command_list[2:f_command])
+    if not os.path.exists(catalog):
+        os.makedirs(catalog)
+
+if "-f" in command_list:
+    f_index = command_list.index('-f')
+    with open(str(os.path.join(catalog, command_list[f_index + 1])),
+              "a") as file:
+        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        line_number = 1
+        while True:
+            line = str(input("Enter content line: "))
+            if line == "stop":
+                file.write("\n")
+                break
+            file.write(f"{line_number} {line}\n")
+            line_number += 1
