c736108a70569de3ce6e4e03dffd5acb403c5954
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-07 15:27:00 +0300

    dire -> directory_name and add number_of line

--- app/create_file.py
+++ app/create_file.py
@@ -9,17 +9,19 @@ parser.add_argument("-f", type=str, default="")
 
 args = parser.parse_args()
 
-dire = args.d
+directory_name = args.d
 file_name = args.f
-complete_name = os.path.join("/".join(dire), file_name)
+complete_name = os.path.join("/".join(directory_name), file_name)
 date = datetime.now().strftime("%Y-%m-%d %I:%M:%S")
 
-if dire:
-    os.makedirs("/".join(dire))
+if directory_name:
+    os.makedirs("/".join(directory_name))
 
 if file_name:
     with open(complete_name, "a") as f:
         f.write(f"{date}\n")
 
+        number_of_line = 1
         while (line := input("Enter content line: ")) != "stop":
-            f.write(f"{line}\n")
+            f.write(f"{number_of_line}. {line}\n")
+            number_of_line += 1
