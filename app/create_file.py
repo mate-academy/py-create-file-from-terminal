d525ba38be3ca5b7c46d4e24fc2d7111fd3cf710
Sergii Franchuk <Franchuksergei@gmail.com>
2022-10-09 22:16:21 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,35 +3,44 @@ import sys
 from datetime import datetime
 
 
-def file_write(file_name: str, path: str = "") -> None:
-    with open(path + file_name, "a") as file:
-        line = ""
-        file.write(datetime.now().strftime("%y-%m-%d %H:%M:%S\n"))
-        while line != "stop":
+def file_write(file_name: str) -> None:
+
+    with open(file_name, "a") as file:
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+
+        while True:
             line = input("Enter content line>>>")
-            file.write(line)
+            if line == "stop":
+                file.write("\n")
+                break
+            file.write(line + "\n")
 
 
 def dir_create(command: list[str]) -> str:
     path = ""
-    for folder in command:
+
+    for folder in command[command.index("-d") + 1:]:
         if folder == "-f":
             break
         path = os.path.join(path, folder)
+
     if not os.path.isdir(path):
         os.makedirs(path)
-        return path
+
+    return path
 
 
 command = sys.argv
 
 while "exit" not in command:
     path = ""
+
     if "-d" in command:
         path = dir_create(command)
+
     if "-f" in command:
-        file_name = command[command.index("-f") + 1]
-        file_write(file_name, path)
+        file_name = os.path.join(path, command[command.index("-f") + 1])
+        file_write(file_name)
 
     command = input("Press exit or enter command>>>")
     command = command.split()
