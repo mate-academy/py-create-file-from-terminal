c61af9eb96ae5ee68a1061e36eb77b6eea5ea867
Dmytro Petliovanyi <petliovany@gmail.com>
2022-10-09 16:40:25 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,33 @@
-# write your code here
+from os import chdir, mkdir
+from sys import argv
+from datetime import datetime
+
+
+def create_file() -> None:
+    if "-d" in argv:
+        i = 2
+
+        while len(argv) != i:
+            if argv[i] == "-f":
+                i += 1
+                continue
+
+            mkdir(argv[i])
+            chdir(argv[i])
+            i += 1
+
+    if "-f" in argv:
+        with open(argv[-1], "w") as f:
+            date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            f.write(f"{date}\n")
+
+            current_line = 1
+            inline_content = input("Enter content line: ")
+
+            while inline_content != "stop":
+                f.write(f"Line{current_line} {inline_content}\n")
+                inline_content = input("Enter content line: ")
+                current_line += 1
+
+
+create_file()
