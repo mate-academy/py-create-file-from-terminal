3975ea3cfa5d37b7e86534fe475cf6bf17d7eb66
Zakhar Podporinov <zp162162@gmail.com>
2022-10-10 18:49:02 +0300

    Add a blank lines and fix error

--- app/create_file.py
+++ app/create_file.py
@@ -14,10 +14,26 @@ if command["dir"]:
     path = os.path.join(*command["dir"])
     os.makedirs(path, exist_ok=True)
 
-with open(os.path.join(path, command["file"]), "a") as file:
-    current_time = datetime.datetime.now()
-    file.write(current_time.strftime("%y-%m-%d %H:%M:%S\n"))
-    count = 0
-    while (text := input("Enter content line: ")) != "stop":
-        count += 1
-        file.write(f"{count} " + text + "\n")
+if command["file"]:
+    file_path = os.path.join(path, command["file"])
+
+    try:
+        with open(file_path, "r") as file:
+            if not file.read().isspace():
+                add_line = True
+            else:
+                add_line = False
+    except FileNotFoundError:
+        add_line = False
+
+    with open(file_path, "a") as file:
+        if add_line:
+            file.write("\n")
+
+        current_time = datetime.datetime.now()
+        file.write(current_time.strftime("%y-%m-%d %H:%M:%S\n"))
+        count = 0
+
+        while (text := input("Enter content line: ")) != "stop":
+            count += 1
+            file.write(f"{count} " + text + "\n")
