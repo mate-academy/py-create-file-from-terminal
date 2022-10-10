092832120cdf7a8703afc1f5e1303fba9a5379ae
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2022-10-10 14:13:58 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+for line in sys.stdin:
+    if "python create_file.py" in line.rstrip():
+        file_data: list[str] = line.rstrip().split()
+        path = ""
+        if "-d" in file_data:
+            no_index: int = file_data.index("-d") + 1
+            while file_data[no_index] != "-f":
+                path = os.path.join(path, file_data[no_index])
+                no_index += 1
+            os.makedirs(path, exist_ok=True)
+        if "-f" in file_data:
+            no_index: int = file_data.index("-f") + 1
+            path = os.path.join(path, file_data[no_index])
+        with open(path, "a") as f:
+            f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+            input_data = ""
+            while True:
+                input_data = input("Enter content line: ")
+                if input_data == "stop":
+                    f.write("\n")
+                    break
+                f.write(f"{input_data}\n")
+            break
