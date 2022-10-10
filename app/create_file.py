02e242fe156c620b624ee7617222339372cca272
Dmytro Skrypka <2346677tyu@gmail.com>
2022-10-10 19:04:24 +0300

    Solution add ValueError

--- app/create_file.py
+++ app/create_file.py
@@ -8,22 +8,28 @@ def create_file() -> None:
     path = ""
 
     if "-d" in info:
+        try:
+            f_error = info.index("-f")
+        except ValueError:
+            f_error = None
         path += os.path.join(
             *info[(info.index("-d") + 1):info.index("-f")]
         )
         if os.path.exists(rf"{path}\{info[-1]}") is False:
             os.makedirs(path)
 
-    with open(rf"{path}\{info[-1]}", "a") as file:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        lines = ""
-        line_two = 0
-        while (line := input("Enter content line: ")) != "stop":
-            lines += f"{line_two} {line}\n"
-            line_two += 1
+    if "-f" in info:
+        f_error = info.index("-f")
+        with open(rf"{path}\{info[-1]}", "a") as file:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            lines = ""
+            line_two = 0
+            while (line := input("Enter content line: ")) != "stop":
+                lines += f"{line_two} {line}\n"
+                line_two += 1
 
-        if lines:
-            file.write(f"{current_time}\n{lines}\n")
+            if lines:
+                file.write(f"{current_time}\n{lines}\n")
 
 
 if __name__ == "__main__":
