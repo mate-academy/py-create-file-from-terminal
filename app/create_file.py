4d0394a3eea6646b4bc9b2fef39a875f4914cff1
Sergey <servizah@gmail.com>
2022-10-11 14:00:08 +0300

    check append algorithm

--- app/create_file.py
+++ app/create_file.py
@@ -12,20 +12,24 @@ def create_file(file_name: str) -> None:
             file_out.write(f"{line_num} {line}\n")
             line = input("Enter content line: ")
             line_num += 1
+        file_out.write("\n")
 
 
 if "-d" in sys.argv and "-f" in sys.argv:
     arg_d = sys.argv.index("-d")
     arg_f = sys.argv.index("-f")
     file_name = sys.argv[arg_f + 1]
-    directory = sys.argv[arg_d + 1: arg_f]
-    os.makedirs("/".join(directory))
-    file_name_with_dir = "/".join(directory) + "/" + file_name
+    directory = "/".join(sys.argv[arg_d + 1: arg_f])
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    file_name_with_dir = directory + "/" + file_name
     create_file(file_name_with_dir)
+
 elif "-f" in sys.argv:
     arg_f = sys.argv.index("-f")
     file_name = sys.argv[arg_f + 1]
     create_file(file_name)
+
 elif "-d" in sys.argv:
     arg_d = sys.argv.index("-d")
     directory = sys.argv[arg_d + 1:]
