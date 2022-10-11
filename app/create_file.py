957db9ab74f1f153ff91d45eaa404c1acd340471
Maks Danylchuk <maxitosm00@gmail.com>
2022-10-11 14:50:38 +0300

    Ready

--- app/create_file.py
+++ app/create_file.py
@@ -2,35 +2,33 @@ from datetime import datetime
 from sys import argv
 from os import mkdir, chdir
 
+filename = ""
+directories = []
+create = True
+if "-f" in argv:
+    filename = argv[argv.index("-f") + 1]
+    if "-d" in argv:
+        directories = argv[argv.index("-d") + 1:argv.index("-f")]
+else:
+    filename = "file.txt"
+    if "-d" in argv:
+        directories = argv[argv.index("-d") + 1:len(argv)]
+        create = False
 
-def create_file() -> None:
-    directories = []
-    if "-f" in argv:
-        filename = argv[argv.index("-f") + 1]
-        if "-d" in argv:
-            directories = argv[argv.index("-d") + 1:argv.index("-f")]
-    else:
-        filename = "file.txt"
-        if "-d" in argv:
-            directories = argv[argv.index("-d") + 1:len(argv)]
+file_data = [datetime.today().strftime("%y-%m-%d %H:%M:%S"), "\n"]
 
-    file_data = [datetime.today().strftime("%y-%m-%d %H:%M:%S"), "\n"]
+for directory in directories:
+    try:
+        mkdir(directory)
+    except FileExistsError:
+        pass
+    chdir(directory)
 
+if create is True:
     while True:
         line = input("Enter content line: ")
         if line == "exit":
             break
         file_data.extend([line, "\n"])
-
-    for directory in directories:
-        try:
-            mkdir(directory)
-        except FileExistsError:
-            pass
-        chdir(directory)
-
     with open(filename, "w") as file:
         file.writelines(file_data)
-
-
-create_file()
