482b6bc3529fb02d07130d82fa993b2156780fdc
Katrin Lazarenko <katrin903@gmail.com>
2022-10-11 18:21:54 +0300

    Fixet solution

--- app/create_file.py
+++ app/create_file.py
@@ -6,22 +6,30 @@ from datetime import datetime
 
 def create_content(text: str) -> None:
     with open(text, "a") as f:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        content = int("Enter content line: ")
+        f.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+        content = input("Enter content line: ")
         i = 1
         while content != "stop":
             f.write(f"Line{i} {content}\n")
-            content = int("Enter content line: ")
+            content = input("Enter content line: ")
 
 
 def create_file() -> None:
     commands = sys.argv
-    if "-f" and "-d" in commands:
-        for i in range(2, len(commands) - 2):
-            os.makedirs(commands[i])
-        create_content(commands[-1])
     if "-f" not in commands:
-        for i in range(2, len(commands), -1):
-            os.makedirs(commands[i])
-    if "-d" not in commands:
-        create_content(commands[-1])
+        path = os.path.join(*commands[2:])
+        if not os.path.exists(path):
+            os.makedirs(path)
+        return
+
+    path = ""
+    if "-d" in commands:
+        path = os.path.join(*commands[2:-2])
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    file_name = os.path.join(path, commands[-1])
+    create_content(file_name)
+
+
+create_file()
