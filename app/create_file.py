0ab772ce2931255ea626d75aa173bf7a5085d6d8
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-10-12 19:47:37 +0300

    Solution updated

--- app/create_file.py
+++ app/create_file.py
@@ -17,31 +17,20 @@ def add_information_to_file(file_name: str) -> None:
                 break
             else:
                 document.write(f"{count} {text_input}\n")
-
-
-def create_file(file_name: str, path: list = None) -> None:
-    if path:
-        os.makedirs("/".join(path))
-        file_name = f"{'/'.join(path)}/{file_name}"
-    add_information_to_file(file_name)
+        document.write("\n")
 
 
 def directory_file_creation() -> None:
-    entered_info = sys.argv
 
-    if "-d" in entered_info and "-f" not in entered_info:
-        folders_name = ""
-        for i in range(2, len(entered_info)):
-            folders_name += f"{entered_info[i]}\\"
-        path = os.path.join(folders_name)
-        os.makedirs(path)
+    if "-d" in sys.argv:
+        count = 2
+        while count != len(sys.argv) and sys.argv[count] != "-f":
+            os.mkdir(sys.argv[count])
+            os.chdir(sys.argv[count])
+            count += 1
 
-    if "-f" in entered_info and "-d" not in entered_info:
-        create_file(entered_info[-1])
+    if "-f" in sys.argv:
+        add_information_to_file(sys.argv[-1])
 
-    if "-f" in entered_info and "-d" in entered_info:
-        file_name = entered_info[-1]
-        path = entered_info[
-            (entered_info.index("-d") + 1):entered_info.index("-f")
-        ]
-        create_file(file_name, path)
+
+directory_file_creation()
