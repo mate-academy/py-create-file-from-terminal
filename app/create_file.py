b1b16a6582b538b97166a1177568fb05a35a7a31
Mariia Movchan <mariia.movchan.m@gmail.com>
2022-10-18 18:05:23 +0300

    Improve create_directory enumarate functions and checks

--- app/create_file.py
+++ app/create_file.py
@@ -4,7 +4,9 @@ from datetime import datetime
 
 
 def create_directory(path: list) -> None:
-    os.makedirs("/".join(path))
+    path_ = os.path.join(*path)
+    os.makedirs(path_, exist_ok=True)
+    os.chdir(path_)
 
 
 def create_file(file_name: str) -> None:
@@ -18,8 +20,8 @@ def create_file(file_name: str) -> None:
     with open(file_name, "a") as file:
         file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
 
-        for idx, line in enumerate(content):
-            file.write(f"{idx + 1} {line}\n")
+        for idx, line in enumerate(content, start=1):
+            file.write(f"{idx} {line}\n")
 
 
 def create_file_from_terminal() -> None:
@@ -27,14 +29,13 @@ def create_file_from_terminal() -> None:
     directory_name = command[1:]
     file_name = command[-1]
 
-    if "-d" in command and "-f" not in command:
-        create_directory(directory_name)
-    elif "-d" not in command and "-f" in command:
+    if "-f" in command:
+        if "-d" in command:
+            directory_name = command[1:command.index("-f")]
+            create_directory(directory_name)
         create_file(file_name)
-    elif "-d" in command and "-f" in command:
-        directory_name = command[1:command.index("-f")]
+    else:
         create_directory(directory_name)
-        create_file(file_name)
 
 
 create_file_from_terminal()
