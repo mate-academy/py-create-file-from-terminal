4f582f564b36b6817549057a9646fa86d7a24a88
Mariia Movchan <mariia.movchan.m@gmail.com>
2022-10-21 11:36:53 +0300

    Rewrite with argparse

--- app/create_file.py
+++ app/create_file.py
@@ -1,15 +1,20 @@
-import sys
+import argparse
 import os
 from datetime import datetime
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="File name")
+parser.add_argument("-d", "--dir", nargs="+", help="Path")
+command = parser.parse_args()
 
-def create_directory(path: list) -> None:
-    path_ = os.path.join(*path)
+
+if command.dir:
+    path_ = os.path.join(*command.dir)
     os.makedirs(path_, exist_ok=True)
     os.chdir(path_)
 
 
-def create_file(file_name: str) -> None:
+if command.file:
     content = []
     while True:
         content_line = input("Enter content line: ")
@@ -17,25 +22,8 @@ def create_file(file_name: str) -> None:
             break
         content.append(content_line)
 
-    with open(file_name, "a") as file:
+    with open(command.file, "a") as file:
         file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
 
         for idx, line in enumerate(content, start=1):
             file.write(f"{idx} {line}\n")
-
-
-def create_file_from_terminal() -> None:
-    command = sys.argv[1:]
-    directory_name = command[1:]
-    file_name = command[-1]
-
-    if "-f" in command:
-        if "-d" in command:
-            directory_name = command[1:command.index("-f")]
-            create_directory(directory_name)
-        create_file(file_name)
-    else:
-        create_directory(directory_name)
-
-
-create_file_from_terminal()
