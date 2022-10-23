47794066e6de984fc06fbb26c27eac7c7162fde0
Vitalii Shevchuk <shevch.nl@gmail.com>
2022-10-23 21:20:24 +0300

    fixed

--- app/create_file.py
+++ app/create_file.py
@@ -7,8 +7,7 @@ def create_file() -> None:
     command = sys.argv
     if "-f" not in command:
         file_path = os.path.join(*command[2:])
-        if not os.path.exists(file_path):
-            os.makedirs(file_path)
+        os.makedirs(file_path, exist_ok=True)
         return
     file_path = ""
     if "-d" in command:
@@ -18,7 +17,7 @@ def create_file() -> None:
     file_name = os.path.join(file_path, command[-1])
 
     with open(file_name, "a") as file:
-        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        file.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S\n'))
         line_input = input("Enter content line: ")
         line_number = 1
         while line_input != "stop":
