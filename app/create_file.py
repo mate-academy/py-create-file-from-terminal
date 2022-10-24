ce64ad28f92d1db02eac468da0baa05c059961aa
Vasyl Poremchuk <nleveryone@gmail.com>
2022-10-24 18:04:44 +0300

    The implementation has been fixed

--- app/create_file.py
+++ app/create_file.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 def write_contents_to_file(filename: str) -> None:
     with open(filename, "a") as file:
-        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
 
         line_number = 1
 
@@ -18,13 +18,22 @@ def create_file() -> None:
     input_command = sys.argv
 
     if "-d" in input_command and "-f" in input_command:
-        path = os.path.join(*input_command[2:-2])
+        path = os.path.join(
+            *input_command[input_command.index("-d")
+                           + 1:input_command.index("-f")]
+        )
         os.makedirs(path, exist_ok=True)
-        write_contents_to_file(input_command[-1])
+        write_contents_to_file(
+            os.path.join(path, input_command[input_command.index("-f") + 1])
+        )
 
     if "-f" not in input_command:
-        path = os.path.join(*input_command[-2:])
+        path = os.path.join(
+            *input_command[input_command.index("-d") + 1:]
+        )
         os.makedirs(path, exist_ok=True)
 
     if "-d" not in input_command:
-        write_contents_to_file(input_command[-1])
+        write_contents_to_file(
+            input_command[input_command.index("-f") + 1]
+        )
