69c0f83f0272fe9ce9951d5eab0a778739d17266
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-24 16:00:02 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -5,22 +5,25 @@ from datetime import datetime
 
 def create_file(file_name: str) -> None:
     with open(file_name, "a") as f:
-        num = 0
+        num = 1
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        content = input("Enter content line: ")
-        while content != "stop":
-            f.write(f"{num + 1} {content} \n")
+        while True:
             content = input("Enter content line: ")
+            if content == "stop":
+                break
+
+            f.write(f"{num} {content} \n")
+            num += 1
 
 
 def create_file_from_terminal() -> None:
     command = sys.argv
-    if "-f" not in command and "-d" in command:
-        for i in range(2, len(command) - 1):
-            os.makedirs(command[i])
-    if "-d" not in command and "-f" in command:
-        create_file(command[-1])
     if "-d" in command and "-f" in command:
         for i in range(2, len(command) - 1):
             os.makedirs(command[i])
+        create_file(command[-1])
+    if "-d" in command:
+        for i in range(2, len(command) - 1):
+            os.makedirs(command[i])
+    if "-f" in command:
         create_file(command[-1])
