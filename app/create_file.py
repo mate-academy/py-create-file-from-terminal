f06a9156674f97e4c06f4a8d64b13d6211d986b1
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-10-24 14:35:32 +0300

    prints were renoved

--- app/create_file.py
+++ app/create_file.py
@@ -8,12 +8,11 @@ def create_directories(folders: list) -> None:
     for i in range(len(folders)):
         os.mkdir(folders[i])
         os.chdir(folders[i])
-        print(os.getcwd())
 
 
 def work_with_file(file_name: str) -> None:
     with open(file_name, "a") as file:
-        time = datetime.now().strftime("%y-%m-%d %H:%M:%S") + "\n"
+        time = datetime.now().strftime("%y-%m-%d %H:%M:%S\n")
         file.write(time)
 
         line = input("Enter content line:")
@@ -28,20 +27,19 @@ def controller() -> None:
 
     command_list = sys.argv
 
-    if "-d" in command_list and "-f" not in command_list:
-        folder_list = command_list[2:]
-        create_directories(folder_list)
-        print("Folders were added")
-
-    if "-f" in command_list and "-d" not in command_list:
-        work_with_file(command_list[2])
-        print("File was created")
-
     if "-d" in command_list and "-f" in command_list:
         create_directories(command_list[2: -2])
-        print("Folders were added")
         work_with_file(command_list[-1])
-        print("File was created")
+        return
+
+    if "-d" in command_list:
+        folder_list = command_list[2:]
+        create_directories(folder_list)
+        return
+
+    if "-f" in command_list:
+        work_with_file(command_list[2])
+        return
 
 
 if __name__ == "__main__":
