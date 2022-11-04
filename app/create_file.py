03d1a46d19ac374c8a34070da71f6dbd3bb751dd
George <georgepavlej@gmail.com>
2022-11-04 20:27:49 +0000

    Create 2 functions, refactored the code

--- app/create_file.py
+++ app/create_file.py
@@ -2,29 +2,37 @@ from datetime import datetime
 import sys
 import os
 
-command = sys.argv
-join_folders = ""
 
-if "-d" in command and len(sys.argv) > 2:
-    folders = command[2:]
-    join_folders += "/".join(folders[0:2])
-
-    if not os.path.exists(folders[0]):
-        os.mkdir(folders[0])
-        os.makedirs(os.path.join(folders[0], folders[1]))
-
-if "-f" in command and len(sys.argv) > 2:
+def create_file(file_name: str) -> None:
     line_number = 1
     text = ""
 
     while True:
-        answer = input("Enter content line:")
-        if answer == "stop":
+        content = input("Enter content line:")
+        if content == "stop":
             break
-        text += f"{line_number} {answer}\n"
+        text += f"{line_number} {content}\n"
         line_number += 1
 
-    with open(f"{os.path.join(join_folders, command[-1])}", "a") as file:
+    with open(file_name, "a") as file:
         date_time = datetime.now().strftime("%Y %m %d %H:%M:%S")
-        file.write(f"{date_time}\n"
-                   f"{text}")
+        file.write(f"{date_time}\n{text}")
+
+
+def create_folder() -> None:
+    command = sys.argv
+    if "-d" in command and "-f" in command:
+        folders = os.path.join(*command[2:command.index("-f")])
+        os.makedirs(folders)
+        create_file(os.path.join(folders, command[-1]))
+
+    elif "-d" in command and len(sys.argv) > 2:
+        folders = os.path.join(*command[2:])
+        os.makedirs(folders)
+
+    elif "-f" in command and len(sys.argv) > 2:
+        create_file(command[-1])
+
+
+if __name__ == "__main__":
+    create_folder()
