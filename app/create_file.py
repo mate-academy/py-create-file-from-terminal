df1ad406fa1aaec245332260acd1d5c91d49410e
Oleksandr Mamchur <irex1988@gmail.com>
2022-11-04 17:33:16 +0200

    'fixed_duplicate_code'

--- app/create_file.py
+++ app/create_file.py
@@ -6,30 +6,26 @@ import sys
 def create_file(given: list) -> None:
     if "-d" in given and "-f" not in given:
         os.makedirs("app/dir1/dir2")
+
+    def write_text_to_file() -> None:
+        current = datetime.datetime.now()
+        f.write(f"{current.strftime('%Y-%m-%d %X')}\n")
+        line = 1
+        while True:
+            text = input("Enter new line of content: ")
+            if text == "stop":
+                f.write("\n")
+                break
+            f.write(f"{line} {text}\n")
+            line += 1
+
     if "-d" not in given and "-f" in given:
         with open("app/file.txt", "a") as f:
-            current = datetime.datetime.now()
-            f.write(f"{current.strftime('%Y-%m-%d %X')}\n")
-            line = 1
-            while True:
-                text = input("Enter new line of content: ")
-                if text == "stop":
-                    f.write("\n")
-                    break
-                f.write(f"{line} {text}\n")
-                line += 1
+            write_text_to_file()
+
     if "-d" in given and "-f" in given:
         with open("app/dir1/dir2/file.txt", "a") as f:
-            current = datetime.datetime.now()
-            f.write(f"{current.strftime('%Y-%m-%d %X')}\n")
-            line = 1
-            while True:
-                text = input("Enter new line of content: ")
-                if text == "stop":
-                    f.write("\n")
-                    break
-                f.write(f"{line} {text}\n")
-                line += 1
+            write_text_to_file()
 
 
 create_file(sys.argv)
