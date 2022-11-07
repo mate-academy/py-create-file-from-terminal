e185e38c408b010c15912b8a15e2e3a0855692bd
Denys Tereshchuk <terrya@ukr.net>
2022-11-07 14:33:27 +0400

    Refactoring in function

--- app/create_file.py
+++ app/create_file.py
@@ -27,29 +27,34 @@ def file_create(name: str, path_to_file: str = "") -> None:
             number += 1
 
 
-parser = argparse.ArgumentParser()
+def create_file() -> None:
+    parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "-d", nargs="*",
-    help="use -d to set a directory and subdirectory names to create"
-)
-parser.add_argument(
-    "-f",
-    help="use -f to set a file name to create file with input content "
-         "until input stop"
-)
+    parser.add_argument(
+        "-d", nargs="*",
+        help="use -d to set a directory and subdirectory names to create"
+    )
+    parser.add_argument(
+        "-f",
+        help="use -f to set a file name to create file with input content "
+             "until input stop"
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-dir_names = args.d
-file_name = args.f
+    dir_names = args.d
+    file_name = args.f
 
-if dir_names and file_name:
-    file_create(file_name, path_to_file=make_dir(dir_names))
-    quit()
+    if dir_names and file_name:
+        file_create(file_name, path_to_file=make_dir(dir_names))
+        quit()
 
-if dir_names:
-    make_dir(dir_names)
+    if dir_names:
+        make_dir(dir_names)
 
-if file_name:
-    file_create(file_name)
+    if file_name:
+        file_create(file_name)
+
+
+if create_file().__name__ == "__main__":
+    create_file()
