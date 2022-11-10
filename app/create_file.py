1809648590349008cae45e460e53962e3cadf1dd
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-10 20:03:51 +0100

    Create func

--- app/create_file.py
+++ app/create_file.py
@@ -3,43 +3,56 @@ import sys
 from datetime import datetime
 
 
-def create_file(terminal_command: list[str]) -> None:
+def create_file_from_terminal(terminal: list[str]) -> None:
     full_path = ""
-    if "-d" in terminal_command:
-        index_d = 1
-        index_f = 0
-        for path in range(len(terminal_command)):
-            if terminal_command[path] == "-d":
-                index_d += path
+    if "-d" in terminal:
+        full_path += crate_path(terminal)
 
-            if terminal_command[path] == "-f":
-                index_f += path
+    if "-f" in terminal:
+        create_text_file(terminal, full_path)
 
-        if index_f != 0:
-            directory = terminal_command[index_d:index_f]
-        else:
-            directory = terminal_command[index_d:]
 
-        full_path += "/".join(directory)
+def crate_path(terminal_command: list) -> str:
+    full_path = ""
 
-        if not os.path.exists(full_path):
-            os.makedirs(full_path)
+    index_d = 1
+    index_f = 0
+    for path in range(len(terminal_command)):
+        if terminal_command[path] == "-d":
+            index_d += path
 
+        if terminal_command[path] == "-f":
+            index_f += path
+
+    if index_f != 0:
+        directory = terminal_command[index_d:index_f]
+    else:
+        directory = terminal_command[index_d:]
+
+    full_path += "/".join(directory)
+
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+
+    return full_path
+
+
+def create_text_file(terminal_command: list, full_path: str) -> None:
     name_file = ""
     data_file = ""
-    if "-f" in terminal_command:
-        for file_name in terminal_command:
-            if ".txt" in file_name:
-                name_file += file_name
+
+    for file_name in terminal_command:
+        if ".txt" in file_name:
+            name_file += file_name
 
         line_char = 1
 
-        while True:
-            input_data = str((input("Enter content line: ")))
-            if input_data == "stop":
-                break
-            data_file += str(line_char) + " " + input_data + "\n"
-            line_char += 1
+    while True:
+        input_data = str((input("Enter content line: ")))
+        if input_data == "stop":
+            break
+        data_file += str(line_char) + " " + input_data + "\n"
+        line_char += 1
 
     if full_path != "":
         name_file = full_path + "/" + name_file
@@ -50,4 +63,4 @@ def create_file(terminal_command: list[str]) -> None:
             file.write(data_file + "\n")
 
 
-create_file(terminal_command=sys.argv)
+create_file_from_terminal(terminal=sys.argv)
