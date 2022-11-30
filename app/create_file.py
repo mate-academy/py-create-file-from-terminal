8b427ab6627d59c010e8197f5991c1f71db7c827
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-11-30 22:03:16 +0200

    unconfident in solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,34 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def accept_list_from_terminal_and_make_dirs(commands: list):
+    itinerary = None
+    if len(commands) > 3:
+        itinerary = "/".join(commands[2:])
+    if len(commands) == 3:
+        itinerary = commands[2]
+    os.makedirs(itinerary)
+    return itinerary
+
+
+def accept_list_from_terminal_and_return_file(file: str):
+    with open(file, "a") as f:
+        f.write(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        while content != "stop":
+            content = input("Enter content line:")
+            if content != "stop":
+                f.write(content)
+
+
+information = sys.argv
+if information[1] == "-d" and information[-2] != "-f":
+    accept_list_from_terminal_and_make_dirs(information)
+if information[1] == "-f":
+    path = information[2]
+    accept_list_from_terminal_and_return_file(path)
+if information[1] == "-d" and information[-2] == "-f":
+    path = accept_list_from_terminal_and_make_dirs(information)
+    create_file = f"{path}/{information[-1]}"
+    accept_list_from_terminal_and_return_file(create_file)
