9e73c96ab29530ac31ae7904c96be33b4a8108af
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-12-01 22:40:13 +0200

    I fixed overloading context manager and renamed variables

--- app/create_file.py
+++ app/create_file.py
@@ -14,21 +14,23 @@ def accept_list_from_terminal_and_make_dirs(commands: list) -> str:
 
 
 def accept_list_from_terminal_and_return_file(new_file: str) -> None:
+    content = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    content_for_new_file = f"{content}"
+    while content != "stop":
+        content = input("Enter content line:")
+        if content != "stop":
+            content_for_new_file += f"\n{content}"
     with open(new_file, "a") as f:
-        content = f.write(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
-        while content != "stop":
-            content = input("Enter content line:")
-            if content != "stop":
-                f.write(content)
+        f.write(content_for_new_file)
 
 
-information = sys.argv
-if information[1] == "-d" and information[-2] != "-f":
-    accept_list_from_terminal_and_make_dirs(information)
-if information[1] == "-f":
-    path = information[2]
-    accept_list_from_terminal_and_return_file(path)
-if information[1] == "-d" and information[-2] == "-f":
-    path = accept_list_from_terminal_and_make_dirs(information)
-    create_file = f"{path}/{information[-1]}"
-    accept_list_from_terminal_and_return_file(create_file)
+commands_from_terminal = sys.argv
+if commands_from_terminal[1] == "-d" and commands_from_terminal[-2] != "-f":
+    accept_list_from_terminal_and_make_dirs(commands_from_terminal)
+if commands_from_terminal[1] == "-f":
+    name_for_file = commands_from_terminal[2]
+    accept_list_from_terminal_and_return_file(name_for_file)
+if commands_from_terminal[1] == "-d" and commands_from_terminal[-2] == "-f":
+    file_path = accept_list_from_terminal_and_make_dirs(commands_from_terminal)
+    name_for_file = f"{file_path}/{commands_from_terminal[-1]}"
+    accept_list_from_terminal_and_return_file(name_for_file)
