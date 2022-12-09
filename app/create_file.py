44cc1d110c0417924c0c01a0744996c869a3bc3a
Ivan Mayor <maramorosh2@gmail.com>
2022-12-09 15:59:06 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,43 +3,52 @@ import os
 from datetime import datetime
 
 
-input_info = argv
-num_for_d = 0
-row_number = 1
-for item in input_info:
-    if item == "-d" or num_for_d >= 1:
-        num_for_d += 1
+def make_directors(input_info: list) -> None:
+    num_for_make_directory = 0
+    num_for_make_file = 0
 
-        if item != "-d" and \
-                item != "-f" and \
-                item != "file.txt" and \
-                item not in os.getcwd():
+    while True:
+        for item in input_info:
+            if item == "-d" or num_for_make_directory >= 1:
+                num_for_make_directory += 1
+                if item not in os.getcwd() and\
+                        item != "-d" and\
+                        item != "file.txt":
 
-            path = os.path.join(os.getcwd(), item)
-            try:
-                os.mkdir(path)
-                os.chdir(item)
-            except FileExistsError:
-                os.chdir(item)
+                    path = os.path.join(os.getcwd(), item)
+                    try:
+                        os.mkdir(path)
+                        os.chdir(item)
+                    except FileExistsError:
+                        os.chdir(item)
 
-    if item == "-f":
-        count_for_make_datetime = 0
-        with open("file.txt", "a") as file:
+            if item == "-f" or num_for_make_file > 0:
+                num_for_make_file += 1
+                make_file(item)
+                if item == input_info[-1] and "-f" in input_info:
+                    return
 
-            while True:
-                content = input(f"Enter content line: {row_number} ")
 
-                if content.lower() == "stop":
-                    break
+def make_file(file_info: str) -> None:
+    count_for_make_datetime = 0
+    variable_for_rows = 1
+    if file_info == "-f":
+        return
+    with open("file.txt", "a") as file:
 
-                if count_for_make_datetime == 0:
-                    file.write(
-                        f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
-                        f"Line{row_number}: {content}\n"
-                    )
+        while True:
+            content = input(f"Enter content line: {variable_for_rows} ")
 
-                    count_for_make_datetime += 1
-                file.write(f"Line{row_number}: {content}\n")
+            if content.lower() == "stop":
+                break
 
-                row_number += 1
-            count_for_make_date = 0
+            if count_for_make_datetime == 0:
+                file.write(
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
+                )
+                count_for_make_datetime += 1
+            file.write(f"Line{variable_for_rows}: {content}\n")
+            variable_for_rows += 1
+
+
+make_directors(argv)
