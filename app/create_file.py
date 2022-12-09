6897523d8c69d0b568f8080869a378c5f92308ac
Vika <viktorykoroliova@gmail.com>
2022-12-09 21:45:32 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -13,25 +13,24 @@ def create_dirs(path_name: str) -> None:
 
 def create_file(file_name: str) -> None:
     with open(file_name, "a") as file_out:
-        i = 1
+        line_number = 1
         print(datetime.strftime(datetime.now(), "%y-%m-%d %H:%M:%S"),
               file=file_out)
         while True:
             word = input("Enter content line: ")
             if word == "stop":
                 break
-            print(i, word, file=file_out)
-            i += 1
+            print(line_number, word, file=file_out)
+            line_number += 1
 
 
-if "-d" in args and "-f" in args:
-    create_dirs(os.path.join(*args[1:-2]))
-    create_file(os.path.join(os.path.join(*args[1:-2]), args[-1]))
+if __name__ == "__main__":
+    if "-d" in args and "-f" in args:
+        create_dirs(os.path.join(*args[1:-2]))
+        create_file(os.path.join(os.path.join(*args[1:-2]), args[-1]))
 
+    elif "-d" in args:
+        create_dirs(os.path.join(*args[1:]))
 
-elif "-d" in args:
-    create_dirs(os.path.join(*args[1:]))
-
-
-elif "-f" in args:
-    create_file(args[1])
+    elif "-f" in args:
+        create_file(args[1])
