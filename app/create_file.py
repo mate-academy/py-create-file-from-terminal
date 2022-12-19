47fa9eb185a431526bcdd204429674330a7fcd6d
Roman Kuryliak <romahakl@gmail.com>
2022-12-19 05:15:16 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,79 @@
-# write your code here
+import sys
+import os
+import datetime
+from typing import List
+
+
+def find_file_name(args: List[str]) -> str:
+    for index, arg in enumerate(args):
+        if arg == "-f":
+            return args[index + 1]
+
+    return ""
+
+
+def create_directories(args: List[str]) -> str:
+    path = ""
+
+    try:
+        dir_index = args.index("-d")
+    except ValueError:
+        return path
+    
+    for i in range(dir_index + 1, len(args)):
+        if args[i] == "-f":
+            return path
+
+        path = os.path.join(path, args[i])
+        
+        os.makedirs(path, exist_ok=True)
+
+    return path
+
+
+def handle_input() -> List[str]:
+    inputs_to_file = []
+
+    while True:
+        input_text = input("Enter content line: ")
+
+        if input_text == "stop":
+            break
+
+        inputs_to_file.append(input_text)
+
+    return inputs_to_file
+
+
+def append_to_file(
+    path: str,
+    file_name: str,
+    inputs: List[str]
+) -> None:
+    file_path = os.path.join(path, file_name)
+
+    file_existed = os.path.isfile(file_path)
+
+    with open(file_path, "a") as file:
+        if file_existed:
+            file.write("\n")
+
+        file.write(
+            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            + "\n"
+        )
+
+        for i in range(len(inputs)):
+            file.write(" ".join([str(i + 1), inputs[i], "\n"]))
+
+
+arguments = sys.argv
+
+path = create_directories(arguments)
+
+file_name = find_file_name(arguments)
+
+if file_name:
+    inputs = handle_input()
+
+    append_to_file(path, file_name, inputs)
