ca1c0d65daa7466bdf2df7b15ea38538e987a2b8
ViktorKr <kravchukviktors@gmail.com>
2022-12-23 19:36:39 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,34 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+argv = sys.argv[1::]
+
+
+def create_folder(route: list) -> None:
+    path_name = os.path.join(*route)
+    if not os.path.exists(path_name):
+        os.makedirs(path_name)
+
+
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as file:
+        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        line_number = 1
+        while True:
+            text = input("Enter content line: ")
+            if text == "stop":
+                break
+            file.write(f"{line_number} {text} \n")
+            line_number += 1
+
+
+if __name__ == "__main__":
+    if "-d" in argv and "-f" in argv:
+        create_folder(argv[1:-2])
+        create_file(f"{os.path.join(*argv[1:-2])}/{argv[-1]}")
+    elif "-d" in argv:
+        create_folder(argv[1::])
+    elif "-f" in argv:
+        create_file(argv[-1])
