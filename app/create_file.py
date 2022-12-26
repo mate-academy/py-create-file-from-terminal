1014750df17222b9bac191a15aeefa7cc3abd4ed
Іван Березянський <mamanadarin@gmail.com>
2022-12-26 12:43:12 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -4,24 +4,33 @@ import sys
 from datetime import datetime as datetime
 
 
-def create() -> None:
-    args = sys.argv[1::]
-    path = os.getcwd
+args = sys.argv[1::]
 
-    if "-d" in args:
-        if args[-2] == "-f":
-            path = os.path.join(*args[1:-2], "")
-        else:
-            path = os.path.join(*args[1::], "")
+
+def create_dir(path: str) -> None:
+    if not os.path.exists(path):
         os.makedirs(path)
 
-    if "-f" in args:
-        with open(os.path.join(path, args[args.index("-f") + 1]), "a") as f:
-            f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \n")
-            line_number = 1
-            while True:
-                input_line = input("Enter content line: ")
-                if input_line == "stop":
-                    break
-                f.write(f"{line_number} {input_line} \n")
-                line_number += 1
+
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as f:
+        f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \n")
+        line_number = 1
+        while True:
+            input_line = input("Enter content line: ")
+            if input_line == "stop":
+                break
+            f.write(f"{line_number} {input_line} \n")
+            line_number += 1
+
+
+if __name__ == "__main__":
+    if "-d" in args and "-f" in args:
+        create_dir(os.path.join(*args[1:-2]))
+        create_file(os.path.join(os.path.join(*args[1:-2]), args[-1]))
+
+    elif "-d" in args:
+        create_dir(os.path.join(*args[1:]))
+
+    elif "-f" in args:
+        create_file(args[1])
