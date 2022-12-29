627f863fe57771d1900f2b6bb02a5664f2a36de5
Tetiana Kozakova <kleotan901@gmail.com>
2022-12-29 16:37:35 +0100

    fixed

--- app/create_file.py
+++ app/create_file.py
@@ -3,31 +3,42 @@ import argparse
 import datetime
 
 
-parser = argparse.ArgumentParser(description="Create directories and files")
-create_dir = parser.add_argument("-d", nargs="*")
-create_file = parser.add_argument("-f", nargs=1)
-args = parser.parse_args()
+def file_creation(file_path: str) -> None:
+    try:
+        with open(file_path, "a+") as new_file:
+            count = 0
+            current_date = datetime.datetime.now()
+            new_file.write(current_date.strftime("%Y-%m-%d %H:%M:%S \n"))
+            while True:
+                input_string = input(
+                    "Input content lines until you input stop: "
+                )
+                count += 1
+                if input_string == "stop":
+                    break
+                new_file.writelines(f"{count} {input_string}\n")
+    except FileNotFoundError:
+        print(f"Cannot open file: {file_path}")
+    except PermissionError:
+        print(f"You do not have permission to write to file: {file_path}")
 
-if args.d:
-    path = os.path.join("app", *args.d)
-    os.makedirs(path, exist_ok=True)
 
-if args.f:
-    file_path = ""
+def directory_creation() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", nargs="*")
+    parser.add_argument("-f", nargs=1)
+    args = parser.parse_args()
+    if args.d:
+        path = os.path.join("app", *args.d)
+        os.makedirs(path, exist_ok=True)
 
-    if args.d and args.f:
-        file_path = os.path.join("app", *args.d, *args.f)
+    if args.f:
+        if args.d and args.f:
+            file_path = os.path.join("app", *args.d, *args.f)
+            file_creation(file_path)
+        else:
+            file_path = os.path.join("app", *args.f)
+            file_creation(file_path)
 
-    else:
-        file_path = os.path.join("app", *args.f)
 
-    with open(file_path, "a+") as new_file:
-        count = 0
-        current_date = datetime.datetime.now()
-        new_file.write(current_date.strftime("%Y-%m-%d %H:%M:%S \n"))
-        while True:
-            input_string = input("Input content lines until you input stop: ")
-            count += 1
-            if input_string == "stop":
-                break
-            new_file.writelines(f"{count} {input_string}\n")
+directory_creation()
