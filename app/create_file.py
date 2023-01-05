6c527a6c32f75b53a6c4334ab892825fd5f458d6
Artem Motrunich <motrunich2013@gmail.com>
2023-01-05 19:54:24 +0100

    Fixed problem with flag position. Create "main" function.

--- app/create_file.py
+++ app/create_file.py
@@ -6,35 +6,36 @@ from datetime import datetime
 args_from_terminal = sys.argv[1:]
 
 
-def create_directory(args: list) -> str:
-    if args[0] == "-d":
-        for directory in args[1:]:
-            if directory == "-f":
-                break
-            if os.path.isdir(directory):
-                os.chdir(directory)
-            else:
-                os.mkdir(directory)
-                os.chdir(directory)
-    return os.getcwd()
+def create_directory(args: list) -> None:
+    for directory in args[args.index("-d") + 1:]:
+        if directory == "-f":
+            break
+        if os.path.isdir(directory):
+            os.chdir(directory)
+        else:
+            os.mkdir(directory)
+            os.chdir(directory)
 
 
 def create_and_write_file(args: list) -> None:
-    if "-f" in args:
-        page_number = 1
-        filename = args[-1]
-        os.chdir(create_directory(args_from_terminal))
-        with open(filename, "a") as new_file:
-            new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-            while True:
-                input_text = input("Enter content line: ")
-                if input_text == "stop":
-                    break
-                new_file.write(f"{page_number} {input_text}\n")
-                page_number += 1
-    else:
+    page_number = 1
+    filename = args[args.index("-f") + 1]
+    with open(filename, "a") as new_file:
+        new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+        while True:
+            input_text = input("Enter content line: ")
+            if input_text == "stop":
+                break
+            new_file.write(f"{page_number} {input_text}\n")
+            page_number += 1
+
+
+def main(args: list) -> None:
+    if "-d" in args:
         create_directory(args_from_terminal)
+    if "-f" in args:
+        create_and_write_file(args_from_terminal)
 
 
 if __name__ == "__main__":
-    create_and_write_file(args_from_terminal)
+    main(args_from_terminal)
