1ea6aea6401c9b837be84f0cf2057752619d1bd7
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-01-09 11:58:32 +0200

    Create file again

--- app/create_file.py
+++ app/create_file.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def create_file_and_input_data(path_to_new_file: str) -> None:
     with open(path_to_new_file, "a") as data_to_write:
         date_now = datetime.now()
-        data_to_write.write(date_now.strftime("%m-%d-%Y, %H:%M:%S") + "\n")
+        data_to_write.write(date_now.strftime("%m-%d-%Y %H:%M:%S") + "\n")
         line = input("Enter content line:")
         while line != "stop":
             data_to_write.write(line + "\n")
@@ -14,13 +14,16 @@ def create_file_and_input_data(path_to_new_file: str) -> None:
 
 
 def create_file_from_terminal() -> None:
+    list_of_input_data = sys.argv
+    file_name = list_of_input_data[-1]
+    directory_path = os.join.path(list_of_input_data[2], list_of_input_data[3])
     if "-d" in sys.argv:
-        os.makedirs(os.join.path(sys.argv[2], sys.argv[3]))
+        os.makedirs(directory_path)
 
         if "-f" in sys.argv:
             create_file_and_input_data(
-                os.join.path(sys.argv[2], sys.argv[3], sys.argv[-1])
+                os.join.path(directory_path, file_name)
             )
 
     else:
-        create_file_and_input_data(sys.argv[-1])
+        create_file_and_input_data(file_name)
