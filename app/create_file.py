f76bc7f9dfa4a2ab759ac6e5e0ed9002f5ef5135
Anna Kabatova <kabatova.anna@ukr.net>
2023-01-10 14:22:15 +0200

    1st sol create-file

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,33 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+arguments_list = sys.argv
+
+if "-d" in arguments_list:
+    if "-f" in arguments_list:
+        new_dir = "/".join(arguments_list[2:-2])
+    else:
+        new_dir = "/".join(arguments_list[2:])
+    os.makedirs(new_dir)
+
+if "-f" in arguments_list:
+    file_name = arguments_list[-1]
+    if "-d" in arguments_list:
+        path_to_file = f"{new_dir}/{file_name}"
+    else:
+        path_to_file = file_name
+    with open(path_to_file, "a") as new_file:
+        now = datetime.datetime.now()
+        time_to_append = now.strftime("%Y-%d-%m %H:%M:%S")
+        new_file.write(time_to_append + "\n")
+        input_line = "start"
+        count = 1
+        while input_line != "stop":
+            input_line = input("Enter content line: ")
+            if input_line != "stop":
+                new_file.write(str(count) + " " + input_line + "\n")
+                count += 1
+            else:
+                new_file.write("\n")
