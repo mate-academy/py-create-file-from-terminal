024ef29401b21050d9e7cc56c3555457ca66ac43
Artur Leleiko <artur_98@ukr.net>
2023-01-19 12:59:32 +0200

    'Solution#3'

--- app/create_file.py
+++ app/create_file.py
@@ -3,15 +3,12 @@ import os
 import sys
 
 
-def create_directory(args: list) -> str:
-    directory = os.getcwd()
-
-    if "-d" in args in args:
-        for i in range(args.index("-d"), len(args)):
-            if args[i] == "-f":
-                break
-            directory = os.path.join(directory, args[i])
-        os.makedirs(directory)
+def create_directory(args: list, directory: str) -> str:
+    for i in range(args.index("-d"), len(args)):
+        if args[i] == "-f":
+            break
+        directory = os.path.join(directory, args[i])
+    os.makedirs(directory)
 
     return directory
 
@@ -34,8 +31,13 @@ def create_file(path_to_file: str) -> None:
 
 def main() -> None:
     args = sys.argv
-    path_to_file = create_directory(args)
-    create_file(path_to_file)
+    directory = os.getcwd()
+    if "-d" in args:
+        directory = create_directory(args, directory)
+    if "-f" in args:
+        filename = args[args.index("-f") + 1]
+        directory = os.path.join(directory, filename)
+        create_file(directory)
 
 
 if __name__ == "__main__":
