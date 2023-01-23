b0b72e5c23ec3722cd7f6eff237ad6a0d17f48a8
Yuliia Stopkyna <styopkinayuliya@gmail.com>
2023-01-23 16:58:04 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -15,24 +15,29 @@ def add_info() -> str:
     return text
 
 
-path = os.getcwd()
-if "-d" in sys.argv:
-    for i in range(sys.argv.index("-d") + 1, len(sys.argv)):
-        if sys.argv[i] == "-f":
-            break
-        path = os.path.join(path, sys.argv[i])
-    os.makedirs(path, exist_ok=True)
-if "-f" in sys.argv:
-    path = os.path.join(path, sys.argv[sys.argv.index("-f") + 1])
+def create_or_get_dir() -> str:
+    path = os.getcwd()
+    if "-d" in sys.argv:
+        for i in range(sys.argv.index("-d") + 1, len(sys.argv)):
+            if sys.argv[i] == "-f":
+                break
+            path = os.path.join(path, sys.argv[i])
+        os.makedirs(path, exist_ok=True)
+    return path
 
-now = datetime.now()
-now_string = datetime.strftime(now, "%Y-%m-%d %H:%M:%S")
 
-if not os.path.exists(path):
-    with open(path, "w") as new_file:
-        new_file.write(now_string + "\n")
-        new_file.write(add_info())
-elif "-f" in sys.argv:
-    with open(path, "a") as existing_file:
-        existing_file.write(f"\n{now_string}\n")
-        existing_file.write(add_info())
+def creating_file() -> None:
+    path = os.path.join(create_or_get_dir(),
+                        sys.argv[sys.argv.index("-f") + 1])
+    now = datetime.now()
+    now_string = datetime.strftime(now, "%Y-%m-%d %H:%M:%S")
+    with open(path, "a") as info_file:
+        info_file.write(f"{now_string}\n")
+        info_file.write(add_info())
+        info_file.write("\n")
+
+
+if "-d" in sys.argv and "-f" not in sys.argv:
+    create_or_get_dir()
+else:
+    creating_file()
