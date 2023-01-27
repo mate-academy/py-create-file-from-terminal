412cb7e444a60a75077fce3a54c410f077b0a6a1
6aPa6aH <6apa6ah666@gmail.com>
2023-01-27 18:14:48 +0200

    fix_code

--- app/create_file.py
+++ app/create_file.py
@@ -4,40 +4,44 @@ import sys
 
 
 def create_dir(path_dir: str) -> None:
-    if not os.path.exists(path_dir):
-        os.makedirs(os.path.dirname(path_dir))
+    os.makedirs(path_dir, exist_ok=True)
 
 
 def create_file(file_name: str) -> None:
-    file_attribute = "w"
-    if os.path.exists(file_name):
-        file_attribute = "a"
-    with open(f"{file_name}", f"{file_attribute}") as file:
-        i = 1
+    with open(file_name, "a") as file:
+        line_number = 1
         now = datetime.datetime.now()
         file.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')}\n")
         while True:
             new_line = input("Enter content line: ")
             if new_line == "stop":
                 break
-            file.write(f"{i} {new_line} \n")
-            i += 1
+            file.write(f"{line_number} {new_line} \n")
+            line_number += 1
         file.write("\n")
 
 
 def create_dir_and_file(command: list) -> None:
-    file_name = command[-1]
-    path_dir = ""
-    if "-d" in command and "-f" in command:
-        for i in command[2:-2]:
-            path_dir += f"{i}/"
+    path_dir = os.path.abspath(os.curdir)
+    if "-d" in command and "-f" in command and\
+            command.index("-d") < command.index("-f"):
+        for directory in command[command.index("-d") + 1:command.index("-f")]:
+            path_dir = os.path.join(path_dir, directory)
+        file_name = command[-1]
         create_dir(path_dir)
-        create_file(path_dir + file_name)
+        create_file(os.path.join(path_dir, file_name))
+    elif "-d" in command and "-f" in command:
+        for directory in command[command.index("-d") + 1:]:
+            path_dir = os.path.join(path_dir, directory)
+        file_name = command[2]
+        create_dir(path_dir)
+        create_file(os.path.join(path_dir, file_name))
     elif "-f" in command:
+        file_name = command[-1]
         create_file(file_name)
     elif "-d" in command:
-        for i in command[2:]:
-            path_dir += f"{i}/"
+        for directory in command[command.index("-d") + 1:]:
+            path_dir = os.path.join(path_dir, directory)
         create_dir(path_dir)
 
 
