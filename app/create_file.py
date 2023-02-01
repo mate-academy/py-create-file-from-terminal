f4bc2ad36ff50153a398b7017b11c59d939a1060
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-02-01 20:58:44 +0200

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,31 @@
-# write your code here
+import argparse
+import os
+from datetime import datetime
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", action="extend", nargs="+")
+parser.add_argument("-f")
+args = vars(parser.parse_args())
+dir_path = ''
+
+if args["d"] is not None:
+    try:
+        dir_path = os.path.join(*args["d"])
+        os.makedirs(dir_path)
+    except FileExistsError:
+        pass
+
+if args["f"] is not None:
+    content = [f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"]
+    line_number = 1
+    while True:
+        line = input("Enter content line:")
+        if line == "stop":
+            content.append("\n")
+            break
+        content.append(f"{line_number} {line}\n")
+        line_number += 1
+    file_path = os.path.join(dir_path, args["f"])
+    with open(file_path, "a") as file:
+        file.writelines(content)
