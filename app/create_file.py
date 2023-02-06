a9896d79f9a9b6ad0214f072e4fb0ae95fbe3718
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-02-06 15:52:06 +0100

    'Solution6'

--- app/create_file.py
+++ app/create_file.py
@@ -26,13 +26,16 @@ def create_file() -> None:
     command = get_command()
     if "-f" not in command:
         os.makedirs(os.path.join(*(["app"] + command[1:])))
+    elif "-d" not in command:
+        get_file_content(os.path.join("app", command[1]))
     else:
         if command[0] == "-d":
             os.makedirs(os.path.join(*(["app"] + command[1:-2])))
             file_path = os.path.join(os.path.join(*(["app"] + command[1:-2])),
                                      command[-1])
         else:
-            file_path = os.path.join("app", command[-1])
+            os.makedirs(os.path.join(*(["app"] + command[3:])))
+            file_path = os.path.join(*(["app"] + command[3:]), command[1])
         get_file_content(file_path)
 
 
