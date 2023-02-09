9e200ccd4fd607c13e03c92680d51c2258610cd5
Paul Maslov <maslovp67@gmail.com>
2023-02-09 18:06:34 +0200

    'Solution.v02'

--- app/create_file.py
+++ app/create_file.py
@@ -3,22 +3,42 @@ import datetime
 import os
 
 
-amount_of_argv = len(sys.argv)
-name_dir = sys.path[0]
-if sys.argv[1] == "-d":
-    for item in range(2, amount_of_argv):
-        if sys.argv[item] == "-f":
-            break
-        name_dir = name_dir + "/" + str(sys.argv[item])
-        os.mkdir(name_dir)
+def make_file_directory(
+        comm_list: list,
+        name_direct: str,
+        amount_argv: int
+) -> str:
+    if comm_list[1] == "-d":
+        for item in range(2, amount_argv):
+            if comm_list[item] == "-f":
+                break
+            name_direct = name_direct + "/" + str(sys.argv[item])
+            os.mkdir(name_direct)
+    return name_direct
 
-for i in range(1, amount_of_argv):
-    if sys.argv[i] == "-f":
-        now = datetime.datetime.now()
-        with open(f"{name_dir}/{str(sys.argv[i + 1])}", "w") as f:
-            f.write(f"{now.date()} {now.hour}:{now.minute}:{now.second}\n")
-            for line in sys.stdin:
-                if "stop" == line.rstrip():
-                    break
+
+def write_to_file(comm_list: list, name_direct: str) -> None:
+    now = datetime.datetime.now()
+    with open(f"{name_direct}/{comm_list[-1]}", "w") as f:
+        f.write(f"{now.date()} {now.hour}:{now.minute}:{now.second}\n")
+        for line in sys.stdin:
+            if "stop" != line.rstrip():
                 print(f"Enter content line: {line}")
                 f.write(line)
+                continue
+            break
+
+
+if __name__ == "__main__":
+    command_list = sys.argv
+    if len(command_list) > 2:
+        amount_of_argv = len(command_list)
+        path_name = sys.path[0]
+        name_dir = make_file_directory(
+            command_list,
+            path_name,
+            amount_of_argv
+        )
+        for i in range(1, amount_of_argv):
+            if command_list[i] == "-f":
+                write_to_file(command_list, name_dir)
