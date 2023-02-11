9a1a0a258f20f6f713106586cf68d548fd38a37e
Yehor Kornev <dmekkindom00@gmail.com>
2023-02-11 21:58:24 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,46 +1,18 @@
-import argparse
 import os
-# from sys import argv
 
 from writer_text import writer_text
-
-
-# line_argv = argv
-# if "-d" in line_argv:
-#     path_all = None
-#     a = line_argv.index("-d")
-#     if "-f" in line_argv:
-#         b = line_argv.index("-f")
-#         path_all = line_argv[a + 1: b]
-#     else:
-#         path_all = line_argv[a + 1]
-#     path_dir = os.path.join(*path_all)
+from create_argparser import create_argparser
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "-d",
-        "--createdir",
-        help="Creates directory somethings inside current directory.",
-        type=str,
-    )
-    parser.add_argument(
-        "-f",
-        "--createfile",
-        help="Creates some file and then terminal should"
-             " ask you to input content lines until you input \"stop\"",
-        type=str
-    )
-
+    parser = create_argparser()
     args = parser.parse_args()
 
     path_dir = None
     path_file = None
 
     if args.createdir:
-        path_dir = os.path.join(args.createdir)
+        path_dir = os.path.join(*args.createdir)
         if not os.path.isdir(path_dir):
             os.makedirs(path_dir)
 
