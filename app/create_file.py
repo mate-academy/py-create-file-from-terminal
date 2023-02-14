d9d046d5255bdf9b95865b315a3d4363d1a7b3c8
Mykhaylo Kobylianskyi <mvkobylianskyi@gmail.com>
2023-02-14 13:38:25 +0200

    with new edits

--- app/create_file.py
+++ app/create_file.py
@@ -4,27 +4,22 @@ import sys
 
 
 def making_directories(directory_path: list) -> str:
-    directory = ""
-    if "-d" in directory_path and "-f" not in directory_path:
-        for directory_name in directory_path[
-            directory_path.index("-d") + 1:
-        ]:
-            directory += directory_name + "/"
-    else:
-        for directory_name in directory_path[
-            directory_path.index("-d") + 1:
-            directory_path.index("-f")
-        ]:
-            directory += directory_name + "/"
-    path_directory = os.path.join(os.getcwd(), directory)
-    os.makedirs(path_directory, exist_ok=True)
-    return path_directory
+    path = directory_path[directory_path.index("-d") + 1:]
+
+    if "-f" in directory_path:
+        path = directory_path[
+               directory_path.index("-d") + 1:
+               directory_path.index("-f")
+               ]
+    path = os.path.join(*path)
+    os.makedirs(path, exist_ok=True)
+    return path
 
 
 def making_file_and_content(directory_path: list) -> None:
     file_path = ""
     if "-d" in directory_path:
-        file_path += making_directories(directory_path)
+        file_path += making_directories(directory_path) + "/"
 
     if "-f" in directory_path:
         file_path += directory_path[directory_path.index("-f") + 1]
