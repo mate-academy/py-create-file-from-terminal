b2cb4b9e85284eb390be70f85f302795b4e99502
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-16 17:21:01 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -11,7 +11,7 @@ def add_new_content_line(path_to_file: str) -> None:
         created_file.write("\n")
 
 
-def create_path_to_folders(path_data: list) -> str:
+def create_path_to_folders(path_data: list[str]) -> str:
     input_list = []
     for command in path_data[1:]:
         if "-f" not in command and "-d" not in command and "." not in command:
@@ -19,19 +19,19 @@ def create_path_to_folders(path_data: list) -> str:
     return os.path.join(*input_list)
 
 
-def create_path_to_file(path_data: list) -> str:
+def create_path_to_file(path_data: list[str]) -> str:
     return os.path.join(path_data[path_data.index("-f") + 1])
 
 
-def create_only_file(input_data: list) -> None:
+def create_only_file(input_data: list[str]) -> None:
     add_new_content_line(create_path_to_file(input_data))
 
 
-def create_only_folders(input_data: list) -> None:
+def create_only_folders(input_data: list[str]) -> None:
     os.makedirs(create_path_to_folders(input_data), exist_ok=True)
 
 
-def create_folders_and_file(input_data: list) -> None:
+def create_folders_and_file(input_data: list[str]) -> None:
     create_only_folders(input_data)
     add_new_content_line(
         f"{create_path_to_folders(input_data)}"
