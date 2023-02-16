2b3bed6af77ac14bd1f199f90ab3ef42c49f5665
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-16 11:50:59 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -15,15 +15,27 @@ def add_new_content_line(new_file: IO) -> None:
             new_file.write("\n")
 
 
-if "-f" in sys.argv and "-d" not in sys.argv:
-    with open(sys.argv[-1], "a") as created_file:
-        add_new_content_line(created_file)
+def create_only_file() -> None:
+    if "-f" in sys.argv and "-d" not in sys.argv:
+        with open(sys.argv[-1], "a") as created_file:
+            add_new_content_line(created_file)
 
-if "-d" in sys.argv and "-f" not in sys.argv:
-    os.makedirs(os.path.join(*sys.argv[2:]), exist_ok=True)
 
-if "-d" in sys.argv and "-f" in sys.argv:
-    os.makedirs(os.path.join(*sys.argv[2:-2]), exist_ok=True)
-    sys.argv.remove("-f")
-    with open(os.path.join(*sys.argv[2:]), "a") as created_file:
-        add_new_content_line(created_file)
+def create_only_folders() -> None:
+    if "-d" in sys.argv and "-f" not in sys.argv:
+        os.makedirs(os.path.join(*sys.argv[2:]), exist_ok=True)
+
+
+def create_folders_and_file() -> None:
+    if "-d" in sys.argv and "-f" in sys.argv:
+        sys.argv.remove("-f")
+        sys.argv.remove("-d")
+        os.makedirs(os.path.join(*sys.argv[1:-1]), exist_ok=True)
+        with open(os.path.join(*sys.argv[2:]), "a") as created_file:
+            add_new_content_line(created_file)
+
+
+if __name__ == "__main__":
+    create_only_file()
+    create_only_folders()
+    create_folders_and_file()
