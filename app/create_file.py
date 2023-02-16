0527e8216360b4246d0f72709f6f7a571956b2f5
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-16 17:06:36 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,41 +1,48 @@
 import os
 import sys
 from datetime import datetime
-from typing import IO
 
 
-def add_new_content_line(new_file: IO) -> None:
-    new_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-    added_line = ""
-    while added_line != "stop":
-        added_line = input("Enter content line: ")
-        if added_line != "stop":
-            new_file.write(f"{added_line}\n")
-        else:
-            new_file.write("\n")
+def add_new_content_line(path_to_file: str) -> None:
+    with open(path_to_file, "a") as created_file:
+        created_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        while (added_line := input("Enter content line: ")) != "stop":
+            created_file.write(f"{added_line}\n")
+        created_file.write("\n")
 
 
-def create_only_file() -> None:
-    if "-f" in sys.argv and "-d" not in sys.argv:
-        with open(sys.argv[-1], "a") as created_file:
-            add_new_content_line(created_file)
+def create_path_to_folders(path_data: list) -> str:
+    input_list = []
+    for command in path_data[1:]:
+        if "-f" not in command and "-d" not in command and "." not in command:
+            input_list.append(command)
+    return os.path.join(*input_list)
 
 
-def create_only_folders() -> None:
-    if "-d" in sys.argv and "-f" not in sys.argv:
-        os.makedirs(os.path.join(*sys.argv[2:]), exist_ok=True)
+def create_path_to_file(path_data: list) -> str:
+    return os.path.join(path_data[path_data.index("-f") + 1])
 
 
-def create_folders_and_file() -> None:
-    if "-d" in sys.argv and "-f" in sys.argv:
-        sys.argv.remove("-f")
-        sys.argv.remove("-d")
-        os.makedirs(os.path.join(*sys.argv[1:-1]), exist_ok=True)
-        with open(os.path.join(*sys.argv[2:]), "a") as created_file:
-            add_new_content_line(created_file)
+def create_only_file(input_data: list) -> None:
+    add_new_content_line(create_path_to_file(input_data))
+
+
+def create_only_folders(input_data: list) -> None:
+    os.makedirs(create_path_to_folders(input_data), exist_ok=True)
+
+
+def create_folders_and_file(input_data: list) -> None:
+    create_only_folders(input_data)
+    add_new_content_line(
+        f"{create_path_to_folders(input_data)}"
+        f"/{create_path_to_file(input_data)}"
+    )
 
 
 if __name__ == "__main__":
-    create_only_file()
-    create_only_folders()
-    create_folders_and_file()
+    if "-f" in sys.argv and "-d" not in sys.argv:
+        create_only_file(sys.argv)
+    if "-d" in sys.argv and "-f" not in sys.argv:
+        create_only_folders(sys.argv)
+    if "-d" in sys.argv and "-f" in sys.argv:
+        create_folders_and_file(sys.argv)
