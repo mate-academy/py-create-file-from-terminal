4b1ab6359ba80f59b50721e726a8e645eba495a4
Vyacheslav Kungin <acheros4@gmail.com>
2023-02-16 14:51:09 +0200

    fixed v5

--- app/create_file.py
+++ app/create_file.py
@@ -3,32 +3,34 @@ import sys
 from datetime import datetime
 
 
-def create_file(date: list[str]) -> str:
+def create_file(data: list[str]) -> str:
     file_name = ""
     folders_name = ""
 
-    if "-d" in date and "-f" not in date:
-        folders_name = os.path.join(*date[2:])
+    if "-d" in data and "-f" not in data:
+        folders_name = os.path.join(*data[2:])
 
-    elif "-f" in date and "-d" not in date:
-        file_name = os.path.join(*date[2:])
+    elif "-f" in data and "-d" not in data:
+        file_name = os.path.join(*data[2:])
 
-    elif "-d" in date and "-f" in date:
-        d_index = (date.index("-d") + 1)
-        f_index = (date.index("-f") + 1)
-        if f_index > d_index:
-            folders_name = os.path.join(*date[d_index:date.index("-f")])
+    elif "-d" in data and "-f" in data:
+        start_directory_index = (data.index("-d") + 1)
+        file_index = (data.index("-f") + 1)
+        if file_index > start_directory_index:
+            folders_name = (
+                os.path.join(*data[start_directory_index:data.index("-f")])
+            )
         else:
-            folders_name = os.path.join(*date[d_index:])
-        file_name = date[f_index]
+            folders_name = os.path.join(*data[start_directory_index:])
+        file_name = data[file_index]
 
     os.makedirs(folders_name, exist_ok=True)
     return os.path.join(folders_name, file_name)
 
 
 def writing_to_file() -> None:
-    date = sys.argv
-    with open(create_file(date), "a") as file:
+    data = sys.argv
+    with open(create_file(data), "a") as file:
         file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
         line_number = 1
         while True:
