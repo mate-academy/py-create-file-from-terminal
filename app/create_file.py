5e097aaa92fe8907f0ccdd787bd87e609951b085
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-18 21:18:22 +0200

    refactor

--- app/create_file.py
+++ app/create_file.py
@@ -10,18 +10,16 @@ def create_path(directories: list) -> str:
 
 def create_file() -> None:
     content = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-    file_name = sys.argv[-1]
     line_number = 1
-    while True:
-        print_line = input("Enter content line: ")
-        if print_line == "stop":
-            content += "\n"
-            break
-        content += f"{line_number} {print_line}\n"
-        line_number += 1
-
-    with open(file_name, "a") as file_out:
+    with open(sys.argv[-1], "a") as file_out:
         file_out.write(content)
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                file_out.write("\n")
+                break
+            file_out.write(f"{line_number} {content}\n")
+            line_number += 1
 
 
 def create_directory(file_name: str = None) -> None:
