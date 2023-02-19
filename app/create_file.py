2075c0e3505985fc3b8517e574b4f8b22a4bec76
Mykhaylo Kobylianskyi <mvkobylianskyi@gmail.com>
2023-02-19 16:04:27 +0200

    solution v.7

--- app/create_file.py
+++ app/create_file.py
@@ -3,45 +3,30 @@ import os
 import sys
 
 
-def create_file_and_content(path_to_func: str) -> None:
-    if "-f" in path_to_func and "-d" in path_to_func:
-        if path_to_func.index("-d") > path_to_func.index("-f"):
-            flag_f = (
-                path_to_func[path_to_func.index("-f"):path_to_func.index("-d")]
-            )
-            flag_d = path_to_func[path_to_func.index("-d"):]
-            path_to_func_replaced = flag_d + flag_f
-            path = path_to_func_replaced[
-                path_to_func_replaced.index("-d") + 1:
-                path_to_func_replaced.index("-f")
-            ]
-            path = (
-                making_directories(path) + "/"
-                + path_to_func_replaced[path_to_func_replaced.index("-f") + 1]
-            )
-            making_file_and_content(path)
-
-        else:
-            path = path_to_func[
-                path_to_func.index("-d") + 1: path_to_func.index("-f")
-            ]
-            path = (
-                making_directories(path) + "/"
-                + path_to_func[path_to_func.index("-f") + 1]
-            )
-            making_file_and_content(path)
-    elif "-f" in path_to_func and "-d" not in path_to_func:
-        path = path_to_func[path_to_func.index("-f") + 1]
+def create_file_and_content(path_to_func: list[str]) -> None:
+    if "-d" in path_to_func and "-f" in path_to_func:
+        path_dir = (
+            path_to_func[path_to_func.index("-d") + 1:path_to_func.index("-f")]
+            or path_to_func[path_to_func.index("-d") + 1:]
+        )
+        path_file = (
+            path_to_func[path_to_func.index("-f") + 1:path_to_func.index("-d")]
+            or path_to_func[path_to_func.index("-f") + 1:]
+        )
+        making_directories(path_dir)
+        path = os.path.join(*path_dir, *path_file)
         making_file_and_content(path)
-    elif "-d" in path_to_func and "-f" not in path_to_func:
-        path = path_to_func[path_to_func.index("-d") + 1:]
-        making_directories(path)
+    elif "-d" in path_to_func:
+        path_dir = path_to_func[path_to_func.index("-d") + 1:]
+        making_directories(path_dir)
+    else:
+        path_file = path_to_func[path_to_func.index("-f") + 1:]
+        making_file_and_content(*path_file)
 
 
-def making_directories(path: str) -> str:
+def making_directories(path: list[str]) -> None:
     path = os.path.join(*path)
     os.makedirs(path, exist_ok=True)
-    return path
 
 
 def making_file_and_content(path: str) -> None:
