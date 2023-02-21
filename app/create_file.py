f263f00e5dd33ec8387e85f93ebdc843b9645cb2
Andriy <andriykrav@gmail.com>
2023-02-21 14:16:55 +0200

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -7,6 +7,8 @@ create_file("python create_file.py -f file.txt")
 create_file("python create_file.py -d dir1 dir2 -f file.txt")
 
 """
+
+
 def create_file(input_string: str) -> None:
     _, __, flag, *destination_path = input_string.split()
     dir_path = [s for s in destination_path if "-f" not in s]
@@ -22,7 +24,8 @@ def create_file(input_string: str) -> None:
         with open(output_file, "a") as target_file:
             write_content(target_file)
 
-def write_content(target_file):
+
+def write_content(target_file: callable) -> None:
     ts = d.timestamp(d.now())
     ts = d.fromtimestamp(int(ts))
     target_file.write(str(ts) + "\n")
@@ -33,4 +36,4 @@ def write_content(target_file):
             target_file.write("\n")
             break
         number += 1
-        target_file.write('%d  %s' % (number, new_line + "\n"))
+        target_file.write("%d  %s" % (number, new_line + "\n"))
