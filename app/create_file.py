81d5df6c748ef5a28398321bc720c2a0e3e9321f
usrofgh <mailforgimail@gmail.com>
2023-02-22 22:54:04 +0200

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,36 @@
-# write your code here
+import argparse
+import os
+from datetime import datetime
+
+
+def writing_file(full_path: str) -> None:
+    with open(full_path, "a") as file:
+        if os.stat(full_path).st_size != 0:
+            file.write("\n")
+
+        file.write(f"{str(datetime.today()).rsplit('.')[0]}\n")
+
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                break
+            file.write(f"{content}\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", nargs="+", help="directories name")
+    parser.add_argument("-f", help="name of a file")
+    args = parser.parse_args()
+    args.d = "/".join(args.d)
+
+    if args.d and not args.f:
+        os.makedirs(args.d, exist_ok=True)
+
+    if args.f and not args.d:
+        writing_file(args.f)
+
+    if args.d and args.f:
+        os.makedirs(args.d, exist_ok=True)
+        path_to_file = os.path.join(args.d, args.f)
+        writing_file(path_to_file)
