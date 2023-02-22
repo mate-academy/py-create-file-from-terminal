75efbc5ff748dc8ea72c9370914a3ff32c0670a1
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-02-22 17:01:54 +0200

    v2

--- app/create_file.py
+++ app/create_file.py
@@ -2,48 +2,37 @@ import os
 import sys
 from datetime import datetime
 
+
 command = sys.argv
 
 
 def create_file(command: list) -> None:
 
     if command[1] == "-f":
-        open_file(command, "-f")
+        open_file(command[-1])
 
     if command[1] == "-d" and "-f" in command:
-        path = "/".join(command[2:-2])
-        if not os.path.exists(path):
-            os.makedirs(path)
-        file_path = os.path.join(path, command[-2])
-        open_file(file_path, "-d-f")
+        path = os.path.join(*command[2:-2])
+        os.makedirs(path, exist_ok=True)
+        file_path = os.path.join(path, command[-1])
+        open_file(file_path)
 
-    if command[1] == "-d":
-        os.makedirs("/".join(command[2:]))
-
-
-def open_file(command, flag):
-
-    if flag == "-f":
-        with open(command[2], "w") as file:
-            write_file(file)
-
-    if flag == "-d-f":
-        with open(command[-2], "w") as file:
-            write_file(file)
+    if command[1] == "-d" and "-f" not in command:
+        os.makedirs(os.path.join(*command[2:]))
 
 
+def open_file(command: str) -> None:
+    with open(command, "w") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(timestamp + "\n")
+        line_number = 1
+        while True:
+            line_content = input("Enter content line: ")
+            if line_content == "stop":
+                break
+            file.write(f"{line_number} {line_content}\n")
+            line_number += 1
 
-def write_file(file):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    file.write(timestamp + '\n')
-    line_number = 1
-    while True:
-        line_content = input("Enter content line: ")
-        if line_content == "stop":
-            break
-        file.write(f"{line_number} {line_content}\n")
-        line_number += 1
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_file(command)
