d776a5112644896384ec3bbf407258a6d187621b
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-23 22:45:10 +0200

    more namespaces

--- app/create_file.py
+++ app/create_file.py
@@ -14,7 +14,7 @@ def read_content() -> list:
     return content
 
 
-def create_file(file_path):
+def create_file(file_path: str) -> None:
     content = read_content()
 
     file_existed = os.path.exists(file_path)
@@ -24,19 +24,31 @@ def create_file(file_path):
         f.write("\n".join(content))
 
 
-def main(args):
+def only_dir_creation(args: list) -> None:
+    dir_path = os.path.join(*args[args.index("-d") + 1:])
+    os.makedirs(dir_path, exist_ok=True)
+
+
+def only_file_creation(args: list) -> None:
+    file_path = args[args.index("-f") + 1]
+    create_file(file_path)
+
+
+def dir_and_file_creation(args: list) -> None:
+    dir_path = os.path.join(*args[args.index("-d") + 1:args.index("-f")])
+    file_name = args[args.index("-f") + 1]
+    os.makedirs(dir_path, exist_ok=True)
+    file_path = os.path.join(dir_path, file_name)
+    create_file(file_path)
+
+
+def main(args: list) -> None:
     if "-d" in args and "-f" in args:
-        dir_path = os.path.join(*args[args.index("-d")+1:args.index("-f")])
-        file_name = args[args.index("-f")+1]
-        os.makedirs(dir_path, exist_ok=True)
-        file_path = os.path.join(dir_path, file_name)
-        create_file(file_path)
+        dir_and_file_creation(args)
     elif "-d" in args:
-        dir_path = os.path.join(*args[args.index("-d")+1:])
-        os.makedirs(dir_path, exist_ok=True)
+        only_dir_creation(args)
     elif "-f" in args:
-        file_path = args[args.index("-f")+1]
-        create_file(file_path)
+        only_file_creation(args)
 
 
 if __name__ == "__main__":
