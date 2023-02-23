ed6f4ad269b3fbf3b74c656ed48fdb11cb78790a
Sergii Machulin <machulinsv@gmail.com>
2023-02-23 22:00:58 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,59 @@
-# write your code here
+import argparse
+import os
+from datetime import datetime
+
+
+def create_path(*args) -> str:
+    path = os.path.join(*args)
+    return path
+
+
+def create_directory(directories: list) -> None:
+    directory_path = create_path(*directories)
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    return
+
+
+def write_to_file(file_name: str) -> None:
+    with open(f"{file_name}", "a+") as created_file:
+        first_line_ever = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        created_file.write(first_line_ever + "\n")
+        counter_lines = 0
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            counter_lines += 1
+            content = f"{counter_lines} {line}"
+            created_file.write(content + "\n")
+        created_file.write("\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="script to create files and directories"
+    )
+
+    parser.add_argument(
+        "-d",
+        nargs="+",
+        metavar="directory",
+        help="'create directory' flag"
+    )
+    parser.add_argument(
+        "-f",
+        nargs="+",
+        metavar="filename",
+        help="'create file' flag"
+    )
+
+    options = parser.parse_args()
+
+    if options.f and not options.d:
+        write_to_file(*options.f)
+    elif options.d and not options.f:
+        create_directory(options.d)
+    elif options.d and options.f:
+        create_directory(options.d)
+        write_to_file(create_path(*options.d, *options.f))
