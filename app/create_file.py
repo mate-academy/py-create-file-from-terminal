2c8e8033d9159fefb7952deb1f260444204f2289
ramsty <ramsty27@gmail.com>
2023-02-24 20:04:52 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,13 +3,13 @@ from sys import argv
 from datetime import datetime
 
 
-def create_file(file_path):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+def create_file(file_path: str) -> None:
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     content = []
     line_number = 1
 
     if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
+        with open(file_path) as f:
             content = f.readlines()
         line_number = len(content) + 1
 
@@ -20,26 +20,26 @@ def create_file(file_path):
         content.append(f"{line_number} {line}\n")
         line_number += 1
 
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         f.write(f"{timestamp}\n")
         f.writelines(content)
 
 
-def create_directory(dir_path):
+def create_directory(dir_path: str) -> None:
     os.makedirs(dir_path, exist_ok=True)
 
 
-def main():
-    if '-d' in argv:
-        dir_flag_index = argv.index('-d')
-        dir_path = os.path.join(*argv[dir_flag_index+1:])
+def main() -> None:
+    if "-d" in argv:
+        dir_flag_index = argv.index("-d")
+        dir_path = os.path.join(*argv[dir_flag_index + 1:])
         create_directory(dir_path)
-    elif '-f' in argv:
-        file_flag_index = argv.index('-f')
-        file_name = argv[file_flag_index+1]
+    elif "-f" in argv:
+        file_flag_index = argv.index("-f")
+        file_name = argv[file_flag_index + 1]
         file_path = os.path.abspath(file_name)
         create_file(file_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
