f80785ce5473e8550625b40ba6fd271727e4b0bf
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-24 13:23:33 +0200

    Changes

--- app/create_file.py
+++ app/create_file.py
@@ -22,15 +22,18 @@ def create_file() -> None:
 
 def create_file_in_directory() -> None:
     os.makedirs("/".join(command[2: command.index("-f")]))
-
-    with open(f"{'/'.join(command[2: command.index('-f')])}/{command[-1]}",
-              "w") as file:
+    file = f"{'/'.join(command[2: command.index('-f')])}/{command[-1]}"
+    with open(file, "w") as file:
         file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        line = 1
         while True:
             line_text = input("Enter content line: ")
+
             if line_text == "stop":
+                file.write("\n")
                 break
-            file.write(f"1 {line_text}\n")
+            file.write(f"{line} {line_text}\n")
+            line += 1
 
 
 if "-d" in command and "-f" not in command:
