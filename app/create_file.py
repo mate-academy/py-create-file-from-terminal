c7499e59c13c6aa00221fcec6499cabbc1228c14
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-24 21:26:08 +0200

    Fixed bug with swaping -f and -d

--- app/create_file.py
+++ app/create_file.py
@@ -3,20 +3,21 @@ import sys
 import datetime
 
 
-def create_directory(dir_path: str, filename: str = None) -> None:
-    dir_path = dir_path.split(" ")
+def create_directory(dir_path: str) -> None:
+    dir_path = dir_path.split()
     directory = []
     d_in_directory = False
+
     for word in dir_path:
-        if word == "-f":
-            directory.append(filename)
-            create_file(os.path.join(*directory))
+        if d_in_directory and word == "-f":
             break
         if d_in_directory:
             directory.append(word)
-            os.mkdir(os.path.join(*directory))
         if word == "-d":
             d_in_directory = True
+    os.makedirs(os.path.join(*directory))
+    if "-f" in dir_path:
+        create_file(os.path.join(*directory, dir_path[dir_path.index("-f") + 1]))
 
 
 def create_file(file_name: str) -> None:
@@ -26,6 +27,7 @@ def create_file(file_name: str) -> None:
         while True:
             command = input("Enter content line: ")
             if command == "stop":
+                file.write("\n")
                 break
             file.write(command + "\n")
         file.write("\n")
@@ -34,10 +36,6 @@ def create_file(file_name: str) -> None:
 sys_argv = " ".join(sys.argv)
 sys_argv_last = sys.argv[-1]
 if len(sys.argv) > 1 and "-d" in sys.argv and "-f" in sys.argv:
-    create_directory(sys_argv, sys_argv_last)
-
-elif len(sys.argv) > 1 and "-d" in sys.argv:
     create_directory(sys_argv)
-
 elif len(sys.argv) > 1 and "-f" in sys.argv:
     create_file(sys_argv_last)
