8cfb0dafac7b64c5d19d2f6d9711acabfff40d2c
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-24 16:56:51 +0200

    refactor

--- app/create_file.py
+++ app/create_file.py
@@ -9,14 +9,22 @@ def create_path(directories: list) -> str:
 
 
 def create_file() -> None:
-    content = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
     line_number = 1
+
     with open(sys.argv[-1], "a") as file_out:
+        file_out.seek(0, os.SEEK_END)
+        data = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+
+        if file_out.tell() == 0:
+            content = data
+        else:
+            content = "\n" + data
+
         file_out.write(content)
+
         while True:
             content = input("Enter content line: ")
             if content == "stop":
-                file_out.write("\n")
                 break
             file_out.write(f"{line_number} {content}\n")
             line_number += 1
