435688e873e9fbebf2fc6bb1bc137a587636cbd7
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-24 20:35:40 +0200

    Cofe after refactoring

--- app/create_file.py
+++ app/create_file.py
@@ -3,19 +3,7 @@ import os
 import datetime
 
 
-def file_creating() -> None:
-    path, first_flag, *content = sys.argv
-    if "-f" in content:
-        *content, _, file_name = content
-        path = os.path.join(os.getcwd(), *content, file_name)
-
-    if first_flag == "-d":
-        path_directories = os.path.join(os.getcwd(), *content)
-        if not os.path.exists(path_directories):
-            os.makedirs(path_directories)
-    elif first_flag == "-f":
-        path = os.path.join(os.getcwd(), *content)
-
+def file_creating(path: str) -> None:
     with open(path, "a") as file_out:
         if file_out.tell() != 0:
             file_out.write("\n")
@@ -30,3 +18,22 @@ def file_creating() -> None:
         file_out.write(date_str)
         for index, element in enumerate(user_input):
             file_out.write(f"{index + 1} {element}\n")
+
+
+def path_creating(first_flag: str, *path_details) -> str:
+    if "-f" in path_details:
+        *path_details, _, file_name = path_details
+        path = os.path.join(os.getcwd(), *path_details, file_name)
+    if first_flag == "-d":
+        path_directories = os.path.join(os.getcwd(), *path_details)
+        if not os.path.exists(path_directories):
+            os.makedirs(path_directories)
+    elif first_flag == "-f":
+        path = os.path.join(os.getcwd(), *path_details)
+    return path
+
+
+if __name__ == "__main__":
+    _, first_flag, *path_details = sys.argv
+    path = path_creating(first_flag, *path_details)
+    file_creating(path)
