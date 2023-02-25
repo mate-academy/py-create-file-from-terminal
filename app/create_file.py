d905976b1c65130941d89c54dd1a2d4091eaa221
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-25 13:27:50 +0200

    Changes

--- app/create_file.py
+++ app/create_file.py
@@ -10,30 +10,24 @@ def make_dir() -> None:
     os.makedirs("/".join(command[2:]))
 
 
-def create_file() -> None:
-    with open(command[-1], "w") as file:
+def create_file(directory: str = "") -> None:
+    with open(f"{directory}{command[-1]}", "w") as file:
         file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-        while True:
-            line_text = input("Enter content line: ")
-            if line_text == "stop":
-                break
-            file.write(f"1 {line_text}\n")
-
-
-def create_file_in_directory() -> None:
-    os.makedirs("/".join(command[2: command.index("-f")]))
-    direc_file = f"{'/'.join(command[2: command.index('-f')])}/{command[-1]}"
-    with open(direc_file, "w") as direc_file:
-        direc_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         line = 1
         while True:
             line_text = input("Enter content line: ")
 
             if line_text == "stop":
-                direc_file.write("\n")
+                file.write("\n")
                 break
-            direc_file.write(f"{line} {line_text}\n")
+            file.write(f"{line} {line_text}\n")
             line += 1
+
+
+def create_file_in_directory() -> None:
+    direc_file = "/".join(command[2: command.index("-f")])
+    os.makedirs(direc_file)
+    create_file(f"{direc_file}/")
 
 
 if "-d" in command and "-f" not in command:
