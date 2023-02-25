f1f98fe03950ce2d18c61801d189ccec8e845e51
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-25 14:59:53 +0200

    Reworked with argparse lib

--- app/create_file.py
+++ app/create_file.py
@@ -1,9 +1,40 @@
-import sys
 import os
 import datetime
+import argparse
+import sys
+
+parser = argparse.ArgumentParser("create_file_from_terminal")
+parser.add_argument(
+    "-f",
+    metavar="filename",
+    nargs="+",
+    help="creates file in current directory"
+)
+parser.add_argument(
+    "-d",
+    metavar="dir_name",
+    nargs="+",
+    help="creates directories with provided path in form of dir1 dir2 dir3"
+)
+args = parser.parse_args()
 
 
-def file_creating(path: str) -> None:
+def creates_directories() -> None:
+    directories_path = os.path.join(*args.d)
+    if not os.path.exists(directories_path):
+        os.makedirs(directories_path)
+
+
+def creates_file_path() -> str:
+    if args.f and args.d:
+        return os.path.join(*args.d, *args.f)
+    elif args.f:
+        return os.path.join(*args.f)
+    elif args.d:
+        return os.path.join(*args.d)
+
+
+def creates_file(path: str) -> None:
     with open(path, "a") as file_out:
         if file_out.tell() != 0:
             file_out.write("\n")
@@ -13,6 +44,9 @@ def file_creating(path: str) -> None:
             if prompt_user_input == "stop":
                 break
             user_input.append(prompt_user_input)
+        if not user_input:
+            sys.exit("You didn't provide any content!")
+
         current_time = datetime.datetime.now()
         date_str = current_time.strftime("%Y-%m-%d %H:%M:%S\n")
         file_out.write(date_str)
@@ -20,20 +54,16 @@ def file_creating(path: str) -> None:
             file_out.write(f"{index + 1} {element}\n")
 
 
-def path_creating(first_flag: str, *path_details) -> str:
-    if "-f" in path_details:
-        *path_details, _, file_name = path_details
-        path = os.path.join(os.getcwd(), *path_details, file_name)
-    if first_flag == "-d":
-        path_directories = os.path.join(os.getcwd(), *path_details)
-        if not os.path.exists(path_directories):
-            os.makedirs(path_directories)
-    elif first_flag == "-f":
-        path = os.path.join(os.getcwd(), *path_details)
-    return path
-
-
 if __name__ == "__main__":
-    _, first_flag, *path_details = sys.argv
-    path = path_creating(first_flag, *path_details)
-    file_creating(path)
+    try:
+        path = creates_file_path()
+        if args.f and args.d:
+            creates_directories()
+            creates_file(path)
+        elif args.f:
+            creates_file(path)
+        elif args.d:
+            creates_directories()
+    except Exception as e:
+        print(f"{type(e)} was raised: \n{e}")
+        raise
