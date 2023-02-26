a442a3dd037572b809bb9074c000c80ddb850d0b
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-02-26 22:21:29 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,32 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+task = sys.argv[1:]
+
+
+def create_file_and_content(name: str) -> None:
+    with open(name, "a") as f:
+        content = []
+        content.append(datetime.now().strftime("%Y-%m-%d %X"))
+        i = 1
+
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            content.append(str(i) + " " + line)
+            i += 1
+
+        f.writelines(content)
+
+
+if "-f" in task:
+    if "-d" in task:
+        path = os.path.join(*task[1:-2])
+        os.makedirs(path)
+    create_file_and_content(task[-1])
+elif "-d" in task:
+    path = os.path.join(*task[1:])
+    os.makedirs(path)
