1707cf5c1acfce05008b6fd4cae1b68df7534ee9
Oleksandr Leha <legus96@gmail.com>
2023-02-27 11:53:29 +0200

    'Solution_with_numeration'

--- app/create_file.py
+++ app/create_file.py
@@ -11,13 +11,15 @@ if "-d" in argv and "-f" in argv:
     file_name = argv[argv.index("-f") + 1]
     path = os.path.join(path, file_name)
     with open(path, "a") as file_w:
+        n = 1
         date = datetime.now()
         file_w.write(date.strftime("%Y-%m-%d %H:%M:%S") + "\n")
         while True:
             content = input("Enter content line (type 'stop' to finish): ")
             if content == "stop":
                 break
-            file_w.write(content + "\n")
+            file_w.write(f"{n}" + content + "\n")
+            n += 1
 if argv[1] == "-d":
     path = ""
     for directory in argv[2:]:
@@ -32,4 +34,5 @@ elif argv[1] == "-f":
             content = input("Enter content line (type 'stop' to finish): ")
             if content == "stop":
                 break
-            file_w.write(content + "\n")
+            file_w.write(f"{n}" + content + "\n")
+            n += 1
