acd9bebf573e43f9f9e9dc18bca5426a71af2b25
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-02-27 02:51:41 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,27 +1,27 @@
-import datetime
+from datetime import datetime
 
 import os
 import sys
 
 
 command = sys.argv
-directory = os.getcwd()
 
-if "-d" in command:
-    for line in range(2, len(command)):
-        if command[line] == "-f":
-            break
-        directory = os.path.join(directory, command[line])
-    os.mkdir(directory)
 
-if "-f" in command:
-    current_file = os.path.join(directory, command[-1])
-    with open(current_file, "w") as new_file:
-        new_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        line_number = 1
-        while True:
-            line_input = input("Enter content line: ")
-            if line_input == "stop":
-                break
-            new_file.write(f"{line_number} {line_input} \n")
-            line_number += 1
+def create_file() -> None:
+    if "-d" in command:
+        index = 2
+        while len(command) != 2 and command[index] != "-f":
+            os.mkdir(command[index])
+            os.chdir(command[index])
+            index += 1
+
+    if "-f" in command:
+        with open(command[-1], "w") as new_file:
+            new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            line_number = 1
+            while True:
+                line_input = input("Enter content line: ")
+                if line_input == "stop":
+                    break
+                new_file.write(f"{line_number} {line_input} \n")
+                line_number += 1
