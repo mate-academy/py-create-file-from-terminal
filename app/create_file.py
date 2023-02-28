da013a8a3463258c92ca235836c10b76788720b6
name <ksyusha0kubay@gmail.com>
2023-02-28 18:15:05 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -5,17 +5,22 @@ from datetime import datetime
 
 
 def create_file() -> None:
+    dir_path = ""
     if "-d" in sys.argv:
         dir_path = os.path.join(*sys.argv[sys.argv.index("-d") + 1:])
-        os.makedirs(dir_path, exist_ok=True)
     if "-f" in sys.argv:
-        writing_in_file()
+        file_name = sys.argv[sys.argv.index("-f") + 1]
+        if dir_path:
+            os.makedirs(dir_path, exist_ok=True)
+            file_path = os.path.join(dir_path, file_name)
+        else:
+            file_path = file_name
+        writing_in_file(file_path)
 
 
-def writing_in_file() -> None:
-    file_name = sys.argv[sys.argv.index("-f") + 1]
+def writing_in_file(file_path: str) -> None:
     time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open(file_name, "w") as f:
+    with open(file_path, "w") as f:
         f.write(f"{time_now}\n")
         line = 1
         while True:
