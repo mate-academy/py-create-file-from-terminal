335cab869ff90c31020298bb174466f92732adb5
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-28 20:45:56 +0200

    Fixed

--- app/create_file.py
+++ app/create_file.py
@@ -12,20 +12,12 @@ def create_file(name_of_file: str) -> None:
     line_of_text = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
     line_number = 1
     while True:
-        if not os.path.isfile(f"./{name_of_file}"):
-            input_text = input("Enter content line: ")
-            if input_text == "stop":
-                line_of_text += "\n"
-                break
-            line_of_text += f"{line_number} {input_text}\n"
-            line_number += 1
-        if os.path.isfile(f"./{name_of_file}"):
-            input_text = input("Enter enother line of content: ")
-            if input_text == "stop":
-                line_of_text += "\n"
-                break
-            line_of_text += f"{line_number} {input_text}\n"
-            line_number += 1
+        input_text = input("Enter line of content: ")
+        if input_text == "stop":
+            line_of_text += "\n"
+            break
+        line_of_text += f"{line_number} {input_text}\n"
+        line_number += 1
 
     with open(name_of_file, "a") as result_file:
         result_file.write(line_of_text)
@@ -42,12 +34,12 @@ if "-d" in command and "-f" in command:
     full_file_name = f"{path}/{file_name}"
     create_file(full_file_name)
 
-if "-d" in command and "-f" not in command:
+elif "-d" in command:
     direct = command[command.index("-d") + 1:]
     path = create_path(direct)
     if not os.path.exists(path):
         os.makedirs(path)
 
-if "-f" in command and "-d" not in command:
+elif "-f" in command:
     file_name = "".join(command[command.index("-f") + 1:])
     create_file(file_name)
