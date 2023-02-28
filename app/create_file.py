2b98dff88f8b0f9dc0d06321709d673106ff41ad
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-02-28 15:05:28 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -10,23 +10,29 @@ def create_file_and_content(name: str) -> None:
     with open(name, "a") as f:
         content = []
         content.append(datetime.now().strftime("%Y-%m-%d %X"))
-        i = 1
+        index = 1
 
         while True:
             line = input("Enter content line: ")
             if line == "stop":
+                content.append("\n")
                 break
-            content.append(str(i) + " " + line)
-            i += 1
+            content.append(str(index) + " " + line)
+            index += 1
 
         f.writelines(content)
+
+
+def create_directory(path: str) -> None:
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
 if "-f" in task:
     if "-d" in task:
         path = os.path.join(*task[1:-2])
-        os.makedirs(path)
+        create_directory(path)
     create_file_and_content(task[-1])
 elif "-d" in task:
     path = os.path.join(*task[1:])
-    os.makedirs(path)
+    create_directory(path)
