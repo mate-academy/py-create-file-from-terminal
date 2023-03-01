1440c7b2976553925c534d59845116501695c5ca
MIlan <milan.manoylov@gmail.com>
2023-03-01 11:53:31 -0600

    create-file-from-terminal-Solution

--- app/create_file.py
+++ app/create_file.py
@@ -2,19 +2,24 @@ import os
 import sys
 from datetime import datetime
 
-if "-d" in sys.argv:
-    dir_path = os.path.join(*sys.argv[sys.argv.index("-d") + 1:])
-    os.makedirs(dir_path, exist_ok=True)
-else:
-    dir_path = "."
 
-if "-f" in sys.argv:
-    file_path = os.path.join(dir_path, sys.argv[sys.argv.index("-f") + 1])
-    with open(file_path, "a" if os.path.exists(file_path) else "w") as f:
+def create_file(file_path: str) -> None:
+    with open(file_path, "a") as f:
         if f.tell() == 0:
             f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
         for i, line in enumerate(iter(input, "stop"), 1):
             f.write(f"{i} {line}\n")
-else:
-    print("Error: -f flag is missing")
-    sys.exit(1)
+
+    if "-d" in sys.argv:
+        dir_path = os.path.join(*sys.argv[sys.argv.index("-d") + 1:])
+        os.makedirs(dir_path, exist_ok=True)
+    else:
+        dir_path = "."
+
+    if "-f" in sys.argv:
+        file_name = sys.argv[sys.argv.index("-f") + 1]
+        file_path = os.path.join(dir_path, file_name)
+        create_file(file_path)
+    else:
+        print("Error: -f flag is missing")
+        sys.exit(1)
