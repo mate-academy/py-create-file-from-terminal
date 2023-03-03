759d5216a841f9b7c167ef4114a50529e908cca2
Roman <parom95@ukr.net>
2023-03-03 15:37:58 +0200

    Solution4

--- app/create_file.py
+++ app/create_file.py
@@ -1,51 +1,41 @@
-import sys
+import datetime
 import os
-from datetime import datetime
-
-home_directory = sys.argv
+import sys
 
 
-def command_d() -> None:
-    if "-d" in home_directory and "-f" not in home_directory:
-        file_path = "\\".join(home_directory[home_directory.index("-d") + 1::])
-        path = os.path.join(file_path)
-        os.makedirs(path)
+def file_from_terminal() -> None:
+    home_directory = sys.argv
 
-
-def command_f() -> None:
-    count = 0
-    if "-f" in home_directory and "-d" not in home_directory:
-        with open(home_directory[home_directory.index("-f") + 1], "w") as file:
-            file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-            while True:
-                count += 1
-                word = input("Enter content line:")
-                if word == "stop":
-                    break
-                file.write(f"{count} {word}\n")
-
-
-def command_d_and_f() -> None:
     if "-d" in home_directory and "-f" in home_directory:
-        file_path = "\\".join(
-            home_directory[home_directory.index("-d") + 1:
-                           home_directory.index("-f")]
-        )
-        path = os.path.join(file_path)
-        os.makedirs(path)
+        create_directory(home_directory[2:-2])
+        create_file(home_directory[-1])
+        return
+
+    if "-d" in home_directory:
+        create_directory(home_directory[2:])
+
+    if "-f" in home_directory:
+        create_file(home_directory[2])
+
+
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as file:
+        file.write(datetime.datetime.now().strftime(
+            "%y-%m-%d %H:%M:%S\n"))
         count = 0
-        path += "\\" + home_directory[home_directory.index("-f") + 1]
-        with open(path, "w") as file:
-            file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-            while True:
-                count += 1
-                word = input("Enter content line:")
-                if word == "stop":
-                    break
-                file.write(f"{count} {word}\n")
+        while True:
+            count += 1
+            text = input("Enter content line: ")
+            if text == "stop":
+                break
+            file.write(f"{count} {text}\n")
+
+
+def create_directory(directories: list) -> None:
+    for directory in directories:
+        os.mkdir(directory)
+        os.chdir(directory)
 
 
 if __name__ == "__main__":
-    command_f()
-    command_d_and_f()
-    command_d()
+    file_from_terminal()
