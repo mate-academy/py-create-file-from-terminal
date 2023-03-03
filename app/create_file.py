3784efd269152d7a8a4dafb85767ca4cac445af7
Oleh Chornyi <ochernyi04@gmail.com>
2023-03-03 16:15:01 +0200

    Use makedirs, and clean while loop

--- app/create_file.py
+++ app/create_file.py
@@ -10,7 +10,7 @@ def create_file() -> None:
     if "-d" in command:
         index = 2
         while len(command) != 2 and command[index] != "-f":
-            os.mkdir(command[index])
+            os.makedirs(command[index], exist_ok=True)
             os.chdir(command[index])
             index += 1
 
@@ -18,9 +18,7 @@ def create_file() -> None:
         with open(command[-1], "w") as new_file:
             new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             line_number = 1
-            while True:
+            while line_input != "stop":
                 line_input = input("Enter content line: ")
-                if line_input == "stop":
-                    break
                 new_file.write(f"{line_number} {line_input} \n")
                 line_number += 1
