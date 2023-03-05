5d8c559279b020a13f8d976a2d75766505e56780
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-03-05 10:36:06 -0600

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,30 +3,45 @@ import os
 from datetime import datetime
 
 
-terminal_command = sys.argv[1:]
 index = 0
-path = []
 
-while index < len(terminal_command) - 1:
 
-    if terminal_command[index] == "-d":
+def create_folders() -> str:
+    global index
+    index += 1
+    path_arr = []
+
+    while terminal_command[index] not in ["-f", "-d"]:
+        path_arr.append(terminal_command[index])
         index += 1
-        while terminal_command[index] not in ["-f", "-d"]:
-            path.append(terminal_command[index])
-            index += 1
-        path = os.path.join(*path)
-        os.makedirs(path, exist_ok=True)
 
-    if terminal_command[index] == "-f":
-        index += 1
-        path = os.path.join(path, terminal_command[index])
-        with open(path, "a") as new_file:
-            new_file.write(
-                str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + "\n"
-            )
-            string_number = 1
+    directory_path = os.path.join(*path_arr)
+    os.makedirs(directory_path, exist_ok=True)
+
+    return directory_path
+
+
+def create_file(path: str = ""):
+    global index
+    index += 1
+
+    path_with_file = os.path.join(path, terminal_command[index])
+
+    with open(path_with_file, "a") as new_file:
+        new_file.write(
+            str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + "\n"
+        )
+        string_number = 1
+        string = input()
+        while string != "stop":
+            new_file.write(f"{str(string_number)} {string} \n")
+            string_number += 1
             string = input()
-            while string != "stop":
-                new_file.write(f"{str(string_number)} {string} \n")
-                string_number += 1
-                string = input()
+
+
+if __name__ == '__main__':
+    terminal_command = sys.argv[1:]
+    if terminal_command[index] == "-d":
+        path = create_folders()
+    if terminal_command[index] == "-f":
+        create_file(path)
