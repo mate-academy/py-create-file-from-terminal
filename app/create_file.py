d5492da59a42cf98a8bc7afe2fb1f86ebd6a764b
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-03-06 14:58:39 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,31 +3,30 @@ import sys
 import os
 
 
-def create_derectory() -> None:
-    current = os.getcwd()
+def create_directory() -> None:
     command = sys.argv
-    command_f = "-f"
-    command_d = "-d"
-    if command_d in command:
+    if "-d" in command:
         for line in range(2, len(command)):
-            if command[line] == command_f:
+            if command[line] == "-f":
                 break
-            current = os.path.join(current, command[line])
-        os.mkdir(current)
+            current = os.path.join(os.getcwd(), command[line])
+            os.makedirs(current, exist_ok=True)
 
 
-def create_file(file_name: str) -> None:
+def create_file() -> None:
+    file_name = input("Enter a name of file")
     with open(file_name, "w") as new_file:
         new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         line_counter = 1
         while True:
             asker = input("Enter content line: ")
             if asker == "stop":
+                new_file.write("\n")
                 break
             new_file.write(f"{line_counter} {asker}")
             line_counter += 1
 
 
 if __name__ == "__main__":
-    create_file("hello.txt")
-    create_derectory()
+    create_file()
+    create_directory()
