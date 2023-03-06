6e15de2ffdbcf1f603f071db39fddf8198083e64
Mykhailo Benzel <mishabenzel1997@gmail.com>
2023-03-06 23:13:47 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,31 +3,33 @@ import os
 from datetime import datetime
 
 
-terminal_command = sys.argv
-count = 0
-if "-d" in terminal_command:
-    if "-f" in terminal_command:
-        file_path =\
-            "\\".join(terminal_command[
-                      terminal_command.index("-d") + 1:
-                      terminal_command.index("-f")])
-    else:
-        file_path = "\\".join(terminal_command[2:])
-    os.makedirs(file_path)
-
-if "-f" in terminal_command:
+def create_file():
+    terminal_command = sys.argv
+    count = 0
     if "-d" in terminal_command:
-        path = os.path.join(file_path, terminal_command[
-            terminal_command.index("-f") + 1
-        ])
-    else:
-        path = terminal_command[terminal_command.index("-f") + 1]
+        if "-f" in terminal_command:
+            file_path =\
+                "\\".join(terminal_command[
+                          terminal_command.index("-d") + 1:
+                          terminal_command.index("-f")])
+        else:
+            file_path = "\\".join(terminal_command[2:])
+        os.makedirs(file_path)
 
-    with open(path, "w") as file_name:
-        file_name.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-        while True:
-            count += 1
-            content = input("Enter content line:")
-            if content == "stop":
-                break
-            file_name.write(f"{count} {content}\n")
+    if "-f" in terminal_command:
+        if "-d" in terminal_command:
+            path = os.path.join(file_path, terminal_command[
+                terminal_command.index("-f") + 1
+            ])
+        else:
+            path = terminal_command[terminal_command.index("-f") + 1]
+
+        with open(path, "w") as file_name:
+            file_name.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+            while content != "stop":
+                count += 1
+                content = input("Enter content line:")
+                file_name.write(f"{count} {content}\n")
+
+
+create_file()
