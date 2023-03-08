13af14aecba1edcea82ad1d07653f741fb3e8cdf
Arsen Makovei <makoveyarsen@gmail.com>
2023-03-08 21:14:09 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,41 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+def create_content(path_to_file: str) -> None:
+    with open(path_to_file, "w") as f:
+        time_now = datetime.now()
+        f.write(time_now.strftime("%Y-%m-%d %H:%M:%S") + "\n")
+
+        content = input("Enter content line: ")
+        line_number = 1
+        while content != "stop":
+            f.write(str(line_number) + " " + content + "\n")
+            content = input("Enter content line: ")
+            line_number += 1
+
+
+def create_file() -> None:
+    commands = sys.argv
+
+    if "-d" in commands and "-f" in commands:
+        dirs_path = "/".join(
+            command for command in commands[2:-1] if command != "-f"
+        )
+        os.makedirs(dirs_path, exist_ok=True)
+
+        path_to_file = dirs_path + "/" + commands[-1]
+        create_content(path_to_file)
+
+    elif "-d" in commands and "-f" not in commands:
+        dirs_path = "/".join(commands[2:])
+        os.makedirs(dirs_path, exist_ok=True)
+
+    elif "-f" in commands and "-d" not in commands:
+        path_to_file = commands[-1]
+        create_content(path_to_file)
+
+
+if __name__ == "__main__":
+    create_file()
