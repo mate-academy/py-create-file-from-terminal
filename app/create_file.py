1b60496ca72409f4643f6e6617919c1c6c61f401
Sashkoooo <o.denysenko@hotmail.com>
2023-03-12 14:57:23 +0200

    Solution v2.0

--- app/create_file.py
+++ app/create_file.py
@@ -11,8 +11,15 @@ def create_directory(path: str) -> None:
     os.makedirs(path, exist_ok=True)
 
 
+def add_timestamp() -> str:
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    return timestamp
+
+
 def content_input(path: str) -> None:
     with open(path, "a") as file:
+        file.write(f"{add_timestamp()}\n")
         line_counter = 1
         while True:
             line = input("Enter content line: ")
@@ -23,34 +30,49 @@ def content_input(path: str) -> None:
             line_counter += 1
 
 
-def add_timestamp(path: str) -> None:
-    now = datetime.now()
-    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-    with open(path, "a") as file:
-        file.write(f"{timestamp}\n")
-
-
-terminal_parser = argparse.ArgumentParser()
-terminal_parser.add_argument(
-    "-d", dest="directories", type=str, nargs="*", metavar="directories names",
-    help="creates directories inside current directory"
-)
-terminal_parser.add_argument(
-    "-f", dest="file_name", type=str, nargs=1, metavar="file name",
-    help="create file and input content lines until 'stop'"
-)
-argument_in_terminal = terminal_parser.parse_args()
-
-current_working_directory = os.getcwd()
-
-if argument_in_terminal.directories:
-    current_working_directory = create_path(argument_in_terminal.directories)
-    create_directory(current_working_directory)
-
-if argument_in_terminal.file_name:
-    path_to_file = [
-        current_working_directory, argument_in_terminal.file_name[0]
+def add_custom_commands(parser: argparse) -> None:
+    args_list = [
+        {
+            "option": "-d",
+            "dest": "directories",
+            "type": str,
+            "nargs": "*",
+            "metavar": "directory",
+            "help": "creates directories inside current directory",
+        },
+        {
+            "option": "-f",
+            "dest": "file_name",
+            "type": str,
+            "nargs": 1,
+            "metavar": "filename",
+            "help": "create file and input content lines until 'stop'",
+        },
     ]
-    path = create_path(path_to_file)
-    add_timestamp(path)
-    content_input(path)
+
+    for arg in args_list:
+        parser.add_argument(
+            arg["option"], dest=arg["dest"], type=arg["type"],
+            nargs=arg["nargs"], metavar=arg["metavar"], help=arg["help"]
+        )
+
+
+if __name__ == "__main__":
+
+    terminal_parser = argparse.ArgumentParser()
+    add_custom_commands(terminal_parser)
+    argument_in_terminal = terminal_parser.parse_args()
+    current_working_directory = os.getcwd()
+
+    if argument_in_terminal.directories:
+        current_working_directory = create_path(
+            argument_in_terminal.directories
+        )
+        create_directory(current_working_directory)
+
+    if argument_in_terminal.file_name:
+        path_to_file = [
+            current_working_directory, argument_in_terminal.file_name[0]
+        ]
+        path = create_path(path_to_file)
+        content_input(path)
