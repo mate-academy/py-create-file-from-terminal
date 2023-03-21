bbdec181588cef07b79437c89acb7b2dabde7a97
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-03-21 06:33:03 +0200

    put code into function

--- app/create_file.py
+++ app/create_file.py
@@ -3,31 +3,35 @@ from datetime import datetime
 import sys
 
 
-if "-d" in sys.argv:
-    dir_index = sys.argv.index("-d")
-    file_index = sys.argv.index("-f")
-    filepath = os.getcwd()
-    new_dirs = sys.argv[dir_index + 1: file_index]
-    new_dirs = os.path.join(*new_dirs)
-    filepath = os.path.join(filepath, new_dirs)
-    if not os.path.exists(filepath):
-        os.makedirs(new_dirs)
-        os.chdir(filepath)
+def create_directories_and_file() -> None:
+    if "-d" in sys.argv:
+        dir_index = sys.argv.index("-d")
+        file_index = sys.argv.index("-f")
+        filepath = os.getcwd()
+        new_dirs = sys.argv[dir_index + 1: file_index]
+        new_dirs = os.path.join(*new_dirs)
+        filepath = os.path.join(filepath, new_dirs)
+        if not os.path.exists(filepath):
+            os.makedirs(new_dirs)
+            os.chdir(filepath)
+
+    if "-f" in sys.argv:
+        with open(sys.argv[-1], "a+") as new_file:
+            time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            new_file.write(time + "\n")
+            while True:
+                input_text = input("Enter content line: ")
+                if input_text == "stop":
+                    break
+                new_file.write(input_text + "\n")
+
+    for line in sys.stdin:
+        if "q" == line.rstrip():
+            break
+        print(f"Input: {line}")
+
+    print("Exit")
 
 
-if "-f" in sys.argv:
-    with open(sys.argv[-1], "a+") as new_file:
-        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        new_file.write(time + "\n")
-        while True:
-            input_text = input("Enter content line: ")
-            if input_text == "stop":
-                break
-            new_file.write(input_text + "\n")
-
-for line in sys.stdin:
-    if "q" == line.rstrip():
-        break
-    print(f"Input: {line}")
-
-print("Exit")
+if __name__ == "__main__":
+    create_directories_and_file()
