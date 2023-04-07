02724a2ffcabcaf45ff3610f64103d08db61b838
Oleksandr <@gmail.com>
2023-04-07 08:26:40 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,45 @@
-# write your code here
+from datetime import datetime
+import os
+import sys
+
+
+def write_on_file(path: str) -> None:
+    with open(path, "a") as file:
+        line = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        while line != "stop":
+            file.write(line + "\n")
+            line = input("Enter content line: ")
+        file.write("\n")
+
+
+def create_dir(path: str) -> None:
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def main() -> None:
+    args = sys.argv[1:]
+    file_path = []
+    if "-d" in args and "-f" in args:
+
+        if args[0] == "-d":
+            file_path = [arg for arg in args[1:-2]]
+            file_path.append(args[-1])
+
+        if args[0] == "-f":
+            file_path = [arg for arg in args[3:]]
+            file_path.append(args[1])
+
+        create_dir(os.path.join(*file_path[:-1]))
+        write_on_file(os.path.join(*file_path))
+
+    elif "-d" in args:
+        file_path = [arg for arg in args[1:]]
+        create_dir(os.path.join(*file_path))
+
+    elif "-f" in args:
+        write_on_file(args[-1])
+
+
+if __name__ == "__main__":
+    main()
