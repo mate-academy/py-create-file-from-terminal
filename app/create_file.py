7a0777f04135b833c1819da916b5e779457676ab
MilMan <104576473+MilManIT@users.noreply.github.com>
2023-04-09 14:37:01 -0500

    Update create_file.py

--- app/create_file.py
+++ app/create_file.py
@@ -1,33 +1,49 @@
-import sys
 import os
+import sys
 from datetime import datetime
 
-args = sys.argv[1:]
-dir_path = ""
-file_name = ""
-file_content = ""
+def create_file():
+    args = sys.argv[1:]
 
-if "dir" in args:
-    idx = args.index("dir")
-    dir_path = os.path.join(*args[idx + 1:])
-    os.makedirs(dir_path, exist_ok=True)
+    if '-d' in args and '-f' in args:
+        dir_index = args.index('-d')
+        dir_path = os.path.join(*args[dir_index+1:])
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        file_index = args.index('-f')
+        file_name = args[file_index+1]
+        file_path = os.path.join(dir_path, file_name)
 
-if "file" in args:
-    idx = args.index("file")
-    file_name = args[idx + 1]
-    file_path = os.path.join(dir_path, file_name) if dir_path else file_name
+    elif '-d' in args:
+        dir_index = args.index('-d')
+        dir_path = os.path.join(*args[dir_index+1:])
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        return
 
-    lines = []
-    while True:
+    elif '-f' in args:
+        file_index = args.index('-f')
+        file_name = args[file_index+1]
+        file_path = file_name
+    else:
+        print("Please specify either the '-d' flag to create a directory or the '-f' flag to create a file.")
+        return
+
+    content = []
+    line = input("Enter content line: ")
+    while line != 'stop':
+        content.append(line)
         line = input("Enter content line: ")
-        if line == "stop":
-            break
-        lines.append(line)
 
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    lines = [f"{i} {line}" for i, line in enumerate(lines, start=1)]
-    content = f"{timestamp}\n" + "\n".join(lines) + "\n"
+    if os.path.exists(file_path):
+        with open(file_path, 'a') as file:
+            file.write('\n')
+            file.write('\n'.join(content))
+    else:
+        with open(file_path, 'w') as file:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(timestamp + '\n')
+            for i, line in enumerate(content):
+                file.write(f"{i+1} {line}\n")
 
-    mode = "a" if os.path.isfile(file_path) else "w"
-    with open(file_path, mode) as f:
-        f.write(content)
+create_file()
