fea2232d8fe07d8a4dcc685859b9d3bd1c73a7e1
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-14 11:58:50 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,41 @@
-# write your code here
+import argparse
+from datetime import datetime
+import os
+
+
+def create_file() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d", dest="dirs", nargs="*", help="create directories"
+    )
+    parser.add_argument("-f", dest="file", help="create file")
+    args = parser.parse_args()
+
+    path = ""
+
+    if args.dirs:
+        path = os.path.join(path, *args.dirs)
+        os.makedirs(path, exist_ok=True)
+        print(f"Directory created: {path}")
+
+    if args.file:
+        path = os.path.join(path, args.file)
+        with open(path, "a") as file_out:
+            lines = []
+            line_number = 0
+            while True:
+                line = input("Enter content line: ")
+                if not line or line == "stop":
+                    break
+                line_number += 1
+                lines.append(f"\n{line_number} {line}")
+            lines.append("\n" * 2)
+
+            current_time = datetime.now()
+            file_out.write(current_time.strftime("%Y-%m-%d %H:%M:%S"))
+            file_out.writelines(lines)
+            print(f"File created: {path}")
+
+
+if __name__ == "__main__":
+    create_file()
