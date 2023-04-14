b26007cfd7c25330fb0c2960d57d4b861a4556c6
“Ruslan <callogan217@gmail.com>
2023-04-14 13:31:08 +0300

    'Enter command' has been removed and separated functions have been created with adding of start-point

--- app/create_file.py
+++ app/create_file.py
@@ -1,26 +1,42 @@
 import os
 from datetime import datetime
+from typing import List
 
-command = input("Enter command: ")
 
-if "-f" in command:
-    file_index = command.index("-f")
-    file_name = command[file_index + 1]
-else:
-    file_name = None
+def create_directory(dir_name: str) -> None:
+    if dir_name:
+        path = os.path.join(*dir_name)
+        os.makedirs(path, exist_ok=True)
+        os.chdir(path)
 
-if "-d" in command:
-    dir_index = command.index("-d")
-    dir_name = command[dir_index + 1:]
-else:
-    dir_name = None
 
-if dir_name:
-    path = os.path.join(*dir_name)
-    os.makedirs(path, exist_ok=True)
-    os.chdir(path)
+def write_content_to_file(file_name: str, content: List[str]) -> None:
+    with open(file_name, "a") as file:
+        file.write(f"{datetime.now():%Y-%m-%d %H:%M:%S\n}")
+        index = 1
+        for line in content:
+            if line != "\n":
+                file.write(f"{index} {line}\n")
+                index += 1
+            else:
+                file.write("\n")
 
-if file_name:
+
+def main() -> None:
+    command = input()
+
+    if "-f" in command:
+        file_index = command.index("-f")
+        file_name = command[file_index + 1]
+    else:
+        file_name = None
+
+    if "-d" in command:
+        dir_index = command.index("-d")
+        dir_name = command[dir_index + 1:]
+    else:
+        dir_name = None
+
     content = []
     while True:
         content_line = input("Enter content line: ")
@@ -29,13 +45,11 @@ if file_name:
             break
         content.append(content_line)
 
-    with open(file_name, "a") as file:
-        file.write(f"{datetime.now():%Y-%m-%d %H:%M:%S\n}")
+    create_directory(dir_name)
+    write_content_to_file(file_name, content)
 
-        index = 1
-        for line in content:
-            if line != "\n":
-                file.write(f"{index} {line}\n")
-                index += 1
-            else:
-                file.write("\n")
+
+if __name__ == "__main__":
+    main()
+
+print(main())
