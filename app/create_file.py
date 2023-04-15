576f92135cdef11add33d85981ec478f0b026a71
“Ruslan <callogan217@gmail.com>
2023-04-15 20:19:04 +0300

    Seventh commit

--- app/create_file.py
+++ app/create_file.py
@@ -1,17 +1,19 @@
 import os
 import sys
 import datetime
+from typing import List
 
 
 def get_directory_names() -> list:
     dir_names = []
     if "-d" in sys.argv:
         d_index = sys.argv.index("-d")
-        if d_index < len(sys.argv) - 1:
-            i = d_index + 1
-            while i < len(sys.argv) and not sys.argv[i].startswith("-"):
-                dir_names.extend(sys.argv[i].split(","))
-                i += 1
+        f_index = sys.argv.index("-f") if "-f" in sys.argv else None
+        dir_names = (
+            sys.argv[d_index + 1:]
+            if f_index is None or d_index > f_index else
+            sys.argv[d_index + 1: f_index]
+        )
     return dir_names
 
 
@@ -19,27 +21,34 @@ def get_file_name() -> str:
     file_name = ""
     if "-f" in sys.argv:
         f_index = sys.argv.index("-f")
-        if f_index < len(sys.argv) - 1:
-            file_name = sys.argv[f_index + 1]
+        file_name = sys.argv[f_index + 1]
     return file_name
 
 
-def create_directories(dir_names: list) -> None:
-    nested_dir = ""
-    for dir_name in dir_names:
-        nested_dir = os.path.join(nested_dir, dir_name)
-        os.makedirs(nested_dir, exist_ok=True)
+def create_directories(dir_names: List[str]) -> None:
+    if dir_names:
+        directory_path = os.path.join(*dir_names)
+        os.makedirs(directory_path, exist_ok=True)
 
 
-def create_file(file_name: str, dir_names: list) -> None:
-    if file_name and not file_name.startswith("-") and dir_names:
-        nested_dir = os.path.join(*dir_names)
-        file_path = os.path.join(nested_dir, file_name)
+def create_file(file_path: str) -> None:
+    if os.path.exists(file_path):
+        print(f"File '{file_path}' already exists.")
+        print(f"Enter content to append to '{file_path}' "
+              f"(press Enter on an empty line to finish):")
+        content = ""
+        while True:
+            line = input()
+            if line == "stop":
+                break
+            content += line + "\n"
+        with open(file_path, "a") as f:
+            f.write(content)
+    else:
         with open(file_path, "w") as f:
             # Adding date and time to the file
             now = datetime.datetime.now()
             f.write(f"Creation time: {now.strftime('%Y-%m-%d %H:%M:%S')}\n")
-            # Inputting file content
             print(f"Enter content for {file_path} "
                   f"(press Enter on an empty line to finish):")
             content = ""
@@ -56,7 +65,12 @@ def main() -> None:
     dir_names = get_directory_names()
     file_name = get_file_name()
     create_directories(dir_names)
-    create_file(file_name, dir_names)
+    file_path = (
+        os.path.join(*dir_names, file_name)
+        if dir_names or file_name else None
+    )
+    if file_path:
+        create_file(file_path)
 
 
 if __name__ == "__main__":
