5498eec468610973e740268b767337950e600005
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-04-15 17:42:46 +0300

    add exist_ok mode, remove redundant check

--- app/create_file.py
+++ app/create_file.py
@@ -6,17 +6,24 @@ import sys
 def create_directories_and_file() -> None:
     if "-d" in sys.argv:
         dir_index = sys.argv.index("-d")
-        file_index = sys.argv.index("-f")
+        if "-f" in sys.argv:
+            file_index = sys.argv.index("-f")
+            if dir_index < file_index:
+                new_dirs_name = sys.argv[dir_index + 1: file_index]
+            else:
+                new_dirs_name = sys.argv[dir_index + 1:]
+        else:
+            new_dirs_name = sys.argv[dir_index + 1:]
         filepath = os.getcwd()
-        new_dirs = sys.argv[dir_index + 1: file_index]
-        new_dirs = os.path.join(*new_dirs)
+        new_dirs = os.path.join(*new_dirs_name)
         filepath = os.path.join(filepath, new_dirs)
         if not os.path.exists(filepath):
-            os.makedirs(new_dirs)
+            os.makedirs(new_dirs, exist_ok=False)
             os.chdir(filepath)
 
     if "-f" in sys.argv:
-        with open(sys.argv[-1], "a+") as new_file:
+        file_index = sys.argv.index("-f")
+        with open(sys.argv[file_index + 1], "a+") as new_file:
             time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             new_file.write(time + "\n")
             while True:
@@ -24,13 +31,6 @@ def create_directories_and_file() -> None:
                 if input_text == "stop":
                     break
                 new_file.write(input_text + "\n")
-
-    for line in sys.stdin:
-        if "q" == line.rstrip():
-            break
-        print(f"Input: {line}")
-
-    print("Exit")
 
 
 if __name__ == "__main__":
