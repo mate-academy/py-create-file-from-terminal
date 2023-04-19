1e9115dbb271f5ffe3420420a0c51a19ac00f047
Dmytry Bondariev <bondarievmd@gmail.com>
2023-04-19 16:02:31 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -10,7 +10,13 @@ if "-d" in args_from_terminal and "-f" in args_from_terminal:
     directories_path = "/".join(args_from_terminal[d_index + 1:f_index])
     file_name = args_from_terminal[-1]
     os.makedirs(directories_path)
-    with open(os.path.join(directories_path, file_name), "a") as file_to_create:
+    with open(
+            os.path.join(
+                directories_path,
+                file_name
+            ),
+            "a"
+    ) as file_to_create:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")
         file_to_create.write(timestamp)
         line_number = 1
