3656127832a89ddcbae590c1b62a0cc53e85a9c0
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-04-22 17:06:06 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,31 +3,37 @@ import os
 import datetime
 
 
-cmd_ls = sys.argv
-parent_path = os.getcwd()
-dir_path = ""
+def create_file(cmd_ls: list) -> None:
 
-for _ in cmd_ls:
-    if cmd_ls[-2] == "-f" and cmd_ls[1] == "-d":
-        dir_path = os.path.join(*cmd_ls[2:-2])
-        break
-    elif cmd_ls[1] == "-d":
-        dir_path = os.path.join(*cmd_ls[2:])
-        break
+    parent_path = os.getcwd()
+    dir_path = ""
 
-path = os.path.join(parent_path, dir_path)
-os.makedirs(name=path, exist_ok=True)
+    for _ in cmd_ls:
+        if cmd_ls[-2] == "-f" and cmd_ls[1] == "-d":
+            dir_path = os.path.join(*cmd_ls[2:-2])
+            break
+        elif cmd_ls[1] == "-d":
+            dir_path = os.path.join(*cmd_ls[2:])
+            break
 
-if cmd_ls[-2] == "-f":
-    file_name = cmd_ls[-1]
-    file_path = os.path.join(path, file_name)
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open(file_path, "a") as file:
-        file.write(f"{now}\n")
-        content = input("Enter content line: ")
-        i = 0
-        while content != "stop":
-            i += 1
-            file.write(f"{i} {content}\n")
-            content = input("Enter next content line: ")
-        file.write("\n")
+    path = os.path.join(parent_path, dir_path)
+    os.makedirs(name=path, exist_ok=True)
+
+    if cmd_ls[-2] == "-f":
+        file_name = cmd_ls[-1]
+        file_path = os.path.join(path, file_name)
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(file_path, "a") as file:
+            file.write(f"{now}\n")
+            content = input("Enter content line: ")
+            i = 0
+            while content != "stop":
+                i += 1
+                file.write(f"{i} {content}\n")
+                content = input("Enter next content line: ")
+            file.write("\n")
+
+
+if __name__ == '__main__':
+    cmd = sys.argv
+    create_file(cmd_ls=cmd)
