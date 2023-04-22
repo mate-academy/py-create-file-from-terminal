de214e0a2c5e6000b85ec2d19a067b5dbd862890
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-04-22 16:19:20 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -5,23 +5,29 @@ import datetime
 
 cmd_ls = sys.argv
 parent_path = os.getcwd()
-file_name = cmd_ls[-1]
+dir_path = ""
 
-if cmd_ls[1] == "-d":
-    dir_path = "/".join(cmd_ls[2:-2])
-    path = os.path.join(parent_path, dir_path)
-else:
-    path = parent_path
+for _ in cmd_ls:
+    if cmd_ls[1] == "-d" and cmd_ls[-2] == "-f":
+        dir_path = "/".join(cmd_ls[2:-2])
+        break
+    elif cmd_ls[1] == "-d":
+        dir_path = "/".join(cmd_ls[2:])
+        break
 
+path = os.path.join(parent_path, dir_path)
 os.makedirs(name=path, exist_ok=True)
-file_path = os.path.join(path, file_name)
-now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-with open(file_path, "a") as file:
-    file.write(f"{now}\n")
-    content = input("Enter content line ...")
-    i = 0
-    while content != "stop":
-        i += 1
-        file.write(f"{i} {content}\n")
-        content = input("Enter next content line ...")
-    file.write("\n")
+
+if cmd_ls[-2] == "-f":
+    file_name = cmd_ls[-1]
+    file_path = os.path.join(path, file_name)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(file_path, "a") as file:
+        file.write(f"{now}\n")
+        content = input("Enter content line: ")
+        i = 0
+        while content != "stop":
+            i += 1
+            file.write(f"{i} {content}\n")
+            content = input("Enter next content line: ")
+        file.write("\n")
