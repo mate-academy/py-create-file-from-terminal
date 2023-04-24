4dce33aa009d397ff7414359c6b711de6996821a
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-04-24 18:14:24 +0300

    Solution +_

--- app/create_file.py
+++ app/create_file.py
@@ -1,37 +1,40 @@
-import datetime
-from sys import argv
-from os import makedirs, path
-from pathlib import Path
+import sys
+import os
+from datetime import datetime
 
 
-def create_dirs(file_path: str) -> None:
-    makedirs(file_path, exist_ok=True)
+def create_dir() -> None:
+    input_command = sys.argv
+    file_name = ""
+    parent_dir = ""
+    if ("-f" and "-d") in input_command:
+        index_f, index_d = input_command.index("-f"), input_command.index("-d")
+        file_name = input_command[index_f + 1]
+        if index_d < index_f:
+            parent_dir = "/".join(input_command[(index_d + 1):index_f])
+        if index_d > index_f:
+            parent_dir = "/".join(input_command[index_d + 1:])
+    if "-f" not in input_command and "-d" not in input_command:
+        print("Please specify at least one flag -f or -d")
+        sys.exit(1)
+    if "-f" in input_command and "-d" not in input_command:
+        file_name = input_command[1]
+    if "-d" in input_command and "-f" not in input_command:
+        parent_dir = "/".join(input_command[1:])
+    path = os.path.join(parent_dir, file_name)
+    os.makedirs(path)
 
 
-def create_path(file_path: str, file_name: str) -> None:
-    create_dirs(file_path)
+def create_file(name_file: str) -> None:
+    with open(name_file, "a") as file:
+        file.write(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        line_number = 1
+        while True:
+            line = input("Enter content line: ")
+            if line.lower() != "stop":
+                file.write(f"{line_number} {line}\n")
+                line_number += 1
 
-    file_path = Path(file_path) / file_name
-    with file_path.open("a") as file:
-        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-        for count, input_string in enumerate(iter(input, "stop")):
-            file.write(f"{count} {input_string}\n")
 
-
-def create_file() -> None:
-    if "-f" in argv:
-        file_index = argv.index("-f")
-        file_name = argv[file_index + 1]
-        dir_index = None
-        for i, arg in enumerate(argv):
-            if arg == "-d" and i < file_index:
-                dir_index = i
-
-        if dir_index is not None:
-            dir_path = path.sep.join(argv[dir_index + 1:file_index])
-        else:
-            dir_path = path.curdir
-
-        create_path(dir_path, file_name)
-    elif "-d" in argv:
-        create_dirs(path.sep.join(argv[argv.index("-d") + 1:]))
+if __name__ == "__main__":
+    create_dir()
