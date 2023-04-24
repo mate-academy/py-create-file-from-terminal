a1586e193ec37efe5081376463e8dc318f725cae
Maksym Bezhevets <begik4@gmail.com>
2023-04-24 12:43:58 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -5,30 +5,36 @@ import datetime
 
 def create_file(argv: list) -> None:
     path_list = ""
-    if "-d" in argv:
-        if "-f" in argv:
-            path_list = argv[argv.index("-d") + 1: argv.index("-f")]
-        else:
-            path_list = argv[argv.index("-d") + 1:]
-        if not os.path.exists(os.path.join(*path_list)):
-            os.makedirs(os.path.join(*path_list))
+    try:
+        if "-d" in argv:
+            if "-f" in argv:
+                path_list = argv[argv.index("-d") + 1: argv.index("-f")]
+            else:
+                path_list = argv[argv.index("-d") + 1:]
+            os.makedirs(os.path.join(*path_list), exist_ok=True)
 
-    if "-f" in argv:
-        file_name = argv[argv.index("-f") + 1]
-        path = os.path.join(*path_list, file_name)
-        with open(path, "a") as file:
-            file.write(
-                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-            )
-            number_line = 1
-            while True:
-                line_text = input("Enter content line: ")
-                if line_text == "stop" or line_text == "Stop":
-                    file.write("\n")
-                    break
-                else:
+        if "-f" in argv:
+            file_name = argv[argv.index("-f") + 1]
+            path = os.path.join(*path_list, file_name)
+            with open(path, "a") as file:
+                file.write(
+                    datetime.datetime.now().strftime(
+                        "%Y-%m-%d %H:%M:%S") + "\n"
+                )
+                number_line = 1
+                while True:
+                    line_text = input("Enter content line: ")
+                    if line_text == "stop" or line_text == "Stop":
+                        file.write("\n")
+                        break
                     file.write(f"{number_line} {line_text}\n")
                     number_line += 1
+    except Exception:
+        print(
+            f"ERROR! Write correct command. Example: "
+            f"\n\tpython {os.path.basename(__file__)} "
+            f"-d name_dir -f name_file.txt"
+        )
 
 
 create_file(sys.argv)
