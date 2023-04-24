8f493c788af379bd5efceaac5a1fc66b409e71e8
Maksym Bezhevets <begik4@gmail.com>
2023-04-24 13:36:55 +0300

    New solution

--- app/create_file.py
+++ app/create_file.py
@@ -5,36 +5,31 @@ import datetime
 
 def create_file(argv: list) -> None:
     path_list = ""
-    try:
-        if "-d" in argv:
-            if "-f" in argv:
-                path_list = argv[argv.index("-d") + 1: argv.index("-f")]
-            else:
-                path_list = argv[argv.index("-d") + 1:]
-            os.makedirs(os.path.join(*path_list), exist_ok=True)
-
+    if "-d" in argv:
         if "-f" in argv:
-            file_name = argv[argv.index("-f") + 1]
-            path = os.path.join(*path_list, file_name)
-            with open(path, "a") as file:
-                file.write(
-                    datetime.datetime.now().strftime(
-                        "%Y-%m-%d %H:%M:%S") + "\n"
-                )
-                number_line = 1
-                while True:
-                    line_text = input("Enter content line: ")
-                    if line_text == "stop" or line_text == "Stop":
-                        file.write("\n")
-                        break
-                    file.write(f"{number_line} {line_text}\n")
-                    number_line += 1
-    except Exception:
-        print(
-            f"ERROR! Write correct command. Example: "
-            f"\n\tpython {os.path.basename(__file__)} "
-            f"-d name_dir -f name_file.txt"
-        )
+            if argv.index("-d") > argv.index("-f"):
+                path_list = argv[argv.index("-d") + 1:]
+            else:
+                path_list = argv[argv.index("-d") + 1: argv.index("-f")]
+        else:
+            path_list = argv[argv.index("-d") + 1:]
+        os.makedirs(os.path.join(*path_list), exist_ok=True)
+
+    if "-f" in argv:
+        file_name = argv[argv.index("-f") + 1]
+        path = os.path.join(*path_list, file_name)
+        with open(path, "a") as file:
+            file.write(
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+            )
+            number_line = 1
+            while True:
+                line_text = input("Enter content line: ")
+                if line_text == "stop":
+                    file.write("\n")
+                    break
+                file.write(f"{number_line} {line_text}\n")
+                number_line += 1
 
 
 create_file(sys.argv)
