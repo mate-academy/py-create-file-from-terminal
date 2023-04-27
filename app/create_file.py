32f0b0b6068495b6ff5bc9b1480e7577749b947d
Volodymyr Borovets <vborovets0@gmail.com>
2023-04-27 21:38:22 +0300

    change name in context manager

--- app/create_file.py
+++ app/create_file.py
@@ -18,14 +18,14 @@ if "-d" in commands:
 if "-f" in commands:
     new_file = commands[commands.index("-f") + 1]
 
-    with open(os.path.join(parent_dir, directory, new_file), "a") as f:
+    with open(os.path.join(parent_dir, directory, new_file), "a") as file:
 
-        f.write(str(datetime.datetime.now().
-                    strftime("%Y-%m-%d %H:%M:%S")) + "\n")
+        file.write(str(datetime.datetime.now().
+                       strftime("%Y-%m-%d %H:%M:%S")) + "\n")
         text = input("Enter content line: ")
         count = 0
         while text != "stop":
             count += 1
-            f.write(f"{count} {text} \n")
+            file.write(f"{count} {text} \n")
             text = input("Enter content line: ")
-        f.write("\n")
+        file.write("\n")
