c27b50f89d1d011c5a6dfde7aab1c72d3d65476b
Iryna Ishchenko <irynaira@gmail.com>
2023-04-27 19:00:42 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -6,19 +6,19 @@ from datetime import datetime
 def create_file(argv: list) -> None:
     f_appeared = False
     d_appeared = False
-    index_i = 1
-    while index_i < len(argv):
-        if argv[index_i] == "-f":
+    index_f_appeared = 1
+    while index_f_appeared < len(argv):
+        if argv[index_f_appeared] == "-f":
             f_appeared = True
-            file_path = argv[index_i + 1]
-            index_i += 2
+            file_path = argv[index_f_appeared + 1]
+            index_f_appeared += 2
         else:
             d_appeared = True
-            index_j = index_i + 1
-            while index_j < len(argv) and argv[index_j] != "-f":
-                index_j += 1
-            folders = os.path.join(*argv[index_i + 1:index_j])
-            index_i = index_j
+            index_d_appeared = index_f_appeared + 1
+            while index_d_appeared < len(argv) and argv[index_d_appeared] != "-f":
+                index_d_appeared += 1
+            folders = os.path.join(*argv[index_f_appeared + 1:index_d_appeared])
+            index_f_appeared = index_d_appeared
     if d_appeared:
         os.makedirs(folders, exist_ok=True)
     if f_appeared:
@@ -29,11 +29,11 @@ def create_file(argv: list) -> None:
             if file_existed:
                 print(file=file)
             print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file=file)
-            index_i = 1
+            index_f_appeared = 1
             row = input("Enter content line: ")
             while row != "stop":
-                print(index_i, row, file=file)
-                index_i += 1
+                print(index_f_appeared, row, file=file)
+                index_f_appeared += 1
                 row = input("Enter content line: ")
 
 
