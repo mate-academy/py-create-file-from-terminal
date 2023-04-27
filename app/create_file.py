ec454485746ce6af4a3d6ade0cba7e4fed6199e0
Iryna Ishchenko <irynaira@gmail.com>
2023-04-27 14:13:30 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,34 +3,38 @@ import os
 from datetime import datetime
 
 
-f_appeared = False
-d_appeared = False
-index_i = 1
-while index_i < len(argv):
-    if argv[index_i] == "-f":
-        f_appeared = True
-        file_path = argv[index_i + 1]
-        index_i += 2
-    else:
-        d_appeared = True
-        index_j = index_i + 1
-        while index_j < len(argv) and argv[index_j] != "-f":
-            index_j += 1
-        folders = os.path.join(*argv[index_i + 1:index_j])
-        index_i = index_j
-if d_appeared:
-    os.makedirs(folders, exist_ok=True)
-if f_appeared:
+def create_file(argv: list) -> None:
+    f_appeared = False
+    d_appeared = False
+    index_i = 1
+    while index_i < len(argv):
+        if argv[index_i] == "-f":
+            f_appeared = True
+            file_path = argv[index_i + 1]
+            index_i += 2
+        else:
+            d_appeared = True
+            index_j = index_i + 1
+            while index_j < len(argv) and argv[index_j] != "-f":
+                index_j += 1
+            folders = os.path.join(*argv[index_i + 1:index_j])
+            index_i = index_j
     if d_appeared:
-        file_path = os.path.join(folders, file_path)
-    file_existed = os.path.isfile(file_path)
-    with open(file_path, "a") as file:
-        if file_existed:
-            print(file=file)
-        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file=file)
-        index_i = 1
-        row = input("Enter content line: ")
-        while row != "stop":
-            print(index_i, row, file=file)
-            index_i += 1
+        os.makedirs(folders, exist_ok=True)
+    if f_appeared:
+        if d_appeared:
+            file_path = os.path.join(folders, file_path)
+        file_existed = os.path.isfile(file_path)
+        with open(file_path, "a") as file:
+            if file_existed:
+                print(file=file)
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file=file)
+            index_i = 1
             row = input("Enter content line: ")
+            while row != "stop":
+                print(index_i, row, file=file)
+                index_i += 1
+                row = input("Enter content line: ")
+
+
+create_file(argv)
