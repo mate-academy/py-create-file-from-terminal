b86d88d1a829a46b6fd1ad118cfe4dff4b1e54fc
Nikita Provorov <nikiyorkovich@gmail.com>
2023-05-03 22:33:45 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -4,8 +4,8 @@ import datetime
 
 
 # function, which create text file and fill it
-def file_write(filename: str) -> None:
-    text = open(filename, "a")
+def file_write(*filename) -> None:
+    text = open(os.path.join(*filename), "a")
     text.write(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M%:%S")) + "\n")
 
     while True:
@@ -17,25 +17,27 @@ def file_write(filename: str) -> None:
 
 
 # function, which create dictionary
-def create_dic(filesname: list) -> None:
-    path = os.path.join(filesname[2], filesname[3])
+def create_dic(filesname: list) -> str:
+    path = os.path.join(*filesname)
     os.makedirs(path)
+    return path
 
 
 def create_file() -> None:
     # take message from terminal
     terminal = sys.argv
     # check, does we need to make a directory and text file
-    if len(terminal) > 4:
-        create_dic(terminal)
-        path = os.path.join(terminal[2], terminal[3], terminal[5])
-        file_write(path)
+    if "-d" in terminal and "-f" in terminal:
+        text_name = terminal[terminal.index("-f") + 1]
+        path = create_dic(terminal[terminal.index("-d") + 1: terminal.index("-f")]
+                          or terminal[terminal.index("-d") + 1:])
+        file_write(path, text_name)
     # check, does we need to make a directory
     elif "-d" in terminal:
-        create_dic(terminal)
+        create_dic(terminal[terminal.index("-d") + 1:])
     # # check, does we need to make a text file
     elif "-f" in terminal:
-        file_write(terminal[2])
+        file_write(terminal[terminal.index("-f") + 1])
 
 
 if __name__ == "__main__":
