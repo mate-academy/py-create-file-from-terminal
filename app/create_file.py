f16a4c3473873643494e0710df841f550ac617a3
Nikita Provorov <nikiyorkovich@gmail.com>
2023-05-04 10:06:30 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -6,7 +6,8 @@ import datetime
 # function, which create text file and fill it
 def file_write(*filename) -> None:
     text = open(os.path.join(*filename), "a")
-    text.write(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M%:%S")) + "\n")
+    text.write(str(datetime.datetime.now()
+                   .strftime("%Y-%m-%d %H:%M%:%S")) + "\n")
 
     while True:
         message = input("Enter content line: ") + "\n"
@@ -29,7 +30,8 @@ def create_file() -> None:
     # check, does we need to make a directory and text file
     if "-d" in terminal and "-f" in terminal:
         text_name = terminal[terminal.index("-f") + 1]
-        path = create_dic(terminal[terminal.index("-d") + 1: terminal.index("-f")]
+        path = create_dic(terminal[terminal.index("-d")
+                                   + 1: terminal.index("-f")]
                           or terminal[terminal.index("-d") + 1:])
         file_write(path, text_name)
     # check, does we need to make a directory
