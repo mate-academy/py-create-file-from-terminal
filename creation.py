7355d8ec464f2688518cad841b5741847ede191f
Maks Blazhko <makssimm1@gmail.com>
2023-05-14 18:20:09 +0200

    solution v2

--- creation.py
+++ creation.py
@@ -7,13 +7,15 @@ def create_file(file_name: str, destination: Optional[list] = None) -> None:
     if destination is None:
         destination = [""]
     with open(os.path.join(*destination, file_name), "a") as source_file:
-        source_file.write(datetime.now().strftime("%m-%d-%Y %H:%M:%S") + "\n")
+        source_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        counter = 1
         while True:
             new_str = input("Enter: ")
             if new_str == "stop":
                 source_file.write("\n")
                 break
-            source_file.write(new_str + "\n")
+            source_file.write(f"{counter} {new_str}\n")
+            counter += 1
 
 
 def create_dir(cmd: list[str]) -> list:
