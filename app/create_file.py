0b5d70066bf9994f379f6e0add1934c73c3b4270
Dmitry <dimior1992@gmail.com>
2023-05-16 16:50:27 +0300

    fix2

--- app/create_file.py
+++ app/create_file.py
@@ -5,23 +5,23 @@ from datetime import datetime
 
 def create_file() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", dest="dirs")
+    parser.add_argument("-d", nargs="*", dest="dirs")
     parser.add_argument("-f", dest="file")
     path = ""
 
     if parser.parse_args().dirs:
-        path = os.path.join(path, parser.parse_args().dirs)
+        path = os.path.join(path, *parser.parse_args().dirs)
         os.makedirs(path, exist_ok=True)
     if parser.parse_args().file:
         path = os.path.join(path, parser.parse_args().file)
         with open(path, "a") as f:
-            f.write(str(datetime.now()))
+            f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
             line_index = 1
             while True:
                 text = input("Enter content line: ")
                 if text.lower() == "stop":
                     break
-                f.write(f"{line_index}: {text}\n")
+                f.write(f"{line_index} {text}\n")
                 line_index += 1
 
 
