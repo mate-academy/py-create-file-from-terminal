8530ca669219710d00f18777a1f87f48e5b2f593
Dmitry <dimior1992@gmail.com>
2023-05-16 13:13:43 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,25 @@
-# write your code here
+import os
+import argparse
+from datetime import datetime
+
+
+def create_file() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", dest="dirs")
+    parser.add_argument("-f", dest="file")
+    path = ""
+
+    if parser.parse_args().dirs:
+        path = os.path.join(path, parser.parse_args().dirs)
+        os.makedirs(path, exist_ok=True)
+    if parser.parse_args().file:
+        path = os.path.join(path, parser.parse_args().file)
+        with open(path, "a") as f:
+            f.write(str(datetime.now()))
+            line_index = 1
+            while True:
+                text = input("Enter content line: ")
+                if text.lower() == "stop":
+                    break
+                f.write(f"{line_index}: {text}\n")
+                line_index += 1
