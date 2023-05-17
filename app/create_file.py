d284096a4160f43ccf92f5f23e4edef26b284c19
Vladimir Titarenko <tytarenkova@gmail.com>
2023-05-17 20:50:37 +0300

    Checking for directory existence and adding line numbers has been added. File overwriting has been fixed.

--- app/create_file.py
+++ app/create_file.py
@@ -3,20 +3,29 @@ import os
 from datetime import datetime
 
 
-def create_content_for_new_file() -> list:
-    content = []
+def create_new_content_for_file() -> list:
+    content, line_number = [], 0
     while True:
+        line_number += 1
         line = input()
         if line == "stop":
             break
-        content.append(line + "\n")
+        content.append(f"{line_number} {line}\n")
+    content.append("\n")
     return content
 
 
-def write_content_into_new_file(file_name: str, content: list) -> None:
-    with open(file_name, "w") as f:
+def write_content_into_file(file_name: str, content: list) -> None:
+    with open(file_name, "a") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
         f.writelines(content)
+
+
+def create_directory_with_path(path_: str) -> None:
+    try:
+        os.makedirs(path_)
+    except FileExistsError:
+        pass
 
 
 def create_file() -> None:
@@ -32,14 +41,14 @@ def create_file() -> None:
                 path_ = os.path.join(
                     *command_line[command_line.index("-d") + 1::]
                 )
-            os.makedirs(path_)
+            create_directory_with_path(path_)
             os.chdir(path_)
         file_name = command_line[command_line.index("-f") + 1]
-        content = create_content_for_new_file()
-        write_content_into_new_file(file_name, content)
+        content = create_new_content_for_file()
+        write_content_into_file(file_name, content)
     elif "-d" in command_line:
         path_ = os.path.join(*command_line[command_line.index("-d") + 1::])
-        os.makedirs(path_)
+        create_directory_with_path(path_)
 
 
 if __name__ == "__main__":
