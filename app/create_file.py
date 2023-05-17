c9a0f6f203448bf30611f9e34d1eeaf7906a8773
Vladyslav Kaminskyi <119516716+VladyslavKaminskyi@users.noreply.github.com>
2023-05-17 18:22:13 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,27 @@
-# write your code here
+from os import makedirs, path
+from datetime import datetime
+from sys import argv
+
+command = argv
+
+
+def creating_file(path: str) -> None:
+    with open(path, "a") as file:
+        file.write(datetime.now().strftime("%Y-%m-%d, %H:%M:%S") + "\n")
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                file.write("\n")
+                break
+            file.write(line + "\n")
+
+
+if "-f" in command:
+    if "-d" in command:
+        command.pop(command.index("-f"))
+        makedirs(path.join(*command[2:-1]))
+        creating_file(path.join(*command[2:]))
+    else:
+        creating_file(command[command.index("-f") + 1])
+if "-d" in command:
+    makedirs(path.join(*command[2:]))
