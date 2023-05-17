2bd06ae8880280c6ad2772024fe537e01395cf90
OleksandrLeliukh <alexander.leluh@gmail.com>
2023-05-17 09:40:58 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,38 @@
-# write your code here
+import os
+import sys
+
+from datetime import datetime
+
+argv = sys.argv
+
+
+def create_folder() -> list:
+    if "-f" in argv and argv.index("-d") < argv.index("-f"):
+        dirs = argv[argv.index("-d") + 1:argv.index("-f")]
+    else:
+        dirs = argv[argv.index("-d") + 1:]
+    os.makedirs(os.path.join(*dirs), exist_ok=True)
+    return dirs
+
+
+def create_file() -> None:
+    if "-d" in argv:
+        path = create_folder()
+    else:
+        path = ""
+    with open(f"{path}{argv[argv.index('-f') + 1]}", "a") as f:
+        time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write("\n" + time_now + "\n")
+        num = 1
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            f.write(f"{num} {line}\n")
+            num += 1
+
+
+if "-d" in argv:
+    create_folder()
+if "-f" in argv:
+    create_file()
