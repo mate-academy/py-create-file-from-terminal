2c021ced87350282087b6bd45497ebaafa4fc382
Oksana Makieieva <oksana.zhornyk@gmail.com>
2023-05-25 12:33:23 +0200

    py-create-file-from-terminal-v2

--- app/create_file.py
+++ app/create_file.py
@@ -4,16 +4,13 @@ import datetime
 
 
 command = sys.argv
-print(command)
 if "-f" in command:
     file_name = command[-1]
-    print(f"i find file name. {file_name}")
     if "-d" in command:
         dir_list = command[command.index("-d") + 1:command.index("-f")]
         path = os.path.dirname("/".join(dir_list) + "/")
         os.makedirs(path, exist_ok=True)
         file_name = f"{path}/{file_name}"
-    print(file_name)
     with open(file_name, "a") as file:
         file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
         input_command = ""
@@ -21,7 +18,7 @@ if "-f" in command:
         while input_command != "stop":
             input_command = input("Enter content line: ")
             if input_command == "stop":
-                file.write(f"\n")
+                file.write("\n")
                 break
             lines += 1
             file.write(f"{lines} {input_command}\n")
