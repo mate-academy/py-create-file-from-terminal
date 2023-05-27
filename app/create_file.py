dfbd2ea9a4ae9f1cdb4170673fb7da19242833b6
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-05-27 16:48:38 +0300

    add condition that without -f flag the file isn't created

--- app/create_file.py
+++ app/create_file.py
@@ -7,39 +7,48 @@ def create_file() -> None:
     arguments = sys.argv[1:]
     file_name = "file.txt"
     directories = []
-    if "-d" in arguments and "-f" in arguments:
-        index_d = arguments.index("-d")
-        index_f = arguments.index("-f")
-        directories = arguments[(index_d + 1) : index_f]
-        file_name = arguments[-1]
-    if "-d" in arguments and "-f" not in arguments:
-        index_d = arguments.index("-d")
-        directories = arguments[(index_d + 1) :]
-    if "-f" in arguments and "-d" not in arguments:
-        file_name = arguments[-1]
+    if "-f" in arguments:
+        if "-d" in arguments and "-f" in arguments:
+            index_d = arguments.index("-d")
+            index_f = arguments.index("-f")
+            directories = arguments[(index_d + 1) : index_f]
+            file_name = arguments[-1]
+        if "-f" in arguments and "-d" not in arguments:
+            file_name = arguments[-1]
 
-    directories_path = os.path.join(*directories) if directories else ""
-    file_name = str(file_name)
-    destination = os.path.join(directories_path, file_name)
-    if directories_path != "":
+        directories_path = os.path.join(*directories) if directories else ""
+        file_name = str(file_name)
+        destination = os.path.join(directories_path, file_name)
+        if directories_path != "":
+            if not os.path.exists(directories_path):
+                os.makedirs(directories_path)
+
+        with open(destination, "a+") as destination_file:
+            content = []
+            line = ""
+            counter = 1
+            while line != "stop":
+                line = input("Enter content line: ")
+                if line != "stop":
+                    if destination_file.tell() == 0:
+                        content.append(f"{counter} Line{counter} " + line)
+                    else:
+                        content.append(
+                            f"{counter} Another line{counter} " + line
+                        )
+                counter += 1
+
+            now = datetime.now()
+            date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+            content.insert(0, date_time)
+            content.append("")
+            destination_file.write("\n".join(content))
+    else:
+        directories = arguments[(arguments.index("-d") + 1) :]
+        directories_path = os.path.join(*directories)
         if not os.path.exists(directories_path):
             os.makedirs(directories_path)
 
-    with open(destination, "a+") as destination_file:
-        content = []
-        line = ""
-        counter = 1
-        while line != "stop":
-            line = input("Enter content line: ")
-            if line != "stop":
-                if destination_file.tell() == 0:
-                    content.append(f"{counter} Line{counter} " + line)
-                else:
-                    content.append(f"{counter} Another line{counter} " + line)
-            counter += 1
 
-        now = datetime.now()
-        date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-        content.insert(0, date_time)
-        content.append("")
-        destination_file.write("\n".join(content))
+if __name__ == "__main__":
+    create_file()
