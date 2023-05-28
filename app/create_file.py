8744e992c5bfc8b7488eea2da88ab043399a55fd
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-05-29 00:33:31 +0300

    refactoring code

--- app/create_file.py
+++ app/create_file.py
@@ -5,37 +5,31 @@ from datetime import datetime
 
 def create_file() -> None:
     arguments = sys.argv[1:]
-    file_name = "file.txt"
     directories = []
     if "-f" in arguments:
-        if "-d" in arguments and "-f" in arguments:
+        if "-d" in arguments:
             index_d = arguments.index("-d")
             index_f = arguments.index("-f")
             directories = arguments[(index_d + 1) : index_f]
-            file_name = arguments[-1]
-        if "-f" in arguments and "-d" not in arguments:
-            file_name = arguments[-1]
 
         directories_path = os.path.join(*directories) if directories else ""
-        file_name = str(file_name)
+        file_name = arguments[-1]
         destination = os.path.join(directories_path, file_name)
         if directories_path != "":
-            if not os.path.exists(directories_path):
-                os.makedirs(directories_path)
+            os.makedirs(directories_path, exist_ok=True)
 
         with open(destination, "a+") as destination_file:
             content = []
             line = ""
             counter = 1
-            while line != "stop":
+            while True:
                 line = input("Enter content line: ")
-                if line != "stop":
-                    if destination_file.tell() == 0:
-                        content.append(f"{counter} Line{counter} " + line)
-                    else:
-                        content.append(
-                            f"{counter} Another line{counter} " + line
-                        )
+                if line == "stop":
+                    break
+                if destination_file.tell() == 0:
+                    content.append(f"{counter} Line{counter} " + line)
+                else:
+                    content.append(f"{counter} Another line{counter} " + line)
                 counter += 1
 
             now = datetime.now()
@@ -46,8 +40,7 @@ def create_file() -> None:
     else:
         directories = arguments[(arguments.index("-d") + 1) :]
         directories_path = os.path.join(*directories)
-        if not os.path.exists(directories_path):
-            os.makedirs(directories_path)
+        os.makedirs(directories_path, exist_ok=True)
 
 
 if __name__ == "__main__":
