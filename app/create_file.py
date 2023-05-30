3ca5af0c3c879233396276553d53d69e74ab6c5c
Munkoor <eee1lvlboss@gmail.com>
2023-05-30 03:43:23 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,96 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+def create_directory(path):
+    try:
+        os.makedirs(path)
+        print(f"Created directory: {path}")
+    except FileExistsError:
+        print(f"Directory already exists: {path}")
+    except OSError as e:
+        print(f"Failed to create directory: {path}")
+        print(f"Error: {str(e)}")
+
+
+def create_file(file_path):
+    try:
+        with open(file_path, 'a') as file:
+            file.write('\n')
+            print(f"Created file: {file_path}")
+    except OSError as e:
+        print(f"Failed to create file: {file_path}")
+        print(f"Error: {str(e)}")
+
+
+def get_timestamp():
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def append_content(file_path, lines):
+    try:
+        with open(file_path, 'a') as file:
+            file.write('\n')
+            file.write(get_timestamp())
+            file.write('\n')
+            for line in lines:
+                file.write(line)
+                file.write('\n')
+            print(f"Added content to file: {file_path}")
+    except OSError as e:
+        print(f"Failed to add content to file: {file_path}")
+        print(f"Error: {str(e)}")
+
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print("Please provide arguments.")
+        return
+
+    if args[0] == '-d':
+        directory_path = os.path.join(*args[1:])
+        create_directory(directory_path)
+    elif args[0] == '-f':
+        file_name = args[1]
+        file_path = os.path.abspath(file_name)
+        if os.path.exists(file_path):
+            print(f"File already exists: {file_path}")
+            lines = []
+            while True:
+                line = input("Enter content line: ")
+                if line.lower() == 'stop':
+                    break
+                lines.append(line)
+            append_content(file_path, lines)
+        else:
+            create_file(file_path)
+            lines = []
+            while True:
+                line = input("Enter content line: ")
+                if line.lower() == 'stop':
+                    break
+                lines.append(line)
+            append_content(file_path, lines)
+    elif args[0] == '-d' and args[2] == '-f':
+        directory_path = os.path.join(*args[1:-2])
+        create_directory(directory_path)
+        file_name = args[-1]
+        file_path = os.path.join(directory_path, file_name)
+        create_file(file_path)
+        lines = []
+        while True:
+            line = input("Enter content line: ")
+            if line.lower() == 'stop':
+                break
+            lines.append(line)
+        append_content(file_path, lines)
+    else:
+        print("Invalid arguments.")
+
+
+if __name__ == '__main__':
+    main()
+
