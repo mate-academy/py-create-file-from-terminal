319128131eded556606161c14c8a149a5956b6b5
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-06-04 13:53:32 +0300

    'Solution5'

--- app/create_file.py
+++ app/create_file.py
@@ -5,14 +5,18 @@ from datetime import datetime
 
 def main() -> None:
     args = sys.argv
-    if "-d" in args:
+    if "-d" in args and "-f" not in args:
         dir_index = args.index("-d")
         directory_path = os.path.join(*args[dir_index + 1:])
-        if "-f" in args:
-            file_index = args.index("-f")
-            file_name = args[file_index + 1]
-            file_path = os.path.join(directory_path, file_name)
-            create_file(file_path)
+        create_directory(directory_path)
+    if "-f" and "-d" in args:
+        file_index = args.index("-f")
+        file_name = args[file_index + 1]
+        dir_index = args.index("-d")
+        directory_path = os.path.join(*args[dir_index + 1:file_index])
+        create_directory(directory_path)
+        file_path = os.path.join(directory_path, file_name)
+        create_file(file_path)
     if "-f" in args:
         file_index = args.index("-f")
         file_name = args[file_index + 1]
@@ -25,10 +29,7 @@ def create_directory(path: str) -> None:
 
 
 def create_file(file_name: str) -> None:
-    if os.path.exists(file_name):
-        mode = "a"
-    else:
-        mode = "w"
+    mode = "a" if os.path.exists(file_name) else "w"
 
     with open(file_name, mode) as file:
         file.write(datetime.now().strftime("&Y-%m-%d, %H:%M:%S\n"))
