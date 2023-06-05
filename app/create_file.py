3cba1fff9d5c796e696db4f73b245e1db40cd4f8
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-06-05 19:36:09 +0300

    input

--- app/create_file.py
+++ app/create_file.py
@@ -6,22 +6,26 @@ import datetime  # .strftime()
 
 print("start")
 ARGS = ""
-
+terminal_input = sys.argv[1::]
 dir_path = ""  # from term -d all items = path
 file_name = ""  # from term -f first item = file name
 file_content = ""  # from term
 flag = ""  # -d -f
 
+print(terminal_input)
 
-def argument_parser():
+
+def argument_parser() -> None:
+    if len(terminal_input) <= 1:
+        print("exit code")
+        return
     print("parsefunc start")
-    parser = argparse.ArgumentParser()
     print("SYS.ARGV: ", sys.argv)
-    print("argparse.ArgumentParser(): ", parser)
+
+    # parser = argparse.ArgumentParser()
+    # print("argparse.ArgumentParser(): ", parser, argparse)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     argument_parser()
 # python create_file.py -d dir1 dir2 #- creates directory dir1/dir2 inside current directory.
