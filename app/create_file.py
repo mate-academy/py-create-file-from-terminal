91c24dd27dbb2fc29d25690e0bac85b534a6340e
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-06-05 18:47:08 +0300

    init

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,12 @@
-# write your code here
+import argparse
+import sys
+
+print("glob")
+def parsefunc():
+    parser = argparse.ArgumentParser()
+    print(sys.argv)
+    print(parser)
+    print("func")
+
+if __name__ == '__main__':
+    parsefunc()
