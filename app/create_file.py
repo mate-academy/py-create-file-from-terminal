d32c0e4cfea75af17ddab21b1ab38a78bf21379c
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-06-05 19:05:56 +0300

    imports

--- app/create_file.py
+++ app/create_file.py
@@ -1,12 +1,25 @@
 import argparse
-import sys
+#
+import sys  # argv
+import os  # makedirs
+import datetime  # .strftime()
+
+ARGS = ""
+
+dir_path = ""  # from term -d all items = path
+file_name = ""  # from term -f first item = file name
+file_content = ""  # from term
+flag = ""  # -d -f
 
 print("glob")
+
+
 def parsefunc():
     parser = argparse.ArgumentParser()
     print(sys.argv)
     print(parser)
     print("func")
+
 
 if __name__ == '__main__':
     parsefunc()
