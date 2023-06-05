f6fed19da9ceecb167022a275ac1916181f6fe2c
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-06-05 20:52:36 +0300

    conditions

--- app/create_file.py
+++ app/create_file.py
@@ -4,23 +4,44 @@ import sys  # argv
 import os  # makedirs
 import datetime  # .strftime()
 
-print("start")
-ARGS = ""
-terminal_input = sys.argv[1::]
-dir_path = ""  # from term -d all items = path
-file_name = ""  # from term -f first item = file name
-file_content = ""  # from term
-flag = ""  # -d -f
+print(f"{'*' * 10} START {'*' * 10}")
+# terminal_input = sys.argv[::]
 
-print(terminal_input)
+
+terminal_input = ['create_file.py', '-d', 'dir1', 'dir2', '-f', 'file.txt']
+
+"""
+1 = create dir/s
+2 = create file
+3 = create dir/s and file
+"""
 
 
 def argument_parser() -> None:
     if len(terminal_input) <= 1:
-        print("exit code")
+        print(f"{'*' * 10} EXIT CODE {'*' * 10}")
         return
-    print("parsefunc start")
-    print("SYS.ARGV: ", sys.argv)
+
+    if "-d" in terminal_input:
+        print("d here")
+        condition = 1
+    if "-f" in terminal_input:
+        print("f here")
+        condition = 2
+    if "-d" in terminal_input and "-f" in terminal_input:
+        print("d and f here")
+        condition = 3
+    print(condition)
+    # next_flow(condition)
+
+    # if "-d" in terminal_input and "-f" in terminal_input:
+    #     start_path_range = terminal_input.index("-d") + 1
+    #     end_path_range = terminal_input.index("-f")
+    #     path = terminal_input[start_path_range:end_path_range]
+    #     print("-d and -f case: ", path)
+    #     create_dirs()
+    #     create_file()
+    #     retur
 
     # parser = argparse.ArgumentParser()
     # print("argparse.ArgumentParser(): ", parser, argparse)
@@ -28,4 +49,10 @@ def argument_parser() -> None:
 
 if __name__ == "__main__":
     argument_parser()
-# python create_file.py -d dir1 dir2 #- creates directory dir1/dir2 inside current directory.
+
+# try:
+#     for folder in terminal_input:
+#         os.mkdir(os.path.join(os.getcwd(), folder))
+# except FileExistsError:
+#     print("Cannot create a file when that file already exists.\n"
+#           "Choose another name.")
