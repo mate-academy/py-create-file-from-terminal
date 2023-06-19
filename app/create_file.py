5773718f9fcdfd28c44fb3adccd158d094221e98
DmytroTsehelnyk <dmytrotsehelnyk@yahoo.com>
2023-06-19 14:18:57 +0300

    Solution fix

--- app/create_file.py
+++ app/create_file.py
@@ -1,8 +1,9 @@
 from os import path, makedirs
 from datetime import datetime
+from sys import argv
 
 
-def create_file(command: str) -> None:
+def create_file(command: list) -> None:
     def create_directory(directories: list) -> None:
         dir_path = path.join(*directories)
         makedirs(dir_path, exist_ok=True)
@@ -20,21 +21,22 @@ def create_file(command: str) -> None:
             line_number += 1
 
     if ("-f" in command) and ("-d" in command):
-        f_index = command.find("-f")
-        file_name = command[f_index + 3:]
-        directory = command[3:f_index]
-        create_directory(directory.split())
-        with open(path.join(*directory.split(), file_name), "w") as file:
+        f_index = command.index("-f")
+        file_name = command[-1]
+        directory = command[1:f_index]
+        create_directory(directory)
+        with open(path.join(*directory, file_name), "w") as file:
             write_into_file()
 
-    elif command.startswith("-f "):
-        file_name = command[3:]
+    elif "-f" in command:
+        file_name = command[-1]
         with open(file_name, "w") as file:
             write_into_file()
 
-    elif command.startswith("-d "):
-        directory = command[3:]
-        create_directory(directory.split())
+    elif "-d" in command:
+        directory = command[1:]
+        create_directory(directory)
+
 
 if __name__ == "__main__":
-    create_file(input("Enter the command: "))
+    create_file(argv[1:])
