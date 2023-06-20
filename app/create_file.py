7e33a75a04be36b5c15ab45068b38ebaac0d47a3
YOUR_NAME <YOUR_EMAIL>
2023-06-20 15:18:46 +0300

    Create file from terminal: update version

--- app/create_file.py
+++ app/create_file.py
@@ -3,32 +3,48 @@ import sys
 import datetime
 
 
-def get_new_path(args: list) -> str:
-    path_list = [name for name in args if name not in ["-d", "-f"]]
-    dir_path = os.path.join(*path_list[:-1])
+def create_directory(directories: list) -> None:
+    dir_path = os.path.join(*directories)
     os.makedirs(dir_path, exist_ok=True)
-    return os.path.join(*path_list)
+
+
+def parse_data(command_data: list) -> tuple:
+    file_name = ""
+    dir_path = []
+
+    for value in command_data:
+        if value.endswith(".txt"):
+            file_name = value
+            continue
+        elif value in ["-f", "-d"]:
+            continue
+        dir_path.append(value)
+
+    return file_name, dir_path
 
 
 def create_file() -> None:
-    args = sys.argv[1:]
-    file_name = args[-1]
+    command_data = sys.argv[1:]
+    file_name, dir_path = parse_data(command_data)
 
-    if "-d" in args:
-        file_name = get_new_path(args)
+    if dir_path:
+        create_directory(dir_path)
 
-    with open(file_name, "a") as file:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        content_to_write = f"{timestamp}\n"
-        line_counter = 1
-        while True:
-            user_data = input("Enter content line (or 'stop' to finish):")
-            if user_data == "stop":
-                content_to_write += "\n"
-                break
-            content_to_write += f"{line_counter} {user_data}\n"
-            line_counter += 1
-        file.write(content_to_write)
+    if file_name:
+        file_name = os.path.join(*dir_path, file_name)
+
+        with open(file_name, "a") as file:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            content_to_write = f"{timestamp}\n"
+            line_counter = 1
+            while True:
+                user_data = input("Enter content line (or 'stop' to finish):")
+                if user_data == "stop":
+                    content_to_write += "\n"
+                    break
+                content_to_write += f"{line_counter} {user_data}\n"
+                line_counter += 1
+            file.write(content_to_write)
 
 
 if __name__ == "__main__":
