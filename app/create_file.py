761d6b7e69d1daf3e2ad2cb0ebc9e6ed6f1d6662
Erik Agayan <erik.agayan1@gmail.com>
2023-06-21 13:53:12 +0300

    new Solution #1

--- app/create_file.py
+++ app/create_file.py
@@ -1,48 +1,58 @@
 import sys
 import os
 from datetime import datetime
+from typing import Tuple, Any, List
 
 
-def create_file(filename: str) -> None:
-    content = []
-
-    if os.path.exists(filename):
-        with open(filename, "a") as file:
-            file.writelines(content)
-
-    line_number = len(content) + 1
-    new_line = input("Enter content line: ")
-    while new_line != "stop":
-        content.append(f"{line_number} {new_line}\n")
-        line_number += 1
-        new_line = input("Enter content line: ")
-
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    content.insert(0, timestamp + "\n")
-
-    # Write content to file
-    with open(filename, "w") as file:
-        file.writelines(content)
+def create_directory(directory: list) -> None:
+    dir_path = os.path.join(*directory)
+    os.makedirs(dir_path, exist_ok=True)
 
 
-def create_directory(directory: str) -> None:
-    os.makedirs(directory)
-    print("Directory created successfully.")
+def parse(commands: list) -> tuple[str | Any, list[Any]]:
+    file_name = ""
+    dir_path = []
+
+    if "-f" in commands:
+        file_name = commands.pop(commands.index("-f") + 1)
+
+    for command in commands:
+        if command in ["-f", "-d"]:
+            continue
+
+        dir_path.append(command)
+
+    return file_name, dir_path
 
 
-def run_app() -> None:
-    if "-d" in sys.argv:
-        directory_index = sys.argv.index("-d") + 1
-        directory = os.path.join(*sys.argv[directory_index:])
-        create_directory(directory)
+def create_file() -> None:
+    commands = sys.argv[1:]
+    filename, dir_path = parse(commands)
 
-    path = ""
-    if "-f" in sys.argv:
-        file_index = sys.argv.index("-f")
-        file_name = sys.argv[file_index + 1]
-        file_path = os.path.join(path, file_name)
-        create_directory(file_path)
+    content = ""
+    counter_of_lines = 1
+
+    if dir_path:
+        create_directory(dir_path)
+
+    if filename:
+        filename = os.path.join(*dir_path, filename)
+        file_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+        content = f"{file_time}\n"
+        counter_of_lines = 1
+
+    while True:
+        user_input = input(f"Enter content: ")
+
+        if user_input == "stop":
+            content += "\n"
+            break
+        content += f"{counter_of_lines} {user_input}\n"
+        counter_of_lines += 1
+
+    with open(filename, "a") as file:
+        file.write(content)
 
 
 if __name__ == "__main__":
-    run_app()
+    create_file()
