bdb4adf44f15f6efd5fa49470461d6e1a33a0b75
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-06-21 16:14:03 +0200

    Update solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,32 +1,44 @@
+import argparse
 import os
-import sys
 from datetime import datetime
 
 
-def create_file() -> None:
-    command = sys.argv
-    if "-f" in command:
-        file_name = command[-1]
-        if "-d" in command:
-            dir_list = command[command.index("-d") + 1 : command.index("-f")]
-            path = os.path.join(*dir_list)
-            os.makedirs(path, exist_ok=True)
-            file_name = f"{path}/{file_name}"
-        with open(file_name, "a") as file:
-            file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-            line = 0
-            while True:
-                content = input("Enter content line:")
-                if content == "stop":
-                    file.write("\n")
-                    break
-                line += 1
-                file.write(f"{line} {content} \n")
+def create_directory(dir_list: list) -> None:
+    path = os.path.join(*dir_list)
+    os.makedirs(path, exist_ok=True)
 
-    elif "-d" in command:
-        dir_list = command[command.index("-d") + 1:]
-        path = os.path.join(*dir_list)
-        os.makedirs(path, exist_ok=True)
+
+def write_content(file_name: str) -> None:
+    lines = []
+    line = 0
+    while True:
+        content = input("Enter content line:")
+        if content == "stop":
+            break
+        line += 1
+        lines.append(f"{line} {content}")
+
+    with open(file_name, "a") as file:
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+        file.write("\n".join(lines))
+        file.write("\n")
+
+
+def create_file() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", nargs="+")
+    parser.add_argument("-f")
+    args = parser.parse_args()
+
+    if args.d:
+        dir_list = args.d
+        create_directory(dir_list)
+
+    if args.f:
+        file_name = args.f
+        if args.d:
+            file_name = os.path.join(*dir_list, file_name)
+        write_content(file_name)
 
 
 if __name__ == "__main__":
