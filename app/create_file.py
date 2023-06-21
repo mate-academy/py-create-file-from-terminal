9a05a846837e697e0171562eb2beeb63cec046b2
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-06-21 21:57:52 +0300

    major fix

--- app/create_file.py
+++ app/create_file.py
@@ -1,59 +1,49 @@
 import datetime
-import sys
-
-from os import path, makedirs
+import os
+import argparse
 
 
 def work_with_file(file_path: str) -> None:
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    content = f"{timestamp}\n"
+
+    print("Enter content for new_file (Enter 'stop' to finish):")
+    line_number = 1
+    while True:
+        line = input("Enter content line: ")
+        if line.lower() == "stop":
+            break
+        content += f"{line_number} {line}\n"
+        line_number += 1
+
     with open(file_path, "a") as file:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(timestamp + "\n")
-
-        print("Enter content for new_file (Enter 'stop' to finish):")
-        line_number = 1
-        while True:
-            line = input("Enter content line: ")
-            if line.lower() == "stop":
-                break
-            file.write(f"{line_number} {line}\n")
-            line_number += 1
-        file.write("\n")
+        file.write(content + "\n")
 
 
-def main() -> None:
-    terminal_input = sys.argv
+def create_dirs(directories: list) -> str:
+    directory = os.path.join(*directories)
+    os.makedirs(directory, exist_ok=True)
+    return directory
 
-    if len(terminal_input) < 2:
-        print("Need more arguments!")
-        return
 
-    filename = ""
-    directory = ""
-    try:
-        flags = terminal_input[1:]
+def parse_commands() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directories",
+                        nargs="+",
+                        help="Enter directories for the file")
+    parser.add_argument("-f", "--filename", help="Enter the name of the file")
+    args = parser.parse_args()
 
-        if "-d" in flags:
-            directories = []
-            for argument in range(flags.index("-d") + 1, len(flags)):
-                if flags[argument] == "-f":
-                    break
-                directories.append(flags[argument])
-            directory = path.join(*directories)
-            makedirs(directory, exist_ok=True)
-        if "-f" in flags:
-            filename = flags[flags.index("-f") + 1]
-        file_path = path.join(directory, filename)
-        if not filename:
-            print("Please provide the -f flag for the filename.")
-            return
+    filename = args.filename
+    directories = args.directories if args.directories else []
+    directory = create_dirs(directories) if directories else os.getcwd()
 
+    if filename:
+        file_path = os.path.join(directory, filename)
         work_with_file(file_path)
-
-    except (IndexError, TypeError, ValueError, FileNotFoundError) as e:
-        print(f"{type(e).__name__}: {e}")
-    except Exception as e:
-        print(f"{type(e).__name__}: {e}")
+    else:
+        print("Please provide the --filename argument to create a file.")
 
 
 if __name__ == "__main__":
-    main()
+    parse_commands()
