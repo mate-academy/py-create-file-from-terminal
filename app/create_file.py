2a7a94bd8558bdf7d9603945ee21b6027f366ea0
yakhlamova <yanaakhlamova@gmail.com>
2023-06-21 14:34:58 +0300

    'Update'

--- app/create_file.py
+++ app/create_file.py
@@ -1,14 +1,11 @@
-import sys
 import os
+import argparse
 
 from datetime import datetime
 
 
 def create_directory(directory: str) -> None:
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    else:
-        print("Directory already exists.")
+    os.makedirs(directory, exist_ok=True)
 
 
 def create_file(file_creation: str) -> None:
@@ -23,30 +20,22 @@ def create_file(file_creation: str) -> None:
 
     with open(file_creation, "a") as new_file:
         new_file.write(timestamp)
-        for i, line in enumerate(file_content):
-            new_file.write(f"{i + 1} {line}\n")
+        for i, line in enumerate(file_content, start=1):
+            new_file.write(f"{i} {line}\n")
 
 
 def main() -> None:
-    line = sys.argv
-    if "-d" in line and "-f" in line:
-        dir_index = line.index("-d") + 1
-        file_index = line.index("-f") + 1
-        directory_path = os.path.join(*line[dir_index:file_index - 1])
-        file_name = line[file_index]
-        file_path = os.path.join(directory_path, file_name)
-        create_directory(directory_path)
-        create_file(file_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", nargs="+", help="directory path")
+    parser.add_argument("-f", help="file name")
+    args = parser.parse_args()
 
-    elif "-d" in line:
-        directory_index = line.index("-d") + 1
-        directory_path = os.path.join(*line[directory_index:])
+    if args.d:
+        directory_path = os.path.join(*args.d)
         create_directory(directory_path)
 
-    elif "-f" in line:
-        file_index = line.index("-f") + 1
-        file_path = line[file_index]
-        create_file(file_path)
+    if args.f:
+        create_file(args.f)
 
 
 if __name__ == "__main__":
