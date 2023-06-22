122acb9ef8acdce2dba375962e0be1b6b53a6427
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-22 10:50:07 +0300

    First Argparse Improvement

--- app/create_file.py
+++ app/create_file.py
@@ -1,36 +1,37 @@
 import sys
 import os
 from datetime import datetime
+from argparse import ArgumentParser
 
 
 def create_file(path: str) -> None:
-    path = os.path.join(path, sys.argv[sys.argv.index("-f") + 1])
+    path = os.path.join(path, args.f)
 
-    mode = "w"
-    if os.path.exists(path):
-        mode = "a"
-
-    with open(path, mode) as file:
-        if mode == "a":
+    with open(path, "a") as file:
+        if os.path.exists(path):
             file.write("\n")
         file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
         while True:
             input_line = input("Enter content line: ")
             if input_line == "stop":
                 break
-            else:
-                file.write(f"{input_line}\n")
+            file.write(f"{input_line}\n")
 
+
+parser = ArgumentParser()
+
+parser.add_argument("-d", nargs="*")
+parser.add_argument("-f")
+
+args = parser.parse_args()
 
 if "-d" in sys.argv:
     if "-f" in sys.argv:
-        path = os.path.join(*sys.argv[sys.argv.index("-d")
-                                      + 1:sys.argv.index("-f")])
-        if not os.path.exists(path):
-            os.makedirs(path)
+        path = os.path.join(*args.d)
+        os.makedirs(path, exist_ok=True)
         create_file(path)
     else:
-        path = os.path.join(*sys.argv[sys.argv.index("-d") + 1:])
+        path = os.path.join(*args.d)
         os.makedirs(path)
 elif "-f" in sys.argv:
     create_file("")
