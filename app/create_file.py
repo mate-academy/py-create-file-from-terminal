373e49b7095eab62418c4a648b2f2d30f7b06976
Rostyslav <rostyslav.furman@gmail.com>
2023-06-23 13:45:17 +0300

    my_commit2

--- app/create_file.py
+++ app/create_file.py
@@ -4,11 +4,8 @@ from datetime import datetime
 
 
 def create_directory(directory_path: str) -> None:
-    if os.path.exists(directory_path):
-        print(f"Directory '{directory_path}' already exists.")
-    else:
-        os.makedirs(directory_path)
-        print(f"Directory '{directory_path}' created successfully.")
+    dir_path = os.path.join(*directory_path)
+    os.makedirs(dir_path, exist_ok=True)
 
 
 def create_file(file_path: str) -> None:
@@ -20,25 +17,26 @@ def create_file(file_path: str) -> None:
             break
         content_lines.append(line)
 
-    if not content_lines:
+    if content_lines:
+        with open(file_path, "w") as file:
+            file.write("\n".join(content_lines))
+        print(f"File '{file_path}' created successfully.")
+    else:
         print("No content provided. File creation aborted.")
-        return
 
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     content = f"{timestamp}\n"
     content += "\n".join(f"{i+1}"
                          f"{line}" for i, line in enumerate(content_lines))
 
-    mode = "a" if os.path.isfile(file_path) else "w"
-    with open(file_path, mode) as file:
+    with open(file_path, "a") as file:
         file.write(content)
     print(f"File '{file_path}' created successfully.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Create a directory "
-                    "and file with specified content.")
+        description="Create a directory and file with specified content.")
 
     parser.add_argument("-d", "--directory", nargs="+", help="Directory path")
     parser.add_argument("-f", "--filename", type=str, help="File name")
@@ -50,7 +48,7 @@ if __name__ == "__main__":
         create_directory(directory_path)
 
     if args.filename:
-        directory_path = os.getcwd() if not args.directory \
-            else os.path.join(*args.directory)
+        directory_path = (os.getcwd() if not args.directory
+                          else os.path.join(*args.directory))
         file_path = os.path.join(directory_path, args.filename)
         create_file(file_path)
