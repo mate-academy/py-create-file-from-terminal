c0544d184d67dff59a9a114941cbb8da960ad93b
Jorjio <Jorjio22@gmail.com>
2023-06-23 16:15:29 +0000

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,50 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+def command_definition(commands: list[str]) -> None:
+    if "-f" in commands and "-d" in commands:
+        command_d(commands)
+        command_f(commands)
+    elif "-f" in commands:
+        command_f(commands)
+    elif "-d" in commands:
+        command_d(commands)
+    else:
+        raise ValueError("No valid flag (-f or -d) provided in the command.")
+
+
+def command_d(commands: list[str]) -> None:
+
+    parent_dir = os.getcwd()
+
+    for directory in commands[2:]:
+        if directory != "-f":
+            new_dir = os.path.join(parent_dir, directory)
+            os.makedirs(new_dir, exist_ok=True)
+            parent_dir = new_dir
+        else:
+            break
+
+
+def command_f(commands: list[str]) -> None:
+
+    path = os.path.join("/".join(commands[2:-2]))
+    file_path = os.path.join(path, commands[-1])
+
+    with open(file_path, "a") as file:
+
+        if os.stat(file_path).st_size != 0:
+            file.write("\n")
+
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S" + "\n"))
+        input_line = input("Enter content line: ")
+
+        while input_line != "stop":
+            file.write(input_line + "\n")
+            input_line = input("Enter content line: ")
+
+
+if __name__ == "__main__":
+    command_definition(sys.argv)
