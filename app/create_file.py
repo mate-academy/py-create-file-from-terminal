9fa6fc8f27026726c03d11d807182b5bae54ebe4
Vlad Larin <lordgadyka71@gmail.com>
2023-06-23 21:30:59 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+terminal_command = sys.argv
+
+directory = ""
+if "-d" in terminal_command:
+    for argument in terminal_command[2:]:
+        if argument == "-f":
+            break
+        directory = os.path.join(directory, argument)
+    os.makedirs(directory, exist_ok=True)
+
+if "-f" in terminal_command:
+    with open(os.path.join(directory, terminal_command[-1]), "a+") as new_file:
+        new_file.seek(0)
+        if new_file.readline():
+            new_file.write("\n")
+        new_file.write(datetime.now().strftime("%Y-%d-%m %H:%M:%S\n"))
+        line_counter = 1
+        while True:
+            new_line = input("Enter content line: ")
+            if new_line == "stop":
+                break
+            new_file.write(f"{line_counter} {new_line}\n")
+            line_counter += 1
