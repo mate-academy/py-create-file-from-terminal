ca5f9ac5e651b864b725871aba8978e74d0b7e26
Vlad Larin <lordgadyka71@gmail.com>
2023-06-24 16:34:36 +0300

    solution5

--- app/create_file.py
+++ app/create_file.py
@@ -5,23 +5,18 @@ from datetime import datetime
 
 def create_file() -> None:
     terminal_command = sys.argv
+    file_name = terminal_command[terminal_command.index("-f") + 1]
+    directory_name = ""
 
-    directory = ""
     if "-d" in terminal_command:
         for argument in terminal_command[terminal_command.index("-d") + 1:]:
             if argument == "-f":
                 break
-            directory = os.path.join(directory, argument)
-        os.makedirs(directory, exist_ok=True)
+            directory_name = os.path.join(directory_name, argument)
+        os.makedirs(directory_name, exist_ok=True)
 
     if "-f" in terminal_command:
-        with open(
-                os.path.join(
-                    directory,
-                    terminal_command[terminal_command.index("-f") + 1]
-                ),
-                "a+"
-        ) as new_file:
+        with open(os.path.join(directory_name, file_name), "a+") as new_file:
             new_file.seek(0)
             if new_file.readline():
                 new_file.write("\n")
@@ -35,4 +30,5 @@ def create_file() -> None:
                 line_counter += 1
 
 
-create_file()
+if __name__ == "__main__":
+    create_file()
