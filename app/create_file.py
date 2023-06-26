781671656fa4e2caa3d9a2ec4e3547ec03271d43
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-26 18:00:56 +0300

    Correct all of issues.

--- app/create_file.py
+++ app/create_file.py
@@ -3,30 +3,33 @@ from sys import argv
 from datetime import datetime
 
 
-def recieve_command_form_console_and_make_a_dict_of_flags() -> dict:
+def get_flags_dict() -> dict:
     dict_of_flags = {"-d": None, "-f": None}
-    splited_command = argv
 
     try:
-        indedx_d = splited_command.index("-d")
+        index_d = argv.index("-d")
     except ValueError:
-        indedx_d = False
+        index_d = False
 
     try:
-        indedx_f = splited_command.index("-f")
+        index_f = argv.index("-f")
     except ValueError:
-        indedx_f = False
+        index_f = False
 
-    if indedx_f and indedx_d:
-        arguments_d = splited_command[indedx_d + 1:indedx_f]
-        arguments_f = splited_command[indedx_f + 1:]
+    if index_f and index_d:
+        if index_f > index_d:
+            arguments_d = argv[index_d + 1:index_f]
+            arguments_f = argv[index_f + 1:]
+        else:
+            arguments_d = argv[index_d + 1:]
+            arguments_f = argv[index_f + 1: index_d]
         dict_of_flags["-d"] = arguments_d
         dict_of_flags["-f"] = arguments_f
-    elif indedx_d:
-        arguments_d = splited_command[indedx_d + 1:]
+    elif index_d:
+        arguments_d = argv[index_d + 1:]
         dict_of_flags["-d"] = arguments_d
-    elif indedx_f:
-        arguments_f = splited_command[indedx_f + 1:]
+    elif index_f:
+        arguments_f = argv[index_f + 1:]
         dict_of_flags["-f"] = arguments_f
 
     return dict_of_flags
@@ -34,14 +37,13 @@ def recieve_command_form_console_and_make_a_dict_of_flags() -> dict:
 
 def create_files_and_folders() -> None:
     if dict_with_flags["-d"]:
-        folders_to_create = "/".join(dict_with_flags["-d"])
+        folders_to_create = os.path.join(*dict_with_flags["-d"])
         os.makedirs(folders_to_create)
     if dict_with_flags["-f"]:
-        way_to_file = f"./{''.join(dict_with_flags['-f'])}"
+        path_to_file = os.path.join(*dict_with_flags['-f'])
         if dict_with_flags["-d"]:
-            way_to_file = (f"./{folders_to_create}/"
-                           f"{''.join(dict_with_flags['-f'])}")
-        with open(way_to_file, "w") as file:
+            path_to_file = os.path.join(folders_to_create, *dict_with_flags['-f'])
+        with open(path_to_file, "w") as file:
             date_time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             file.write(f"{date_time_now}\n")
             counter = 0
@@ -54,5 +56,5 @@ def create_files_and_folders() -> None:
 
 
 if __name__ == "__main__":
-    dict_with_flags = recieve_command_form_console_and_make_a_dict_of_flags()
+    dict_with_flags = get_flags_dict()
     create_files_and_folders()
