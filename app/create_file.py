fa71d6fa49aec0c26617ae2b83ac92bddcf72f0b
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-26 16:28:30 +0300

    add exist_ok and simplify blank line

--- app/create_file.py
+++ app/create_file.py
@@ -4,16 +4,12 @@ from argparse import ArgumentParser
 
 
 def create_file(path: str) -> None:
-    blank_line = ""
-    if os.path.exists(path):
-        blank_line = "\n"
     with open(path, "a") as file:
-        if os.path.exists(path):
-            file.write(blank_line)
         file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
         while True:
             input_line = input("Enter content line: ")
             if input_line == "stop":
+                file.write("\n")
                 break
             file.write(f"{input_line}\n")
 
@@ -29,8 +25,7 @@ args = parser.parse_args()
 path = ""
 if args.d is not None:
     path = os.path.join(*args.d)
-    if not os.path.exists(path):
-        os.makedirs(path)
+    os.makedirs(path, exist_ok=True)
 if args.f is not None:
     path = os.path.join(path, args.f)
     create_file(path)
