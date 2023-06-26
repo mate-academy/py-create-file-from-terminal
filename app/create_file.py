e43e2d34fbd3de6b6372ed05c0fa6a5c50d01d30
Vlad <vladislav.zhyla@gmail.com>
2023-06-26 18:18:22 +0300

    Solution was optimised

--- app/create_file.py
+++ app/create_file.py
@@ -8,26 +8,32 @@ def create_file(directory: str, filename: str) -> None:
         if not os.path.exists(directory):
             os.makedirs(directory)
         path = os.path.join(directory, filename)
+    else:
+        path = filename
+
     counter = 1
-    if filename:
-        with open(f"{path}.txt", "a") as new_file:
-            new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+    with open(f"{path}.txt", "a") as new_file:
+        new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        new_line = None
+        while new_line != "stop":
             new_line = input("Enter content line: ")
-            while new_line != "stop":
+            if new_line != "stop":
                 new_file.write(f"{counter} {new_line}\n")
                 counter += 1
-                new_line = input("Enter content line: ")
 
 
 def main() -> None:
     directory = ""
     filename = ""
-    if "-d" in sys.argv:
-        directory = "/".join(sys.argv[sys.argv.index("-d") + 1:])
-    if "-f" in sys.argv:
-        filename = sys.argv[sys.argv.index("-f") + 1:]
-    if directory and filename:
-        directory = ("/".join(sys.argv[sys.argv.index("-d")
-                                       + 1:sys.argv.index("-f") - 1]))
-        create_file(directory, filename)
+
+    if len(sys.argv) > 1:
+        if "-d" in sys.argv:
+            directory = sys.argv[sys.argv.index("-d") + 1]
+        if "-f" in sys.argv:
+            filename = sys.argv[sys.argv.index("-f") + 1]
+
     create_file(directory, filename)
+
+
+if __name__ == "__main__":
+    main()
