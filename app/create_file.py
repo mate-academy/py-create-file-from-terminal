9531e94637eaa7050df1276b543716e3bdb93c9d
Rostyslav <rostyslav.furman@gmail.com>
2023-06-26 14:31:59 +0300

    my_commit3

--- app/create_file.py
+++ app/create_file.py
@@ -17,38 +17,43 @@ def create_file(file_path: str) -> None:
             break
         content_lines.append(line)
 
-    if content_lines:
-        with open(file_path, "w") as file:
-            file.write("\n".join(content_lines))
-        print(f"File '{file_path}' created successfully.")
-    else:
+    if not content_lines:
         print("No content provided. File creation aborted.")
 
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    content = f"{timestamp}\n"
-    content += "\n".join(f"{i+1}"
-                         f"{line}" for i, line in enumerate(content_lines))
-
     with open(file_path, "a") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        content = f"{timestamp}\n"
+        content += "\n".join(f"{i+1}. "
+                             f"{line}" for i, line in enumerate(content_lines))
         file.write(content)
     print(f"File '{file_path}' created successfully.")
 
 
-if __name__ == "__main__":
+def parse_arguments() -> argparse:
     parser = argparse.ArgumentParser(
         description="Create a directory and file with specified content.")
 
     parser.add_argument("-d", "--directory", nargs="+", help="Directory path")
     parser.add_argument("-f", "--filename", type=str, help="File name")
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_arguments()
 
     if args.directory:
         directory_path = os.path.join(*args.directory)
         create_directory(directory_path)
 
     if args.filename:
-        directory_path = (os.getcwd() if not args.directory
-                          else os.path.join(*args.directory))
+        directory_path = (
+            os.getcwd() if not args.directory
+            else os.path.join(*args.directory)
+        )
         file_path = os.path.join(directory_path, args.filename)
         create_file(file_path)
+
+
+if __name__ == "__main__":
+    main()
