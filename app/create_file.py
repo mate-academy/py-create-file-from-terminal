9386bfbe52ff50bd30dc34d885c9d3b9e78932cb
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-06-28 15:57:50 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,46 @@
-# write your code here
+import datetime
+import os
+import sys
+
+
+arguments = sys.argv
+
+if arguments:
+
+    path = []
+    file_name = ""
+
+    for argument in arguments[1:]:
+        if argument == "-f":
+            file_name = arguments[-1]
+            break
+
+        if argument == "-d":
+            continue
+        path.append(argument)
+
+    if path:
+        str_path = os.path.join(*path)
+        if not os.path.exists(str_path):
+            os.makedirs(str_path)
+
+    def write_in_file(file_path: str, line: str, number: str) -> None:
+        with open(file_path, "a") as file:
+            file.write(f"{number}{line}\n")
+
+    if file_name:
+        path_to_file = os.path.join(*path, file_name)
+
+        timestamp = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+        write_in_file(path_to_file, timestamp, "")
+
+        lines_number = 1
+        while True:
+            content = input("Enter content line: ")
+
+            if content == "stop":
+                break
+            write_in_file(path_to_file, content, f"{lines_number} ")
+            lines_number += 1
+
+        write_in_file(path_to_file, "", "")
