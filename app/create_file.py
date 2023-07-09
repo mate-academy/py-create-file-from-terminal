1bc0d76090a3059e2ed3d47adbf86f57fd83e7e0
Bohdan Liutko <luitko007@gmail.com>
2023-07-09 16:17:08 +0000

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,23 +3,44 @@ import sys
 from datetime import datetime
 
 
-def file_input() -> None:
-    time_now = datetime.now()
-    if "-d" in sys.argv:
-        directory_index = sys.argv.index("-d") + 1
-        directory_path = os.path.join(*sys.argv[directory_index:])
-        os.makedirs(directory_path, exist_ok=True)
+def create_folders(folders_names: list) -> str:
+    parent_path = folders_names[0]
+    os.makedirs(parent_path, exist_ok=True)
+    for directory in folders_names[1:]:
+        subdir_path = os.path.join(parent_path, directory)
+        os.makedirs(subdir_path, exist_ok=True)
+        parent_path = subdir_path
+    return parent_path
 
-    if "-f" in sys.argv:
-        file_index = sys.argv.index("-f") + 1
-        file_name = sys.argv[file_index]
-        with open(file_name, "a") as file:
-            file.write(f"{time_now.strftime('%m/%d/%Y, %H:%M:%S')}\n")
-            line = 1
-            while True:
-                content_input = input("Enter content line: ")
-                if content_input != "stop":
-                    file.write(f"{line} {content_input}\n")
-                    line += 1
-                else:
-                    break
+
+def fill_in_file(file_path: str) -> None:
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data = f"{timestamp}\n"
+    count = 1
+    while True:
+        user_input = input("Enter content line:")
+        if user_input == "stop":
+            break
+        data += f"{count} {user_input}\n"
+        count += 1
+    with open(file_path, "a") as output:
+        output.write(data)
+
+
+def create_file() -> None:
+    if ("-d", "-f") in sys.argv:
+        dir_path = sys.argv[sys.argv.index("-d") + 1:sys.argv.index("-f")]
+        file_name = sys.argv[-1]
+        des_path = create_folders(dir_path)
+        file_path = os.path.join(des_path, file_name)
+        fill_in_file(file_path)
+    elif "-d" in sys.argv:
+        dir_path = sys.argv[sys.argv.index("-d") + 1:]
+        create_folders(dir_path)
+    elif "-f" in sys.argv:
+        file_name = sys.argv[-1]
+        fill_in_file(file_name)
+
+
+if __name__ == "__main__":
+    create_file()
