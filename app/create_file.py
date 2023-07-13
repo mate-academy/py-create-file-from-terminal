bcdd523690f315fddc2567586bfd99841af73f5b
Michael <manchester6913@gmail.com>
2023-07-13 07:59:09 -0400

    New Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,46 +1,51 @@
-import sys
 import os
-import datetime
+import sys
+from datetime import datetime
 
 
-def create_folders(folders_names: list) -> str:
-    parent_path = folders_names[0]
-    os.makedirs(parent_path, exist_ok=True)
-    for directory in folders_names[1:]:
-        subdir_path = os.path.join(parent_path, directory)
-        os.makedirs(subdir_path, exist_ok=True)
-        parent_path = subdir_path
-    return parent_path
+def command_definition(commands: list[str]) -> None:
+    if "-f" in commands and "-d" in commands:
+        command_d(commands)
+        command_f(commands)
+    elif "-f" in commands:
+        command_f(commands)
+    elif "-d" in commands:
+        command_d(commands)
+    else:
+        raise ValueError("No -f or -d provided.")
 
 
-def fill_in_file(file_path: str) -> None:
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    data = f"{timestamp}\n"
-    count = 1
-    while True:
-        user_input = input("Enter content line:")
-        if user_input == "stop":
+def command_d(commands: list[str]) -> None:
+    parent_dir = os.getcwd()
+    for directory in commands[2:]:
+        if directory != "-f":
+            new_dir = os.path.join(parent_dir, directory)
+            os.makedirs(new_dir, exist_ok=True)
+            parent_dir = new_dir
+        else:
             break
-        data += f"{count} {user_input}\n"
-        count += 1
-    with open(file_path, "a") as output:
-        output.write(data)
 
 
-def create_file() -> None:
-    if ("-d", "-f") in sys.argv:
-        dir_path = sys.argv[sys.argv.index("-d") + 1:sys.argv.index("-f")]
-        file_name = sys.argv[-1]
-        des_path = create_folders(dir_path)
-        file_path = os.path.join(des_path, file_name)
-        fill_in_file(file_path)
-    elif "-d" in sys.argv:
-        dir_path = sys.argv[sys.argv.index("-d") + 1:]
-        create_folders(dir_path)
-    elif "-f" in sys.argv:
-        file_name = sys.argv[-1]
-        fill_in_file(file_name)
+def command_f(commands: list[str]) -> None:
+
+    path = os.path.join("/".join(commands[2:-2]))
+    file_path = os.path.join(path, commands[-1])
+
+    with open(file_path, "a") as file:
+
+        if os.stat(file_path).st_size != 0:
+            file.write("\n")
+
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S" + "\n"))
+        input_line = input("Enter content line: ")
+        line_number = 1
+
+        while input_line != "stop":
+            output = f"{line_number} {input_line}"
+            file.write(output + "\n")
+            input_line = input("Enter content line: ")
+            line_number += 1
 
 
 if __name__ == "__main__":
-    create_file()
+    command_definition(sys.argv)
