073a5b85834d04eb643b9754f026aecf53bbb32c
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-26 13:04:09 +0300

    'functions'

--- app/create_file.py
+++ app/create_file.py
@@ -12,45 +12,57 @@ def get_directory_path(command: list[str]) -> str:
     )
 
 
+def get_current_date(command: list[str]) -> str:
+    return (
+        ("\n\n" + str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        if os.path.isfile(
+            os.path.join(
+                os.getcwd(),
+                command[command.index("-f") + 1]
+            )
+        )
+        else str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    )
+
+
+def create_directory(command: list[str]) -> None:
+    required_directory = get_directory_path(command)
+
+    if not os.path.isdir(required_directory):
+        os.makedirs(required_directory)
+
+    os.chdir(required_directory)
+
+
+def create_file_with_content(command: list[str]) -> None:
+    current_date = get_current_date(command)
+
+    with open(
+            command[command.index("-f") + 1],
+            "a"
+    ) as created_modified_file:
+        created_modified_file.write(current_date)
+
+        line_to_write = input("Enter content line: ")
+        line_number = 1
+        while line_to_write != "stop":
+            created_modified_file.write(
+                "\n" + str(line_number) + " " + line_to_write
+            )
+            line_to_write = input("Enter content line: ")
+            line_number += 1
+
+
 def create_file(command: list[str]) -> None:
     start_dir = os.getcwd()
 
     if "-d" in command:
-        required_directory = get_directory_path(command)
-
-        if not os.path.isdir(required_directory):
-            os.makedirs(required_directory)
-
-        os.chdir(required_directory)
+        create_directory(command)
 
     if "-f" in command:
-        current_date = (
-            ("\n\n" + str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-            if os.path.isfile(
-                os.path.join(
-                    os.getcwd(),
-                    command[command.index("-f") + 1]
-                )
-            )
-            else str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        )
+        create_file_with_content(command)
 
-        with open(
-                command[command.index("-f") + 1],
-                "a"
-        ) as created_modified_file:
-            created_modified_file.write(current_date)
-
-            line_to_write = input("Enter content line: ")
-            line_number = 1
-            while line_to_write != "stop":
-                created_modified_file.write(
-                    "\n" + str(line_number) + " " + line_to_write
-                )
-                line_to_write = input("Enter content line: ")
-                line_number += 1
-
-        os.chdir(start_dir)
+    os.chdir(start_dir)
 
 
 create_file(sys.argv)
