601a1137ae006ba4b113d71a07971fdcaec975b4
Illya Zhmurskiy <thebrawler6@gmail.com>
2023-07-26 21:49:38 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,25 +3,37 @@ import sys
 from datetime import datetime
 
 
-def get_timestamp() -> datetime:
+def get_timestamp() -> str:
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-def create_file(file_path: str, content_lines: int) -> None:
+def create_file(file_path: str, content_lines: list) -> None:
     with open(file_path, "a") as file:
         file.write("\n".join(content_lines))
 
 
 def main() -> None:
-    if "-d" in sys.argv:
+    dir_flag = "-d" in sys.argv
+    file_flag = "-f" in sys.argv
+
+    if not (dir_flag or file_flag):
+        print("Please provide either the -d flag or the -f flag.")
+        return
+
+    if dir_flag:
         dir_index = sys.argv.index("-d") + 1
         directory_path = os.path.join(*sys.argv[dir_index:])
         os.makedirs(directory_path, exist_ok=True)
         os.chdir(directory_path)
 
-    if "-f" in sys.argv:
+    if file_flag:
         file_index = sys.argv.index("-f") + 1
-        file_name = sys.argv[file_index]
+
+        if dir_flag and file_index < dir_index:
+            file_name = os.path.join(*sys.argv[dir_index:])
+        else:
+            file_name = sys.argv[file_index]
+
         file_path = os.path.join(os.getcwd(), file_name)
 
         if os.path.exists(file_path):
