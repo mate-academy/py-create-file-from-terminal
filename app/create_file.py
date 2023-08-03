5ad537c1753835f85a081fc5ea66af111e7b9f15
Tanya Burdenko <burdenko.tetyana@gmail.com>
2023-08-03 12:58:29 +0300

    V2

--- app/create_file.py
+++ app/create_file.py
@@ -22,15 +22,24 @@ def create_only_file(file_to_create: str) -> None:
             page += 1
 
 
+def create_directories_and_file(
+        directories_to_create: list,
+        file_to_create: str
+) -> None:
+    create_only_directories(os.path.join(*directories_to_create))
+    file_path = os.path.join(*directories, file_to_create)
+    create_only_file(file_path)
+
+
 if __name__ == "__main__":
     if sys.argv[1] == "-d" and sys.argv[-2] == "-f":
         directories = sys.argv[2:-2]
         file_name = sys.argv[-1]
-
-        create_only_directories(os.path.join(*directories))
-
-        file_path = os.path.join(*directories, file_name)
-        create_only_file(file_path)
+        create_directories_and_file(directories, file_name)
+    elif sys.argv[1] == "-f" and sys.argv[3] == "-d":
+        directories = sys.argv[4:]
+        file_name = sys.argv[2]
+        create_directories_and_file(directories, file_name)
     elif sys.argv[1] == "-d":
         directories = sys.argv[2:]
         create_only_directories(os.path.join(*directories))
