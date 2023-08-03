24c2291d844eb72a3e4bbe9057e6b2468e8faebe
Tanya Burdenko <burdenko.tetyana@gmail.com>
2023-08-03 16:10:56 +0300

    V4

--- app/create_file.py
+++ app/create_file.py
@@ -22,30 +22,27 @@ def create_only_file(file_to_create: str) -> None:
             page += 1
 
 
-def create_directories_and_file(
-        directories_to_create: list,
-        file_to_create: str
-) -> None:
+def create_directories_and_file() -> None:
+    file_index = sys.argv.index("-f") + 1
+    directories_index = sys.argv.index("-d") + 1
+    file_name = sys.argv[file_index]
+
+    if file_index < directories_index:
+        directories_to_create = (sys.argv[directories_index:
+                                 max(file_index - 1, len(sys.argv))])
+    else:
+        directories_to_create = sys.argv[directories_index: file_index - 1]
+
     create_only_directories(os.path.join(*directories_to_create))
-    file_path = os.path.join(*directories, file_to_create)
+    file_path = os.path.join(*directories_to_create, file_name)
     create_only_file(file_path)
 
 
 if __name__ == "__main__":
     if "-d" in sys.argv and "-f" in sys.argv:
-        file_index = sys.argv.index("-f") + 1
-        directories_index = sys.argv.index("-d") + 1
-        file_name = sys.argv[file_index]
-
-        if file_index < directories_index:
-            directories = (sys.argv[directories_index:
-                           max(file_index - 1, len(sys.argv))])
-        else:
-            directories = sys.argv[directories_index: file_index - 1]
-        create_directories_and_file(directories, file_name)
-
+        create_directories_and_file()
     elif "-d" in sys.argv and "-f" not in sys.argv:
-        directories = sys.argv[2:]
+        directories = sys.argv[sys.argv.index("-d") + 1:]
         create_only_directories(os.path.join(*directories))
     else:
-        create_only_file(sys.argv[2])
+        create_only_file(sys.argv[sys.argv.index("-f") + 1])
