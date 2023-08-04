76aa85676f68ed919c9f73fd7bb01f17ee290fda
Tatasha <natashaleonidovna319@gmail.com>
2023-08-05 00:57:18 +0300

    'Solution4'

--- app/create_file.py
+++ app/create_file.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 
-def make_path(cmd_arguments: list) -> tuple:
+def make_path(cmd_arguments: list[str]) -> tuple[str]:
     path, filename = " ", " "
     cmd_arguments.pop(0)  # delete executed filename from arguments
     if "-f" in cmd_arguments:
@@ -13,22 +13,29 @@ def make_path(cmd_arguments: list) -> tuple:
     if "-d" in cmd_arguments:
         cmd_arguments.pop(cmd_arguments.index("-d"))
         path = os.path.join(*cmd_arguments)
-    filepath = os.path.join(path, filename)
-    return path, filename, filepath
+        if path == " ":
+            filepath = filename
+        else:
+            filepath = os.path.join(path, filename)
+        return path, filename, filepath
 
 
 def create_file() -> None:
     path, filename, filepath = make_path(sys.argv)
-    os.makedirs(path, exist_ok=True)
-    with open(filepath, "w") as source_file:
-        current_time = datetime.now()
-        source_file.write(str(current_time.strftime("%Y-%m-%d %H:%M:%S")))
+    if filename == " ":
+        os.makedirs(path, exist_ok=True)
+    else:
+        if path != " ":
+            os.makedirs(path, exist_ok=True)
+        with open(filepath, "w") as source_file:
+            current_time = datetime.now()
+            source_file.write(str(current_time.strftime("%Y-%m-%d %H:%M:%S")))
 
-    while True:
-        user_cmd = input("Enter content line:")
-        if user_cmd == "stop":
-            break
-        source_file.write("\n" + user_cmd)
+            while True:
+                user_cmd = input("Enter content line:")
+                if user_cmd == "stop":
+                    break
+                source_file.write("\n" + user_cmd)
 
 
 if __name__ == "__main__":
