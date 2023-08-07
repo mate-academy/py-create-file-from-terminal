eb9322adee79c8046a64977729ade66323680e35
Tatasha <natashaleonidovna319@gmail.com>
2023-08-08 00:10:35 +0300

    'Solution5'

--- app/create_file.py
+++ app/create_file.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
+
 import sys
 import os
 from datetime import datetime
+from typing import Union
 
 
-def make_path(cmd_arguments: list[str]) -> tuple[str]:
-    path, filename = " ", " "
+def make_path(cmd_arguments: list[Union[bytes, str]]) -> tuple[Union[bytes, str], Union[bytes, str], Union[bytes, str]]:
+    path, filename, filepath = " ", " ", " "
     cmd_arguments.pop(0)  # delete executed filename from arguments
     if "-f" in cmd_arguments:
         filename = cmd_arguments[cmd_arguments.index("-f") + 1]
@@ -13,11 +16,11 @@ def make_path(cmd_arguments: list[str]) -> tuple[str]:
     if "-d" in cmd_arguments:
         cmd_arguments.pop(cmd_arguments.index("-d"))
         path = os.path.join(*cmd_arguments)
-        if path == " ":
-            filepath = filename
-        else:
-            filepath = os.path.join(path, filename)
-        return path, filename, filepath
+    if path == " ":
+        filepath = filename
+    else:
+        filepath = os.path.join(path, filename)
+    return path, filename, filepath
 
 
 def create_file() -> None:
