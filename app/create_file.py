740dcd9e085dd7d7155b9a20fe37f452984ed8c7
Anastasia <anastasiiya.p@gmail.com>
2023-08-23 17:09:54 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,51 @@
-# write your code here
+import os
+import datetime
+import argparse
+
+
+def create_file() -> None:
+    parser = argparse.ArgumentParser(description="Create a file")
+    parser.add_argument(
+        "-d", "--directories", nargs="+", help="List of directory names"
+    )
+    parser.add_argument(
+        "-f", "--file_name", help="Name of the file to create"
+    )
+
+    args = parser.parse_args()
+
+    directories = args.directories
+    file_name = args.file_name
+
+    if directories:
+        directory_path = os.path.join(*directories)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+
+    if file_name:
+        file_path = (os.path.join(*directories, file_name)
+                     if directories
+                     else file_name
+                     )
+
+        with open(file_path, "a") as file_to_create:
+            add_space = False
+            current_timestamp = datetime.datetime.now().strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+            if os.path.getsize(file_path) > 0:
+                add_space = True
+
+            if add_space:
+                file_to_create.write("\n")
+
+            file_to_create.write(current_timestamp + "\n")
+
+            line_number = 0
+            while 1:
+                file_content = input("Enter content line: ")
+                line_number += 1
+                if file_content == "stop":
+                    break
+
+                file_to_create.write(f"{line_number} {file_content}\n")
