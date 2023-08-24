d16a8ab89c7eb5325980b24cab8d4fe48b289416
Marina <marishka28023@gmail.com>
2023-08-24 11:40:49 +0200

    'Solution6'

--- app/create_file.py
+++ app/create_file.py
@@ -3,6 +3,40 @@ import os
 import sys
 
 
+def handle_directory(args) -> str:
+    directory_path = "/".join(args[args.index("-d") + 1:])
+    if "-f" in args:
+        if args.index("-d") < args.index("-f"):
+            directory_path = "/".join(args[args.index("-d") + 1:args.index("-f")])
+
+    os.makedirs(directory_path, exist_ok=True)
+    print(directory_path)
+    return directory_path
+
+
+def handle_file(args, directory_path=None) -> None:
+    num_index = 0
+    content = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+    while True:
+        num_index += 1
+        answer = input("Enter content line: ")
+        if answer == "stop":
+            break
+        content += str(num_index) + " " + answer + "\n"
+    content += "\n"
+
+    if "-d" in args:
+        file_path = directory_path + "/" + args[args.index("-f") + 1]
+    else:
+        file_path = args[args.index("-f") + 1]
+    if os.path.exists(file_path):
+        with open(file_path, "a") as file:
+            file.write(content)
+    else:
+        with open(file_path, "w") as file:
+            file.write(content)
+
+
 def create_file() -> None:
     args = sys.argv[1:]
     if "-f" not in args and "-d" not in args:
@@ -10,38 +44,13 @@ def create_file() -> None:
     else:
         directory_path = ""
         if "-d" in args:
-            directory_path = "/".join(args[args.index("-d") + 1:])
-            if "-f" in args:
-                if args.index("-d") < args.index("-f"):
-                    directory_path = (
-                        "/".join(args[args.index("-d") + 1:args.index("-f")])
-                    )
-
-            os.makedirs(directory_path, exist_ok=True)
+            directory_path = handle_directory(args)
 
         if "-f" in args:
-            num_index = 0
-            content = (
-                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-            )
-            while True:
-                num_index += 1
-                answer = input("Enter content line: ")
-                if answer == "stop":
-                    break
-                content += str(num_index) + " " + answer + "\n"
-            content += "\n"
-
             if "-d" in args:
-                file_path = directory_path + "/" + args[args.index("-f") + 1]
+                handle_file(args, directory_path)
             else:
-                file_path = args[args.index("-f") + 1]
-            if os.path.exists(file_path):
-                with open(file_path, "a") as file:
-                    file.write(content)
-            else:
-                with open(file_path, "w") as file:
-                    file.write(content)
+                handle_file(args)
 
 
 create_file()
