97b0bdc0949f45b904743e1919ef0f37127febff
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-08-27 23:19:41 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,44 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def create_dir() -> None:
+    if "-f" in sys.argv:
+        dir_path = os.path.join(*sys.argv[2:-2])
+    else:
+        dir_path = os.path.join(*sys.argv[2:])
+    os.makedirs(dir_path, exist_ok=True)
+
+
+def create_file() -> None:
+    if "-d" in sys.argv:
+        file_path = os.path.join(*sys.argv[2:-2]) + "\\" + sys.argv[-1]
+    else:
+        file_path = sys.argv[2]
+
+    with open(file_path, "a") as file:
+        text = str(datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
+        file.write(text + "\n")
+
+        while text != "stop":
+            text = input("Enter content line: ")
+            if text != "stop":
+                file.write(text + "\n")
+        file.write("\n")
+
+
+if "-d" in sys.argv and "-f" not in sys.argv:
+
+    create_dir()
+
+
+if "-f" in sys.argv and "-d" not in sys.argv:
+
+    create_file()
+
+
+if "-d" in sys.argv and "-f" in sys.argv:
+
+    create_dir()
+    create_file()
