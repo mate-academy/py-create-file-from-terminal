b6db6ae92da906ff83086cc21cf2ad84525096f5
Taras Borshch <tarasborshch4@gmail.com>
2023-08-28 17:49:13 +0300

    Completed py-create-file-from-terminal

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,32 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+directory = ""
+if "-d" in sys.argv:
+    directory = sys.argv[sys.argv.index("-d") + 1:]
+    if "-f" in directory:
+        directory = directory[:directory.index("-f")]
+    directory = "\\".join(directory) + "\\"
+
+    if os.path.dirname(directory) != "":
+        os.makedirs(os.path.dirname(directory), exist_ok=True)
+
+
+if "-f" in sys.argv:
+    file_name = sys.argv[sys.argv.index("-f") + 1:]
+    if "-d" in directory:
+        file_name = file_name[:directory.index("-f")]
+    file_name = directory + "".join(file_name)
+
+    with open(file_name, "a") as file:
+        file.write(str(datetime.datetime.now().replace(microsecond=0)) + "\n")
+        line_index = 1
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                file.write("\n")
+                break
+            file.writelines(f"{line_index} {content}\n")
+            line_index += 1
