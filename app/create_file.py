c2fa049f54f4045fb4ca5189baf10c728d887715
oreny <orenycdota@gmail.com>
2023-08-29 13:54:06 +0300

    Implemented a function to read data from Terminal

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,29 @@
-# write your code here
+import argparse
+import os
+import datetime
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", "--file_name")
+parser.add_argument("-d", "--directories_to_create", nargs="+")
+
+args = parser.parse_args()
+current_path = os.getcwd()
+
+if args.directories_to_create is not None:
+    for folder in args.directories_to_create:
+        try:
+            os.mkdir(os.path.join(current_path, folder))
+        except FileExistsError:
+            pass
+        current_path = os.path.join(current_path, folder)
+
+if args.file_name is not None:
+    with open(os.path.join(current_path, args.file_name), "a") as f:
+        f.write(datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S") + "\n")
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                f.write("\n")
+                break
+            f.write(line + "\n")
