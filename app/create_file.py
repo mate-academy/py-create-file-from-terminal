b0766d705f53ea1752af18d8c0046fbef9be17dc
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-08-29 21:45:48 +0300

    Moved implemention into functions

--- app/create_file.py
+++ app/create_file.py
@@ -2,41 +2,52 @@ from datetime import datetime
 import os
 from sys import argv
 
-if "-d" not in argv and "-f" not in argv:
-    raise Exception("You should provide at least one flag")
 
-directories = []
-file_name = ""
+def create_directory() -> str:
+    directories = []
 
-if "-d" in argv:
     for value in range(argv.index("-d") + 1, len(argv)):
         if argv[value] == "-f":
             break
 
         directories.append(argv[value])
-if "-f" in argv:
+
+    current_path = ""
+
+    if directories:
+        current_path = os.path.join(*directories)
+
+        if not os.path.exists(current_path):
+            os.makedirs(current_path)
+
+    return current_path
+
+
+def create_file(path: str) -> None:
     file_name = argv[argv.index("-f") + 1]
 
-path = ""
+    if file_name:
+        with open(os.path.join(path, file_name), "a") as file:
+            file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
 
-if directories:
-    path = os.path.join(*directories)
+            line_number = 1
 
-    if not os.path.exists(path):
-        os.makedirs(path)
+            while True:
+                entered_content = input("Enter content line: ")
+
+                if entered_content == "stop":
+                    break
+
+                file.write(f"{line_number} {entered_content}\n")
+                line_number += 1
 
 
-if file_name:
-    with open(os.path.join(path, file_name), "a") as file:
-        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+if "-d" not in argv and "-f" not in argv:
+    raise Exception("You should provide at least one flag")
 
-        line_number = 1
+final_path = ""
 
-        while True:
-            entered_content = input("Enter content line: ")
-
-            if entered_content == "stop":
-                break
-
-            file.write(f"{line_number} {entered_content}\n")
-            line_number += 1
+if "-d" in argv:
+    final_path = create_directory()
+if "-f" in argv:
+    create_file(final_path)
