b3f2fca194f01c29a33a6bbbcd666ec03aa2b86e
Taras Borshch <tarasborshch4@gmail.com>
2023-08-29 20:34:24 +0300

    splitted logic into functions

--- app/create_file.py
+++ app/create_file.py
@@ -3,30 +3,42 @@ import os
 import datetime
 
 
-directory = ""
-if "-d" in sys.argv:
-    directory = sys.argv[sys.argv.index("-d") + 1:]
-    if "-f" in directory:
-        directory = directory[:directory.index("-f")]
-    directory = "\\".join(directory) + "\\"
+def check_for_directories():
+    directory = ""
+    if "-d" in sys.argv:
+        directory = sys.argv[sys.argv.index("-d") + 1:]
+        if "-f" in directory:
+            directory = directory[:directory.index("-f")]
+        directory = os.path.join(*directory) + "\\"
 
-    if os.path.dirname(directory) != "":
-        os.makedirs(os.path.dirname(directory), exist_ok=True)
+        if os.path.dirname(directory) != "":
+            os.makedirs(os.path.dirname(directory), exist_ok=True)
+    return directory
 
 
-if "-f" in sys.argv:
-    file_name = sys.argv[sys.argv.index("-f") + 1:]
-    if "-d" in directory:
-        file_name = file_name[:directory.index("-f")]
-    file_name = directory + "".join(file_name)
+def check_files(directory):
+    if "-f" in sys.argv:
+        file_name = sys.argv[sys.argv.index("-f") + 1:]
+        if "-d" in directory:
+            file_name = file_name[:directory.index("-f")]
+        file_name = f"{directory}" + "".join(file_name)
 
-    with open(file_name, "a") as file:
-        file.write(str(datetime.datetime.now().replace(microsecond=0)) + "\n")
-        line_index = 1
-        while True:
-            content = input("Enter content line: ")
-            if content == "stop":
-                file.write("\n")
-                break
-            file.writelines(f"{line_index} {content}\n")
-            line_index += 1
+        with open(file_name, "a") as file:
+            file.write(str(datetime.datetime.now().replace(microsecond=0)) + "\n")
+            line_index = 1
+            while True:
+                content = input("Enter content line: ")
+                if content == "stop":
+                    file.write("\n")
+                    break
+                file.writelines(f"{line_index} {content}\n")
+                line_index += 1
+
+
+def main():
+    directory = check_for_directories()
+    check_files(directory)
+
+
+if __name__ == "__main__":
+    main()
