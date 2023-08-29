0d2bb5cde8278dcf3722e19676d4c865d7937945
Taras Borshch <tarasborshch4@gmail.com>
2023-08-29 21:09:12 +0300

    used better namings and separated check file block

--- app/create_file.py
+++ app/create_file.py
@@ -3,7 +3,22 @@ import os
 import datetime
 
 
-def check_for_directories() -> str:
+def open_write_file(file_name):
+    with open(file_name, "a") as file:
+        file.write(
+            str(datetime.datetime.now().replace(microsecond=0)) + "\n"
+        )
+        line_index = 1
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                file.write("\n")
+                break
+            file.writelines(f"{line_index} {content}\n")
+            line_index += 1
+
+
+def check_if_directories_in_command() -> str:
     directory = ""
     if "-d" in sys.argv:
         directory = sys.argv[sys.argv.index("-d") + 1:]
@@ -16,30 +31,18 @@ def check_for_directories() -> str:
     return directory
 
 
-def check_files(directory: str) -> None:
+def check_if_file_in_command(directory: str) -> None:
     if "-f" in sys.argv:
         file_name = sys.argv[sys.argv.index("-f") + 1:]
         if "-d" in directory:
             file_name = file_name[:directory.index("-f")]
         file_name = f"{directory}" + "".join(file_name)
-
-        with open(file_name, "a") as file:
-            file.write(
-                str(datetime.datetime.now().replace(microsecond=0)) + "\n"
-            )
-            line_index = 1
-            while True:
-                content = input("Enter content line: ")
-                if content == "stop":
-                    file.write("\n")
-                    break
-                file.writelines(f"{line_index} {content}\n")
-                line_index += 1
+        open_write_file(file_name)
 
 
 def main() -> None:
-    directory = check_for_directories()
-    check_files(directory)
+    directory = check_if_directories_in_command()
+    check_if_file_in_command(directory)
 
 
 if __name__ == "__main__":
