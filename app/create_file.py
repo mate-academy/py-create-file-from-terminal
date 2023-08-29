13f500f076f7982463f6fbcf7f65a69742f978f1
Pavlo Harbuz <lgsilay1991@gmail.com>
2023-08-29 15:49:46 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -10,32 +10,25 @@ def create_path(directories: list) -> str:
     return path
 
 
-def add_content(file_to_write: str) -> None:
-    with open(file_to_write, "a") as file:
-        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-
+def create_file(file_path: str) -> None:
+    with open(file_path, "a") as file:
+        file.write(
+            f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \n"
+        )
         line = 1
-
-        while (data := input("Enter content line: ")) != "stop":
-            file.write(f"{line} {data}" + "\n")
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                break
+            file.write(f"{line} {content} \n")
             line += 1
-
-
-def write_file(path_file: str) -> None:
-    if not os.path.exists(path_file):
-        with open(path_file, "w"):
-            add_content(path_file)
-            return
-
-    with open(path_file, "a") as f:
-        f.write("\n")
 
 
 def main() -> None:
     if "-d" in sys.argv and "-f" in sys.argv:
         _, command_d, *directories, command_f, file_name = sys.argv
         path = create_path(directories)
-        write_file(f"{path}/{sys.argv[-1]}")
+        create_file(f"{path}/{file_name}")
         return
 
     if "-d" in sys.argv:
@@ -44,7 +37,7 @@ def main() -> None:
         return
 
     if "-f" in sys.argv:
-        write_file(sys.argv[-1])
+        create_file(sys.argv[-1])
         return
 
 
