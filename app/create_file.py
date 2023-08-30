03c662b8ad63dfed5f23810208347ee3c45c8af5
oreny <orenycdota@gmail.com>
2023-08-30 10:10:37 +0300

    Implemented two functions

--- app/create_file.py
+++ app/create_file.py
@@ -2,6 +2,33 @@ import argparse
 import os
 import datetime
 
+
+def create_directories(directories_to_create: str, create_path: str) -> str:
+    if directories_to_create is not None:
+        for folder in args.directories_to_create:
+            try:
+                os.mkdir(os.path.join(create_path, folder))
+            except FileExistsError:
+                pass
+            create_path = os.path.join(create_path, folder)
+    return create_path
+
+
+def write_in_file(file_name: str, path_of_file: str) -> None:
+    if file_name is not None:
+        with open(os.path.join(path_of_file, file_name), "a") as f:
+            f.write(
+                datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S") + "\n")
+            line_count = 1
+            while True:
+                line = input("Enter content line: ")
+                if line == "stop":
+                    f.write("\n")
+                    break
+                f.write(f"{line_count} {line} \n")
+                line_count += 1
+
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-f", "--file_name")
@@ -10,22 +37,5 @@ parser.add_argument("-d", "--directories_to_create", nargs="+")
 args = parser.parse_args()
 current_path = os.getcwd()
 
-if args.directories_to_create is not None:
-    for folder in args.directories_to_create:
-        try:
-            os.mkdir(os.path.join(current_path, folder))
-        except FileExistsError:
-            pass
-        current_path = os.path.join(current_path, folder)
-
-if args.file_name is not None:
-    with open(os.path.join(current_path, args.file_name), "a") as f:
-        f.write(datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S") + "\n")
-        line_count = 1
-        while True:
-            line = input("Enter content line: ")
-            if line == "stop":
-                f.write("\n")
-                break
-            f.write(f"{line_count} {line} \n")
-            line_count += 1
+current_path = create_directories(args.directories_to_create, current_path)
+write_in_file(args.file_name, current_path)
