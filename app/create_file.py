542f53aaecd68439121984e50155392bdcf80f92
Vladyslav Shepilov <shepsterya@gmail.com>
2023-08-30 18:44:49 +0300

    resloved

--- app/create_file.py
+++ app/create_file.py
@@ -3,45 +3,62 @@ import os
 import datetime
 
 
-def parse_input(arguments: str) -> dict:
-    actions = {"file_name": "", "directory_path": []}
-    paths_with_flags = arguments.split("-")
-    for single_command in paths_with_flags:
-        if single_command[0] == "f":
-            actions["file_name"] = single_command[2:]
-            continue
-        actions["directory_path"].append(single_command[2:])
-    return actions
+def parse_input() -> dict:
+    terminal_input = sys.argv[1:]
+    to_do_dict = {"file": None, "directory": None}
+    if "-d" in terminal_input and "-f" in terminal_input and len(terminal_input) >= 4:
+        file_index = terminal_input.index("-f")
+        dir_index = terminal_input.index("-d")
+        if file_index > dir_index:
+            to_do_dict["directory"] = terminal_input[1:file_index]
+            to_do_dict["file"] = terminal_input[file_index + 1]
+            return to_do_dict
+
+        to_do_dict["file"] = terminal_input[1:dir_index]
+        to_do_dict["directory"] = terminal_input[dir_index + 1:]
+
+    elif "-d" in terminal_input and len(terminal_input) > 2:
+        dir_index = terminal_input.index("-d")
+        to_do_dict["directory"] = terminal_input[dir_index:]
+
+    elif "-f" in terminal_input and len(terminal_input) > 2:
+        file_index = terminal_input.index("-f")
+        to_do_dict["directory"] = terminal_input[file_index + 1]
+
+    else:
+        raise ValueError
+
+    return to_do_dict
 
 
-def create_directory(directory_path: dict) -> str:
-    destination_directory = os.path.join(os.getcwd(), *directory_path)
+def create_directory(directory_path: str | list) -> str:
+    if len(directory_path) == 1:
+        destination_directory = os.path.join(os.getcwd(), directory_path)
+    else:
+        destination_directory = os.path.join(os.getcwd(), *directory_path)
+
+    if os.path.exists(destination_directory):
+        return destination_directory
     os.makedirs(destination_directory)
     return destination_directory
 
 
-def add_content_to_file(directory: str, file_name: str) -> bool:
-    if not os.path.exists(directory):
-        return False
-    content = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-    with open(os.path.join(directory, str(file_name)), "a") as file:
-        file.write(content)
+def add_content_to_file(file_name: str, directory_path: str) -> None:
+    with open(os.path.join(directory_path, file_name), "a") as file:
+
+        time_opened = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+        file.write(time_opened)
+
         counter = 1
-        content = input(f"Enter content line: Line{counter}")
+        content = input(f"Enter content line: Line{counter} ")
         while content != "stop":
             file.write(f"line{counter} " + content + "\n")
-            content = input(f"Enter content line: Line{counter}")
+            content = input(f"Enter content line: Line{counter} ")
             counter += 1
 
 
 if __name__ == "__main__":
-    input_string = " ".join(sys.argv[1:])
-    file_name, directory_path = parse_input(input_string).values()
-    print(directory_path)
-    directory_path = create_directory(directory_path)
-    add_content_to_file(directory_path, file_name)
-
-# print(type(sys.argv))
-# print(sys.argv)
-# print(type(" ".join(sys.argv)))
-# print(" ".join(sys.argv))
+    terminal_input = parse_input()
+    file_name = "".join(terminal_input["file"])
+    directory_path = create_directory(terminal_input["directory"])
+    add_content_to_file(file_name=file_name, directory_path=directory_path)
