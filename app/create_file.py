f07b50cef290a263e745b9936d8949d600620651
Vladyslav Shepilov <shepsterya@gmail.com>
2023-08-31 20:21:15 +0300

    adapted to single -f or -d cases

--- app/create_file.py
+++ app/create_file.py
@@ -18,13 +18,13 @@ def parse_input() -> dict:
         to_do_dict["file"] = terminal_input[1:dir_index]
         to_do_dict["directory"] = terminal_input[dir_index + 1:]
 
-    elif "-d" in terminal_input and len(terminal_input) > 2:
+    elif "-d" in terminal_input and len(terminal_input) >= 2:
         dir_index = terminal_input.index("-d")
-        to_do_dict["directory"] = terminal_input[dir_index:]
+        to_do_dict["directory"] = terminal_input[dir_index + 1:]
 
-    elif "-f" in terminal_input and len(terminal_input) > 2:
+    elif "-f" in terminal_input and len(terminal_input) >= 2:
         file_index = terminal_input.index("-f")
-        to_do_dict["directory"] = terminal_input[file_index + 1]
+        to_do_dict["file"] = terminal_input[file_index + 1]
 
     else:
         raise ValueError
@@ -32,11 +32,10 @@ def parse_input() -> dict:
     return to_do_dict
 
 
-def create_directory(directory_path: str | list) -> str:
-    if len(directory_path) == 1:
-        destination_directory = os.path.join(os.getcwd(), directory_path)
-    else:
-        destination_directory = os.path.join(os.getcwd(), *directory_path)
+def create_directory(directory_path: list) -> str | None:
+    if directory_path is None:
+        return None
+    destination_directory = os.path.join(os.getcwd(), *directory_path)
 
     if os.path.exists(destination_directory):
         return destination_directory
@@ -44,7 +43,12 @@ def create_directory(directory_path: str | list) -> str:
     return destination_directory
 
 
-def add_content_to_file(file_name: str, directory_path: str) -> None:
+def add_content_to_file(file_name: str, directory_path: str = None) -> None:
+    if file_name is None:
+        return
+    if directory_path is None:
+        directory_path = os.getcwd()
+
     with open(os.path.join(directory_path, file_name), "a") as file:
         time_opened = (datetime.datetime.now()
                        .strftime("%Y-%m-%d %H:%M:%S") + "\n")
@@ -60,6 +64,7 @@ def add_content_to_file(file_name: str, directory_path: str) -> None:
 
 if __name__ == "__main__":
     terminal_input = parse_input()
-    file_name = "".join(terminal_input["file"])
-    directory_path = create_directory(terminal_input["directory"])
-    add_content_to_file(file_name=file_name, directory_path=directory_path)
+    file_name = (None if terminal_input["file"] is None
+                 else "".join(terminal_input["file"]))
+    directory = create_directory(terminal_input["directory"])
+    add_content_to_file(file_name=file_name, directory_path=directory)
