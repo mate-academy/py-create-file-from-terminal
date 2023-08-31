3ebd5d42fd51e2d71f970f5643c1e165784ec031
Olexander Bryl <edlrian814@gmail.com>
2023-08-31 12:59:52 +0300

    finally got it

--- app/create_file.py
+++ app/create_file.py
@@ -5,20 +5,37 @@ from datetime import datetime
 
 def get_timestamp() -> str:
     time = datetime.now()
-    return time.strftime("%A, %b %d %Y, %I:%M%p.")
+    return time.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def get_input(path: str) -> None:
     line_number = 0
     with open(path, "a") as file:
-        file.write(get_timestamp() + "\n")
+        file.write("\n" + get_timestamp() + "\n")
         while True:
             line = input("Enter content line: ")
             if line == "stop":
-                file.write("\n")
+                # file.write("\n")
                 break
             line_number += 1
             file.write(f"{line_number} {line}\n")
+
+
+def create_path() -> str | None:
+    current_dir = ""
+    command = sys.argv[1:]
+    if "-d" in command:
+        directories = command[
+            1: command.index("-f") if "-f" in command else len(command)
+        ]
+        current_dir = directories[0]
+        for directory in directories[1:]:
+            current_dir = os.path.join(current_dir, directory)
+        if not os.path.exists(current_dir):
+            os.makedirs(current_dir)
+    if "-f" in command:
+        filename = os.path.join(current_dir, command[-1])
+        return filename
 
 
 def create_file_from_terminal() -> None:
@@ -28,19 +45,4 @@ def create_file_from_terminal() -> None:
 
 
 if __name__ == "__main__":
-
-    def create_path() -> str | None:
-        current_dir = ""
-        command = sys.argv[1:]
-        if "-d" in command:
-            directories = command[
-                1: command.index("-f") if "-f" in command else len(command)
-            ]
-            current_dir = directories[0]
-            for directory in directories[1:]:
-                current_dir = os.path.join(current_dir, directory)
-            if not os.path.exists(current_dir):
-                os.makedirs(current_dir)
-        if "-f" in command:
-            filename = os.path.join(current_dir, command[-1])
-            return filename
+    create_file_from_terminal()
