bc4de9666cea10eb20a7115d7291bf77a947ce6f
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-09-05 00:28:06 +0300

    Solution 2.0

--- app/create_file.py
+++ app/create_file.py
@@ -1,82 +1,43 @@
-import sys
+import argparse
 import os
+import sys
 from datetime import datetime
 
-
-def create_path(directories: list) -> str:
+def create_path(directories) -> str:
     path = os.path.join(*directories)
     if not os.path.exists(path):
-        try:
-            os.makedirs(path)
-        except FileNotFoundError:
-            print("Unsupported characters in file path!!!")
-            sys.exit()
+        os.makedirs(path)
     return path
 
-
 def create_file(path: str) -> None:
-    try:
-        with open(path, "w") as file:
-            file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-            line_count = 0
-            while True:
-                content = input("Enter content line: ")
-                if content == "stop":
-                    break
-                line_count += 1
-                file.write(f"{line_count}. {content}\n")
-    except FileNotFoundError:
-        print("Unsupported characters in file_name!!!")
-        sys.exit()
+    with open(path, "w") as file:
+        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        line_count = 0
+        content = ""
+        while content != "stop":
+            content = input("Enter content line: ")
+            line_count += 1
+            file.write(f"{line_count}. {content}\n")
 
-
-def main() -> None:
-    terminal_input = sys.argv
-    if "-f" not in terminal_input and "-d" not in terminal_input:
-        print(
-            " Please, provide a valid command:"
-            "'-d <directory_1> <directory_2>' or '-f <file_name>'"
+def main():
+    terminal_input = argparse.ArgumentParser()
+    terminal_input.add_argument('-d', '--directories', nargs='+')
+    terminal_input.add_argument('-f', '--file_name')
+    args = terminal_input.parse_args()
+    directories, file_name = args.directories, args.file_name
+    if not file_name and not directories:
+        print("Please, provide a valid command: "
+              "'-d <directory_1> <directory_2>' or '-f <file_name>'"
         )
-        sys.exit()
-    if "-d" not in terminal_input:
-        if len(terminal_input) < 3:
-            print("'file_name' argument is missing")
-            sys.exit()
-        _, flag_f, file_name, *_ = terminal_input
-        if flag_f != "-f":
-            print("Provide command in a valid format: '-f <file_name>'")
-            sys.exit()
-        create_file(file_name)
-    if "-f" not in terminal_input:
-        if len(terminal_input) < 3:
-            print("'directory' argument is missing")
-            sys.exit()
-        _, flag_d, *directories = terminal_input
-        if flag_d != "-d":
-            print(
-                "Provide command in a valid format: "
-                "'-d <directory_1> <directory_2>...'"
-            )
-            sys.exit()
-        create_path(directories)
-    if "-f" in terminal_input and "-d" in terminal_input:
-        if terminal_input[-1] == "-f":
-            if terminal_input[1] == "-f":
-                print("'directory' argument is missing")
-            print("'file_name' argument is missing")
-            sys.exit()
-        if terminal_input.index("-d") > terminal_input.index("-f"):
-            print(
-                " Please, provide commands in a valid order:"
-                "'-d <directory_1> <directory_2>... -f <file_name>'"
-            )
-            sys.exit()
-        _, flag_d, *directories, flag_f, file_name = terminal_input
-        if not directories:
-            print("'directory' argument is missing")
-            sys.exit()
-        path = fr"{create_path(directories)}\{file_name}"
+        sys.exit(1)
+    path = ""
+    if directories:
+        path = create_path(directories)
+
+    if file_name:
+        path = os.path.join(path, file_name)
         create_file(path)
+    sys.exit(1)
 
 
 if __name__ == "__main__":
