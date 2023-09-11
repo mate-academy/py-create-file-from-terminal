fd47aa7c2780198ed34c81939bdc513328b3539f
WoyDiam <freedom.channel2003@gmail.com>
2023-09-11 12:27:08 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -7,14 +7,6 @@ def get_time_stamp() -> str:
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-def read_existing_content(file_path: str) -> list:
-    if os.path.exists(file_path):
-        with open(file_path, "r") as file:
-            content = file.readlines()
-        return [line.strip() for line in content[1:]]
-    return []
-
-
 def get_user_input() -> list:
     content_lines = []
     while True:
@@ -34,36 +26,37 @@ def create_file(file_path: str, content_lines: list) -> None:
 
 
 def main() -> None:
-    if len(sys.argv) < 3:
-        print(
-            "Usage: python create_file.py -d <directory_path> -f <file_name>")
+    if len(sys.argv) < 2:
+        print("Usage: python create_file.py -d <directory_path> OR -f <file_name>")
         sys.exit(1)
 
-    if "-d" not in sys.argv or "-f" not in sys.argv:
-        print("You must specify both -d and -f flags.")
+    if "-d" in sys.argv:
+        dir_index = sys.argv.index("-d")
+        if dir_index + 1 >= len(sys.argv):
+            print("Directory path is missing.")
+            sys.exit(1)
+        directory_path = os.path.join(*sys.argv[dir_index + 1:])
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path, exist_ok=True)
+        file_name = None
+    elif "-f" in sys.argv:
+        file_index = sys.argv.index("-f")
+        if file_index + 1 >= len(sys.argv):
+            print("File name is missing.")
+            sys.exit(1)
+        file_name = sys.argv[file_index + 1]
+        directory_path = os.getcwd()
+    else:
+        print("You must specify either -d or -f flag.")
         sys.exit(1)
 
-    dir_index = sys.argv.index("-d")
-    file_index = sys.argv.index("-f")
-
-    if file_index - dir_index == 1:
-        print("Directory path is missing.")
-        sys.exit(1)
-
-    directory_path = os.path.join(*sys.argv[dir_index + 1:file_index])
-    file_name = sys.argv[file_index + 1]
-
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
-
-    file_path = os.path.join(directory_path, file_name)
-
-    content_lines = read_existing_content(file_path)
-    new_content_lines = get_user_input()
-    content_lines.extend(new_content_lines)
-
-    create_file(file_path, content_lines)
-    print(f"File {file_path} created successfully.")
+    if file_name:
+        file_path = os.path.join(directory_path, file_name)
+        new_content_lines = get_user_input()
+        create_file(file_path, new_content_lines)
+        print(f"File {file_path} created successfully.")
+    else:
+        print(f"Directory {directory_path} created successfully.")
 
 
 if __name__ == "__main__":
