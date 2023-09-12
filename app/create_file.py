fbfa2cc8093525c5128b1c07bb969950d884726c
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-09-12 20:17:06 +0300

    Corrected

--- app/create_file.py
+++ app/create_file.py
@@ -34,12 +34,31 @@ def create_file(file_path: str) -> None:
 def terminal() -> None:
     args = sys.argv[1:]
 
-    if "-d" in args:
-        command_d, *directories = args
+    directories = []
+    file_path = None
+
+    for i, arg in enumerate(args):
+        if arg == "-d" and i + 1 < len(args):
+            directories.append(args[i + 1])
+        elif arg == "-f" and i + 1 < len(args):
+            file_path = args[i + 1]
+
+    if directories and file_path:
+        path = create_path(directories)
+        file_path = os.path.join(path, file_path)
+        create_file(file_path)
+
+    elif directories:
         create_path(directories)
 
-    elif "-f" in args:
-        create_file(args[-1])
+    elif file_path:
+        create_file(file_path)
+
+    else:
+        print("You should write correct request: "
+              "python create_file.py "
+              "(-d directory_path1 -d directory_path2 ...) "
+              "(-f file_name)")
 
 
 if __name__ == "__main__":
