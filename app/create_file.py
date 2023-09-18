48ed58fea8607fba85589f6e17725ad75826318a
Ostap <ostapbojko04@gmail.com>
2023-09-18 20:39:43 +0300

    py-create(fixed)

--- app/create_file.py
+++ app/create_file.py
@@ -16,6 +16,8 @@ def create_file_with_content(file_path: str) -> str:
 
     with open(file_path, "r") as file:
         content = file.read()
+        file.write(f"{timestamp}\n")
+        file.write(content)
 
 
 def main() -> None:
