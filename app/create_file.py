8862bd5425219fcc4e7c55fc3549609f17c76b33
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-09-19 14:44:58 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -8,50 +8,62 @@ def create_path(directories: list) -> str:
     return path
 
 
-def fopen(file_name: str, mode: str, str_text: str) -> object:
-    source = open(file_name, mode)
-    source.write(str_text)
-    return source
+def write_text_content(f_sourse: object,
+                       page_num: int, line_cont: str) -> None:
+    while True:
+        input_text = input(f"Enter content line: {line_cont} ")
+
+        if input_text == "stop":
+            break
+        f_sourse.write(f"{page_num} {line_cont} {input_text} \n")
+        page_num += 1
 
 
-def funct_create_file() -> None:
-    file_path = create_path(["app"] + [""])
+def create_dir(file_path: list) -> None:
+    page = file_path[0].split("/")[0]
+    index_d = file_path.index("-d") + 1
+    file_path = create_path([page] + file_path[index_d:])
 
-    if sys.argv.count("-d"):
-        index = sys.argv.index("-d") + 1
-
-        if sys.argv.count("-f"):
-            index_f = sys.argv.index("-f")
-            file_path += create_path(sys.argv[index:index_f] + [""])
-        else:
-            file_path += create_path(sys.argv[index:])
+    if not os.path.exists(file_path):
         os.makedirs(file_path, exist_ok=True)
 
-    if sys.argv.count("-f"):
-        index_f = sys.argv.index("-f") + 1
-        file_path += create_path(sys.argv[index_f:])
 
-        t_now = datetime.now()
-        date = t_now.strftime("%Y-%m-%d %H:%M:%S")
+def create_file(file_path: list) -> None:
+    page = file_path[0].split("/")[0]
+    index_f = file_path.index("-f")
 
-        page_num = 1
+    path1 = file_path[2:index_f]
+    path2 = file_path[index_f + 1:]
+    path = path1 + path2
+    file_path = create_path([page] + path)
+    page_num = 1
 
-        if os.path.exists(file_path):
-            source_f = fopen(file_path, "a", f"\n{date}\n")
-            line_cont = f"Another line{page_num}"
-        else:
-            source_f = fopen(file_path, "w", f"\n{date}\n")
-            line_cont = f"Line{page_num}"
-        if source_f:
-            while True:
-                input_text = input(f"Enter content line: {line_cont} ")
-
-                if input_text != "stop":
-                    source_f.write(f"{page_num} {line_cont} {input_text} \n")
-                    page_num += 1
-                else:
-                    source_f.close()
-                    break
+    if os.path.exists(file_path):
+        with open(file_path, "a") as f:
+            f.write(f"\n{current_date}\n")
+            write_text_content(f, page_num, "Another")
+    else:
+        with open(file_path, "w") as f:
+            f.write(f"\n{current_date}\n")
+            write_text_content(f, page_num, "Line ")
 
 
-funct_create_file()
+current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def funct_create_file_from_terminal() -> None:
+    list_avrg = sys.argv
+
+    if sys.argv.count("-d") and sys.argv.count("-f") == 0:
+        create_dir(list_avrg)
+
+    if sys.argv.count("-f") and sys.argv.count("-d") == 0:
+        create_file(list_avrg)
+
+    if sys.argv.count("-d") and sys.argv.count("-f"):
+        path_dir = list_avrg[:list_avrg.index("-f")]
+        create_dir(path_dir)
+        create_file(list_avrg)
+
+
+funct_create_file_from_terminal()
