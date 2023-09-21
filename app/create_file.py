7853e484901de3242738a440e791d53fb33ee0b2
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-09-21 12:36:23 +0200

    Correction4

--- app/create_file.py
+++ app/create_file.py
@@ -9,41 +9,20 @@ def create_file(file_path: str, content: str) -> None:
 
 
 def main() -> None:
-    directory_path = "."
-    file_index = sys.argv.index("-f")
-    if len(sys.argv) < 2:
-        print(
-            "Usage: python create_file.py -d <directory_path> -f <file_name>")
-        return
+    path = "."
+    if sys.argv[1] == "-d":
+        directory_path = sys.argv[2:] if "-f" != sys.argv[-2] else sys.argv[2:-2]
+        path = os.path.join(*directory_path)
+    elif sys.argv[1] == "-f" and "-d" in sys.argv:
+        path = os.path.join(*sys.argv[4:])
 
-    if "-d" in sys.argv:
-        directory_index = sys.argv.index("-d")
-        if directory_index + 1 >= len(sys.argv):
-            print("Please provide a directory path after -d flag.")
-            return
-
-        directory_path = os.path.join(
-            *sys.argv[directory_index + 1:file_index]
-        )
-        os.makedirs(directory_path, exist_ok=True)
+    if path:
+        os.makedirs(path, exist_ok=True)
 
     if "-f" in sys.argv:
-        if file_index + 1 >= len(sys.argv):
-            print("Please provide a file name after -f flag.")
-            return
-
-        file_name = sys.argv[file_index + 1]
-        file_path = os.path.join(
-            directory_path, file_name
-        ) if "-d" in sys.argv else file_name
-
-        if os.path.exists(file_path):
-            print(f"Appending content to {file_path}:")
-        else:
-            print(f"Creating new file {file_path}:")
-
+        path = os.path.join(path, sys.argv[sys.argv.index("-f") + 1])
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        create_file(file_path, f"\n{timestamp}")
+        create_file(path, f"\n{timestamp}")
 
         line_number = 1
         while True:
@@ -52,7 +31,7 @@ def main() -> None:
             )
             if line == "stop":
                 break
-            create_file(file_path, f"\n{line_number} {line}")
+            create_file(path, f"\n{line_number} {line}")
             line_number += 1
 
 
