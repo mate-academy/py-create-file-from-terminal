c3cabfd1da5526e179046f87d5c3f776d8b7e982
Ostap <ostapbojko04@gmail.com>
2023-09-21 19:23:47 +0300

    WORKING VERSION

--- app/create_file.py
+++ app/create_file.py
@@ -1,23 +1,25 @@
 import os
 import sys
+from datetime import datetime
 
 
 def create_file_with_content(file_path: str) -> str:
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(file_path, "a") as file:
+        file.write(f"{timestamp}\n")
     line_count = 1
     while True:
-        content_line = input("Enter content line (or 'stop' to finish): ")
+        content_line = input("Enter content line: ")
         if content_line.lower() == "stop":
             break
         with open(file_path, "a") as file:
             file.write(f"{line_count} {content_line}\n")
         line_count += 1
+    with open(file_path, "a") as file:
+        file.write("\n")
 
 
 def main() -> None:
-    if len(sys.argv) < 2:
-        print("Usage: python create_file.py"
-              " [-d <directory_path>] [-f <file_name>]")
-        return
     directory_path = ""
 
     if "-d" in sys.argv and "-f" in sys.argv:
@@ -40,13 +42,7 @@ def main() -> None:
         else:
             file_path = file_name
 
-        if os.path.exists(file_path):
-            print(f"File '{file_path}' already exists. Appending content.")
-        else:
-            print(f"Creating file {file_path}.")
-
         create_file_with_content(file_path)
-        print(f"File {file_path} has been created with content.")
 
 
 if __name__ == "__main__":
