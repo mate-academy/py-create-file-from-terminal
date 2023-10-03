66a8a7ad6212c8f36e9009f2d4355e3399baeeff
Anton Tkachuk <tkacuk2291@gmail.com>
2023-10-03 18:25:22 +0300

    Solutions

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,39 @@
-# write your code here
+import argparse
+import datetime
+import os
+
+
+parser = argparse.ArgumentParser(description="Create directories and files")
+parser.add_argument("-f")
+parser.add_argument("-d", nargs="+")
+args = parser.parse_args()
+
+if args.d:
+    if len(args.d) > 1:
+        os.makedirs(f"{args.d[0]}/{args.d[1]}")
+    else:
+        os.makedirs(args.d[0])
+
+if args.f:
+    count_string = 1
+
+    if args.d:
+
+        file_patch = os.path.join(args.d[0], args.d[1], args.f)
+    else:
+
+        file_patch = args.f
+
+    with open(file_patch, "a") as files:
+        user_data = datetime.datetime.now()
+        files.write(user_data.strftime("%Y-%m-%d %H:%M:%S") + "\n")
+
+        while True:
+            user_text = input("Enter content line:")
+            if user_text != "stop":
+
+                files.write(f"{count_string} {user_text}" + "\n")
+                count_string += 1
+            else:
+                files.write("\n")
+                break
