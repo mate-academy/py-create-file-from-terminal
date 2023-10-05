425e43332d63736d85b15dad60b8812178a33581
Roman Sachavskyi <roman.sachavskyi.ki.2019@lpnu.ua>
2023-10-05 16:30:09 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -5,17 +5,20 @@ import os
 
 def create_file() -> None:
     command = sys.argv[1:]
-    file_name = command[command.index("-f") + 1:]
     try:
         directory = command[command.index("-d") + 1:command.index("-f")]
     except ValueError:
         directory = []
 
     if "-d" in command:
-        directory = command[command.index("-d") + 1:command.index("-f")]
+        if "-f" in command:
+            directory = command[command.index("-d") + 1:command.index("-f")]
+        else:
+            directory = command[command.index("-d") + 1:]
         os.makedirs("/".join(directory), exist_ok=True)
 
     if "-f" in command:
+        file_name = command[command.index("-f") + 1:]
         with open("/".join(directory + file_name), "a") as f:
             f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             i = 1
@@ -26,7 +29,6 @@ def create_file() -> None:
                 f.write("\n")
                 f.write(f"{i} {words}")
                 i += 1
-            f.write("\n")
             f.write("\n")
 
 
