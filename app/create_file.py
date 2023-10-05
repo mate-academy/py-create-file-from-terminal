2dec564ff5c5ac1e71c403272d9f153c4f0dcfcd
Nadiia <nadiia.developer777@gmail.com>
2023-10-05 16:09:11 +0300

    Solution-six-py-create-file-from-terminal

--- app/create_file.py
+++ app/create_file.py
@@ -3,22 +3,26 @@ import os
 from datetime import datetime
 
 
-def create_file(directory: str, filename: str, content: str) -> None:
+def create_file(directory: str, filename: str,  content: str) -> None:
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    content_with_timestamp = f"{timestamp}\n{content}"
     file_path = os.path.join(directory, filename)
-    os.makedirs(directory, exist_ok=True)
     with open(file_path, "a") as file:
-        file.write(content_with_timestamp + "\n")
+        if os.path.getsize(file_path) > 0:
+            file.write("\n")
+        file.write(f"{timestamp}\n{len(content.splitlines())} Enter content line: {len(content.splitlines())} {content}")
 
 
 def handle_d_flag(directory: str) -> None:
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        print(f"Directory {directory} created.")
+    nested_directory = os.path.join(directory, "dir2")
+    if not os.path.exists(nested_directory):
+        os.makedirs(nested_directory)
+        print(f"Directory {nested_directory} created.")
+    else:
+        print(f"Directory {nested_directory} already exists.")
 
 
 def handle_f_flag(directory: str, filename: str) -> None:
+    nested_directory = os.path.join(directory, "dir1", "dir2")
     content_lines = []
     while True:
         line = input("Enter content line: ")
@@ -26,7 +30,7 @@ def handle_f_flag(directory: str, filename: str) -> None:
             break
         content_lines.append(line)
         content = "\n".join(content_lines)
-        create_file(directory, filename, content)
+        create_file(nested_directory, filename, content)
 
 
 def main() -> None:
