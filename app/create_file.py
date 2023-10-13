b35011d63f58e7220c5de7adec3d960cdd215e60
Vadym Tiron <tironvadim1@gmail.com>
2023-10-13 14:25:14 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,52 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+from typing import List
+
+
+def create_file(
+        directory_path: str,
+        file_name: str,
+        content: List[str]
+) -> None:
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    file_path = os.path.join(directory_path, file_name)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    if os.path.exists(file_path):
+        with open(file_path, "a") as file:
+            file.write("\n" + timestamp + "\n")
+    else:
+        with open(file_path, "w") as file:
+            file.write(timestamp + "\n")
+
+    line_number = 1
+    for line in content:
+        if line == "stop":
+            break
+        with open(file_path, "a") as file:
+            file.write(f"{line_number} {line}\n")
+        line_number += 1
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
+        flag = args[0]
+
+        if flag == "-d":
+            directory_path = os.path.join(*args[1:])
+            os.makedirs(directory_path)
+
+        elif flag == "-f":
+            file_name = args[1]
+            content = []
+            while True:
+                line = input("Enter content line: ")
+                if line == "stop":
+                    break
+                content.append(line)
+            create_file(os.getcwd(), file_name, content)
