9791b66c793ef567ca321231aa469fd5b968c745
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-21 20:39:25 +0300

    s

--- app/create_file.py
+++ app/create_file.py
@@ -13,13 +13,13 @@ def make_file(file_name: str) -> None:
     with open(file_name, "a") as file:
         file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         print("Enter content or 'stop'")
-        for counter, line in enumerate(iter(input, 'stop'), 1):
+        for counter, line in enumerate(iter(input, "stop"), 1):
             file.write(f"{counter} {line}\n")
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", nargs='+', help="List of directory names")
+    parser.add_argument("-d", nargs="+", help="List of directory names")
     parser.add_argument("-f", help="File name")
     args = parser.parse_args()
 
