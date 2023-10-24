a4d9aa515a770e4bc0cf22f2c14a4bbb1ef880c8
ANMI <migunov1au@gmail.com>
2023-10-24 21:28:37 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,54 +3,53 @@ import os
 from datetime import datetime
 
 
-def get_command_flags() -> dict:
+def get_command_flags() -> tuple[list, list]:
     command = sys.argv[1:]
-    flag_items = {}
+    size = len(command)
+    d_index, f_index = size, size
 
-    for i in range(len(command)):
-        f = None
-        d = None
+    for i in range(size):
         if command[i] == "-f":
-            flag_items["-f"] = command[i + 1]
-            f = i
+            f_index = i
         elif command[i] == "-d":
-            d = i
+            d_index = i
 
-
-
-
-
-
+    if d_index < f_index:
+        # f_args = [] if flag does not exist
+        return (command[d_index + 1:min(f_index, size)],
+                command[f_index + 1:size])
+    else:
+        # d_args = [] if flag does not exist
+        return (command[d_index + 1:size],
+                command[f_index + 1:min(d_index, size)])
 
 
 def create_path(
         directories: list[str],
-        file_name: str
+        file_name: list[str]
 ) -> str:
     path = ""
 
-    if directories != []:
+    if directories:
         path = os.path.join(*directories)
         if not os.path.exists(path):
             os.makedirs(path)
 
-    return os.path.join(path, file_name)
+    return os.path.join(path, " ".join(file_name))
 
 
-def create_file():
-    flag_values = get_command_flags()
-    path = create_path(flag_values["-d"], flag_values["-f"])
-
-    with open(path, "a") as file:
-        file.write(datetime.now().isoformat())
+def create_file() -> None:
+    with open(create_path(*get_command_flags()), "a") as file:
+        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         line_pointer = 1
+
         while True:
             content = input("Enter content line: ")
 
             if content == "stop":
                 break
 
-            file.write(f"{line_pointer} {content}")
+            file.write(f"{line_pointer} {content}\n")
             line_pointer += 1
 
 
