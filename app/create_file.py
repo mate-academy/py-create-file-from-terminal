be91d8de45be11142cf45bbb2a40aca8ab0c013f
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-25 23:56:25 +0300

    Solution?

--- app/create_file.py
+++ app/create_file.py
@@ -4,35 +4,38 @@ from sys import argv
 
 
 def create_file(path_with_file: str) -> None:
-    with open(os.path.join(path_with_file), "w") as file:
+    with open(os.path.join(path_with_file), "a") as file:
         content = ""
+        enumerate_line = 1
         while True:
             line = str(input("Enter content line: "))
             if line == "stop":
                 break
-            content += line + "\n"
+            content += f"{enumerate_line} {line}\n"
+            enumerate_line += 1
         file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
         file.write(content)
 
 
 def create_path(path_to_create: list) -> str:
-    # creating path from list
-    return os.path.join("/".join(path_to_create))
+    return os.path.join(*path_to_create)
 
 
-if "-f" in argv and "-d" in argv:
-    path = []
-    for arg in argv[2:]:
-        if arg == "-f":
-            break
-        path.append(arg)
-    os.makedirs(create_path(path))  # making directories
-    path.append(argv[-1])  # making path with file
-    create_file(create_path(path))  # creating file with path with file
+if __name__ == "__main__":
+    if "-f" in argv and "-d" in argv:
+        path = []
+        for arg in argv[2:]:
+            if arg == "-f":
+                break
+            path.append(arg)
+        if not os.path.exists(create_path(path)):
+            os.makedirs(create_path(path))
+        path.append(argv[-1])
+        create_file(create_path(path))
 
-elif argv[1] == "-d":
-    path = create_path(argv[2:])
-    os.makedirs(path)  # create just a path if -d is an only flag
+    elif argv[1] == "-d":
+        path = create_path(argv[2:])
+        os.makedirs(path)
 
-elif argv[1] == "-f":
-    create_file(argv[2])  # create just a file if -f is an only flag
+    elif argv[1] == "-f":
+        create_file(argv[2])
