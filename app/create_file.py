3478562cdcf1d87acbc068e5386d5044e2c34a78
Olena Lisova <lisovausa@gmail.com>
2023-11-01 14:21:04 -0400

    Refactor create file from terminal

--- app/create_file.py
+++ app/create_file.py
@@ -2,40 +2,47 @@ import os
 import sys
 from datetime import datetime
 
-args = sys.argv
-file_name = ""
-dir_path = ""
 
-if len(args) >= 3 and args[1] == "-d":
-    is_f = False
-    for arg in args[2:]:
-        if arg == "-f":
-            is_f = True
-            continue
-        if is_f:
-            file_name = arg
-            break
+def parse_dir_file(args: list[str]) -> str:
+    file_name = ""
+    dir_path = ""
+    if len(args) >= 3 and args[1] == "-d":
+        for i in range(2, len(args)):
+            if args[i] == "-f":
+                if (i + 1) <= (len(args) - 1):
+                    file_name = args[i + 1]
+                    print("file_name = ", file_name)
+                break
 
-        dir_path = os.path.join(dir_path, arg)
+            dir_path = os.path.join(dir_path, args[i])
 
-    if dir_path:
-        os.makedirs(dir_path, exist_ok=True)
+        if dir_path:
+            os.makedirs(dir_path, exist_ok=True)
 
-elif len(args) >= 3 and args[1] == "-f":
-    file_name = args[2]
+    elif len(args) >= 3 and args[1] == "-f":
+        file_name = args[2]
 
-if file_name:
     if dir_path:
         file_name = os.path.join(dir_path, file_name)
 
+    return file_name
+
+
+def creat_file(file_name: str) -> None:
     with open(file_name, "a") as f:
         date = datetime.now()
         f.write(date.strftime("%Y-%m-%d %H:%M:%S\n"))
         text = input("Enter content line:")
         is_stop = text.lower()
-        i = 1
+        coгnter = 1
         while is_stop != "stop":
-            f.write(f"{str(i)}. {text}\n")
+            f.write(f"{str(coгnter)}. {text}\n")
             text = input("Enter content line:")
-            i += 1
+            coгnter += 1
             is_stop = text.lower()
+
+
+file_name = parse_dir_file(sys.argv)
+
+if file_name:
+    creat_file(file_name)
