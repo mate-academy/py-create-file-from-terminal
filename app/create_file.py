a4b091d7a3c76466fc4f3b31bdf9a8a71880fa01
Your_Name Your_Surname <your@email>
2023-11-02 14:02:23 +0200

    add os.path.join in program

--- app/create_file.py
+++ app/create_file.py
@@ -20,14 +20,19 @@ def create_file_for_writing(
 
 
 if sys_argv[1] == "-d":
-    os.makedirs("/".join(sys_argv[2:len(sys_argv) - 1]))
+    path = os.path.join(sys_argv[2:])
+    os.makedirs(path)
 elif sys_argv[1] == "-f":
     if os.path.isfile(sys_argv[2]):
         create_file_for_writing(sys_argv[2], "a")
     else:
         create_file_for_writing(sys_argv[2], "w")
-elif "-f" in sys.argv and "-d" in sys.argv:
-    os.makedirs("/".join(sys_argv[
-                         sys_argv.index("-d"):sys_argv.index("-f")
-                         ]))
-    create_file_for_writing(sys_argv[-1], "w")
+elif all(value in sys.argv for value in ["-d", "-f"]):
+    index_f = sys_argv.index("-f")
+    index_d = sys_argv.index("-d")
+    if index_f > index_d:
+        path = os.path.join(sys_argv[index_d: index_f])
+        os.makedirs(path)
+    path = os.path.join(sys_argv[index_f: index_d])
+    os.makedirs(path)
+    create_file_for_writing(sys_argv[index_f + 1], "w")
