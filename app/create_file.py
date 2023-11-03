5b7beb6fe9756233181fa0cfa379efea8d34f57a
Your_Name Your_Surname <your@email>
2023-11-03 11:49:50 +0200

    add exist_ox in os.makedirs

--- app/create_file.py
+++ app/create_file.py
@@ -21,7 +21,7 @@ def create_file_for_writing(
 
 if sys_argv[1] == "-d":
     path = os.path.join(*sys_argv[2:])
-    os.makedirs(path)
+    os.makedirs(path, exist_ok=True)
 elif sys_argv[1] == "-f":
     if os.path.isfile(sys_argv[2]):
         create_file_for_writing(sys_argv[2], "a")
@@ -32,7 +32,7 @@ elif all(value in sys.argv for value in ["-d", "-f"]):
     index_d = sys_argv.index("-d")
     if index_f > index_d:
         path = os.path.join(*sys_argv[index_d: index_f])
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
     path = os.path.join(*sys_argv[index_f: index_d])
-    os.makedirs(path)
+    os.makedirs(path, exist_ok=True)
     create_file_for_writing(sys_argv[index_f + 1], "w")
