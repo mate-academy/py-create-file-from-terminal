3c4b540e96738b44e53e74fc568798d79113b30f
Svitlana Savikhina <savihina111@gmail.com>
2023-11-04 19:11:37 +0200

    Corrected solution

--- app/create_file.py
+++ app/create_file.py
@@ -6,25 +6,27 @@ import os
 def create_file() -> None:
     current_time = datetime.datetime.now()
     if "-d" in sys.argv:
-        for i in range(2, len(sys.argv)):
-            if "-f" not in sys.argv[i]:
-                os.mkdir(sys.argv[i])
-                os.chdir(sys.argv[i])
-            else:
-                break
+        index_d = sys.argv.index("-d")
+        directory_path = sys.argv[index_d + 1:]
+        directory_path = os.path.join(*directory_path)
+        os.makedirs(directory_path, exist_ok=True)
 
     if "-f" in sys.argv:
-        with open(sys.argv[-1], "a") as file:
-            file.write(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
-            count_lines = 1
+        index_f = sys.argv.index("-f")
+        output_filename = sys.argv[index_f + 1]
 
-            while True:
-                user_typing = input("Enter content line:")
-                if user_typing == "stop":
-                    file.write("\n")
-                    break
-                file.write(f"{count_lines} {user_typing}" + "\n")
-                count_lines += 1
+        if output_filename:
+            with open(output_filename, "a") as file:
+                file.write(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+                count_lines = 1
+
+                while True:
+                    user_typing = input("Enter content line:")
+                    if user_typing == "stop":
+                        file.write("\n")
+                        break
+                    file.write(f"{count_lines} {user_typing}" + "\n")
+                    count_lines += 1
 
 
 create_file()
