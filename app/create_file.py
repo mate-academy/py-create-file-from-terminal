a28c08f6a9656c350c9a5aa6846d6ab3aca0d819
Ivan <mozharivan17@gmail.com>
2023-11-09 14:11:08 +0200

    Fixed chdir, blank lines, exist_ok solution

--- app/create_file.py
+++ app/create_file.py
@@ -4,18 +4,13 @@ from datetime import datetime
 
 
 def create_directory(directories: list) -> None:
-
     if directories:
         base_directory = os.path.join(*directories)
-
-        os.makedirs(base_directory)
-        os.chdir(base_directory)
+        os.makedirs(base_directory, exist_ok=True)
 
 
 def create_file(file_name: str, path: str) -> None:
-
     content_file = []
-
     line_number = 0
     while True:
         user_input = input("Enter content line: ")
@@ -23,7 +18,6 @@ def create_file(file_name: str, path: str) -> None:
             break
         line_number += 1
         content_file.append(f"\n{line_number} {user_input}")
-
     with open(os.path.join(path, file_name), "a") as file:
         file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         file.write(" ".join(content_file))
@@ -31,28 +25,21 @@ def create_file(file_name: str, path: str) -> None:
 
 
 if __name__ == "__main__":
-
     command = sys.argv
-
     if "-d" in command and "-f" not in command:
-
         d_index = command.index("-d")
         directories = command[d_index + 1:]
         create_directory(directories)
-
     elif "-f" in command and "-d" not in command:
-
         f_index = command.index("-f")
         file_name = command[f_index + 1]
         path = os.getcwd()
         create_file(file_name, path)
-
     elif "-f" in command and "-d" in command:
-
         d_index = command.index("-d")
         f_index = command.index("-f")
         directories = command[d_index + 1:f_index]
         file_name = command[f_index + 1]
+        path = os.path.join(*directories)
         create_directory(directories)
-        path = os.getcwd()
         create_file(file_name, path)
