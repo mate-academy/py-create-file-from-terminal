7d740f483817948550e2e6e6ab78c9cba8110adf
unknown <temakot254@gmail.com>
2023-11-11 13:14:12 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -10,23 +10,25 @@ def main() -> None:
         file_index = args.index("-f")
         directory = os.path.join(*args[dir_index + 1:file_index])
         filename = args[file_index + 1]
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        create_file(os.path.join(directory, filename))
     elif "-d" in args:
         directory = os.path.join(*args[args.index("-d") + 1:])
+        os.makedirs(directory)
         filename = ""
     elif "-f" in args:
         directory = ""
         filename = args[args.index("-f") + 1]
+        create_file(filename)
     else:
         print("Invalid arguments")
         return
-    create_file(directory, filename)
 
 
-def create_file(directory: str, filename: str) -> None:
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    with open(os.path.join(directory, filename), "a") as f:
-        f.write(datetime.now().strftime() + "%Y-%m-%d %H:%M:%S")
+def create_file(filename: str) -> None:
+    with open(filename, "a") as f:
+        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         line_number = 1
         while True:
             line = input("Enter content line: ")
