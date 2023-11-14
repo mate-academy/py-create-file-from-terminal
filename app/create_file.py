f2752f8d4a233cb508f933f138620f334a42db67
valikdeb <73042683+valikdeb@users.noreply.github.com>
2023-11-14 03:23:29 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,24 +1,50 @@
 from sys import argv
-from os import makedirs
-from os import path
+from os import makedirs, path
 from datetime import datetime
 
 
-f_index = argv.index("-f")
-path_to_file = ""
-
-if "-d" in argv:
+def find_dir():
     d_index = argv.index("-d")
-    path_to_file = "/".join(argv[d_index + 1: f_index])
-    if not path.exists(path_to_file):
-        makedirs(path_to_file)
+    if "-f" in argv:
+        f_index = argv.index("-f")
+        directory = "/".join(argv[d_index + 1: f_index]) + "/"
+    else:
+        directory = "/".join(argv[d_index + 1:]) + "/"
+
+    if not path.exists(directory):
+        makedirs(directory)
+
+    return path.join(directory)
 
 
-name = argv[f_index + 1]
-with open(path_to_file + "/" + name, "w") as f:
-    f.write(datetime.now().strftime("%Y/%m/%d, %H:%M:%S") + "\n")
-    while True:
-        line = input("Enter content line: ")
-        if line == "stop":
-            break
-        f.write(line + "\n")
+def find_name():
+    if "-f" not in argv:
+        return None
+    f_index = argv.index("-f")
+    name = argv[f_index + 1]
+    return name
+
+
+def write_content(file_path):
+    with open(file_path, "w") as f:
+        f.write(datetime.now().strftime("%Y/%m/%d, %H:%M:%S") + "\n")
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            f.write(line + "\n")
+
+
+def create_file():
+    if "-f" not in argv:
+        directory = find_dir()
+    if "-d" not in argv:
+        file_name = find_name()
+        write_content(file_name)
+    if "-d" in argv and "-f" in argv:
+        file_name = find_name()
+        file_path = find_dir()
+        write_content(file_path + file_name)
+
+
+create_file()
--- checklist.md
+++ checklist.md
@@ -29,7 +29,7 @@ Good example:
 
 ```python
 def create_path(directories: list) -> str:
-    path = os.path_to_file.join(*directories)
+    path = os.path.join(*directories)
     return path
 ```
 
