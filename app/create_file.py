5105df8e7134bfb484071689710bda998ef9edeb
unknown <temakot254@gmail.com>
2023-11-18 14:35:44 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -12,7 +12,9 @@ def main() -> None:
         filename = args[file_index + 1]
         if not os.path.exists(directory):
             os.makedirs(directory)
-        create_file(os.path.join(directory, filename))
+        file_join = os.path.join(directory, filename)
+        if os.path.exists(file_join):
+            create_file(file_join)
     elif "-d" in args:
         directory = os.path.join(*args[args.index("-d") + 1:])
         os.makedirs(directory)
@@ -27,15 +29,17 @@ def main() -> None:
 
 
 def create_file(filename: str) -> None:
-    with open(filename, "a") as f:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    with open(filename, "a") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(timestamp + "\n")
         line_number = 1
         while True:
             line = input("Enter content line: ")
             if line.lower() == "stop":
                 break
-            f.write(f"{line_number} {line}\n")
+            file.write(f"{line_number} {line}\n")
             line_number += 1
+        file.write("\n")
 
 
 if __name__ == "__main__":
