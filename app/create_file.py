9b131bd9ed02624996b8a8229d2e6eabfe0ae2f8
Valeriia Tymchenko <maksimova.tymchenko@gmail.com>
2023-11-21 10:32:14 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -4,6 +4,19 @@ import sys
 import os
 
 
+def create_file(path_to_file: str, line: str) -> None:
+    with open(path_to_file, "a") as file:
+        file.write(str(datetime.now().
+                       strftime("%Y-%m-%d %H:%M:%S")) + "\n")
+        line_count = 0
+        while True:
+            content = input("Enter content line: ")
+            line_count += 1
+            if content == "stop":
+                break
+            file.write(f"{line}{line_count} " + content + "\n")
+
+
 def main() -> None:
     if sys.argv[1] == "-d":
         path_to_directory = os.path.join(*sys.argv[2:])
@@ -13,45 +26,20 @@ def main() -> None:
         path_to_file = str(sys.argv[2])
 
         if os.path.exists(path_to_file):
-            with open(path_to_file, "a") as file:
-                file.write(str(datetime.now().
-                               strftime("%Y-%m-%d %H:%M:%S")) + "\n")
-                line = 0
-                while True:
-                    content = input("Enter content line: ")
-                    line += 1
-                    if content == "stop":
-                        break
-                    file.write(f"Another line{line} " + content + "\n")
+            line = "Another line"
+            create_file(path_to_file, line)
 
         else:
-            with open(path_to_file, "a") as file:
-                file.write(str(datetime.now().
-                               strftime("%Y-%m-%d %H:%M:%S")) + "\n")
-                line = 0
-                while True:
-                    content = input("Enter content line: ")
-                    line += 1
-                    if content == "stop":
-                        break
-                    file.write(f"Line{line} " + content + "\n")
+            line = "Line"
+            create_file(path_to_file, line)
 
     if "-d" in sys.argv and "-f" in sys.argv:
         path_to_directory = os.path.join(sys.argv[2], sys.argv[3])
         os.makedirs(path_to_directory, exist_ok=True)
 
         path_to_file = os.path.join(path_to_directory, str(sys.argv[5]))
-
-        with open(path_to_file, "a") as source_file:
-            source_file.write(str(datetime.now().
-                              strftime("%Y-%m-%d %H:%M:%S")) + "\n")
-            line = 0
-            while True:
-                content = input("Enter content line: ")
-                line += 1
-                if content == "stop":
-                    break
-                source_file.write(f"Line{line} " + content + "\n")
+        line = "Line"
+        create_file(path_to_file, line)
 
 
 if __name__ == "__main__":
