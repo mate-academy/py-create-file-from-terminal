64a6892c8b4866ed0e9179bc89496b17ad7e1042
Volodymyr Ivanchuk <Volodymyr_Ivanchuk@jabil.com>
2023-11-24 14:53:23 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,14 +3,17 @@ import sys
 from datetime import datetime
 
 
+destination = ""
+
+
 def work_with_folder(command_list: list[str]) -> None:
     global destination
-    destination = os.path.join(command[command.index("-d") + 1])
+    destination = os.path.join(command_list[command_list.index("-d") + 1])
     os.makedirs(destination, exist_ok=True)
 
 
 def work_with_file(command_list: list[str]) -> None:
-    file_name = command[command.index("-f") + 1]
+    file_name = command_list[command_list.index("-f") + 1]
     new_file_path = os.path.join(destination, file_name)
     with open(new_file_path, "a") as new_file:
         new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
@@ -21,14 +24,16 @@ def work_with_file(command_list: list[str]) -> None:
         new_file.write("\n")
 
 
-if __name__ == "__main__":
-    command = sys.argv
-
+def terminal_command(command: list[str]) -> None:
     try:
         work_with_folder(command)
     except ValueError:
-        destination = ""
+        pass
     try:
         work_with_file(command)
     except ValueError:
         pass
+
+
+if __name__ == "__main__":
+    terminal_command(sys.argv)
