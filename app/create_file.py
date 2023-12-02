dd80d7c54f7a59ba038ad806092e3c5e4605ad6b
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2023-12-02 23:56:22 +0200

    fix flake

--- app/create_file.py
+++ app/create_file.py
@@ -2,20 +2,19 @@ import os
 
 import argparse
 
-import sys
-
 from datetime import datetime
+from typing import Any
 
 
-def create_directory(path_parts):
+def create_directory(path_parts: str) -> Any:
     directory_path = os.path.join(*path_parts)
     os.makedirs(directory_path, exist_ok=True)
     return directory_path
 
 
-def create_file(file_name) -> None:
+def create_file(file_name: str) -> None:
 
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(file_name, "a") as f:
         f.write(str(current_time))
     with open(file_name, "a") as f:
@@ -26,10 +25,10 @@ def create_file(file_name) -> None:
             f.write("\n".join(content))
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory", nargs="+")
-    parser.add_argument("-f", "--filename")#args.filename
+    parser.add_argument("-f", "--filename")
     args = parser.parse_args()
     directory_path = ""
     if args.directory:
