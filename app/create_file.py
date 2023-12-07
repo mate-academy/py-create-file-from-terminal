be9e562c56f9519ae042e2002d5c4f776090b569
MykolaMazhula <mykm3ua@gmail.com>
2023-12-07 18:14:34 +0100

    edited

--- app/create_file.py
+++ app/create_file.py
@@ -7,7 +7,6 @@ def create_content(file_name: str) -> None:
     if os.path.exists(file_name):
         with (open(file_name, "a") as file):
             file.write("\n\n")
-            # the following write deletes 1 '\n', and i don't know why
 
     content = ""
     line_number = 1
@@ -19,7 +18,6 @@ def create_content(file_name: str) -> None:
         content += str(line_number) + " " + line + "\n"
         line_number += 1
 
-    # stop can be the first input
     if content:
         with (open(file_name, "a") as file):
             content = datetime.now().strftime(
@@ -30,12 +28,12 @@ def create_content(file_name: str) -> None:
 if __name__ == "__main__":
     arguments = sys.argv
 
-    """the order of entering parameters "-d" and "-f" does not matter"""
     f_index = d_index = None
     if "-d" in arguments:
         d_index = arguments.index("-d")
     if "-f" in arguments:
         f_index = arguments.index("-f")
+
     if f_index and d_index:
         if arguments[1] == "-f":
             f_command = arguments[2:d_index]
@@ -43,20 +41,15 @@ if __name__ == "__main__":
         else:
             d_command = os.path.join(*arguments[2:f_index])
             f_command = arguments[f_index + 1:]
-        # <-f file1 file2> raises an error
         if len(f_command) > 1:
             raise SystemExit(
-                "use '-f file_name' format to create and write to a file")
-        if not os.path.exists(d_command):
-            os.makedirs(d_command)
+                "use '-f file_name' format to create and write to a file"
+            )
+        os.makedirs(d_command, exist_ok=True)
         create_content(os.path.join(d_command, arguments[f_index + 1]))
     elif d_index:
         d_command = os.path.join(*arguments[2:])
-        if not os.path.exists(d_command):
-            os.makedirs(d_command)
-        else:
-            raise SystemExit(
-                f"the directory already exists({d_command})")
+        os.makedirs(d_command, exist_ok=True)
     else:
         if len(arguments) > 3:
             raise SystemExit(
