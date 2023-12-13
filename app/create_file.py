971535923ccf5c3869f133a1e5411dd99aa2bc23
Даня Пономаренко <you@example.com>
2023-12-13 13:16:20 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,38 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+def create_file(path: str) -> None:
+    with open(path, "a") as file:
+        file.write(str(datetime.now().
+                       strftime("%Y-%m-%d %H:%M:%S")) + "\n")
+        line = 0
+        while True:
+            content = input("Enter content line: ")
+            line += 1
+            if content == "stop":
+                break
+            file.write(f"{line} " + content + "\n")
+        file.write("\n")
+
+
+def finding_path() -> None:
+    cwd = sys.argv
+    to_dir = ""
+
+    if cwd[1] == "-d":
+        if "-f" not in cwd:
+            to_dir = os.path.join(*cwd[2:])
+        else:
+            to_dir = os.path.join(*cwd[cwd.index("-d") + 1:cwd.index("-f")])
+
+        os.makedirs(to_dir, exist_ok=True)
+
+    if "-f" in cwd:
+        to_file = os.path.join(to_dir, cwd[cwd.index("-f") + 1])
+        create_file(to_file)
+
+
+if __name__ == "__main__":
+    finding_path()
