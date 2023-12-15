e8a15373bd6f00834e01be0bda9f5b8e1d6f78f7
Liliya <liliyakhu@ukr.net>
2023-12-15 21:06:00 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -13,7 +13,6 @@ if "-f" in sys.argv:
         # to_join_all = os.path.join(parent_dir, to_join)
         os.makedirs(to_join, exist_ok=True)
         file_name = os.path.join(to_join, file_name)
-
     with open(file_name, "a") as file:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{timestamp}\n")
@@ -24,7 +23,6 @@ if "-f" in sys.argv:
                 break
             file.write(f"{line_num} {line}\n")
             line_num += 1
-
 if "-d" in sys.argv and "-f" not in sys.argv:
     directories_list = sys.argv[2:-2]
     to_join = os.path.sep.join(directories_list)
