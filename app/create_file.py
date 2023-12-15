262f78ef535108fb85e34749c1a7fe54f9daa040
Alex Moon <moon.ra.lex@gmail.com>
2023-12-15 01:30:48 +0100

    Done

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,30 @@
-# write your code here
+import argparse
+import os
+
+
+def append_file_line(full_path_plus_filename: str) -> None:
+    with open(full_path_plus_filename, "a") as file:
+        while True:
+            input_line = input("Enter content line: ")
+            if input_line == "stop":
+                break
+            file.write(input_line + "\n")
+
+
+def create_file() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", nargs='*', action="store", dest="file_directory")
+    parser.add_argument("-f", "--filename", action="store", dest="file_name")
+    args = parser.parse_args()
+
+    full_path = ""
+    if args.file_directory:
+        full_path = os.path.join(*args.file_directory)
+        os.makedirs(full_path, exist_ok=True)
+    if args.file_name:
+        full_path_plus_filename = os.path.join(full_path, args.file_name)
+        append_file_line(full_path_plus_filename)
+
+
+if __name__ == "__main__":
+    create_file()
