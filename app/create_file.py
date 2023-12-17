fc780f26f5f6b4cefb962ba3b13e8e0589cb658a
Liliya <liliyakhu@ukr.net>
2023-12-17 18:30:09 +0200

    Solution with absolute paths and using functions

--- app/create_file.py
+++ app/create_file.py
@@ -2,28 +2,41 @@ import sys
 import os
 import datetime
 
-parent_dir = os.getcwd()
 
-
-if "-f" in sys.argv:
-    file_name = sys.argv[-1]
-    if "-d" in sys.argv:
-        directories_list = sys.argv[2:-2]
-        to_join = os.path.sep.join(directories_list)
-        # to_join_all = os.path.join(parent_dir, to_join)
-        os.makedirs(to_join, exist_ok=True)
-        file_name = os.path.join(to_join, file_name)
-    with open(file_name, "a") as file:
+def write_in_file(file_path: str) -> None:
+    with open(file_path, "a") as file:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{timestamp}\n")
         line_num = 1
         while True:
             line = input("Enter content line: ")
             if line == "stop":
+                file.write("\n")
                 break
             file.write(f"{line_num} {line}\n")
             line_num += 1
-if "-d" in sys.argv and "-f" not in sys.argv:
-    directories_list = sys.argv[2:-2]
-    to_join = os.path.sep.join(directories_list)
-    os.makedirs(to_join, exist_ok=True)
+
+
+def find_path_and_create_file() -> None:
+    command = sys.argv
+    parent_dir = os.getcwd()
+
+    if "-f" in command:
+        file_path = command[command.index("-f") + 1]
+        if "-d" in command:
+            directories_list = command[2:-2] \
+                if command.index("-d") == 1 \
+                else command[command.index("-d") + 1:]
+            to_join = os.path.sep.join(directories_list)
+            to_join_all = os.path.join(parent_dir, to_join)
+            os.makedirs(to_join_all, exist_ok=True)
+            file_path = os.path.join(to_join_all, file_path)
+        write_in_file(file_path)
+    if "-d" in command and "-f" not in command:
+        directories_list = command[2:]
+        to_join = os.path.sep.join(directories_list)
+        to_join_all = os.path.join(parent_dir, to_join)
+        os.makedirs(to_join_all, exist_ok=True)
+
+
+find_path_and_create_file()
