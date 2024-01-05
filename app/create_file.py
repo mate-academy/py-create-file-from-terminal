76dd9733f1f396598884d97400e854ac94991d83
Svitlana Savikhina <savihina111@gmail.com>
2024-01-05 18:22:23 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -5,28 +5,38 @@ import os
 
 def create_file() -> None:
     current_time = datetime.datetime.now()
-    if "-d" in sys.argv:
-        index_d = sys.argv.index("-d")
-        directory_path = sys.argv[index_d + 1:]
-        directory_path = os.path.join(*directory_path)
+    directory_path = None
+    output_filename = None
+    is_directory_flag = False
+    i = 1
+    while i < len(sys.argv):
+        if sys.argv[i] == "-d":
+            is_directory_flag = True
+        elif sys.argv[i] == "-f":
+            is_directory_flag = False
+        else:
+            if is_directory_flag:
+                directory_path = os.path.join(directory_path or '', sys.argv[i])
+            else:
+                output_filename = sys.argv[i]
+        i += 1
+
+    if directory_path:
         os.makedirs(directory_path, exist_ok=True)
 
-    if "-f" in sys.argv:
-        index_f = sys.argv.index("-f")
-        output_filename = sys.argv[index_f + 1]
+    if output_filename:
+        file_path = os.path.join(directory_path or '.', output_filename)
+        with open(file_path, "a") as file:
+            file.write(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+            count_lines = 1
 
-        if output_filename:
-            with open(output_filename, "a") as file:
-                file.write(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
-                count_lines = 1
-
-                while True:
-                    user_typing = input("Enter content line:")
-                    if user_typing == "stop":
-                        file.write("\n")
-                        break
-                    file.write(f"{count_lines} {user_typing}" + "\n")
-                    count_lines += 1
+            while True:
+                user_input = input("Enter content line:")
+                if user_input.lower() == "stop":
+                    file.write("\n")
+                    break
+                file.write(f"{count_lines} {user_input}\n")
+                count_lines += 1
 
 
 create_file()
