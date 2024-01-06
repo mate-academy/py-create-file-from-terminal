379cc7899ed770036f857f950a99a611349d7d0d
Andrii Homeniuk <andreygomenuk@gmail.com>
2024-01-06 16:42:54 +0200

    Added the ability to create more than two directories: - with -d and -f command flag - with -d command flag

--- app/create_file.py
+++ app/create_file.py
@@ -3,8 +3,7 @@ from datetime import datetime
 from sys import argv
 
 
-def create_directory(parent_dir: str, child_dir: str) -> None:
-    directory_path = os.path.join(parent_dir, child_dir)
+def create_directory(directory_path: str) -> None:
     if not os.path.isdir(directory_path):
         os.makedirs(directory_path)
 
@@ -26,20 +25,36 @@ def create_file_with_user_input(file_path: str) -> None:
 
 
 def process_command_line_args(args: list) -> None:
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    target_path = os.path.join(script_dir, args[2])
+    current_script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    if "-d" in args and "-f" in args and len(args) == 6:
-        create_directory(target_path, args[3])
+    if "-d" in args and "-f" in args:
+        directory_args = [
+            args[x] for x in range(args.index("-d") + 1, args.index("-f"))
+        ]
+        target_directory_path = os.path.join(
+            current_script_dir,
+            *directory_args
+        )
+        create_directory(target_directory_path)
 
-        file_path = os.path.join(target_path, args[3], args[5])
+        file_name = args[-1]
+        file_path = os.path.join(target_directory_path, file_name)
         create_file_with_user_input(file_path)
 
-    elif "-d" in args and len(args) == 4:
-        create_directory(target_path, args[3])
+    elif "-d" in args:
+        directory_args = [
+            args[x] for x in range(args.index("-d") + 1, len(args))
+        ]
+        target_directory_path = os.path.join(
+            current_script_dir,
+            *directory_args
+        )
+        create_directory(target_directory_path)
 
-    elif "-f" in args and len(args) == 3:
-        create_file_with_user_input(target_path)
+    elif "-f" in args:
+        file_name = args[2]
+        target_file_path = os.path.join(current_script_dir, file_name)
+        create_file_with_user_input(target_file_path)
 
 
 command_from_terminal = argv
