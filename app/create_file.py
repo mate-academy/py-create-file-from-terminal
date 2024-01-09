0efed2ca65f96b4747dd070224a8d5dda4a4118d
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-01-09 18:25:46 +0100

    rewrote code in more functional way

--- app/create_file.py
+++ app/create_file.py
@@ -3,58 +3,54 @@ import sys
 from datetime import datetime
 
 
-def create_directory(dir_path: str) -> None:
-    os.makedirs(dir_path)
+def process_terminal_input(terminal_input: list) -> str | None:
+    if "-d" in terminal_input and "-f" in terminal_input:
+        index_d = terminal_input.index("-d")
+        index_f = terminal_input.index("-f")
+        dir_path = os.path.join(*terminal_input[(index_d + 1):index_f])
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        file_name = terminal_input[index_f + 1]
+        return os.path.join(dir_path, file_name)
+
+    if "-d" in terminal_input:
+        index = terminal_input.index("-d")
+        dir_path = os.path.join(*terminal_input[(index + 1):])
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+    if "-f" in terminal_input:
+        index = terminal_input.index("-f")
+        file_name = terminal_input[index + 1]
+        return file_name
 
 
-def create_file(file_path_and_name: str) -> None:
-    with open(f"{file_path_and_name}", "a") as new_file:
+def create_file_or_write_into_it(file_path: str):
+    with open(f"{file_path}", "a") as new_file:
         (
             new_file.write(
                 datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
             )
-            if os.path.getsize(file_path_and_name) == 0
+            if os.path.getsize(file_path) == 0
             else new_file.write(
                 "\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
             )
         )
-
-        line = 0
+        nr_of_line = 0
         while True:
-            line += 1
+            nr_of_line += 1
             content = input("Enter content line: ")
-            if content == "stop":
+            if content.lower() == "stop":
                 break
-            new_file.write(f"{line} " + content + "\n")
+            new_file.write(f"{nr_of_line} {content}\n")
 
 
-def create_directory_and_file(
-        directory: str,
-        file_name: str
-) -> None:
-    os.makedirs(directory)
-    dir_and_file_path = os.path.join(directory, file_name)
-    create_file(dir_and_file_path)
+def main() -> None:
+    commands = sys.argv
+    process_terminal = process_terminal_input(commands)
+    if process_terminal is not None:
+        create_file_or_write_into_it(process_terminal)
 
 
-commands = sys.argv
-
-if "-d" in commands and "-f" in commands:
-    index_d = commands.index("-d")
-    index_f = commands.index("-f")
-    directories = os.path.join(*commands[(index_d + 1):index_f])
-    file_name = commands[index_f + 1]
-
-    create_directory_and_file(directories, file_name)
-
-elif "-d" in commands:
-    index = commands.index("-d")
-    directories = os.path.join(*commands[(index + 1):])
-
-    create_directory(directories)
-
-elif "-f" in commands:
-    index = commands.index("-f")
-    file_path_and_name = commands[index + 1]
-
-    create_file(file_path_and_name)
+if __name__ == "__main__":
+    main()
