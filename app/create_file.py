3ee73d0f2340e41430887d9e98f8036d4c71a4c7
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-01-11 00:19:53 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,30 @@
-# write your code here
+import os
+import sys
+import datetime
+
+
+def script() -> None:
+    args = sys.argv[1:]
+    path = ""
+    file_name = ""
+    if "-f" in args:
+        file_name = args[args.index("-f") + 1]
+        args = args[:-2]
+    if "-d" in args:
+        args = args[args.index("-d") + 1:]
+        path = os.path.join(*args)
+        os.makedirs(path, exist_ok=True)
+
+    with open(path + "\\" + file_name, "w") as file:
+        counter = 1
+        file.write(datetime.strftime()(datetime.now))
+        file.write()
+        input_text = input("Enter content line: ")
+        while input_text != "stop":
+            file.write(str(counter) + " " + input_text + "\n")
+            input_text = input("Enter content line: ")
+            counter += 1
+
+
+if __name__ == "__main__":
+    script()
