3cbd6463c79d892577762ca8e7ff2e60de2f6e39
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-01-22 01:40:15 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,47 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_dir(args: list) -> str:
+    path = ""
+
+    for arg in args:
+        path += arg + "/"
+
+    os.makedirs(path, exist_ok=True)
+
+    return path
+
+
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as file:
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+
+        line_counter = 1
+
+        while True:
+            line = input("Enter content line: ")
+
+            if line == "stop":
+                file.write("\n")
+                break
+
+            file.write(f"{line_counter} {line}\n")
+            line_counter += 1
+
+
+if __name__ == "__main__":
+    if "-d" in sys.argv and "-f" in sys.argv:
+        d_index = sys.argv.index("-d")
+        f_index = sys.argv.index("-f")
+
+        dir_path = create_dir(sys.argv[d_index + 1: f_index])
+        file_path = os.path.join(dir_path, sys.argv[f_index + 1])
+        create_file(file_path)
+
+    elif "-d" in sys.argv:
+        create_dir(sys.argv[sys.argv.index("-d") + 1: len(sys.argv)])
+
+    elif "-f" in sys.argv:
+        create_file(sys.argv[sys.argv.index("-f") + 1])
