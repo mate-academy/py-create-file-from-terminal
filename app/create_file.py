4afa531ab0e4ff4b3d13c1e17c397580d76c2831
Yura Push <youdeimos1@gmail.com>
2024-01-23 14:08:15 +0200

    -added create_path_regard_status

--- app/create_file.py
+++ app/create_file.py
@@ -19,7 +19,22 @@ def write_content_to_file(filepath: str, mode: str) -> None:
             file_in.write(f"{counter} {content}\n")
 
 
-# filename = sys.argv[-1]
+def create_path_regard_status(*flag_status: bool) -> str:
+    f_flag_status, d_flag_status = flag_status
+    path = ""
+    if all(flag_status):
+        first_dir = sys.argv.index("-d") + 1
+        last_dir = sys.argv.index("-f")
+        directories = sys.argv[first_dir:last_dir]
+        path = os.path.join(*directories)
+
+    if d_flag_status and not f_flag_status:
+        directories = sys.argv[sys.argv.index("-d") + 1:]
+        path = os.path.join(*directories)
+
+    return path
+
+
 flag_f = "-f" in sys.argv
 flag_d = "-d" in sys.argv
 
@@ -30,13 +45,10 @@ if flag_f and not flag_d:
     else:
         write_content_to_file(filename, "a")
 if flag_d and not flag_f:
-    dir_names = "/".join(sys.argv[sys.argv.index("-d") + 1:])
-    new_path = os.path.abspath(dir_names)
+    new_path = create_path_regard_status(flag_f, flag_d)
     os.makedirs(new_path, exist_ok=True)
 if flag_f and flag_d:
-    first_dir_index = sys.argv.index("-d") + 1
-    last_dir_index = sys.argv.index("-f")
-    dir_names = "/".join(sys.argv[first_dir_index:last_dir_index])
-    new_path = os.path.abspath(dir_names)
+    new_path = create_path_regard_status(flag_f, flag_d)
     os.makedirs(new_path, exist_ok=True)
-    write_content_to_file(new_path + sys.argv[-1], "w")
+    path_to_file = os.path.join(new_path, sys.argv[-1])
+    write_content_to_file(path_to_file, "w")
