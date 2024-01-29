34df66aae95183a5ec0f0b79220be633445ceafc
Mykhailo Slavsky <slavskij2017@gmail.com>
2024-01-29 21:38:57 +0200

    add lines numeric and fix bug

--- app/create_file.py
+++ app/create_file.py
@@ -2,25 +2,38 @@ import os
 import sys
 from datetime import datetime
 
-
-command = sys.argv
+commands = sys.argv
 dir_path = ""
 
-if "-d" in command:
+print(commands)
+
+
+def make_dir() -> None:
 
     try:
-        directories = command[2:command.index("-f")]
+        directories = commands[commands.index("-d") + 1:commands.index("-f")]
     except ValueError:
-        directories = command[2:]
+        directories = commands[commands.index("-d") + 1:]
+    if not directories:
+        directories = commands[commands.index("-d") + 1:]
 
     dir_path = os.path.join(*directories)
     os.makedirs(dir_path, exist_ok=True)
 
 
-if "-f" in command:
-    with open(os.path.join(dir_path, command[-1]), "w") as f:
+def make_file() -> None:
+    with open(os.path.join(dir_path, commands[commands.index("-f") + 1]), "w") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-        line = input("Enter content line:")
-        while line != "stop":
-            f.write(line + "\n")
-            line = input("Enter content line:")
+        content = input("Enter content line:")
+        line_counter = 1
+        while content != "stop":
+            f.write(f"{line_counter} {content}\n")
+            content = input("Enter content line:")
+            line_counter += 1
+
+
+for command in commands:
+    if command == "-d":
+        make_dir()
+    elif command == "-f":
+        make_file()
