78e9e7cde610bf8ea2ec54994406efdad083ecde
Mykola <mykola22dubyna@gmail.com>
2024-01-30 21:50:53 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,54 @@
-# write your code here
+import os
+import sys
+
+from datetime import datetime
+
+
+def unpack_argv() -> tuple:
+    directory_path_structure = []
+    file_name = ""
+    flag_dir = False
+    flag_name = False
+    for arg in sys.argv:
+        if "-d" == arg:
+            flag_dir = True
+            flag_name = False
+            continue
+        if "-f" == arg:
+            flag_name = True
+            flag_dir = False
+            continue
+        if flag_dir:
+            directory_path_structure.append(arg)
+        if flag_name:
+            file_name += arg
+    return directory_path_structure, file_name
+
+
+def creating_path() -> tuple:
+    dir_path_components, file_name = unpack_argv()
+    dir_path = os.path.join(os.getcwd(), *dir_path_components)
+    f_path = os.path.join(dir_path, file_name)
+    return dir_path, f_path
+
+
+def creation_file(content_file_path: str) -> None:
+    with open(content_file_path, "a") as result_file:
+        result_file.write(
+            f"{str(datetime.now().strftime('%y-%m-%d %H:%M:%S'))}\n"
+        )
+        line_number = 1
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            result_file.write(f"{line_number} {line}\n")
+            line_number += 1
+
+
+directory_path, file_path = creating_path()
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
+
+if directory_path != file_path[:-1]:
+    creation_file(file_path)
