45c5012ae7007ab358a6e1eec90ea2e31b235b53
Nataliia Korzh <clarioruberia@gmail.com>
2024-02-01 13:43:36 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,20 +3,19 @@ import sys
 from datetime import datetime
 
 
-print("Enter: ", sys.argv)
-
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-def create_path(directories: list) -> str:
+def create_path(directories: list[str]) -> str:
     path = os.path.join(*directories)
     return path
 
 
 def create_directory(directory_path: str) -> None:
-    if not os.path.exists(directory_path):
+    try:
         os.makedirs(directory_path)
-    print("Directory exists already!")
+    except OSError:
+        print("Directory exists already!")
 
 
 def create_file(file_name: str) -> None:
@@ -33,17 +32,26 @@ def create_file(file_name: str) -> None:
             line_num += 1
 
 
-def main():
-    if "-d" in sys.argv:
+def main() -> None:
+
+    if "-d" in sys.argv and "-f" not in sys.argv:
         dir_index = sys.argv.index("-d")
         directory_path = os.path.join(*sys.argv[dir_index + 1:])
         create_directory(directory_path)
 
-    if "-f" in sys.argv:
+    if "-f" in sys.argv and "-d" not in sys.argv:
         file_index = sys.argv.index("-f")
         file_name = sys.argv[file_index + 1]
         create_file(file_name)
 
+    if "-f" in sys.argv and "-d" in sys.argv:
+        dir_index = sys.argv.index("-d")
+        file_index = sys.argv.index("-f")
+        file_path = create_path(sys.argv[dir_index + 1:file_index])
+        create_directory(file_path)
+        path_to_file = file_path + "/" + sys.argv[-1]
+        create_file(path_to_file)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
