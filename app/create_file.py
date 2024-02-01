ab2133aab785d953d06de8b207dc3be464cc769f
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-02-01 22:49:24 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 def create_folder(folders: list[str]) -> str:
     path = os.path.join(*folders)
-    if not os.path.exists(path):
-        os.makedirs(path)
+    os.makedirs(path, exist_ok=True)
     return path
 
 
 def create_file(file_name: str) -> None:
+
     with open(file_name, "a") as file:
         file.write(datetime.today().strftime("%Y-%m-%d %H:%M:%S") + "\n")
         line = 1
@@ -19,9 +19,8 @@ def create_file(file_name: str) -> None:
             user_input = input("Enter content line: ")
             if user_input == "stop":
                 break
-            file.write(f"{line} Line{line} {user_input}" + "\n")
-
-        file.write("\n")
+            file.write(f"{line} {user_input}\n")
+            line += 1
 
 
 def main() -> None:
@@ -30,23 +29,22 @@ def main() -> None:
     flag_f = args.index("-f") if "-f" in args else None
     flag_d = args.index("-d") if "-d" in args else None
 
-    if flag_f and not flag_d:
+    if flag_d and flag_f:
+        dirs = (
+            args[flag_d + 1: flag_f]
+            if flag_f > flag_d
+            else args[flag_d + 1:]
+        )
+        path = os.path.join(create_folder(dirs), args[flag_f + 1])
+        create_file(path)
+
+    elif flag_f:
         file_name = args[flag_f + 1]
         create_file(file_name)
 
-    elif flag_d:
-        if not flag_f:
-            dirs = args[flag_d + 1:]
-            create_folder(dirs)
-
-        else:
-            dirs = (
-                args[flag_d + 1: flag_f]
-                if flag_f > flag_d
-                else args[flag_d + 1:]
-            )
-            path = os.path.join(create_folder(dirs), args[flag_f + 1])
-            create_file(path)
+    else:
+        dirs = args[flag_d + 1:]
+        create_folder(dirs)
 
 
 if __name__ == "__main__":
