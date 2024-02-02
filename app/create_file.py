6dfb4785f768d7e884330b7333d82fe02a4a0764
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-02-02 13:47:39 +0200

    fixed review changes

--- app/create_file.py
+++ app/create_file.py
@@ -8,9 +8,10 @@ def create_directory(directory_name: str) -> None:
 
 
 def create_file(file_name: str) -> None:
-    mode = "a" if os.path.exists(file_name) else "w"
 
-    with open(file_name, mode) as file:
+    with open(file_name, "a") as file:
+        if os.stat(file_name).st_size > 0:
+            file.write("\n")
         create_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{create_time}\n")
 
@@ -24,15 +25,25 @@ def create_file(file_name: str) -> None:
 
 
 def main() -> None:
+    if "-d" in sys.argv and "-f" in sys.argv:
+        d_index = sys.argv.index("-d")
+        f_index = sys.argv.index("-f")
+
+        if f_index < d_index:
+            raise Exception("Error: Please use -d before -f.")
+
     if "-d" in sys.argv:
-        base_directory = sys.argv[sys.argv.index("-d") + 1]
-        dir2_path = os.path.join(base_directory, "dir2")
-        create_directory(dir2_path)
+        base_directory = (
+            sys.argv[sys.argv.index("-d") + 1:sys.argv.index("-f")]
+            if "-f" in sys.argv
+            else sys.argv[sys.argv.index("-d"):])
+        path = os.path.join(*base_directory)
+        create_directory(path)
 
-        if "-f" in sys.argv:
-            file_name = os.path.join(dir2_path, sys.argv[-1])
-            create_file(file_name)
-
+    if "-f" in sys.argv:
+        file_name = sys.argv[-1]
+        os.chdir(path)
+        create_file(file_name)
 
 if __name__ == "__main__":
     main()
