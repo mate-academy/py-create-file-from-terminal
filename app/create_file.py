45081373a8f1fca35ddf441b11c22a0d841906eb
Kyrylo Storozhenko <busterakel@gmail.com>
2024-02-03 13:02:44 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -21,7 +21,9 @@ def create_file(file_path: str, lines: list) -> None:
     with open(file_path, "a") as f:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"{timestamp}\n")
-        start = sum(1 for line in open(file_path)) if os.path.exists(file_path) else 0
+        start = (sum(1 for line in open(file_path))
+                 if os.path.exists(file_path)
+                 else 0)
         for i, line in enumerate(lines, start=start + 1):
             f.write(f"{i} {line}\n")
 
