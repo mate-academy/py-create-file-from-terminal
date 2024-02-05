b22c5a45be87d9d370e2d67035755910683d2fd8
Yevhen <evgeniy.playid@icloud.com>
2024-02-05 19:35:28 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,35 +1,38 @@
-import os.path
+import os
 from datetime import datetime
-from sys import argv
-
-command = argv
-command_list = command.split()
+import argparse
 
 
-def management_by_terminal() -> None:
-    if "-d" in command and "-f" not in command:
-        create_dir()
-    elif "-f" in command and "-d" not in command:
-        create_file()
-    elif "-f" in command and "-d" in command:
-        create_dir()
-        create_file()
+def create_file(file_path):
+    time_now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(file_path, "a") as file:
+        file.write(f"{time_now_str}\n")
+        line_count = 1
+        while True:
+            line = input()
+            if line.lower() == "stop":
+                break
+            file.write(f"{line_count} {line}\n")
+            line_count += 1
 
 
-def create_file() -> None:
-    if "-f" in command_list:
-        index = command_list.index("-f")
-        file_name = command_list[index + 1]
-        path_file = os.path.join(*file_name.split("/"))
-        time_now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-        with open(path_file, "a") as file:
-            file.write(f"\n\n{time_now_str}")
+def create_dir(directory_path):
+    os.makedirs(directory_path, exist_ok=True)
 
 
-def create_dir() -> None:
-    if "-d" in command_list:
-        index = command_list.index("-d")
-        path_dirs = command_list[index + 1:]
-        path_dir = os.path.join(*path_dirs)
-        os.makedirs(path_dir, exist_ok=True)
+def management_by_terminal():
+    parser = argparse.ArgumentParser(description="Create directory or file with content.")
+    parser.add_argument("-d", nargs="+", help="Create directories")
+    parser.add_argument("-f", help="Create or update file")
+
+    args = parser.parse_args()
+
+    if args.d:
+        create_dir(os.path.join(*args.d))
+
+    if args.f:
+        create_file(args.f)
+
+
+if __name__ == "__main__":
+    management_by_terminal()
