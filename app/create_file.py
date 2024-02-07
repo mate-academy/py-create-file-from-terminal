c035f11e1ea1e586edadd993e44eba5048cf2c47
Maksym Operchuk <operlast529@gmail.com>
2024-02-07 09:37:13 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -5,27 +5,28 @@ import datetime
 import os
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument("-d", nargs="+")
 
-parser.add_argument("-f", nargs="+")
+parser.add_argument("-f")
 
 args = parser.parse_args()
 
 
-def create_path(directories: list) -> str:
-    path = os.path.join(*directories)
+def create_path(path: list[str]) -> str:
+    path = os.path.join(*path)
     return path
 
 
-def make_dirs(way: str) -> None:
-    os.makedirs(way, exist_ok=True)
+def make_dirs(dirs_path: str) -> None:
+    os.makedirs(dirs_path, exist_ok=True)
 
 
-def create_and_write_in_file(file_name: str) -> None:
+def create_and_write_in_file(file_path: str) -> None:
     current_datetime = datetime.datetime.now()
     formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-    with open(file_name, "a") as file:
+    with open(file_path, "a") as file:
         file.write(f"{formatted_datetime}\n")
         number_of_line = 1
 
@@ -44,18 +45,18 @@ def create_file() -> None:
     if args.d and args.f:
         make_dirs(create_path(args.d))
 
-        path_to_create_file = args.d + args.f
+        path_to_create_file = [*args.d, args.f]
 
         create_and_write_in_file(create_path(path_to_create_file))
 
-        return None
+        return
 
     if args.d:
         dears_to_create = create_path(args.d)
         make_dirs(dears_to_create)
 
     if args.f:
-        file_name = args.f[0]
+        file_name = args.f
         create_and_write_in_file(os.path.abspath(file_name))
 
 
