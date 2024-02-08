d1a4e191cf16746bc64fdd9c60103ac54976e614
Artem Milchenko <pl301078map2@gmail.com>
2024-02-08 20:58:45 +0200

    solution rewrite code using function style

--- app/create_file.py
+++ app/create_file.py
@@ -1,25 +1,50 @@
-import sys
 import os
+import sys
 from datetime import datetime
 
 
-com_line = sys.argv
-if "-d" in com_line:
-    dirs = com_line[com_line.index("-d") + 1:]
-    if "-f" in com_line:
-        dirs = com_line[com_line.index("-d") + 1:com_line.index("-f")]
-    path = os.path.join(*dirs)
-    os.makedirs(path, exist_ok=True)
-if "-f" in com_line:
-    path = ""
+def get_file_content() -> str:
     text = ""
     line = 1
     while True:
-        content = input("Enter content line:")
+        content = input("Enter content line: ")
         if content.lower() == "stop":
             break
         text += f"{line} {content}\n"
         line += 1
-    with open(os.path.join(path, sys.argv[-1]), "a") as file:
-        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{time}\n{text}")
+    return text
+
+
+def create_file(file_name: str, text: str) -> None:
+    with open(file_name, "a") as file_out:
+        time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file_out.write(f"{time_now}\n{text}")
+
+
+def create_dirs(path: str) -> None:
+    os.makedirs(path, exist_ok=True)
+
+
+def create_dirs_and_file(path: str,
+                         file_name: str,
+                         text: str) -> None:
+    os.makedirs(path, exist_ok=True)
+    create_file(os.path.join(path, file_name), text)
+
+
+def main() -> None:
+    com_line = sys.argv
+    if "-d" in com_line and "-f" in com_line:
+        dirs = com_line[com_line.index("-d") + 1:com_line.index("-f")]
+        path = os.path.join(*dirs)
+        create_dirs_and_file(path, com_line[-1], get_file_content())
+    elif "-f" in com_line:
+        create_file(com_line[-1], get_file_content())
+    elif "-d" in com_line:
+        dirs = com_line[com_line.index("-d") + 1:]
+        path = os.path.join(*dirs)
+        create_dirs(path)
+
+
+if __name__ == "__main__":
+    main()
