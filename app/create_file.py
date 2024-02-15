456cf20dcc5ae912c052af58a7b9be1cd82118d9
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-02-15 16:58:10 +0000

    Code separated into functions

--- app/create_file.py
+++ app/create_file.py
@@ -3,26 +3,33 @@ import sys
 from datetime import datetime
 
 
-arguments = sys.argv
-file_name = ""
-joined_path = ""
+def make_dir(arguments: list[str]) -> os.path:
+    joined_path = ""
+    if "-d" in arguments:
+        path = []
+        for argument in arguments[2:]:
+            if argument == "-f":
+                break
+            path.append(argument)
+        joined_path = os.path.join(*path)
+        if not os.path.exists(joined_path):
+            os.makedirs(joined_path)
+    return joined_path
 
-if "-d" in arguments:
-    path = []
-    for argument in arguments[2:]:
-        if argument == "-f":
-            break
-        path.append(argument)
 
-    joined_path = os.path.join(*path)
-    if not os.path.exists(joined_path):
-        os.makedirs(joined_path)
+def file_name(arguments: list[str]) -> str:
+    if "-f" in arguments:
+        return arguments[-1]
+    return ""
 
-if "-f" in arguments:
-    file_name = arguments[-1]
 
-    with open(os.path.join(joined_path, file_name), "a") as file:
-        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+def datetime_string() -> str:
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+
+
+def content_manager(path: os.path, name: str) -> None:
+    with open(os.path.join(path, name), "a") as file:
+        file.write(datetime_string())
         line_number = 1
         while True:
             user_input = input("Enter content line: ")
@@ -31,3 +38,15 @@ if "-f" in arguments:
             file.write(f"{line_number} {user_input}\n")
             line_number += 1
         file.write("\n")
+
+
+def main() -> None:
+    arguments = sys.argv
+    name = file_name(arguments)
+    path = make_dir(arguments)
+    if name:
+        content_manager(path, name)
+
+
+if __name__ == "__main__":
+    main()
