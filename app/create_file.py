106325994e75bb3b90bd8fe5abe951fa54b1dc80
sergey <38cerg12021979@jmal.com>
2024-02-18 08:34:36 +0200

    18_02_24_01

--- app/create_file.py
+++ app/create_file.py
@@ -46,7 +46,6 @@ else:
         directory = os.path.join(*sys.argv[dir_index:])
         create_directory(directory)
 
-
     elif sys.argv[1] == "-f":
         file_index = sys.argv.index("-f") + 1
         file_name = sys.argv[file_index]
