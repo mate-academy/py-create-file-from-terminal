7e4682b99037e41ba513253c2d46bd74bc2aeede
Oleksandr Asauliak <varmen67@gmail.com>
2024-02-18 14:20:25 +0200

    Just like this?

--- app/create_file.py
+++ app/create_file.py
@@ -5,17 +5,13 @@ from datetime import datetime
 
 command = sys.argv
 dir_path = ""
-
-if "-d" in command:
-    directories = (
-        command[2:command.index("-f")]
-        if "-f" in command else command[2:]
-    )
-    dir_path = os.path.join(*directories)
-    os.makedirs(dir_path, exist_ok=True)
+file_path = ""
 
 if "-f" in command:
-    file_path = os.path.join(dir_path, command[-1])
+    file_index = command.index("-f")
+    file_path = os.path.join(*command[file_index + 1:])
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
 
     with open(file_path, "w") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
@@ -23,3 +19,12 @@ if "-f" in command:
         while line != "stop":
             f.write(line + "\n")
             line = input("Enter content line: ")
+
+else:
+    if "-d" in command:
+        directories = (
+            command[2:command.index("-f")]
+            if "-f" in command else command[2:]
+        )
+        dir_path = os.path.join(*directories)
+        os.makedirs(dir_path, exist_ok=True)
