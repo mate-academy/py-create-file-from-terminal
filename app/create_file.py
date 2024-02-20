2ccc9888422fab9b8e462e7bab5c37b3fabe7b2a
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-02-20 19:25:46 +0200

    flake8 edit

--- app/create_file.py
+++ app/create_file.py
@@ -7,6 +7,7 @@ def create_dir(path_parts: list) -> None:
     dir_path = os.path.join(*path_parts)
     os.makedirs(dir_path, exist_ok=True)
 
+
 def create_file(file_name: str) -> None:
     content = []
     while True:
@@ -16,22 +17,29 @@ def create_file(file_name: str) -> None:
         content.append(line)
 
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    content_with_timestamp = [f"{timestamp}\n"] + [f"{i+1} {line}\n" for i, line in enumerate(content)]
+    content_with_timestamp = [
+        f"{timestamp}\n",
+        *[f"{i + 1} {line}\n" for i, line in enumerate(content)]
+    ]
 
     with open(file_name, "a" if os.path.exists(file_name) else "w") as file:
         file.write("\n".join(content_with_timestamp))
 
+
 def main() -> None:
     if len(sys.argv) < 3:
-        raise ValueError("Usage: python create_file.py -d <directory_path_parts> -f <file_name>")
+        raise ValueError("Usage: python create_file.py "
+                         "-d <directory_path_parts> -f <file_name>")
 
     if "-d" in sys.argv:
         dir_index = sys.argv.index("-d") + 1
-        create_dir(sys.argv[dir_index:sys.argv.index('-f')] if '-f' in sys.argv else sys.argv[dir_index:])
+        create_dir(sys.argv[dir_index:sys.argv.index('-f')]
+                   if '-f' in sys.argv else sys.argv[dir_index:])
 
-    if '-f' in sys.argv:
-        file_index = sys.argv.index('-f') + 1
+    if "-f" in sys.argv:
+        file_index = sys.argv.index("-f") + 1
         create_file(sys.argv[file_index])
+
 
 if __name__ == "__main__":
     main()
