307a03e3bb035832e240211aa3364974493edfed
Molchanov Ihor <=>
2024-02-20 13:52:21 +0200

    used standart sys.argv

--- app/create_file.py
+++ app/create_file.py
@@ -1,42 +1,41 @@
 from datetime import datetime
 import os.path
 import sys
-import argparse
 
 
-def create_parse() -> argparse:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--filepath", nargs="+", default=None)
-    parser.add_argument("-f", "--filename", nargs="+", default=None)
-    return parser
+def parse_args(arguments: list, param: str) -> tuple[str | bytes, list]:
+    result = ""
+    if param in arguments:
+        cursor = arguments.index(param)
+        result = os.path.join(*arguments[cursor + 1:])
+        if param == "-f":
+            arguments = arguments[:cursor]
+    return result, arguments
 
 
-def create_folders(folders_path: list) -> str | bytes:
-    if folders_path:
-        path = os.path.join(*folders_path)
-        os.makedirs(path, exist_ok=True)
-        return path
-    return ""
-
-
-def file_handler(file_path: str) -> None:
-    with open(file_path, "a") as file:
-        lines = []
-        line_number = 1
-        while True:
-            text = input("Enter content line: ")
-            if text.lower() == "stop":
-                break
-            lines.append(f"{line_number} {text}\n")
-            line_number += 1
-        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{current_datetime}\n{''.join(lines)}\n")
+def handler_file(file_path: str, file_name: str) -> None:
+    full_file_path = os.path.join(file_path, file_name)
+    if file_path:
+        os.makedirs(file_path, exist_ok=True)
+    if file_name:
+        with open(full_file_path, "a") as file:
+            lines = []
+            line_number = 1
+            while True:
+                text = input("Enter content line: ")
+                if text.lower() == "stop":
+                    break
+                lines.append(f"{line_number} {text}")
+                line_number += 1
+            if os.path.getsize(full_file_path) > 0:
+                file.write("\n\n")
+            current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(f"{current_datetime}\n")
+            file.write("\n".join(lines))
 
 
 if __name__ == "__main__":
-    parser_args = create_parse()
-    args = parser_args.parse_args(sys.argv[1:])
-    folders = create_folders(folders_path=args.filepath)
-    if args.filename:
-        filepath = os.path.join(f"{folders}", *args.filename)
-        file_handler(filepath)
+    args = sys.argv[1:]
+    filename, args = parse_args(args, "-f")
+    filepath, args = parse_args(args, "-d")
+    handler_file(filepath, filename)
