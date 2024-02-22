256a55e0cfbeaa80e37cedd4646f81f9c0e449e8
RomanHlodann <roman.hlodann@gmail.com>
2024-02-22 17:52:41 +0200

    improved code style

--- app/create_file.py
+++ app/create_file.py
@@ -3,25 +3,31 @@ import sys
 import datetime
 
 
-args = sys.argv
-path_to_create_file = ""
+def main() -> None:
+    args = sys.argv
+    create_directories_and_file(args)
 
 
-if "-d" in args:
-    d_index = args.index("-d")
-    if "-f" in args[d_index:]:
-        f_index = args[d_index:].index("-f")
-    else:
-        f_index = len(args[d_index:])
-    path_to_create_file = os.path.join(
-        "",
-        *args[d_index + 1:f_index + d_index]
-    )
-    os.makedirs(path_to_create_file, exist_ok=True)
+def create_directories_and_file(args: list[str]) -> None:
+    path_to_create_file = ""
+    if "-d" in args:
+        d_index = args.index("-d")
+        if "-f" in args[d_index:]:
+            f_index = args[d_index:].index("-f")
+        else:
+            f_index = len(args[d_index:])
+        path_to_create_file = os.path.join(
+            "",
+            *args[d_index + 1:f_index + d_index]
+        )
+        os.makedirs(path_to_create_file, exist_ok=True)
+    if "-f" in args:
+        f_index = args.index("-f")
+        create_file(path_to_create_file, args[f_index + 1])
 
-if "-f" in args:
-    f_index = args.index("-f")
-    path_to_create_file = os.path.join(path_to_create_file, args[f_index + 1])
+
+def create_file(path: str, filename: str) -> None:
+    path_to_create_file = os.path.join(path, filename)
     with open(path_to_create_file, "a+") as file:
         file.seek(0)
         row = 1
@@ -37,3 +43,7 @@ if "-f" in args:
                 break
             file.write(f"\n{row} {content}")
             row += 1
+
+
+if __name__ == "__main__":
+    main()
