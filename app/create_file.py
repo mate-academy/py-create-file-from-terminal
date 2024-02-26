73ddbf7f1f96574747da1040e1f08355d8c771d1
Yuliia Dudych <juliadudych@gmail.com>
2024-02-26 18:05:20 +0200

    Add functions to create directory tree

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+from sys import argv
+
+
+def create_file():
+    file_path = os.getcwd()
+    if "-d" in argv:
+        file_path = os.path.join(file_path, *argv[argv.index("-d") + 1:])
+        create_change_dirs(file_path)
+
+
+def create_change_dirs(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    os.chdir(path)
+
+
+if __name__ == "__main__":
+    create_file()
