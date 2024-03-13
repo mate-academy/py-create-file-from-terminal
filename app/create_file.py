dfbd519a2be4b206aca441f71a5cdeb62f9c0555
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-13 16:30:08 +0100

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -6,9 +6,9 @@ from datetime import datetime
 def create_dir_and_file(
         path: list,
         file_name: str,
-        value: list,
+        value: list
 ) -> None:
-    timestamp = datetime.now().strptime("%Y-%m-%d-%H-%M-%S")
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     line = [f"{i} {text}" for i, text in enumerate(value, start=1)]
 
     if path:
@@ -20,7 +20,7 @@ def create_dir_and_file(
         with open(destination, "a") as file:
             file.write("\n\n" + timestamp + "\n" + "\n".join(line))
     else:
-        with open(path, "w") as file:
+        with open(destination, "w") as file:
             file.write(timestamp + "\n" + "\n".join(line))
 
 
@@ -29,18 +29,21 @@ def create_data_for_file() -> None:
     file_name = ""
     value = []
 
-    for element in sys.argv[1:]:
+    for_index = 0
+    while for_index < len(sys.argv[1:]):
+        element = sys.argv[1:][for_index]
         if element == "-d":
-            for_path = True
+            path = sys.argv[1:][for_index + 1:]
+            break
         elif element == "-f":
-            for_path = False
-        elif for_path:
-            path.append(element)
-        elif file_name == "":
-            file_name = element
+            file_name = sys.argv[1:][for_index + 1]
+            break
+        else:
+            for_index += 1
 
     if path:
         os.makedirs(os.path.join(*path), exist_ok=True)
+
     if file_name:
         while True:
             new_line = input("Enter your message: ")
