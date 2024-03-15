eee23381473295311937418b8363c49505855a58
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-03-15 11:20:03 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,7 +3,21 @@ import os
 from datetime import datetime
 
 
-def create_file_from_terminal() -> None:
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as file:
+        file.write(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")))
+        number_of_line = 1
+        while True:
+            input_line = input("Enter content line:")
+            if input_line == "stop":
+                file.write("\n")
+                break
+
+            file.write(f"{number_of_line} {input_line}\n")
+            number_of_line += 1
+
+
+def find_path_from_terminal() -> None:
     command_line = sys.argv
 
     if "-d" in command_line:
@@ -22,18 +36,8 @@ def create_file_from_terminal() -> None:
         file_name = command_line[command_line.index("-f") + 1]
         if "-d" in command_line:
             file_name = os.path.join(dir_path, file_name)
-        with open(file_name, "a") as file:
-            file.write(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")))
-            number_of_line = 1
-            while True:
-                input_line = input("Enter content line:")
-                if input_line == "stop":
-                    file.write("\n")
-                    break
-
-                file.write(f"{number_of_line} {input_line}\n")
-                number_of_line += 1
+        create_file(file_name)
 
 
 if __name__ == "__main__":
-    create_file_from_terminal()
+    find_path_from_terminal()
