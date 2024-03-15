b60d0f6562ebc59bf7ad36e988b5fb4b11fe748a
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-03-15 11:14:26 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,34 +3,37 @@ import os
 from datetime import datetime
 
 
-command_line = sys.argv
+def create_file_from_terminal() -> None:
+    command_line = sys.argv
 
-if "-d" in command_line:
-    index_d = command_line.index("-d")
-    if "-f" in command_line:
-        index_f = command_line.index("-f")
-        dir_path = command_line[index_d + 1:index_f]
-    else:
-        dir_path = command_line[index_d + 1:]
-
-    dir_path = os.path.join(*dir_path)
-
-    os.makedirs(dir_path, exist_ok=True)
-    print(dir_path)
-
-if "-f" in command_line:
-    file_name = command_line[command_line.index("-f") + 1]
     if "-d" in command_line:
-        file_name = dir_path + "/" + file_name
-    with open(file_name, "a") as file:
-        file.write(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")))
-        number_of_line = 1
-        while True:
-            input_line = input("Enter content line:")
-            if input_line == "stop":
-                file.write("\n")
-                break
+        index_d = command_line.index("-d")
+        if "-f" in command_line:
+            index_f = command_line.index("-f")
+            dir_path = command_line[index_d + 1:index_f]
+        else:
+            dir_path = command_line[index_d + 1:]
 
-            file.write(f"{number_of_line} {input_line}\n")
-            number_of_line += 1
-    print(file_name)
+        dir_path = os.path.join(*dir_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+    if "-f" in command_line:
+        file_name = command_line[command_line.index("-f") + 1]
+        if "-d" in command_line:
+            file_name = os.path.join(dir_path, file_name)
+        with open(file_name, "a") as file:
+            file.write(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")))
+            number_of_line = 1
+            while True:
+                input_line = input("Enter content line:")
+                if input_line == "stop":
+                    file.write("\n")
+                    break
+
+                file.write(f"{number_of_line} {input_line}\n")
+                number_of_line += 1
+
+
+if __name__ == "__main__":
+    create_file_from_terminal()
