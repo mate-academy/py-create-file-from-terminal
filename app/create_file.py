49b84a480aaf61c0949a91c2eed34fc665218bc7
skeletronys <mykytcey@gmail.com>
2024-03-21 22:06:38 +0100

    fix flake8 app/

--- app/create_file.py
+++ app/create_file.py
@@ -3,55 +3,55 @@ from datetime import datetime
 import sys
 
 
-def write_content(file_path):
+def write_content(file_path: str) -> None:
     content = []
     print("Enter content line (type 'stop' to finish):")
     while True:
         line = input("Enter content line: ")
-        if line.lower() == 'stop':
+        if line.lower() == "stop":
             break
         content.append(line)
 
-    with open(file_path, 'a') as file:
+    with open(file_path, "a") as file:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{timestamp}\n")
         for i, line in enumerate(content, 1):
             file.write(f"{i} {line}\n")
 
 
-def create_file(file_name):
+def create_file(file_name: str) -> None:
     if os.path.exists(file_name):
         write_content(file_name)
     else:
-        with open(file_name, 'w'):
+        with open(file_name, "w"):
             write_content(file_name)
 
 
-def create_file_with_directory(directory_path, file_name):
+def create_file_with_directory(directory_path: str, file_name: str) -> None:
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
     file_path = os.path.join(directory_path, file_name)
     create_file(file_path)
 
 
-def main():
+def main() -> None:
     args = sys.argv[1:]
     if not args:
         print("Usage: python create_file.py"
               " [-d directory_path] [-f file_name]")
         return
 
-    if '-d' in args:
-        directory_index = args.index('-d') + 1
+    if "-d" in args:
+        directory_index = args.index("-d") + 1
         directory_path = os.path.join(*args[directory_index:])
-        if '-f' in args:
-            file_index = args.index('-f') + 1
+        if "-f" in args:
+            file_index = args.index("-f") + 1
             file_name = args[file_index]
             create_file_with_directory(directory_path, file_name)
         else:
             os.makedirs(directory_path)
-    elif '-f' in args:
-        file_index = args.index('-f') + 1
+    elif "-f" in args:
+        file_index = args.index("-f") + 1
         file_name = args[file_index]
         create_file(file_name)
     else:
