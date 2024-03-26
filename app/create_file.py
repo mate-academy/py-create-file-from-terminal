3fbb53a8eeee5ce211fd81da22cace8ec519d99e
TarasFirst <firsttaras@mail.com>
2024-03-26 08:12:30 +0200

    Solution_GTA_3

--- app/create_file.py
+++ app/create_file.py
@@ -12,32 +12,36 @@ def add_content_to_file(file_path: str) -> None:
     with open(os.path.join(*file_path), "a") as file:
         file.write(
             datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            + "\n"
         )
+        string_number = 1
         while True:
-            content_line = input("Enter content line:")
+            content_line = input("Enter content line: ")
             if content_line == "stop":
-                file.write("\n")
+                if string_number > 1:
+                    file.write("\n")
                 break
-            string_number = 1
             file.write(f"{str(string_number)} {content_line}\n")
             string_number += 1
 
 
 if __name__ == "__main__":
     files = []
+    file_name = ""
 
     if "-d" in sys.argv:
-        for element in sys.argv[sys.argv.index("-d") + 1:]:
-            if element == "-f":
-                break
+        index = sys.argv.index("-d") + 1
+        while index < len(sys.argv) and not sys.argv[index].startswith("-"):
+            files.append(sys.argv[index])
+            index += 1
 
-            files.append(element)
+    if "-f" in sys.argv:
+        file_name = sys.argv[sys.argv.index("-f") + 1]
+    elif len(sys.argv) > 1:
+        file_name = sys.argv[1]
+
+    if files:
         create_path(files)
 
-        if "-f" in sys.argv:
-            file_path = files + [
-                sys.argv[sys.argv.index("-f") + 1]
-            ]
-
-            add_content_to_file(file_path)
+    if file_name:
+        file_path = os.path.join(*(files + [file_name]))
+        add_content_to_file(file_path)
