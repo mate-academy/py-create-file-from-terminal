f9bc733f1a8b133c60af0acd6108dc145cd1a84f
bujhm <Bujhm99@gmail.com>
2024-03-28 02:04:44 +0200

    makedirs, simplisity

--- app/create_file.py
+++ app/create_file.py
@@ -6,10 +6,10 @@ import datetime
 def write_info(file_name: str) -> None:
 
     line = input("Enter content line: ")
-    counts = 1
     with open(file_name, "a") as file_open:
-        if os.stat(file_name).st_size != 0:
+        if os.stat(file_name).st_size:
             file_open.write("\n")
+
         counts = 1
         while line != "stop":
             if counts == 1:
@@ -21,28 +21,27 @@ def write_info(file_name: str) -> None:
             counts += 1
 
 
-current_path = os.path.dirname(os.path.abspath(__file__))
+new_path = os.path.dirname(os.path.abspath(__file__))
 current_messege = sys.argv
-new_path = current_path
 
-try:
-    flag_file_pos = current_messege.index("-f")
-except ValueError:
-    quit("I don't have file to write, so bay.")
-else:
-    new_file_name = current_messege[flag_file_pos + 1]
+for flag in ["-f", "-d"]:
+    try:
+        flag_position = current_messege.index(flag)
+    except ValueError:
+        if flag == "-f":
+            quit("I don't have file to write, so bay.")
+    else:
+        next_messege = current_messege[flag_position + 1]
+        if flag == "-f":
+            new_file_name = next_messege
+        else:
+            while (flag_position < len(current_messege) - 1
+                   and next_messege != "-f"):
+                new_path = os.path.join(new_path,
+                                        next_messege)
+                os.makedirs(new_path, exist_ok=True)
+                flag_position += 1
 
-try:
-    flag_dir_pos = current_messege.index("-d")
-except ValueError:
-    pass
-else:
-    while (flag_dir_pos < len(current_messege) - 1
-           and current_messege[flag_dir_pos + 1] != "-f"):
-        new_path = os.path.join(new_path, current_messege[flag_dir_pos + 1])
-        if not os.path.exists(new_path):
-            os.mkdir(new_path)
-        flag_dir_pos += 1
 
 try:
     _ = open(os.path.join(new_path, new_file_name), "x")
