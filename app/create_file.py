92eaa347d0212516bede307a2bad200cc4d21317
Oleksandr Suligan <suligan.alexander@gmail.com>
2024-04-02 20:25:52 +0200

    Solution:Rewritten code 2

--- app/create_file.py
+++ app/create_file.py
@@ -31,22 +31,39 @@ def main() -> None:
     if "-d" in sys.argv:
         directory_index = sys.argv.index("-d") + 1
         directory = os.path.join(*sys.argv[directory_index:])
-        os.makedirs(directory, exist_ok=True)
+
+        next_flag_index = len(sys.argv)
+        for i in range(directory_index, len(sys.argv)):
+            if sys.argv[i].startswith("-"):
+                next_flag_index = i
+                break
+
+        directory = os.path.join(*sys.argv[directory_index:next_flag_index])
 
     if "-f" in sys.argv:
         filename_index = sys.argv.index("-f") + 1
         filename = sys.argv[filename_index]
 
-    if not directory and not filename:
-        print("Please provide either -d or -f flag.")
-        return
+        if not directory:
+            directory = os.getcwd()
 
-    if filename:
         while True:
             line = input("Enter content line: ")
             if line.lower() == "stop":
                 break
             content_lines.append(line)
+
+    if not directory and not filename:
+        print("Please provide either -d or -f flag.")
+        return
+
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+    else:
+        print("Please provide a directory path using -d flag.")
+        return
+
+    if filename:
         create_file(directory, filename, content_lines)
 
 
