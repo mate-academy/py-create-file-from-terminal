f3320b6673f0b33877aa3ef0b9cbf6ddf94b814a
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-04-03 13:49:40 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -2,16 +2,16 @@ import os
 import sys
 import datetime
 
+from typing import LiteralString
+
 
 command: list = sys.argv
 
 
-def create_dir() -> str:
-    full_path: str = "."
+def create_dir() -> LiteralString | str | bytes:
     end_of_dir: int = (command.index("-f") if "-f" in command
                        else len(command) + 1)
-    for part_path in command[command.index("-d") + 1:end_of_dir]:
-        full_path = os.path.join(full_path, part_path)
+    full_path = os.path.join(*command[command.index("-d") + 1:end_of_dir])
     if full_path:
         os.makedirs(full_path, exist_ok=True)
     return full_path
