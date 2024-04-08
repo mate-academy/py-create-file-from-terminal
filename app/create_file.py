a0a4781f8480a6fa8f88080e8892d46e9a6ceb6a
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-04-08 18:08:46 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,58 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def create_dir(dir_name: list[str]) -> None:
+    path = os.path.join(*dir_name)
+    os.makedirs(path, exist_ok=True)
+
+
+def create_file(file_name: str) -> None:
+    if os.path.exists(file_name):
+        overwrite = input(f"File '{file_name}' already exists. "
+                          f"Do you want to overwrite it? (yes/no): ")
+        if overwrite.lower() != "yes":
+            print("Aborting file creation.")
+            return
+
+    with open(file_name, "a") as f:
+        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+    return
+
+
+def write_content(file_name: str) -> None:
+    print(f"Enter content for '{file_name}' (type 'stop' to finish): ")
+    with open(file_name, "a") as f:
+        line_num = 1
+        while True:
+            content = input(f"Enter content line {line_num}: ")
+            if content.lower() == "stop":
+                break
+            f.write(f"{line_num} {content}\n")
+            line_num += 1
+
+
+def main() -> None:
+    args = sys.argv[1:]
+    if not args:
+        print("Usage: python create_file.py "
+              "[-d <directory_name>] [-f <file_name>]")
+        return
+
+    if "-d" in args:
+        dir_index = args.index("-d") + 1
+        dir_names = args[dir_index:]
+        create_dir(dir_names)
+
+    if "-f" in args:
+        file_index = args.index("-f") + 1
+        file_name = args[file_index]
+        if not create_file(file_name):
+            return
+        write_content(file_name)
+        print(f"File '{file_name}' created with content.")
+
+
+if __name__ == "__main__":
+    main()
