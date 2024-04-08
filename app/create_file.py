a924599387fff099c70d8a5bf51d36bf788ea1d6
Vladyslav Prokopiv <prokopiv.vladyslav@gmail.com>
2024-04-07 22:58:27 -0600

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,55 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def need_directory() -> bool:
+    return "-d" in sys.argv
+
+
+def need_file() -> bool:
+    return "-f" in sys.argv
+
+
+def do_file(file_name: str) -> None:
+    with open(file_name, "a") as file:
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{now}\n")
+
+        line_number = 1
+
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            file.write(f"{line_number} {line}\n")
+            line_number += 1
+
+        file.write("\n")
+
+
+def create_directory(*args) -> None:
+    os.makedirs(os.path.join(*args))
+
+
+def app() -> None:
+    target_dir = os.getcwd()
+
+    if need_directory():
+        new_dir = sys.argv[
+            sys.argv.index("-d") + 1:
+            sys.argv.index("-f") if need_file() else None
+        ]
+        target_dir = os.path.join(target_dir, *new_dir)
+        create_directory(*new_dir)
+
+    if need_file():
+        file_path = os.path.join(
+            target_dir,
+            sys.argv[sys.argv.index("-f") + 1]
+        )
+        do_file(file_path)
+
+
+if __name__ == "__main__":
+    app()
