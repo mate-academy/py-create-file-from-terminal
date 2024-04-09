7c9303c3a1fe415a580c1810cd913101b3161c5f
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-04-09 19:34:17 +0300

    add argparse and improve a logic of app

--- app/create_file.py
+++ app/create_file.py
@@ -1,6 +1,8 @@
-import sys
-import os
 from datetime import datetime
+
+import os
+
+import argparse
 
 
 def create_dir(dir_name: list[str]) -> None:
@@ -8,36 +10,51 @@ def create_dir(dir_name: list[str]) -> None:
     os.makedirs(path, exist_ok=True)
 
 
-def create_and_write_file(file_name: str) -> None:
-    with open(file_name, "a") as f:
+def create_file(file_name: str,
+                content: list[str],
+                dir_name: str = "") -> None:
+    path = os.path.join(*dir_name, file_name)
+    with open(path, "a") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
-        print(f"Enter content for '{file_name}' (type 'stop' to finish): ")
+        for line in content:
+            f.write(f"{line}\n")
 
-        line_num = 1
-        while True:
-            content = input(f"Enter content line {line_num}: ")
-            if content.lower() == "stop":
-                break
-            f.write(f"{line_num} {content}\n")
-            line_num += 1
+
+def write_content() -> list[str]:
+    result_input = []
+    line_num = 1
+    while True:
+        content = input(f"Enter content line {line_num}: ")
+        if content.lower() == "stop":
+            break
+        result_input.append(f"{line_num} {content}")
+        line_num += 1
+    return result_input
 
 
 def main() -> None:
-    args = sys.argv[1:]
-    if not args:
-        print("Usage: python create_file.py "
-              "[-d <directory_name>] [-f <file_name>]")
-        return
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d",
+                        "--directory",
+                        help="Directory path",
+                        nargs="+",
+                        default="")
+    parser.add_argument("-f",
+                        "--filename",
+                        help="File name",
+                        default="")
 
-    if "-d" in args:
-        dir_index = args.index("-d") + 1
-        dir_names = args[dir_index:]
-        create_dir(dir_names)
+    args = parser.parse_args()
 
-    if "-f" in args:
-        file_index = args.index("-f") + 1
-        file_name = args[file_index]
-        create_and_write_file(file_name)
+    dir_paths = args.directory
+    file_name = args.filename
+
+    if dir_paths:
+        create_dir(dir_paths)
+
+    if file_name:
+        input_data = write_content()
+        create_file(file_name, input_data, dir_paths)
 
 
 if __name__ == "__main__":
