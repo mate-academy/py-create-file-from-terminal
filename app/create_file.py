8bb04d5bf3c3109ab5e874697cbd38c4d56c379d
Dmytro Hlazyrin <dmytro.hlazyrin@gmail.com>
2024-04-11 15:38:28 +0300

    Fix

--- app/create_file.py
+++ app/create_file.py
@@ -1,5 +1,6 @@
-import os
 import argparse
+import os
+import re
 from datetime import datetime
 
 
@@ -13,32 +14,59 @@ def main() -> None:
     dir_path = args.dir_path
     file_name = args.file_name
 
+    if file_name and not is_valid_name(file_name):
+        raise NameError("File name is invalid. Don`t use special symbols!")
+    if dir_path and not is_valid_dir_names(dir_path):
+        raise NameError("Dir name is invalid. Don`t use special symbols!")
+
     if dir_path:
-        path = os.path.join(*dir_path)
-        os.makedirs(path, exist_ok=True)
+        create_dir(dir_path)
     if file_name:
-        if dir_path:
-            file_path = os.path.join(*dir_path, file_name)
-            data_input(file_path)
-        else:
-            data_input(file_name)
+        create_file(file_name, dir_path)
+
+
+def is_valid_name(name: str) -> bool:
+    pattern = re.compile(r"^[a-zA-Z0-9_\-\.]+$")
+    return bool(pattern.match(name))
+
+
+def is_valid_dir_names(dirs_list: list[str]) -> bool:
+    return all(is_valid_name(name) for name in dirs_list)
+
+
+def create_dir(dir_path: list) -> None:
+    path = os.path.join(*dir_path)
+    os.makedirs(path, exist_ok=True)
+
+
+def create_file(file_name: str, dir_path: list[str]) -> None:
+    if dir_path:
+        file_path = os.path.join(*dir_path, file_name)
+        data_input(file_path)
+    else:
+        data_input(file_name)
+
+
+def collect_data() -> list:
+    count = 0
+    users_inputs = []
+    while True:
+        count += 1
+        text = input("Enter content line: ")
+        if text.lower() == "stop":
+            break
+        users_inputs.append(f"{count} {text}\n")
+    users_inputs.append("\n")
+    return users_inputs
 
 
 def data_input(file_path: str) -> None:
-    count = 0
+    users_data = collect_data()
     utc_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    mode = "a" if os.path.exists(file_path) else "w"
-    with open(file_path, mode) as file_time:
+    with open(file_path, "a") as file_time:
         file_time.write(f"{utc_time}\n")
-
-        while True:
-            count += 1
-            text = input("Enter content line: ")
-            if text.lower() == "stop":
-                break
-            file_time.write(f"{count} {text}\n")
-
-        file_time.write("\n")
+        for strings in users_data:
+            file_time.write(strings)
 
 
 if __name__ == "__main__":
