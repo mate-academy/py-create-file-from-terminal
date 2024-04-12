3ebc5d136b656ef7553b543ac4667322eb49226e
Vitalii <vitalikbashkiser@gmail.com>
2024-04-12 22:12:59 +0300

    Solution#4 (fix def create_file & create collect input)

--- app/create_file.py
+++ app/create_file.py
@@ -2,6 +2,8 @@ import argparse
 
 import os
 
+from datetime import datetime
+
 from typing import List
 
 
@@ -30,19 +32,32 @@ def create_directories(dirs: List[str]) -> None:
         print(f"Directory already exists: {path}")
 
 
+def collect_input_lines() -> List[str]:
+    print("Enter content line (type 'stop' to finish):")
+    content = []
+    while True:
+        line = input()
+        if line.lower() == "stop":
+            break
+        content.append(line)
+    return content
+
+
 def create_file(filename: str) -> None:
-    with open(filename, "a") as file:
-        print("Enter content line (type 'stop' to finish):")
-        content = []
-        while True:
-            line = input()
-            if line.lower() == "stop":
-                break
-            content.append(line)
+    content = collect_input_lines()
+
+    mode = "w" if not os.path.exists(filename) else "a"
+    with open(filename, mode) as file:
+        if mode == "a":
+            file.write("\n\n")
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{timestamp}\n")
 
         content = [f"{i + 1} {line}" for i, line in enumerate(content)]
         file.write("\n".join(content))
-        print(f"File created: {filename}")
+
+    print(f"File created/updated: {filename}")
 
 
 if __name__ == "__main__":
