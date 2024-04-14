d47677846767bde99c147b061219c5b3cf5a9cb9
Yaroslav Ivanov <165368790+nrjss67@users.noreply.github.com>
2024-04-14 11:10:58 +0300

    Update create_file.py

--- app/create_file.py
+++ app/create_file.py
@@ -1,8 +1,6 @@
 import os
 import sys
-from typing import Any
 from datetime import datetime
-from collections import namedtuple
 
 
 def file_content_printer(current_file: str) -> None:
@@ -17,29 +15,25 @@ def file_content_printer(current_file: str) -> None:
             counter += 1
 
 
-def create_path(argv: list) -> Any | str:
-    PathVariants = namedtuple("PathVariants", ["path", "path_with_fn"])
-    try:
-        path = os.path.join(*filter(
-            lambda x: "." not in x and x not in ("-f", "-d"), argv[1:])
-        )
-        path_with_fn = os.path.join(
-            path,
-            *filter(lambda x: "." in x, sys.argv[1:])
-        )
-        return PathVariants(path, path_with_fn)
-    except TypeError:
-        f_name = list(filter(lambda x: "." in x, sys.argv[1:]))[0]
-        return PathVariants(None, f_name)
+def create_path(filename: str = None) -> str:
+    dirs = filter(lambda x: "." not in x and x not in ["-f", "-d"], sys.argv)
+    if filename and dirs:
+        return os.path.join(*dirs, filename)
+    if dirs:
+        return os.path.join(*dirs)
 
 
-if os.path.exists(create_path(sys.argv).path_with_fn):
-    file_content_printer(create_path(sys.argv).path_with_fn)
-elif "-d" in sys.argv and "-f" in sys.argv:
-    file_name = filter(lambda x: "." in x, sys.argv[1:])
-    os.makedirs(create_path(sys.argv).path, exist_ok=True)
-    file_content_printer(create_path(sys.argv).path_with_fn)
-elif sys.argv[1] == "-d":
-    os.makedirs(create_path(sys.argv).path)
-elif sys.argv[1] == "-f":
-    file_content_printer(sys.argv[2])
+def main():
+    if "-f" in sys.argv and "-d" in sys.argv:
+        file_name = filter(lambda x: "." in x, sys.argv[1:])
+        os.makedirs(create_path(), exist_ok=True)
+        file_content_printer(create_path(*file_name))
+    elif sys.argv[1] == "-f":
+        file_name = filter(lambda x: "." in x, sys.argv[1:])
+        file_content_printer(*file_name)
+    elif sys.argv[1] == "-d":
+        os.makedirs(create_path(), exist_ok=True)
+
+
+if __name__ == "__main__":
+    main()
