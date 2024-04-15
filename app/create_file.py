c82b431525a0a8d4ad062934da48d484f0858faf
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-04-15 21:58:49 +0300

    one function. Is this code readable?

--- app/create_file.py
+++ app/create_file.py
@@ -3,37 +3,10 @@ import sys
 import datetime
 
 
-def add_to_file(filename: str) -> None:
-    count = 0
-    with open(filename, "a") as file:
-        file.write("\n" + datetime.datetime.now().
-                   strftime("%Y-%m-%d %H:%M:%S") + "\n")
-        while True:
-            count += 1
-            terminal_line = input("Enter content line:")
-
-            file.write(str(count) + " " + terminal_line + "\n")
-            if terminal_line.lower() == "stop":
-                break
-
-
-def create_and_write_to_file(filename: str) -> None:
-    count = 0
-    with open(filename, "w") as file:
-        file.write(datetime.datetime.now().
-                   strftime("%Y-%m-%d %H:%M:%S") + "\n")
-        while True:
-            count += 1
-            terminal_line = input("Enter content line:")
-
-            file.write(str(count) + " " + terminal_line + "\n")
-            if terminal_line.lower() == "stop":
-                break
-
-
-def create_file() -> None:
+def main() -> None:
 
     flag = sys.argv[1]
+    count = 0
 
     if flag == "-d" and len(sys.argv) >= 3:
         directory_path = os.path.join(*sys.argv[2:])
@@ -43,11 +16,28 @@ def create_file() -> None:
     elif flag == "-f" and len(sys.argv) >= 3:
         filename = sys.argv[2]
         if os.path.exists(filename):
-            add_to_file(filename)
+            with open(filename, "a") as file:
+                file.write("\n" + datetime.datetime.now().
+                           strftime("%Y-%m-%d %H:%M:%S") + "\n")
+                while True:
+                    count += 1
+                    terminal_line = input("Enter content line:")
 
+                    file.write(str(count) + " " + terminal_line + "\n")
+                    if terminal_line.lower() == "stop":
+                        break
         else:
-            create_and_write_to_file(filename)
+            with open(filename, "w") as file:
+                file.write("\n" + datetime.datetime.now().
+                           strftime("%Y-%m-%d %H:%M:%S") + "\n")
+                while True:
+                    count += 1
+                    terminal_line = input("Enter content line:")
+
+                    file.write(str(count) + " " + terminal_line + "\n")
+                    if terminal_line.lower() == "stop":
+                        break
 
 
 if __name__ == "__main__":
-    create_file()
+    main()
