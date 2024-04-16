63f90ebad52d57aee7d7899e65b47a1567af0e8c
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-04-16 20:22:01 +0300

    success1

--- app/create_file.py
+++ app/create_file.py
@@ -11,27 +11,38 @@ def add_to_file(filename: str) -> None:
         while True:
             count += 1
             terminal_line = input("Enter content line:")
-
+            if terminal_line.lower() == "stop":
+                break
             if terminal_line.strip():
                 file.write(str(count) + " " + terminal_line + "\n")
-            elif terminal_line.lower() == "stop":
-                break
             else:
                 break
 
 
 def create_file(filename: str) -> None:
     count = 0
-    with open(filename, "w") as file:
-        file.write(datetime.datetime.now().
-                   strftime("%Y-%m-%d %H:%M:%S") + "\n")
-        while True:
-            count += 1
-            terminal_line = input("Enter content line:")
-            if terminal_line.strip():
-                file.write(str(count) + " " + terminal_line + "\n")
-            if terminal_line.lower() == "stop":
-                break
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            content = file.read()
+            file.write(datetime.datetime.now().
+                       strftime("%Y-%m-%d %H:%M:%S") + "\n")
+            file.write(content)
+
+    else:
+        with open(filename, "a") as file:
+            file.write(datetime.datetime.now().
+                       strftime("%Y-%m-%d %H:%M:%S") + "\n")
+            while True:
+                count += 1
+                terminal_line = input("Enter content line:")
+                if terminal_line.lower() == "stop":
+                    break
+
+                if terminal_line.strip():
+                    file.write(str(count) + " " + terminal_line + "\n")
+
+                else:
+                    break
 
 
 def main() -> None:
@@ -39,7 +50,6 @@ def main() -> None:
     flag = sys.argv[1]
     if flag == "-d" and len(sys.argv) >= 3:
         directory_path = os.path.join(*sys.argv[2:])
-        print(directory_path)
         os.makedirs(directory_path, exist_ok=True)
     elif flag == "-f" and len(sys.argv) >= 3:
         filename = sys.argv[2]
