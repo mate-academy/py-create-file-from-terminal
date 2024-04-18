42442c6db6c4131ebf95310a35e7d961e2711094
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-04-18 21:50:53 +0300

    I shortened the code block with -f

--- app/create_file.py
+++ app/create_file.py
@@ -36,29 +36,9 @@ def main() -> None:
         os.makedirs(directory_path, exist_ok=True)
 
     elif flag == "-f" and len(sys.argv) >= 3:
-
         filename = sys.argv[2]
         current_dir = os.path.dirname(filename)
-
-        count = 0
-
-        if os.path.exists(filename):
-            with open(filename, "a") as file:
-                file.write("\n" + datetime.datetime.now().
-                           strftime("%Y-%m-%d %H:%M:%S") + "\n")
-                while True:
-                    count += 1
-                    terminal_line = input("Enter content line: ")
-
-                    if terminal_line.lower() == "stop":
-                        break
-
-                    if terminal_line.strip():
-                        file.write(str(count) + " " + terminal_line + "\n")
-                    else:
-                        break
-        else:
-            create_file(os.path.join(current_dir, filename))
+        create_file(os.path.join(current_dir, filename))
 
     if "-d" in sys.argv and "-f" in sys.argv and len(sys.argv) == 6:
         d_index = sys.argv.index("-d")
