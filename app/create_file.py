b2991c9e3639ba24a1d9a11d37244658d5d1c02f
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-04-18 12:08:50 +0200

    Solution: fix flake8 errors

--- app/create_file.py
+++ app/create_file.py
@@ -9,7 +9,9 @@ def create_directory(directory_path: str) -> None:
 
 
 def get_last_line_number(path: str) -> int:
-    """Return the number of lines in a file, or 0 if the file does not exist."""
+    """
+    Return the number of lines in a file, or 0 if the file does not exist.
+    """
     try:
         with open(path, "r") as file:
             return len(file.readlines())
@@ -18,7 +20,10 @@ def get_last_line_number(path: str) -> int:
 
 
 def append_lines_to_file(path: str, start_line: int) -> None:
-    """Append lines to the file with line numbers, starting from the last line number."""
+    """
+    Append lines to the file with line numbers, starting from the
+    last line number.
+    """
     with open(path, "a" if start_line > 0 else "w") as file:
         if not start_line:
             file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
@@ -34,17 +39,31 @@ def append_lines_to_file(path: str, start_line: int) -> None:
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Create a file in specified directory.")
-    parser.add_argument("-d", "--directory", nargs="+", required=False, help="Path to the directory: `-d dir1 dir2`.")
-    parser.add_argument("-f", "--file", required=False, help="Specify the filename to create.")
+    parser = argparse.ArgumentParser(
+        description="Create a file in specified directory."
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        nargs="+",
+        required=False,
+        help="Path to the directory: `-d dir1 dir2`.",
+    )
+    parser.add_argument(
+        "-f", "--file", required=False, help="Specify the filename to create."
+    )
     args = parser.parse_args()
-    directory_path = os.path.join(".", *args.directory if args.directory else "")
+    directory_path = os.path.join(
+        ".", *args.directory if args.directory else ""
+    )
 
     if args.directory:
         create_directory(directory_path)
 
     if args.file:
-        file_path = os.path.join(directory_path if args.directory else "./", args.file)
+        file_path = os.path.join(
+            directory_path if args.directory else ".", args.file
+        )
         last_line = get_last_line_number(file_path)
         append_lines_to_file(file_path, last_line)
 
