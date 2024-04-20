82fbe185b84ad57adb8029c71aad530f1903016d
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-04-20 21:42:07 +0100

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,22 +1,41 @@
-import argparse
 import os
+import sys
+from datetime import datetime
 
 
-def create_dirs(dir1: str, dir2: str) -> None:
-    os.makedirs(os.path.join(os.getcwd(), dir1, dir2), exist_ok=True)
+def create_directory(dirs: list) -> None:
+    path = os.path.join(*dirs)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def create_file(file_path: str) -> None:
+    with open(file_path, "a") as file:
+        current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{current_date}\n")
+        line = 1
+
+        while True:
+            user_input = input("Write your content or enter 'stop' to exit: ")
+            if user_input == "stop":
+                break
+            file.write(f"{line} Line{line} {user_input}" + "\n")
+
+        file.write("\n")
+
+
+def main() -> None:
+    args = sys.argv[1:]
+
+    if "-d" in args:
+        dir_index = args.index("-d")
+        dirs = args[dir_index + 1:]
+        create_directory(dirs)
+    if "-f" in args:
+        file_index = args.index("-f")
+        file_path = args[file_index + 1]
+        create_file(file_path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create a file or directory.")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-d", "--directory", nargs='+', help="Path to the directory to create.")
-    group.add_argument("-f", "--file", help="Name of the file to create.")
-
-    args = parser.parse_args()
-
-    if args.directory:
-        create_dirs(args.directory)
-        print(f"Directory {'/'.join(args.directory)} created successfully.")
-    elif args.file:
-        create_dirs(args.file)
-        print(f"File {args.file} created successfully.")
+    main()
