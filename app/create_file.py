286e8c79e11aa9a1bbdebe0b7a35eb4ceaf6447d
Artem Andriushchenko <link.by.artem@gmail.com>
2024-04-26 07:53:39 +0300

    fixed solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,49 +3,40 @@ import sys
 from datetime import datetime
 
 
-def create_file(directory: str, filename: str, content: list) -> None:
-    if directory:
-        os.makedirs(directory, exist_ok=True)
-    filepath = os.path.join(directory, filename)
-
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    with open(filepath, "a") as file:
-        file.write(timestamp + "\n")
-        for i, line in enumerate(content, start=1):
-            file.write(f"{i} {line}\n")
-
-
-def main() -> None:
-    args = sys.argv[1:]
-    if not args:
-        print("Usage: python create_file.py -d [directory_path] -f [filename]")
-        return
-
-    directory = ""
-    filename = ""
-    content = []
-    while args:
-        flag = args.pop(0)
-        if flag == "-d":
-            directory = os.path.join(directory, args.pop(0))
-        elif flag == "-f":
-            filename = args.pop(0)
-        else:
-            print(f"Invalid flag: {flag}")
-            return
-
-    if filename:
-        print("Enter content line:")
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as file:
+        file.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+        line_number = 1
         while True:
-            line = input()
+            line = input("Enter content line: ")
             if line.lower() == "stop":
+                file.write("\n")
                 break
-            content.append(line)
-
-    create_file(directory, filename, content)
-    print(f"File '{filename}' created successfully.")
+            file.write(f"{line_number} {line}\n")
+            line_number += 1
 
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv
+    if "-d" in args and "-f" in args:
+        d_index = args.index("-d")
+        f_index = args.index("-f")
+        file_name = args[f_index + 1]
+
+        if d_index < f_index:
+            path = os.path.join(*args[d_index + 1: f_index])
+        else:
+            path = os.path.join(*args[d_index + 1:])
+
+        os.makedirs(path, exist_ok=True)
+        create_file(os.path.join(path, file_name))
+
+    elif "-d" in args:
+        d_index = args.index("-d")
+        path = os.path.join(*args[d_index + 1:])
+        os.makedirs(path, exist_ok=True)
+
+    elif "-f" in args:
+        f_index = args.index("-f")
+        file_name = args[f_index + 1]
+        create_file(file_name)
