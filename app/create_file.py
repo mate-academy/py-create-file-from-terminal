0861f7a69902a10f1fb2210b2d997d8472677022
Julia Okhrymenko <Ohrimenko15@gmail.com>
2024-04-27 12:35:54 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -16,44 +16,31 @@ def create_file(directory_path: str, file_name: str, lines: List[str]) -> None:
 
 def main() -> None:
     args = sys.argv[1:]
-    if "-d" in args and "-f" in args:
-        index_d = args.index("-d")
+    file_name = None
+
+    if "-f" in args:
         index_f = args.index("-f")
-        directory_path = os.path.join(*args[index_d + 1:index_f])
-        file_name = args[index_f + 1]
+        if "-d" in args:
+            index_d = args.index("-d")
+            directory_path = os.path.join(*args[index_d + 1:index_f]) if index_f < index_d else os.getcwd()
+        else:
+            directory_path = os.getcwd()
+            file_name = args[index_f + 1]
+
         lines = []
-        print("Enter content line: ")
+        print("Enter content line:")
         while True:
             line = input()
             if line.lower() == "stop":
                 break
             lines.append(line)
-        os.makedirs(directory_path, exist_ok=True)
-        create_file(directory_path, file_name, lines)
-        print(f"Creates directory {directory_path} and "
-              f"file {file_name} inside that directory with content:")
-        print(os.path.join(directory_path, file_name))
-        print("\n".join(lines))
+            if file_name is not None:
+                create_file(directory_path, file_name, lines)
+
     elif "-d" in args:
         index_d = args.index("-d")
         directory_path = os.path.join(*args[index_d + 1:])
         os.makedirs(directory_path, exist_ok=True)
-        print(f"Created directory {directory_path}.")
-    elif "-f" in args:
-        index_f = args.index("-f")
-        file_name = args[index_f + 1]
-        lines = []
-        print("Enter content line: ")
-        while True:
-            line = input()
-            if line.lower() == "stop":
-                break
-            lines.append(line)
-        create_file(os.getcwd(), file_name, lines)
-        print(f"File {file_name} created with content:")
-        print("\n".join(lines))
-    else:
-        print("No flags provided.")
 
 
 if __name__ == "__main__":
