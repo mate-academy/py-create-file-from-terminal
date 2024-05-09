239c0a32091f94b2d5371c1fd0a3c92968d1cc58
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-05-09 18:05:19 +0300

    Update create_file.py
    File fixed

--- app/create_file.py
+++ app/create_file.py
@@ -1,39 +1,39 @@
 import os
-import datetime
-from sys import argv
+import sys
+from datetime import datetime
 
 
-def get_path(argv: list) -> (str, str):
-    f_index = argv.index("-f") if "-f" in argv else ""
-    d_index = argv.index("-d") if "-d" in argv else ""
+def main() -> None:
+    arg_list = sys.argv[1:]
+    file_name = ""
+    current_path = ""
 
-    file_name = argv[-1] if f_index else ""
+    if "-f" in sys.argv:
+        file_name = arg_list.pop(arg_list.index("-f") + 1)
+        arg_list.remove("-f")
 
-    if d_index and f_index:
-        return os.path.join(*argv[d_index + 1:f_index]), file_name
-    elif d_index:
-        return os.path.join(*argv[d_index + 1:]), file_name
-
-    return "", file_name
-
-
-def create_file() -> None:
-    dir_name, file_name = get_path(argv)
-    current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    if dir_name:
-        os.makedirs(dir_name, exist_ok=True)
+    if "-d" in sys.argv:
+        arg_list.remove("-d")
+        current_path = os.path.join(*arg_list)
+        if not os.path.exists(current_path):
+            os.makedirs(current_path)
 
     if file_name:
-        with open(os.path.join(dir_name, file_name), "a") as new_file:
-            new_file.write(current_date_time + "\n")
-            while True:
-                new_line = input("Enter content line: ")
-                if new_line == "stop":
-                    break
-                new_file.write(new_line + "\n")
-            new_file.write("\n")
+        create_file(str(os.path.join(current_path, file_name)))
+
+
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as update_file:
+        update_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+        line_num = 1
+        while True:
+            info = input("Enter content line:")
+            if info == "stop":
+                update_file.write("\n")
+                break
+            update_file.write(f"{line_num} {info}\n")
+            line_num += 1
 
 
 if __name__ == "__main__":
-    create_file()
+    main()
