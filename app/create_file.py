4885779860522914663f44baca112f3ecb3c8529
Felix Poslushnyi <felixp4@gmail.com>
2024-05-23 21:05:18 +0300

    divide code to a few functions

--- app/create_file.py
+++ app/create_file.py
@@ -2,52 +2,63 @@ import datetime
 import sys
 import os
 
-new_path = ""
-file_name = ""
-file_pos = 0
-dirs = []
-dir_pos = 0
-lines = []
-number_line = 0
-input_string = ""
-parent_dir = os.getcwd()
 
+def input_strings() -> list:  
+    str, num, lines = "", 0, []
+    while True:
+        str = input("Enter content line: ")
+        if str == "stop":
+           break
+        num += 1
+        lines.append(f"\n{num} {str}")
+    return lines    
 
-while True:
-    input_string = input("Enter content line: ")
-    if input_string == "stop":
-        break
-    number_line += 1
-    lines.append(f"\n{number_line} {input_string}")
-
-for i, arg in enumerate(sys.argv):
-    if arg == "-f":
-        file_name = sys.argv[i + 1]
-        file_pos = i
-
-    if arg == "-d":
-        dir_pos = i
-
-    if file_pos:
-        dirs = sys.argv[dir_pos + 1 : file_pos]
+def parsing_file_and_directories() -> dict:
+    file, dirs = 0, 0
+    for i, arg in enumerate(sys.argv):
+        if arg == "-f":
+            file = i + 1
+        if arg == "-d":
+            dirs = i + 1
+    
+    if file:
+        file_str = sys.argv[file] 
     else:
-        dirs = sys.argv[dir_pos + 1 :]
+        file_str = ""
 
-if dirs:
-    mode = 0o666
-    new_path = parent_dir
+    if file and dirs and file > dirs:
+         dir_list = sys.argv[dirs : file - 1]
+    elif (file and dirs and file < dirs) or (not file and dirs):
+         dir_list = sys.argv[dirs : ]
+    
+    return {"file" : file_str, "dirs" : dir_list}
 
-    for dir_name in dirs:
+def create_directories(dirs_name: list) -> str:
+    new_path = ""
+    parent_dir = os.getcwd()
+    if dirs_name:
+        mode = 0o666
+        new_path = parent_dir
+
+    for dir_name in dirs_name:
         new_path = os.path.join(new_path, dir_name.strip())
     os.makedirs(new_path, mode)
+    
+    return new_path
 
-if file_name:
-    if new_path:
-        full_path = os.path.join(new_path, file_name)
-    else:
-        full_path = file_name
 
+new_path = os.getcwd()
+params = parsing_file_and_directories()
+print(params)
+
+if params["dirs"]:
+    new_path = create_directories(params["dirs"])
+
+print(new_path)
+
+if params["file"]:
+    full_path = os.path.join(new_path, params["file"])
     with open(full_path, "a") as f:
         date_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"\n{date_str}")
-        f.writelines(lines)
+        f.writelines(input_strings())
