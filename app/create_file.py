bba816a7701643061757c4ba55a341ffa3d078a2
daikj <tyutyunniksolomia@gmail.com>
2024-05-24 13:47:14 +0300

    update implement create-file-from-terminal

--- app/create_file.py
+++ app/create_file.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 def parse_args() -> argparse.Namespace:
     parses = argparse.ArgumentParser()
-    parses.add_argument("-d", "--directory", nargs="+",
-                        help="create directory")
+    parses.add_argument("-d", "--directory",
+                        type=str, nargs="+", help="create directory")
     parses.add_argument("-f", "--file", type=str, help="create file")
     return parses.parse_args()
 
@@ -16,8 +16,10 @@ def create_directory(directory: list[str]) -> None:
     os.makedirs(os.path.join(*directory), exist_ok=True)
 
 
-def create_file(directory: list[str], file_name: str) -> None:
-    with open(os.path.join(directory or "", file_name), "a") as file:
+def create_file(directory: list[str] | None, file_name: str) -> None:
+    if directory is None:
+        directory = ["."]
+    with open(os.path.join(*directory, file_name), "a") as file:
         file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         line_number = 1
         while True:
