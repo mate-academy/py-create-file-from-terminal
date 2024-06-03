42163346477e5a7d0c45e44826197b1882b66a79
AndrewwSSS <clash2clans1one@gmail.com>
2024-06-03 11:53:12 +0300

    refactoring

--- app/create_file.py
+++ app/create_file.py
@@ -2,39 +2,33 @@ import sys
 import os
 from datetime import datetime
 
-# get information from console args
-i = 1
-path = ""
-d_flag = False
-f_flag = False
-file_name = ""
 
-while i != len(sys.argv):
-    if sys.argv[i] == "-d":
-        if d_flag:
-            raise ValueError("-d flag is already defined")
-        if len(sys.argv) - 1 < i + 2:
+def get_path_from_args() -> str:
+    result_path = ""
+    if sys.argv.count("-d") > 1:
+        raise ValueError("-d flag is already defined")
+    elif sys.argv.count("-f") > 1:
+        raise ValueError("-f flag is already defined")
+
+    if "-d" in sys.argv:
+        flag_idx = sys.argv.index("-d")
+        if len(sys.argv) < flag_idx + 2:
             raise ValueError("-d flag requires two parameters")
-        directory_1, directory_2 = sys.argv[i + 1:i + 3]
-        path = os.path.join(directory_1, directory_2)
-        os.makedirs(path)
-        i += 3
-        d_flag = True
-    elif sys.argv[i] == "-f":
-        if f_flag:
-            raise ValueError("-f flag is already defined")
-        if len(sys.argv) - 1 < i + 1:
+        result_path = os.path.join(sys.argv[flag_idx + 1],
+                                   sys.argv[flag_idx + 2])
+        os.makedirs(result_path)
+    if "-f" in sys.argv:
+        flag_idx = sys.argv.index("-f")
+        if len(sys.argv) < flag_idx + 1:
             raise ValueError("-f flag requires one parameter")
-        file_name = sys.argv[i + 1]
-        i += 2
-        f_flag = True
+        result_path = os.path.join(result_path, sys.argv[flag_idx + 1])
     else:
-        raise ValueError("Invalid argument")
+        raise ValueError("There is not -f flag")
+    return result_path
 
-if not f_flag:
-    raise ValueError("There is not -f flag")
 
-path = os.path.join(path, file_name)
+# get information from console args
+path = get_path_from_args()
 
 
 # Write file content
