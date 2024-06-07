be00e24c1a6de9b775dcdf40ef5d3d1d22188048
Vlad <vlad@example.com>
2024-06-08 00:50:05 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,40 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_dirs(dirs: list) -> None:
+    path = os.path.join(*dirs)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def write_file(commands: list, anchor_d: bool) -> None:
+    directory = commands[-1]
+    if anchor_d:
+        path = commands[1: commands.index("-f")]
+        create_dirs(path)
+        path += commands[-1:]
+        directory = os.path.join(*path)
+    with open(directory, "a") as file:
+        file.write(f"{datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )}\n")
+        page_number = 1
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            file.write(f"{page_number} {line}\n")
+            page_number += 1
+        file.write("\n")
+
+
+commands = sys.argv[1:]
+
+if commands[0] == "-d" and not commands.count("-f"):
+    create_dirs(commands[1:])
+elif commands[0] == "-f":
+    write_file(commands, False)
+else:
+    write_file(commands, True)
