008f3bccceeb64b293603055d63db986f25fadb8
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-06-09 10:53:50 +0300

    fix errors, add main func

--- app/create_file.py
+++ app/create_file.py
@@ -1,34 +1,40 @@
+import argparse
 import os
-import sys
 import datetime
-from optparse import OptionParser
+
 
 def create_file(file_path: str) -> None:
     with open(file_path, "a") as f:
         time_now = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
         f.write(f"{time_now}\n")
-        
+
         while True:
             line = input("Enter content line: ")
             if line == "stop":
                 break
             f.write(f"{line}\n")
 
+
 def create_dir(path: str, file_name: str) -> None:
-    if file_name:
-        if path:
-            if not os.path.isdir(path):
-                os.makedirs(path)
-            file_path = os.path.join(path, file_name)
-            create_file(file_path)
-        else:
-            create_file(file_name)
-    else:
-        print("No filename provided.")
+    if path:
+        full_path = os.path.join(*path)
+        if not os.path.isdir(full_path):
+            os.makedirs(full_path)
+        if file_name:
+            path_to_file = os.path.join(full_path, file_name)
+            print(path_to_file)
+            create_file(path_to_file)
+    elif file_name:
+        create_file(file_name)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", dest="filename", required=False)
+    parser.add_argument("-d", dest="dir_name", nargs="+", required=False)
+    args = parser.parse_args()
+    create_dir(args.dir_name, args.filename)
+
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-f", dest="filename")
-    parser.add_option("-d", dest="dir_name")
-    (options, args) = parser.parse_args(sys.argv)
-    create_dir(options.dir_name, options.filename)
+    main()
