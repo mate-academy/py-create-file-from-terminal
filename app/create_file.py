4f00b4ebb11e027604179ac6e960397f4da034bb
Dima <dima2000yan@gmail.com>
2024-06-10 17:58:42 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,33 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+path = ""
+
+if "-d" in sys.argv:
+    if "-f" in sys.argv:
+        end_index = sys.argv.index("-f")
+    else:
+        end_index = len(sys.argv)
+    path = "/".join(sys.argv[sys.argv.index("-d") + 1:end_index])
+
+    os.makedirs(path, exist_ok=True)
+
+    path += "/"
+
+
+if "-f" in sys.argv:
+    path += sys.argv[-1]
+
+    with open(path, 'a') as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f'\n{timestamp}\n')
+
+        numerator = 1
+        while True:
+            line = input("Enter content line: ")
+            if line.lower() == "stop":
+                break
+            file.write(f"{numerator} {line}\n")
+            numerator += 1
