9deeb37c92b06ee4e41f788cb8b9f5319b2cd0d0
timur <stukantimur811@gmail.com>
2024-06-14 11:08:45 +0300

    function of creation file and dirs

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,34 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_file() -> None:
+    all_argv = sys.argv[1:]
+    dirs_path = ""
+
+    if "-d" in all_argv:
+        if "-f" in all_argv:
+            dirs_path = all_argv[1:all_argv.index("-f")]
+        else:
+            dirs_path = all_argv[1:]
+
+        dirs_path = os.path.join(*dirs_path)
+        os.makedirs(dirs_path, exist_ok=True)
+
+    if "-f" in all_argv:
+        file_name = all_argv[all_argv.index("-f") + 1]
+        with open(os.path.join(dirs_path, file_name), "a") as file:
+            file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            line_counter = 1
+            while True:
+                entered_data = input("Enter content line: ")
+                if entered_data == "stop":
+                    break
+                file.write(f"\n{line_counter} {entered_data}")
+                line_counter += 1
+
+            file.write("\n\n")
+
+
+create_file()
