2fab4db7810c6a5b563e887349406de7c91d752e
unlie9 <unlie.devops9@gmail.com>
2024-06-16 16:54:25 +0300

    sol

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,41 @@
-# write your code here
+import os
+import sys
+
+
+def make_path() -> str:
+    file_path = os.getcwd()
+    file_name = "file.txt"
+
+    index = 1
+    while index < len(sys.argv):
+        arg = sys.argv[index]
+        if arg == "-d":
+            while arg != "-f" and index < len(sys.argv):
+                arg = sys.argv[index]
+                file_path += f"\\{arg}"
+                os.mkdir(file_path)
+                index += 1
+            continue
+        if arg == "-f":
+            file_name = sys.argv[index + 1]
+
+    return f"{file_path}\\{file_name}"
+
+
+def write_file(file_name: str) -> None:
+    with open(file_name, "w") as file:
+        while True:
+            input_content = input("Enter content line: ")
+            if input_content == "stop":
+                break
+            file.write(f"{input_content}\n")
+
+
+def main() -> None:
+    file_name = make_path()
+    write_file(file_name)
+
+
+if __name__ == "__main__":
+    main()
+
