11b8ba1994c4d77cc7711ca12bbd1e0240ef9aee
MaglorFeanorson <daniilrog17@gmail.com>
2024-06-18 16:11:49 +0300

    "Solution - revrite"

--- app/create_file.py
+++ app/create_file.py
@@ -1,29 +1,46 @@
-import os
 import sys
-from datetime import datetime
+import os
+import datetime
 
 
-path = ""
-if "-d" in sys.argv:
-    if "-f" in sys.argv:
+def create_directory() -> str:
+    start_index = sys.argv.index("-d") + 1
+    try:
         end_index = sys.argv.index("-f")
-    else:
+    except ValueError:
         end_index = len(sys.argv)
-    path = "/".join(sys.argv[sys.argv.index("-d") + 1:end_index])
-    os.makedirs(path, exist_ok=True)
-    path += "/"
+    directories = os.path.join(*sys.argv[start_index: end_index])
+    os.makedirs(directories, exist_ok=True)
+    return f"{directories}\\"
 
-if "-f" in sys.argv:
-    path += sys.argv[-1]
 
-    with open(path, "a") as file:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"\n{timestamp}\n")
-
-        numerator = 1
+def create_file(directories: str = "") -> None:
+    if "-f" in sys.argv:
+        file_index = sys.argv.index("-f") + 1
+        if file_index < len(sys.argv):
+            file_name = sys.argv[file_index]
+        else:
+            print("Error: File name is missing after -f flag")
+            return
+    else:
+        print("Error: Use -f flag to specify the file name")
+        return
+    file_path = os.path.join(directories, file_name)
+    key = "a" if os.path.exists(file_path) else "w"
+    with open(file_path, key) as file:
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
         while True:
-            line = input("Enter content line: ")
-            if line.lower() == "stop":
+            test = input("Enter content line: ")
+            if test == "stop":
                 break
-            file.write(f"{numerator} {line}\n")
-            numerator += 1
+            file.write(f"{test}\n")
+
+if __name__ == "__main__":
+    directory = ""
+    if "-d" in sys.argv:
+        directory = create_directory()
+    if "-f" in sys.argv:
+        create_file(directory)
+    else:
+        print("Error: Use -b or -f flags")
+
