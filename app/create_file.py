38e6c1ccb57891429c6a5b03210010961e215a4e
Seniv Volodymyr <seniv.05@gmail.com>
2024-06-21 23:29:32 +0200

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -2,8 +2,10 @@ import sys
 import os
 import datetime
 
-def create_file(something):
+
+def create_file(something: list[str]) -> None:
     dir_string = f"{os.getcwd()}\\"
+    date = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     if "-d" in something:
         for i in range(something.index("-d") + 1,
                        something.index("-f")):
@@ -13,11 +15,9 @@ def create_file(something):
     if "-f" in something:
         file_path = os.path.join(dir_string,
                                  something[something.index("-f") + 1])
-        with open(file_path, 'w') as f:
+        with open(file_path, "w") as f:
             flag = ""
-            f.write(str(
-                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                ) + "\n")
+            f.write(date + "\n")
             while True:
                 flag = input("Enter content line (type 'stop' to finish): ")
                 if flag == "stop":
