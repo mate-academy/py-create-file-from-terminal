b3abaccc7a135d15960cc9041214151fbf3fe2d9
Seniv Volodymyr <seniv.05@gmail.com>
2024-06-22 14:08:13 +0200

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -7,22 +7,31 @@ def create_file(something: list[str]) -> None:
     dir_string = f"{os.getcwd()}\\"
     date = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     if "-d" in something:
-        for i in range(something.index("-d") + 1,
-                       something.index("-f")):
-            dir_string = os.path.join(dir_string, something[i])
-            os.makedirs(dir_string)
+        dir_string = directory_creation(something, dir_string)
 
     if "-f" in something:
-        file_path = os.path.join(dir_string,
-                                 something[something.index("-f") + 1])
-        with open(file_path, "w") as f:
-            flag = ""
-            f.write(date + "\n")
-            while True:
-                flag = input("Enter content line (type 'stop' to finish): ")
-                if flag == "stop":
-                    break
-                f.write(flag + "\n")
+        file_creation(something,  dir_string, date)
+
+
+def directory_creation(args_var: str, path: str):
+    for i in range(args_var.index("-d") + 1,
+                       args_var.index("-f")):
+            path = os.path.join(path, args_var[i])
+            os.makedirs(path)
+    return path
+
+
+def file_creation(args_var: str, path: str, date: datetime):
+    file_path = os.path.join(path,
+                                 args_var[args_var.index("-f") + 1])
+    with open(file_path, "w") as f:
+        flag = ""
+        f.write(date + "\n")
+        while True:
+            flag = input("Enter content line (type 'stop' to finish): ")
+            if flag == "stop":
+                break
+            f.write(flag + "\n")
 
 
 create_file(sys.argv)
