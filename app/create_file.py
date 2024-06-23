2daa7da1f3b274934ad44127bcf7717786aa2748
Alex Bul <tarasov57228@gmail.com>
2024-06-23 13:39:37 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -14,7 +14,8 @@ if "-d" == sys.argv[1]:
 if "-f" in sys.argv:
     index_sys = sys.argv.index("-f")
 
-    with open(os.path.join(parent_dir, sys.argv[index_sys + 1]), "a") as source_file:
+    with (open(os.path.join(parent_dir, sys.argv[index_sys + 1]), "a")
+          as source_file):
         number_line = 0
         now = datetime.datetime.now()
         datatime_str = now.strftime("%Y-%m-%d %H:%M:%S")
