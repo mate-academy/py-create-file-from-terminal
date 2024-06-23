3e0f62efcdf670e775379a4e115cc6c0bd60d4bf
Alex Bul <tarasov57228@gmail.com>
2024-06-23 13:39:03 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -2,18 +2,19 @@ import sys
 import datetime
 import os
 
-parent_dir = "C:/Users/taras/projects/py-create-file-from-terminal/app"
+parent_dir = os.path.join(os.getcwd(), "app")
+
 if "-d" == sys.argv[1]:
     for i in range(2, sys.argv.index("-f")):
         directory = sys.argv[i]
         path = os.path.join(parent_dir, directory)
         os.makedirs(path)
-        parent_dir += f"/{directory}"
+        parent_dir = os.path.join(parent_dir, directory)
 
 if "-f" in sys.argv:
     index_sys = sys.argv.index("-f")
 
-    with open(parent_dir + f"/{sys.argv[index_sys + 1]}", "a") as source_file:
+    with open(os.path.join(parent_dir, sys.argv[index_sys + 1]), "a") as source_file:
         number_line = 0
         now = datetime.datetime.now()
         datatime_str = now.strftime("%Y-%m-%d %H:%M:%S")
@@ -21,6 +22,6 @@ if "-f" in sys.argv:
         while True:
             number_line += 1
             new_line = input("Enter content line: ")
-            if new_line == "stop":
+            if new_line.lower() == "stop":
                 break
             source_file.write(f"{number_line} {new_line}\n")
