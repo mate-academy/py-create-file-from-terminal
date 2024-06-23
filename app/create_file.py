2c31a7aababef4a485febfd2a37218cc86a22f13
VR <vladrymarchuk@gmail.com>
2024-06-23 17:06:20 +0300

    fix

--- app/create_file.py
+++ app/create_file.py
@@ -15,7 +15,15 @@ def create_file(file_name: str, path: str = None) -> None:
 
     os.makedirs(os.path.dirname(full_path), exist_ok=True)
 
+    file_exists = os.path.exists(full_path)
+    if file_exists:
+        file_is_empty = os.path.getsize(full_path) == 0
+    else:
+        file_is_empty = False
+
     with open(full_path, "a+") as file_path:
+        if file_exists and not file_is_empty:
+            file_path.write("\n")
         local = datetime.datetime.now()
         file_path.write(local.strftime("%m-%d-%Y %H:%M:%S\n"))
         while True:
@@ -25,23 +33,21 @@ def create_file(file_name: str, path: str = None) -> None:
             file_path.write(f"{line}\n")
 
 
-path_list = sys.argv
+args = sys.argv
+directory_path = None
+file_name = None
 
-if "-d" in path_list and "-f" not in path_list:
-    create_directory_structure(path=os.path.join(*path_list[2:]))
-elif "-f" in path_list and "-d" not in path_list:
-    create_file(file_name=path_list[2])
-else:
-    path_with_directory = {}
-    path_with_file = {}
+if "-d" in args:
+    index_of_directory = args.index("-d") + 1
+    dir_end_index = len(args)
+    for flag in ["-f"]:
+        if flag in args:
+            dir_end_index = min(dir_end_index, args.index(flag))
+    directory_path = os.path.join(*args[index_of_directory:dir_end_index])
+    create_directory_structure(path=directory_path)
 
-    index_of_directory = path_list.index("-d")
-    index_of_file = path_list.index("-f")
-
-    for element in path_list[2:]:
-        path_with_directory = path_list[2:index_of_file]
-        path_with_file = path_list[-1]
-    create_file(
-        file_name=path_with_file,
-        path=os.path.join(*path_with_directory)
-    )
+if "-f" in args:
+    index_of_file = args.index("-f") + 1
+    if index_of_file < len(args):
+        file_name = args[index_of_file]
+        create_file(file_name=file_name, path=directory_path)
