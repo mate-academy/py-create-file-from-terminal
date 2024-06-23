b582283c6ce87526527e1d6f3e039fac909c90f4
Alex Bul <tarasov57228@gmail.com>
2024-06-23 12:59:35 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -16,7 +16,8 @@ if "-f" in sys.argv:
     with open(parent_dir + f"/{sys.argv[index_sys + 1]}", "a") as source_file:
         number_line = 0
         now = datetime.datetime.now()
-        source_file.write(f"{now.strftime("%Y-%m-%d %H:%M:%S")}\n")
+        datatime_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        source_file.write(f"{datatime_str}\n")
         while True:
             number_line += 1
             new_line = input("Enter content line: ")
