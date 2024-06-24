ce93131e9248382ed0fb5a94dcfd8c2e838358a5
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-06-24 18:26:03 +0200

    The solution of the task

--- app/create_file.py
+++ app/create_file.py
@@ -4,24 +4,32 @@ import datetime
 
 
 def create_file() -> None:
-    args = sys.argv[1:]
+
     directory_path = ""
-    if "-d" in args:
-        if "-f" in args:
-            directory_path = args[2:args.index("-f")]
+
+    if "-d" in sys.argv:
+        if "-f" in sys.argv:
+            directory_path = sys.argv[sys.argv.index("-d")
+                                      + 1:sys.argv.index("-f")]
+
         else:
-            directory_path = args[2:]
+            directory_path = sys.argv[sys.argv.index("-d") + 1:]
         os.makedirs(os.path.join(*directory_path), exist_ok=True)
-    if "-f" in args:
-        file_name = args[-1]
+
+    if "-f" in sys.argv:
+        file_name = sys.argv[-1]
         file_exists = False
+
         if os.path.exists(os.path.join(directory_path, file_name)):
             file_exists = True
+
         with open(os.path.join(directory_path, file_name), "a") as file:
             if file_exists:
                 file.write("\n\n")
+
             file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             row_number = 1
+
             while True:
                 entered_info = input("Enter content line: ")
                 if entered_info == "stop":
