dd96b42a278bfe4d177c8a3821d39da50239b1bf
Alex Bul <tarasov57228@gmail.com>
2024-06-25 16:53:57 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -4,25 +4,34 @@ import os
 
 parent_dir = os.path.join(os.getcwd(), "app")
 
-if "-d" == sys.argv[1]:
-    for i in range(2, sys.argv.index("-f")):
-        directory = sys.argv[i]
-        path = os.path.join(parent_dir, directory)
-        os.makedirs(path)
-        parent_dir = os.path.join(parent_dir, directory)
 
-if "-f" in sys.argv:
-    index_sys = sys.argv.index("-f")
+def make_directory() -> None:
+    parent_dir = os.path.join(os.getcwd(), "app")
+    if "-d" == sys.argv[1]:
+        for i in range(2, sys.argv.index("-f")):
+            directory = sys.argv[i]
+            path = os.path.join(parent_dir, directory)
+            os.makedirs(path)
+            parent_dir = os.path.join(parent_dir, directory)
 
-    with (open(os.path.join(parent_dir, sys.argv[index_sys + 1]), "a")
-          as source_file):
-        number_line = 0
-        now = datetime.datetime.now()
-        datatime_str = now.strftime("%Y-%m-%d %H:%M:%S")
-        source_file.write(f"{datatime_str}\n")
-        while True:
-            number_line += 1
-            new_line = input("Enter content line: ")
-            if new_line.lower() == "stop":
-                break
-            source_file.write(f"{number_line} {new_line}\n")
+
+def make_files() -> None:
+    if "-f" in sys.argv:
+        index_sys = sys.argv.index("-f")
+        with (open(os.path.join(parent_dir, sys.argv[index_sys + 1]), "a")
+              as source_file):
+            number_line = 0
+            now = datetime.datetime.now()
+            source_file.write(f"{now.strftime("%Y-%m-%d %H:%M:%S")}\n")
+            while True:
+                new_line = input("Enter content line: ")
+                number_line += 1
+                if new_line.lower() == "stop":
+                    source_file.write("\n")
+                    break
+                source_file.write(f"{number_line} {new_line}\n")
+
+
+if __name__ == "__main__":
+    make_directory()
+    make_files()
