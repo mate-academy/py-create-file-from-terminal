4c88aca8fe5dcd17edc650368470527661ac04c6
Ppllupold <kotnazar2005@gmail.com>
2024-06-27 16:46:51 +0300

    fixed flags order

--- app/create_file.py
+++ app/create_file.py
@@ -6,18 +6,21 @@ from datetime import datetime
 def main() -> None:
     parameters = sys.argv[1:]
 
-    if parameters[0] == "-d" and "-f" not in parameters:
-        create_directory(parameters[1:])
+    if "-d" in parameters and "-f" in parameters:
+        d_index = parameters.index("-d")
+        f_index = parameters.index("-f")
+        directory_parts = parameters[d_index + 1: f_index] \
+            if d_index < f_index \
+            else parameters[d_index + 1:]
 
-    if parameters[0] == "-f":
-        create_file(parameters[1])
-
-    if parameters[0] == "-d" and "-f" in parameters:
-        directory_parts = parameters[1: parameters.index("-f")]
-        file_name = parameters[parameters.index("-f") + 1]
+        file_name = parameters[f_index + 1]
         new_file_location = os.path.join(*directory_parts, file_name)
         create_directory(directory_parts)
         create_file(new_file_location)
+    elif "-d" in parameters:
+        create_directory(parameters[1:])
+    elif "-f" in parameters:
+        create_file(parameters[1])
 
 
 def create_directory(path_parts: list[str]) -> None:
@@ -27,21 +30,15 @@ def create_directory(path_parts: list[str]) -> None:
 
 
 def create_file(file_name: str) -> None:
-    if os.path.exists(file_name):
-        mode = "a"
-    else:
-        mode = "w"
-
+    mode = "a" if os.path.exists(file_name) else "w"
     with open(file_name, mode) as file:
         file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
         line_num = 1
         while True:
             text = input("Enter content line: ")
-
             if text == "stop":
                 file.write("\n")
                 break
-
             file.write(f"{line_num} {text}\n")
             line_num += 1
 
