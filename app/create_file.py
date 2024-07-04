626e8c6f702089f42d71ee1a733ca5256e25706f
Alex93Troy <troianovskiy93alex@gmail.com>
2024-07-04 11:03:07 +0200

    add fix

--- app/create_file.py
+++ app/create_file.py
@@ -4,14 +4,13 @@ from datetime import datetime
 
 
 def create_file(directory: str, filename: str, content_lines: list) -> None:
-
     filepath = os.path.join(directory, filename)
 
     try:
-        with open(filepath, "a") as file:
+        with open(filepath, "a", encoding="utf-8") as file:
             file.writelines(content_lines)
-    except:
-        raise ("Enter correct path, filepath isnt't correct")
+    except OSError as e:
+        raise OSError("Error, path isn't correct") from e
 
 
 def get_content_lines() -> list:
@@ -19,7 +18,7 @@ def get_content_lines() -> list:
     content_lines = [timestamp + "\n"]
     idx = 1
     while True:
-        line = input("Enter content line: ")
+        line = input("Enter content line (type 'stop' to finish): ")
         if line.lower() == "stop":
             break
         content_lines.append(f"{idx} {line}\n")
@@ -33,18 +32,22 @@ def main() -> None:
         description="Create directory or file with content"
     )
     parser.add_argument("-d", "--directory", nargs="+", help="Directory path")
-    parser.add_argument("-f", "--file", help="File name")
+    parser.add_argument("-f", "--file", required=True, help="File name")
 
     args = parser.parse_args()
 
-    if args.directory or args.file:
-        directory_path = (
-            os.path.join(*args.directory)
-        ) if args.directory else "."
-        os.makedirs(directory_path, exist_ok=True)
-        if args.file:
-            content_lines = get_content_lines()
-            create_file(directory_path, args.file, content_lines)
+    if args.directory:
+        directory_path = os.path.join(*args.directory)
+    else:
+        directory_path = "."
+
+    os.makedirs(directory_path, exist_ok=True)
+    
+    if args.file:
+        content_lines = get_content_lines()
+        create_file(directory_path, args.file, content_lines)
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
