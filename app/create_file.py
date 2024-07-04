8150393b7811b89a45abf7efeba37b9deed2e325
MRGL <Sonemongoo@gmail.com>
2024-07-04 12:33:10 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,14 +3,14 @@ import sys
 from datetime import datetime
 
 
-def create_directories(path_parts):
+def create_directories(path_parts: list[str]) -> str:
     path = os.path.join(*path_parts)
     os.makedirs(path, exist_ok=True)
     return path
 
 
-def create_file(file_path):
-    with open(file_path, 'a') as f:
+def create_file(file_path: str) -> None:
+    with open(file_path, "a") as f:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"{timestamp}\n")
         line_number = 1
@@ -22,19 +22,19 @@ def create_file(file_path):
             line_number += 1
 
 
-def main():
+def main() -> None:
     args = sys.argv[1:]
-    if '-d' in args:
-        d_index = args.index('-d')
-        if '-f' in args:
-            f_index = args.index('-f')
+    if "-d" in args:
+        d_index = args.index("-d")
+        if "-f" in args:
+            f_index = args.index("-f")
             dir_path_parts = args[d_index + 1:f_index]
             file_name = args[f_index + 1]
         else:
             dir_path_parts = args[d_index + 1:]
             file_name = None
-    elif '-f' in args:
-        f_index = args.index('-f')
+    elif "-f" in args:
+        f_index = args.index("-f")
         file_name = args[f_index + 1]
         dir_path_parts = []
     else:
@@ -44,7 +44,7 @@ def main():
     if dir_path_parts:
         dir_path = create_directories(dir_path_parts)
     else:
-        dir_path = '.'
+        dir_path = "."
 
     if file_name:
         file_path = os.path.join(dir_path, file_name)
