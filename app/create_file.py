643b3786d8361d88a09c80af9d62b2ff6808a20a
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-07-07 15:22:50 +0300

    "sol"

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,55 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def create_file(path: str, file_name: str) -> None:
+    file_path = os.path.join(path, file_name)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    mode = "a" if os.path.exists(path) else "w"
+
+    with open(file_path, mode) as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{timestamp}\n")
+
+        line_num = 1
+        while True:
+            content = input("Enter content line:")
+            if content.lower() == "stop":
+                break
+            file.write(f"Line{line_num} {content}\n")
+            line_num += 1
+
+
+def main() -> None:
+    if len(sys.argv) < 3:
+        print("Usage: python create_file.py -d dir1 dir2 -f file.txt")
+        return
+
+    if "-d" in sys.argv:
+        d_index = sys.argv.index("-d")
+        f_index = sys.argv.index("-f") if "-f" in sys.argv else None
+
+        if f_index:
+            dir_path = os.path.join(*sys.argv[d_index + 1:f_index])
+            filename = sys.argv[f_index + 1]
+        else:
+            dir_path = os.path.join(*sys.argv[d_index + 1:])
+            filename = None
+
+    elif "-f" in sys.argv:
+        f_index = sys.argv.index("-f")
+        dir_path = os.getcwd()
+        filename = sys.argv[f_index + 1]\
+
+    if filename:
+        create_file(dir_path, filename)
+    os.makedirs(dir_path, exist_ok=True)
+    print(f"Directory {dir_path} created.")
+
+
+if __name__ == "__main__":
+    main()
