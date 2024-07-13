316ac1e047c9ae777202f10dbe5cf196c057769a
bythewaters <bythewaters800@gmail.com>
2024-07-13 14:53:36 +0200

    create_file

--- app/create_file.py
+++ app/create_file.py
@@ -3,25 +3,33 @@ import sys
 from datetime import datetime
 
 
-def create_file_from_terminal(command: str) -> None:
+def create_path(path: str) -> None:
+    file_path = path.strip().split("-d")[-1].strip().split()
+    full_path = os.path.join(*[os.getcwd(), *file_path])
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+    return
+
+
+def create_file(command: list[str]) -> None:
+    command = " ".join(command)
+    file_name = None
     full_path = None
+    if "-f" in command:
+        file_name = command.strip().split("-f")[1].strip()
+        full_path = file_name
+
     if "-d" in command and "-f" in command:
         file_path = command.strip(
 
         ).split("-f")[0].split("-d")[-1].strip().split()
-        file_name = command.strip().split("-f")[1].strip()
         full_path = os.path.join(*[os.getcwd(), *file_path, file_name])
+        create_path(os.path.join(os.getcwd(), *file_path))
 
-    elif "-d" in command:
-        file_path = command.strip().split("-d")[-1].strip().split()
-        full_path = os.path.join(*[os.getcwd(), *file_path])
-        if not os.path.exists(full_path):
-            os.makedirs(full_path)
-        return
-
-    elif "-f" in command:
-        file_name = command.strip().split("-f")[1]
-        full_path = file_name
+    if full_path is None:
+        if "-d" in command:
+            create_path(command)
+            return
 
     mode = "a" if os.path.exists(full_path) else "w"
 
@@ -37,4 +45,5 @@ def create_file_from_terminal(command: str) -> None:
         file.write("\n")
 
 
-create_file_from_terminal(sys.argv[1])
+if __name__ == "__main__":
+    create_file(sys.argv)
