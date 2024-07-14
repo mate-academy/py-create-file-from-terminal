67173ac6c06785ff7075a5cc64009cac3e05d973
Daniil <daniilrizhenko@gmail.com>
2024-07-14 15:28:31 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -3,47 +3,52 @@ import os
 from datetime import datetime
 
 
-def create_path(user_input: str) -> str:
-    args = user_input.split()
-
-    if "-f" in args:
-        file_path = args[args.index("-f") + 1]
-    elif "-d" in args:
-        dir_path = os.path.join(*args[args.index("-d") + 1:])
-        os.makedirs(dir_path, exist_ok=True)
-        file_path = os.path.join(dir_path, "file.txt")
-    elif "-d" in args and "-f" in args:
-        dir_path = os.path.join(
-            *args[args.index("-d") + 1:args.index("-f")]
-        )
-        file_name = args[args.index("-f") + 1]
-        os.makedirs(dir_path, exist_ok=True)
-        file_path = os.path.join(dir_path, file_name)
-    else:
-        raise ValueError("Invalid arguments")
-
-    return file_path
+def create_directory(path_parts: str) -> None:
+    path = os.path.join(*path_parts)
+    os.makedirs(path, exist_ok=True)
 
 
-def main() -> None:
-    try:
-        file_path = create_path(" ".join(sys.argv[1:]))
-    except ValueError as e:
-        print(e)
-        return
+def create_file(file_path: str) -> None:
+    if not os.path.exists(file_path):
+        open(file_path, "w").close()
 
-    if os.path.exists(file_path):
-        mode = "a"
-    else:
-        mode = "w"
+    with open(file_path, "a") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    with open(file_path, mode) as file:
-        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        file.write(f"\n{timestamp}\n")
 
         line_number = 1
         while True:
-            content = input("Enter content line: ")
-            if content.lower() == "stop":
+            content_line = input("Enter content line: ")
+            if content_line.lower() == "stop":
                 break
-            file.write(f"Line{line_number} {content}\n")
+            file.write(f"{line_number} {content_line}\n")
             line_number += 1
+    print(f"File {file_path} updated successfully.")
+
+
+def main() -> None:
+    args = sys.argv[1:]
+    if "-d" in args:
+        d_index = args.index("-d")
+        f_index = args.index("-f") if "-f" in args else None
+
+        if f_index:
+            directories = args[d_index + 1:f_index]
+            file_name = args[f_index + 1]
+            create_directory(directories)
+            file_path = os.path.join(*directories, file_name)
+            create_file(file_path)
+        else:
+            directories = args[d_index + 1:]
+            create_directory(directories)
+    elif "-f" in args:
+        f_index = args.index("-f")
+        file_name = args[f_index + 1]
+        create_file(file_name)
+    else:
+        print("Invalid arguments.")
+
+
+if __name__ == "__main__":
+    main()
