fbfc326e4cb8994a2fd6ffa03051000d846e45b4
Mikola Rozhansky <bymikola@gmail.com>
2024-07-18 19:42:45 +0300

    new release 11

--- app/create_file.py
+++ app/create_file.py
@@ -10,78 +10,20 @@ def parser_simple(key: str) -> str:
         if key in arg:
             out = args[i + 1:]
 
-            for j, new_arg in enumerate(out):
+            for count, new_arg in enumerate(out):
                 if "-" in new_arg:
-                    out = args[i + 1:i + j + 1]
+                    out = args[i + 1:i + count + 1]
                     break
 
     print("Path = ", out)
     return out
 
 
-
 def create_argument() -> dict:
     out = {}
-    args = sys.argv
-
-    new_path = ""
-    new_filename = ""
-    print(args)
-    print(parser_simple("-d"))
-    print(parser_simple("-f"))
-
-
-    # for i, arg in enumerate(args):
-    #     if "-d" in arg:
-    #         path = args[i + 1:]
-    #
-    #         for j, new_arg in enumerate(path):
-    #             if "-" in new_arg:
-    #                 path = args[i + 1:i + j + 1]
-    #                 break
-    #
-    # print("Path = ", path)
-
-    # if arg in "-f":
-    #     new_args = args[i + 1:]
-    #     print("New str f:", new_args)
-    #     for j, new_arg in enumerate(new_args):
-    #         if "-" in new_arg:
-    #             print("Bingo f")
-    #             filename = args[i + 1:j]
-    #     filename = args[args.index("-f"):]
 
     new_filename = " ".join(parser_simple("-f"))
     new_path = os.path.join("/".join(parser_simple("-d")))
-
-    print(new_filename)
-    print(new_path)
-
-    # if ("-d" in args) and ("-f" in args):
-    #     print("Directory and File")
-    #     filename = args[args.index("-f"):]
-    #     path = args[args.index("-d"):args.index("-f")]
-    #
-    #     new_filename = " ".join(filename[1:])
-    #     new_path = os.path.join("/".join(path[1:]))
-    #
-    #     print("File:", filename[1:])
-    #     print(new_filename)
-    #     print("Path:", path[1:])
-    #     print(new_path)
-    #
-    # elif "-d" in args:
-    #     print("only Directory")
-    #     path = args[args.index("-d"):]
-    #     new_path = os.path.join("/".join(path[1:]))
-    #     print(new_path)
-    # elif "-f" in args:
-    #     print("only File")
-    #     filename = args[args.index("-f"):]
-    #     new_filename = " ".join(filename[1:])
-    #     print(new_filename)
-    # else:
-    #     print("No key")
 
     if new_path:
         out["path"] = new_path
@@ -90,21 +32,6 @@ def create_argument() -> dict:
         out["filename"] = new_filename
 
     return out
-
-
-# def create_argument_new() -> dict:
-#     out = {}
-#
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-d")
-#     parser.add_argument("-f")
-#     args = parser.parse_args()
-#
-#     out["path"] = args.d
-#     out["filename"] = args.f
-#     print(out)
-#
-#     return out
 
 
 def create_dir(input_arg: dict) -> str:
@@ -143,7 +70,5 @@ def create_file(input_arg: dict) -> None:
             file.write(f"{i + 1} {data}\n")
 
 
-# create_argument_new()
-# # create_file(create_argument())
 if __name__ == "__main__":
     create_file(create_argument())
