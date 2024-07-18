582784438d43a4e20df1067a10977124ff401a43
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-18 13:58:18 +0300

    refactored some long lines

--- app/create_file.py
+++ app/create_file.py
@@ -9,23 +9,25 @@ def create_file(argv: list[str]) -> None:
     previous_dir = ""
     for i, param in enumerate(argv):
         if param == "-d":
-            for j in range(i + 1, len(argv)):
-                if argv[j] == "-f":
+            for dir_name in range(i + 1, len(argv)):
+                if argv[dir_name] == "-f":
                     break
 
-                os.mkdir(previous_dir + argv[j])
-                previous_dir += argv[j] + "/"
+                os.mkdir(previous_dir + argv[dir_name])
+                previous_dir += argv[dir_name] + "/"
         if param == "-f":
             if os.path.exists(previous_dir + argv[i + 1]):
                 with open(previous_dir + argv[i + 1], "a") as file:
                     has_file = True
                     file_name = previous_dir + argv[i + 1]
-                    file.write("\n" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+                    file.write("\n" + datetime.datetime.now()
+                               .strftime("%Y-%m-%d %H:%M:%S") + "\n")
             else:
                 with open(previous_dir + argv[i + 1], "x") as file:
                     has_file = True
                     file_name = previous_dir + argv[i + 1]
-                    file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+                    file.write(datetime.datetime.now()
+                               .strftime("%Y-%m-%d %H:%M:%S") + "\n")
 
     i = 0
     while has_file:
