dcd5c0a879d70f21b0d226b2d5642d3257749020
Vova <v.kampanchenko@gmail.com>
2024-07-18 16:42:29 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -27,16 +27,42 @@ def main() -> None:
     if len(sys.argv) < 3:
         print("Usage: python create_file.py [-d path_parts] [-f file_name]")
         sys.exit(1)
-
-    flag = sys.argv[1]
-    if flag == "-d":
-        path_parts = sys.argv[2:]
+    try:
+        flag_d = sys.argv.index("-d")
+        flag_f = sys.argv.index("-f")
+    except ValueError:
+        try:
+            flag_d = sys.argv.index("-d")
+        except ValueError:
+            try:
+                flag_f = sys.argv.index("-f")
+            except ValueError:
+                print("Invalid flag. Use either -d or -f.")
+                sys.exit(1)
+            else:
+                file_name = sys.argv[flag_f + 1]
+                if os.path.exists(file_name):
+                    print(
+                        f"File '{file_name}' already exists."
+                        f"Appending content below:"
+                    )
+                else:
+                    print(f"Creating new file: {file_name}")
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                with open(file_name, "a") as file:
+                    file.write(f"\n\n{timestamp}\n")
+                    create_file(file_name)
+        else:
+            path_parts = sys.argv[flag_d + 1:]
+            create_directory(path_parts)
+    else:
+        path_parts = sys.argv[flag_d + 1: flag_f]
         create_directory(path_parts)
-    elif flag == "-f":
-        file_name = sys.argv[2]
+        file_name = sys.argv[flag_f + 1]
         if os.path.exists(file_name):
             print(
-                f"File '{file_name}' already exists. Appending content below:"
+                f"File '{file_name}' already exists."
+                f"Appending content below:"
             )
         else:
             print(f"Creating new file: {file_name}")
@@ -44,9 +70,6 @@ def main() -> None:
         with open(file_name, "a") as file:
             file.write(f"\n\n{timestamp}\n")
             create_file(file_name)
-    else:
-        print("Invalid flag. Use either -d or -f.")
-        sys.exit(1)
 
 
 if __name__ == "__main__":
