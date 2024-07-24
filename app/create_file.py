b35667c2808d28034d889d6f014cba700a310062
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-07-24 13:04:27 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,36 +3,42 @@ import sys
 from datetime import datetime
 
 
-def create_directory(directories: str) -> str:
+def create_directory(directories: list[str]) -> str:
     path = os.getcwd()
-    path = os.path.join(path, *directories)
-    os.makedirs(path)
+    for directory in directories:
+        path = os.path.join(path, directory)
+    os.makedirs(path, exist_ok=True)
     return path
 
 
 def file_creator(file_name: str) -> None:
     with open(file_name, "a") as f:
         pass
-    content = []
+    file_content = []
     i = 1
     while True:
         content_line = input("Enter content line: ")
         if content_line == "stop":
             break
-        content.append(f"{i} {content_line}")
+        file_content.append(f"{i} {content_line}")
         i += 1
-    if content:
+    if file_content:
         with open(file_name, "a") as f:
             time_now = datetime.now()
             f.write(time_now.strftime("%m-%d-%Y %H:%M:%S\n"))
-            for line in content:
+            for line in file_content:
                 f.write(line + "\n")
+            f.write("\n")
 
 
 def create_file() -> None:
     if "-f" in sys.argv and "-d" in sys.argv:
-        directories = sys.argv[sys.argv.index("-d") + 1: sys.argv.index("-f")]
-        file_ = sys.argv[sys.argv.index("-f") + 1]
+        if sys.argv.index("-d") < sys.argv.index("-f"):
+            directories = sys.argv[sys.argv.index("-d") + 1: sys.argv.index("-f")]
+            file_ = sys.argv[sys.argv.index("-f") + 1]
+        else:
+            directories = sys.argv[sys.argv.index("-d") + 1:]
+            file_ = sys.argv[sys.argv.index("-f") + 1: sys.argv.index("-d")]
         file_name = os.path.join(create_directory(directories), file_)
         file_creator(file_name)
     elif "-f" in sys.argv:
