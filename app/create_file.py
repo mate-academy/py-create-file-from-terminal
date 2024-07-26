5ebf9f63447a6e4f37be05b40589e28b7761deb8
Danylo Skepskyi <kol230305@gmail.com>
2024-07-26 18:17:34 +0200

    Soliton

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,51 @@
-# write your code here
+from sys import argv
+from os import makedirs, path
+from datetime import datetime
+
+
+def create_directory(directories_path: list[str]) -> None:
+    directories_path = path.join(*directories_path)
+    makedirs(directories_path, exist_ok=True)
+
+
+def create_file(file: str) -> None:
+    date_today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    file_data = []
+
+    while True:
+        new_line = input("Enter content line: ")
+        if new_line.strip().lower() == "stop":
+            break
+        file_data.append(new_line)
+
+    with open(file, "w") as opened_file:
+        opened_file.write(f"{date_today}\n")
+        for line in file_data:
+            opened_file.write(f"{line}\n")
+
+
+if __name__ == '__main__':
+    commands = argv[1:]
+    if not commands:
+        raise ValueError("No command provided, such as '-f' or '-d'.")
+
+    if "-d" in commands and "-f" in commands:
+        d_index = commands.index("-d")
+        f_index = commands.index("-f")
+
+        if d_index > f_index:
+            directory = commands[d_index + 1:]
+        elif f_index > d_index:
+            directory = commands[d_index + 1: f_index]
+        file_name = commands[f_index + 1]
+        file_path = path.join(*directory, file_name)
+        create_directory(directory)
+        create_file(file_path)
+
+    elif "-d" in commands:
+        d_index = commands.index("-d")
+        create_directory(commands[d_index + 1:])
+
+    elif "-f" in commands:
+        f_index = commands.index("-f")
+        create_file(commands[f_index + 1])
