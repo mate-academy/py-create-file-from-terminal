cdfa24ab82a6002d8f4756dacbc74297cc43bd56
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-07-29 21:13:10 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,29 +3,31 @@ import argparse
 from datetime import datetime
 
 
-parser = argparse.ArgumentParser()
+def create_file() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", type=str, nargs="+")
+    parser.add_argument("-f", "--filename", type=str)
+    args = parser.parse_args()
 
-parser.add_argument("-d", "--directory", type=str, nargs="+")
-parser.add_argument("-f", "--filename", type=str)
+    if args.directory:
+        directory_path = os.path.join(*args.directory)
+        os.makedirs(directory_path, exist_ok=True)
+    else:
+        directory_path = os.getcwd()
 
-args = parser.parse_args()
+    if args.filename:
+        filepath = os.path.join(directory_path, args.filename)
 
-if args.directory:
-    directory_path = os.path.join(*args.directory)
-    os.makedirs(directory_path, exist_ok=True)
-else:
-    directory_path = os.getcwd()
+        with open(filepath, "a") as file:
+            file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+            i = 1
+            while True:
+                content = input("Enter content line: ")
+                if content == "stop":
+                    file.write("\n")
+                    break
+                file.write(f"{i} " + content + "\n")
+                i += 1
 
-if args.filename:
-    filepath = os.path.join(directory_path, args.filename)
 
-    with open(filepath, "a") as file:
-        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
-        i = 1
-        while True:
-            content = input("Enter content line: ")
-            if content == "stop":
-                file.write("\n")
-                break
-            file.write(f"{i} " + content + "\n")
-            i += 1
+create_file()
