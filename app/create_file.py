9c4c089768a5ef60c9f9b62496758b74877a72ae
Vadym Piddubnyi <leegrad@ukr.net>
2024-07-30 11:47:45 -0500

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,27 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def main() -> None:
+    command = sys.argv
+    if "-d" in command:
+        dir_path = os.path.join("/".join(command[2:]))
+        os.makedirs(dir_path, exist_ok=True)
+    elif "-f" in command:
+        name = "".join(command[2:])
+        with open(name, "a") as file:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(f"{timestamp}\n")
+            counter = 0
+            while True:
+                counter += 1
+                content = input("Enter content line (type stop to finish): ")
+                if content.lower() == "stop":
+                    break
+                else:
+                    file.write(f"{counter} {content}\n")
+
+
+if __name__ == "__main__":
+    main()
