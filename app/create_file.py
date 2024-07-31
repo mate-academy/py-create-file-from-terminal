333deab18c44959856c580dffda70fded05e1344
Vadym Piddubnyi <leegrad@ukr.net>
2024-07-31 13:51:45 -0500

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,26 +1,44 @@
-import sys
 import os
+import sys
 from datetime import datetime
 
 
-def main() -> None:
-    command = sys.argv
-    if "-d" in command:
-        dir_path = os.path.join("/".join(command[2:]))
-        os.makedirs(dir_path, exist_ok=True)
-    elif "-f" in command:
-        name = "".join(command[2:])
-        with open(name, "a") as file:
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            file.write(f"{timestamp}\n")
-            counter = 0
-            while True:
+def create_directories(args: list[str]) -> None:
+    dir_path = os.path.join(*args)
+    os.makedirs(dir_path, exist_ok=True)
+
+
+def create_file(file_name: str) -> None:
+    with open(file_name, "a") as new_file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_file.write(f"{timestamp}\n")
+        counter = 1
+        while True:
+            text = input("Enter content line (or type stop to finish): ")
+            if text.lower() == "stop":
+                break
+            else:
+                new_file.write(f"{counter} {text}\n")
                 counter += 1
-                content = input("Enter content line (type stop to finish): ")
-                if content.lower() == "stop":
-                    break
-                else:
-                    file.write(f"{counter} {content}\n")
+
+
+def main() -> None:
+    arg = sys.argv
+    if "-d" in arg:
+        d_index = arg.index("-d")
+        if "-f" in arg:
+            f_index = arg.index("-f")
+            path_dir = arg[d_index + 1:f_index]
+            create_directories(path_dir)
+            path_file = arg[f_index + 1]
+            create_file(path_file)
+        else:
+            path_dir = arg[d_index + 1:]
+            create_directories(path_dir)
+    elif "-f" in arg:
+        f_index = arg.index("-f")
+        path_file = arg[f_index + 1]
+        create_file(path_file)
 
 
 if __name__ == "__main__":
