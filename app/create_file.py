bd99411029fd8a2170cf9a9742143db2c46a50fa
Vadym Piddubnyi <leegrad@ukr.net>
2024-08-01 13:32:38 -0500

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -24,7 +24,7 @@ def create_file(file_name: str) -> None:
 
 def main() -> None:
     arg = sys.argv
-    if "-d" in arg:
+    if "-d" in arg[:2]:
         d_index = arg.index("-d")
         if "-f" in arg:
             f_index = arg.index("-f")
@@ -35,10 +35,17 @@ def main() -> None:
         else:
             path_dir = arg[d_index + 1:]
             create_directories(path_dir)
-    elif "-f" in arg:
+    elif "-f" in arg[:2]:
         f_index = arg.index("-f")
-        path_file = arg[f_index + 1]
-        create_file(path_file)
+        if "-d" in arg:
+            d_index = arg.index("-d")
+            path_file = arg[f_index + 1]
+            create_file(path_file)
+            path_dir = arg[d_index + 1:]
+            create_directories(path_dir)
+        else:
+            path_file = arg[f_index + 1]
+            create_file(path_file)
 
 
 if __name__ == "__main__":
