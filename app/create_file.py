47601cbe14f6f9296a23da597e234aaed755cbb4
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-08-11 11:04:12 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,52 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+from typing import Any
+
+
+data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def make_directory(name_dir: str | bytes) -> Any:
+    os.makedirs(name_dir, exist_ok=True)
+    return name_dir
+
+
+def create_file(file_name: str) -> Any:
+    is_file = os.path.isfile(file_name)
+
+    with open(file_name, "a") as new_file:
+        line_number = 1
+        new_file.write(f"{data}\n")
+        while True:
+            new_line = input("Enter content line:")
+
+            if not is_file:
+                new_file.write(f"{line_number} {new_line} \n")
+
+            if is_file:
+                new_file.write(f"{line_number} Another {new_line} \n")
+
+            line_number += 1
+            if new_line.lower() == "stop":
+                break
+
+        new_file.write("\n")
+
+
+command = sys.argv[1:]
+f_ind = command.index("-f")
+d_ind = command.index("-d")
+
+if "-f" not in command:
+    make_directory(os.path.join(*command[d_ind + 1:]))
+elif "-d" not in command:
+    create_file(command[-1])
+elif "-d" and "-f" in command:
+    if d_ind < f_ind:
+        file_dir = make_directory(os.path.join(*command[d_ind + 1:f_ind]))
+        file_path = os.path.join(file_dir, command[-1])
+    else:
+        file_dir = make_directory(os.path.join(*command[d_ind + 1:]))
+        file_path = os.path.join(file_dir, command[1])
+    create_file(file_path)
