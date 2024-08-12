074e7f62d9306243a9c8a9295d0508ff728d287f
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-08-12 15:36:27 +0300

    'flake8_adjustments'

--- app/create_file.py
+++ app/create_file.py
@@ -9,7 +9,7 @@ def create_directory(path_ending: list[str]) -> None:
 
 
 def create_file(filename: str, content_lines: list[str]) -> None:
-    content = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
+    content = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n'
 
     for i, line in enumerate(content_lines, start=1):
         content += f"{i} {line}\n"
