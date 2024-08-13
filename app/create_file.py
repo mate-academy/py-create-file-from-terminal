906e33831ff4669a95e520e4e45bc5c02a8c9ae6
Andrii <andrii.khamaza0@gmail.com>
2024-08-13 13:35:32 +0300

    add using argparse module

--- app/create_file.py
+++ app/create_file.py
@@ -1,26 +1,6 @@
 import os
-import sys
 import datetime
-
-
-def get_dirs(terminal_req_arr: list) -> list:
-    if "-d" not in terminal_req_arr:
-        return []
-
-    dir_list = []
-    for dir_name in terminal_req_arr[terminal_req_arr.index("-d") + 1:]:
-        if dir_name[0] == "-":
-            break
-        dir_list.append(dir_name)
-
-    return dir_list
-
-
-def get_file_name(terminal_req_arr: list) -> str:
-    if "-f" not in terminal_req_arr:
-        return ""
-
-    return terminal_req_arr[-1]
+import argparse
 
 
 def write_content_to_file(path: str) -> None:
@@ -43,14 +23,18 @@ def write_content_to_file(path: str) -> None:
 
 
 def create_file() -> None:
-    arr = sys.argv
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directories", nargs="*", type=str, default=[])
+    parser.add_argument("-f", "--file_name", type=str)
 
-    file_name = get_file_name(arr)
-    dir_path = os.path.join(*get_dirs(arr))
+    args = parser.parse_args()
+
+    file_name = args.file_name
+    dir_path = os.path.join(*args.directories)
 
     os.makedirs(dir_path, exist_ok=True)
 
-    if file_name != "":
+    if file_name:
         write_content_to_file(os.path.join(dir_path, file_name))
 
 
