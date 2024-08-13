d105163e3b423a56fdda0d21240831268f94dbfd
Stepan <oleksiukstepann@gmail.com>
2024-08-13 14:00:10 +0300

    "fixed mistakes"

--- app/create_file.py
+++ app/create_file.py
@@ -4,40 +4,52 @@ from datetime import datetime
 
 
 def create_file() -> None:
-    args = sys.argv[1:]
-    directory = ""
+    modes, path = parse_arguments(sys.argv[1:])
 
-    if args[0] == "-d":
-        directory = args[1:]
-        if "-f" in args:
-            directory = directory[:args.index("-f") - 1]
-            args = args[args.index("-f"):]
+    if "-d" in modes:
+        _create_dir(modes, path)
+
+    if "-f" in modes:
+        text = get_text_from_user()
 
         try:
-            os.makedirs("/".join(directory))
-        except FileExistsError:
-            print("Directory already exist")
-
-    if args[0] == "-f":
-        try:
-            _make_file(directory, args[1])
+            _write_text_to_file(path, text)
         except FileNotFoundError:
             print("File not found")
 
 
-def _make_file(directory: list[str], filename: str) -> None:
-    mode = "w"
-    if os.path.exists(filename):
-        mode = "a"
-    with open(os.path.join(*directory, filename), mode) as file:
-        current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(current_date + "\n")
+def parse_arguments(args: list) -> tuple:
+    modes = []
 
-        count = 1
-        while True:
-            content = input("Enter content line: ")
-            if content == "stop":
-                break
-            file.write(f"{count} {content}\n")
-            count += 1
-        file.write("\n")
+    for symbol in ["-d", "-f"]:
+        if symbol in args:
+            modes.append(symbol)
+            args.remove(symbol)
+
+    return modes, os.path.join(*args)
+
+
+def _create_dir(modes: list, path: str) -> None:
+    directory = os.path.dirname(path) if "-f" in modes else path
+    os.makedirs(directory, exist_ok=True)
+
+
+def get_text_from_user() -> str:
+    count = 1
+    text = ""
+
+    while True:
+        content = input("Enter content line: ")
+        if content == "stop":
+            break
+        text += f"{count} {content}\n"
+        count += 1
+
+    return text + "\n"
+
+
+def _write_text_to_file(path: str, text: str) -> None:
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(path, "a") as file:
+        file.write(current_date + "\n")
+        file.write(text)
