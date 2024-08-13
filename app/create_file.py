1c611fed6d64609981a81bf9b8a9d534bec95c00
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-08-13 15:58:07 +0300

    Solution7

--- app/create_file.py
+++ app/create_file.py
@@ -7,12 +7,15 @@ import datetime
 
 def get_command() -> None:
     if "-d" in command and "-f" not in command:
-        path_dest = os.path.join(*command[command.index("-d") + 1:])
+        path_dest = os.path.join(*command[command.index("-d") + 1 :])
         create_directory(path_dest)
     elif "-d" in command and "-f" in command:
+        index_d = command.index("-d")
         index_f = command.index("-f")
-        path_dest = os.path.join(*command[command.index("-d") + 1:index_f])
+
+        path_dest = os.path.join(*command[index_d + 1 : index_f])
         create_directory(path_dest)
+
         create_file(path_dest, command[index_f + 1])
     elif "-f" in command and "-d" not in command:
         create_file("", command[command.index("-f") + 1])
@@ -30,9 +33,8 @@ def create_file(path: str, file_name: str) -> None:
 
 def write_to_file(path_file: str, mode: str) -> None:
     with open(path_file, mode) as file:
-        file.write(
-            f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n'
-        )
+        file.write(f'{datetime.
+                   datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
         line_number = 1
         while True:
             text = input("Enter content line: ")
