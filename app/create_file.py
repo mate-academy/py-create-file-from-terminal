e6934a3577f2c9794d60f4be24f5ab02460ee46f
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-08-13 13:02:28 +0300

    Solution3

--- app/create_file.py
+++ app/create_file.py
@@ -16,20 +16,38 @@ def work_with_file(path: str) -> None:
             counter += 1
 
 
-directory_name = ""
+def parse_arguments() -> tuple:
+    directory_name = ""
+    file_name = ""
 
-if "-d" in sys.argv:
-    directory_name = (
-        os.path.join(*sys.argv[sys.argv.index("-d") + 1:])
-        if "-f" not in sys.argv
-        else os.path.join(
-            *sys.argv[sys.argv.index("-d") + 1: sys.argv.index("-f")]
+    if "-d" in sys.argv:
+        directory_name = (
+            os.path.join(*sys.argv[sys.argv.index("-d") + 1:])
+            if "-f" not in sys.argv
+            else os.path.join(
+                *sys.argv[sys.argv.index("-d") + 1: sys.argv.index("-f")]
+            )
         )
-    )
-    if not os.path.isdir(directory_name):
-        os.makedirs(directory_name)
+        if not os.path.isdir(directory_name):
+            os.makedirs(directory_name)
 
-if "-f" in sys.argv:
-    file_name = sys.argv[sys.argv.index("-f") + 1]
-    path_name = os.path.join(directory_name, file_name)
-    work_with_file(path_name)
+    if "-f" in sys.argv:
+        file_name = sys.argv[sys.argv.index("-f") + 1]
+        if not directory_name:
+            directory_name = os.getcwd()
+
+    return directory_name, file_name
+
+
+def main() -> None:
+    directory_name, file_name = parse_arguments()
+    if file_name:
+        path_name = os.path.join(directory_name, file_name)
+        work_with_file(path_name)
+    else:
+        print("Filename not provided. "
+              "Use the -f option to specify the filename.")
+
+
+if __name__ == "__main__":
+    main()
