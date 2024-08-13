e6fdb31f78988877c24f92deea33e5f02a7da1a1
Denys Piuro <denys.pyuro@gmail.com>
2024-08-13 12:36:15 +0300

    Updated according to the last Olga's remarks

--- app/create_file.py
+++ app/create_file.py
@@ -1,40 +1,50 @@
-import sys
+import argparse
 import os
 from datetime import datetime
 
 
-def get_mode(file_path: str) -> str:
-    return "a" if os.path.exists(file_path) else "w"
-
-
 def write_content_to_file(file_path: str) -> None:
-    with open(file_path, get_mode(file_path)) as f:
+    with open(file_path, "a") as f:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"\n{timestamp}\n")
         line_number = 1
         while True:
             content = input("Enter content line: ")
             if content.lower() == "stop":
+                f.write("\n")
                 break
             f.write(f"{line_number} {content}\n")
             line_number += 1
 
 
 def main() -> None:
-    args: list[str] = sys.argv[1:]
+    parser = argparse.ArgumentParser(description="Create file with content.")
+
+    parser.add_argument(
+        "-d", "--directory",
+        type=str,
+        nargs="+",
+        help="Directory path where file should be created."
+    )
+    parser.add_argument(
+        "-f", "--file",
+        type=str,
+        help="File name to be created or appended.")
+
+    args = parser.parse_args()
+
     dir_path = None
 
-    if "-d" in args:
-        d_index = args.index("-d")
-        dir_path = os.path.join(*args[d_index + 1:])
+    if args.directory:
+        dir_path = os.path.join(*args.directory)
         os.makedirs(dir_path, exist_ok=True)
 
-    if "-f" in args:
-        f_index = args.index("-f")
-        file_name = args[f_index + 1]
+    if args.file:
+        file_name = args.file
         file_path = (
             os.path.join(dir_path, file_name)
             if dir_path
             else file_name
         )
+
         write_content_to_file(file_path)
