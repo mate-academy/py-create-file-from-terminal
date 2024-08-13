ea9c64da258ff3f4cb7070ac3f6d6ca1000b8274
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-08-13 15:48:39 +0300

    Solution6

--- app/create_file.py
+++ app/create_file.py
@@ -1,27 +1,29 @@
 import sys
+
 import os
+
 import datetime
 
 
 def get_command() -> None:
-    if "-d" in commamd and "-f" not in commamd:
-        path_dest = os.path.join(*commamd[commamd.index("-d") + 1:])
+    if "-d" in command and "-f" not in command:
+        path_dest = os.path.join(*command[command.index("-d") + 1:])
         create_directory(path_dest)
-    if "-d" in commamd and "-f" in commamd:
-        index_f = commamd.index("-f")
-        path_dest = os.path.join(*commamd[commamd.index("-d") + 1:index_f])
+    elif "-d" in command and "-f" in command:
+        index_f = command.index("-f")
+        path_dest = os.path.join(*command[command.index("-d") + 1:index_f])
         create_directory(path_dest)
-        create_file(path_dest)
-    if "-f" in commamd and "-d" not in commamd:
-        create_file("")
+        create_file(path_dest, command[index_f + 1])
+    elif "-f" in command and "-d" not in command:
+        create_file("", command[command.index("-f") + 1])
 
 
 def create_directory(path: str) -> None:
     os.makedirs(path, exist_ok=True)
 
 
-def create_file(path: str) -> None:
-    path = os.path.join(path, commamd[-1])
+def create_file(path: str, file_name: str) -> None:
+    path = os.path.join(path, file_name)
     mode = "a" if os.path.exists(path) else "w"
     write_to_file(path, mode)
 
@@ -31,13 +33,15 @@ def write_to_file(path_file: str, mode: str) -> None:
         file.write(
             f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n'
         )
+        line_number = 1
         while True:
             text = input("Enter content line: ")
-            if text == "stop":
+            if text.lower() == "stop":
                 break
-            file.write(f"{text}\n")
+            file.write(f"{line_number} {text}\n")
+            line_number += 1
 
 
-commamd = sys.argv
+command = sys.argv
 if __name__ == "__main__":
     get_command()
