8b6befec8295290c665945fb1419658068dd60c2
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-13 11:54:03 +0300

    Fixed

--- app/create_file.py
+++ app/create_file.py
@@ -1,35 +1,47 @@
-from datetime import datetime
-import sys
 import os
-
+import sys
+from datetime import datetime
+from typing import LiteralString
 
 data = sys.argv[1:]
-f_index = data.index("-f")
 
-if "-d" in data:
-    d_index = data.index("-d")
+
+def create_dirs(sys_data: list, f_index) -> LiteralString | str | bytes:
+    d_index = sys_data.index("-d")
     if f_index > d_index:
-        d_data = data[d_index + 1: f_index]
+        d_data = sys_data[d_index + 1: f_index]
     else:
-        d_data = data[d_index + 1:]
-
+        d_data = sys_data[d_index + 1:]
     path_dirs = os.path.join(*d_data)
-    file_path = os.path.join(path_dirs, data[f_index + 1])
     os.makedirs(path_dirs, exist_ok=True)
+    return os.path.join(*d_data, sys_data[f_index + 1])
 
-else:
-    file_path = os.path.join(data[f_index + 1])
 
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-counter = 1
-info_from_user = f"{current_time}\n"
+def write_data(path: LiteralString | str | bytes) -> None:
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    counter = 1
+    info_from_user = f"{current_time}\n"
 
-while True:
-    line_from_user = input("Enter content line: ")
-    if line_from_user == "stop":
-        break
-    info_from_user += f"{counter} {line_from_user}\n"
-    counter += 1
+    while True:
+        line_from_user = input("Enter content line: ")
+        if line_from_user == "stop":
+            info_from_user += "\n"
+            break
+        info_from_user += f"{counter} {line_from_user}\n"
+        counter += 1
 
-with open(file_path, "a") as file:
-    file.write(info_from_user)
+    with open(path, "a") as file:
+        file.write(info_from_user)
+
+
+def create_file(sys_data: list) -> None:
+    f_index = sys_data.index("-f")
+    if "-d" in sys_data:
+        path = create_dirs(sys_data, f_index)
+    else:
+        path = os.path.join(sys_data[f_index + 1])
+    write_data(path)
+
+
+if __name__ == "__main__":
+    create_file(data)
