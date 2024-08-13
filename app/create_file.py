12ed669c8dd87b2e9b91e679d42d54fcbd24b8a7
andreykolomiec <akolomiec982@gmail.com>
2024-08-13 21:08:16 +0300

    work code

--- app/create_file.py
+++ app/create_file.py
@@ -1,14 +1,14 @@
 import sys
 import os
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 
 def create_path(path_dir: List[str]) -> str:
     full_path = os.path.join(*path_dir)
     os.makedirs(full_path, exist_ok=True)
     print(f"Directory created: {full_path}")
-    return full_path
+    return str(full_path)
 
 
 def create_file(file_path: str) -> None:
@@ -26,32 +26,34 @@ def create_file(file_path: str) -> None:
 
 def main() -> None:
     if len(sys.argv) < 2:
-        print("Usage: python create_file [-d directory_path] [-f file_name]")
+        print(
+            "Usage:"
+            " python create_file.py [-d directory_path] [-f file_name]"
+        )
         return
+
+    dir_parts: List[str] = []
+    file_name: Optional[str] = None
+
     if "-d" in sys.argv:
         d_index = sys.argv.index("-d")
         dir_parts = sys.argv[d_index + 1:]
-        if "-f" in dir_parts:
-            f_index = dir_parts.index("-f")
-            dir_parts, file_name = dir_parts[:f_index], dir_parts[f_index + 1]
-        else:
-            file_name = None
 
-        dir_path = create_path(dir_parts)
-
-        if file_name:
-            file_path = os.path.join(dir_path, file_name)
-            create_file(file_path)
-
-    elif "-f" in sys.argv:
+    if "-f" in sys.argv:
         f_index = sys.argv.index("-f")
         file_name = sys.argv[f_index + 1]
-        create_file(file_name)
-    else:
-        print(
-            "No valid flags provided."
-            " Use -d for directories or -f for files"
+
+    if dir_parts:
+        dir_path = create_path(dir_parts)
+        file_path = (
+            os.path.join(dir_path, file_name)
+            if file_name else dir_path
         )
+    else:
+        file_path = file_name
+
+    if file_name:
+        create_file(file_path)
 
 
 if __name__ == "__main__":
