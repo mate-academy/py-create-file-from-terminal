ff697b2bed0c7cb953231e9b6c444a22c45a8449
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-14 11:54:42 +0300

    Fixed review changes

--- app/create_file.py
+++ app/create_file.py
@@ -3,6 +3,18 @@ import os
 import datetime
 
 
+def write_text_to_file() -> list[str]:
+    lines = []
+    line_num = 1
+    while True:
+        text = input("Enter your text: ")
+        if text == "stop":
+            break
+        lines.append(f"{line_num} Line{line_num} {text}\n")
+        line_num += 1
+    return lines
+
+
 def create_file(file_name: str, directory_name: str = None) -> None:
     if directory_name:
         os.makedirs(directory_name, exist_ok=True)
@@ -11,15 +23,8 @@ def create_file(file_name: str, directory_name: str = None) -> None:
     with open(file_name, "a") as file:
         daytime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{daytime}\n")
-        line = 1
-
-        while True:
-            text = input("Enter your text: ")
-            if text == "stop":
-                break
-            file.write(f"Line{line} {text}\n")
-            line += 1
-
+        lines = write_text_to_file()
+        file.writelines(lines)
         file.write("\n")
 
 
