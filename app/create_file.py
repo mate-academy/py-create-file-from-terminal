6304b7e731fdc98f6f3888d67b4550b4565eb134
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-15 13:36:11 +0300

    Fixed review changes

--- app/create_file.py
+++ app/create_file.py
@@ -3,8 +3,10 @@ import os
 import datetime
 
 
-def write_text_to_file() -> list[str]:
+def collect_user_input() -> list[str]:
     lines = []
+    daytime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    lines.append(f"{daytime}\n")
     line_num = 1
     while True:
         text = input("Enter your text: ")
@@ -12,33 +14,33 @@ def write_text_to_file() -> list[str]:
             break
         lines.append(f"{line_num} Line{line_num} {text}\n")
         line_num += 1
+    lines.append("\n")
     return lines
 
 
-def create_file(file_name: str, directory_name: str = None) -> None:
+def create_file(file_name: str, directory_name: str) -> None:
     if directory_name:
         os.makedirs(directory_name, exist_ok=True)
         os.chdir(directory_name)
 
-    with open(file_name, "a") as file:
-        daytime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{daytime}\n")
-        lines = write_text_to_file()
-        file.writelines(lines)
-        file.write("\n")
+    lines = collect_user_input()
+    try:
+        with open(file_name, "a") as file:
+            file.writelines(lines)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--directory", nargs="+")
-    parser.add_argument("-f", "--file")
+    parser.add_argument("-d", "--directory", nargs="+", required=True)
+    parser.add_argument("-f", "--file", required=True)
     args = parser.parse_args()
 
-    directory_name = os.path.join(*args.directory) if args.directory else None
+    directory_name = os.path.join(*args.directory)
     file_name = args.file
 
-    if file_name:
-        create_file(file_name, directory_name)
+    create_file(file_name, directory_name)
 
 
 if __name__ == "__main__":
