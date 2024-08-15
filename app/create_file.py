7bac1637fa49378519c110369ea3d6f7111b2ed4
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-15 08:43:58 +0300

    Fixed

--- app/create_file.py
+++ app/create_file.py
@@ -22,26 +22,23 @@ def collect_data() -> str:
     return "".join(info)
 
 
-def write_data(file_name: str, path: list = None) -> bool:
+def write_data(file_name: str, path: list = None) -> None:
     file_path = os.path.join(*path, file_name) if path else file_name
     data_from_user = collect_data()
     try:
         with open(file_path, "a") as file:
             file.write(data_from_user)
-    except (PermissionError, OSError) as e:
+    except OSError as e:
         print(f"Cannot open/create file: {file}. Error {e}")
-        return False
-    else:
-        return True
 
 
-def parse_argv() -> tuple | None:
+def parse_argv() -> tuple:
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file_name", type=str)
     parser.add_argument("-d", "--dir_path", type=str, nargs="+")
     sys_args = parser.parse_args()
-    file_name = sys_args.file_name if sys_args.file_name else None
-    dir_path = sys_args.dir_path if sys_args.dir_path else None
+    file_name = sys_args.file_name
+    dir_path = sys_args.dir_path
     return file_name, dir_path
 
 
@@ -50,16 +47,9 @@ def create_file() -> None:
     if file:
         if dirs:
             create_dirs(dirs)
-        is_success = write_data(file, dirs)
-        if is_success:
-            print("User data has written")
-        else:
-            print(f"Cannot create/write data in {file}")
+        write_data(file, dirs)
     elif dirs:
         create_dirs(dirs)
-        print("Directories have created")
-    else:
-        print("No f or d keys provided!")
 
 
 if __name__ == "__main__":
