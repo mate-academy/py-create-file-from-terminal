dd338bd88d36267a67f21a5c01a39dd44bf0efb9
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-16 09:39:51 +0300

    Fixed review changes

--- app/create_file.py
+++ app/create_file.py
@@ -18,29 +18,33 @@ def collect_user_input() -> list[str]:
     return lines
 
 
-def create_file(file_name: str, directory_name: str) -> None:
-    if directory_name:
-        os.makedirs(directory_name, exist_ok=True)
-        os.chdir(directory_name)
-
+def create_file(file_name: str) -> None:
     lines = collect_user_input()
     try:
         with open(file_name, "a") as file:
             file.writelines(lines)
-    except Exception as e:
+    except OSError as e:
         print(f"Error: {e}")
+
+
+def create_directory(directory_name: str) -> None:
+    os.makedirs(directory_name, exist_ok=True)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--directory", nargs="+", required=True)
-    parser.add_argument("-f", "--file", required=True)
+    parser.add_argument("-d", "--directory", nargs="+")
+    parser.add_argument("-f", "--file")
     args = parser.parse_args()
 
-    directory_name = os.path.join(*args.directory)
-    file_name = args.file
-
-    create_file(file_name, directory_name)
+    if args.directory:
+        directory_name = os.path.join(*args.directory)
+        create_directory(directory_name)
+        if args.file:
+            os.chdir(directory_name)
+            create_file(args.file)
+    elif args.file:
+        create_file(args.file)
 
 
 if __name__ == "__main__":
