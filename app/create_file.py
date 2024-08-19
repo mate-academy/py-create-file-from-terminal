8e75f783b47c9564571869cb3ba7e980a0fb0e29
serhii <ockar2018@gmail.com>
2024-08-19 15:19:40 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,49 +1,59 @@
 import argparse
 import os
+from argparse import Namespace
 from datetime import datetime
 
 
+def datetime_collect() -> str:
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
 def create_file(file_path: str) -> None:
-    with open(file_path, "a") as file:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"\n{timestamp}\n")
+    try:
+        with open(file_path, "a") as file:
+            timestamp = datetime_collect()
+            file.write(f"{timestamp}\n")
+            line_counter = 1
+            while True:
+                line = input("Enter content line: ")
+                if line.lower() == "stop":
+                    file.write("\n")
+                    break
+                file.write(f"{line_counter} {line}\n")
+                line_counter += 1
 
-        print("(to stop the program write 'stop')")
-        line_counter = 1
-        while True:
-            line = input("Enter content line: ")
-            if line.lower() == "stop":
-                file.write("\n")
-                break
-            file.write(f"{line_counter} {line}\n")
-            line_counter += 1
-
-    print(f"File {file_path} created successfully.")
+        print(f"File {file_path} created/ updated successfully.")
+    except IOError as e:
+        print(f"Error: {e} while work with file")
 
 
-def main() -> None:
+def args_parser() -> Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dir", nargs="*", help="Directory path")
     parser.add_argument("-f", "--file", help="File path")
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = args_parser()
 
     directory_path = ""
 
     if args.dir:
         directory_path = os.path.join(*args.dir)
-        os.makedirs(directory_path)
-        print(f"{directory_path} directory created.")
+        try:
+            os.makedirs(directory_path)
+            print(f"{directory_path} directory created.")
+        except FileExistsError:
+            print(f"{directory_path} directory already exists.")
 
     if args.file:
         file_path = os.path.join(directory_path, args.file)
         create_file(file_path)
 
     if not args.dir and not args.file:
-        parser.error("Neither '-d' nor '-f' flag provided.")
+        args.error("Neither '-d' or '-f' flag provided.")
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except ValueError as e:
-        print(f"Error: {e}")
+    main()
