e03a5a9f4f6823656618ab61c09117bfdfad5424
Arsen <arsen.test.mess@gmail.com>
2024-09-03 22:55:15 +0300

    Added empty line between sessions

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+import sys
+import datetime
+
+
+args = sys.argv
+dirs = ""
+
+if "-d" in args:
+    if args.index("-d") > args.index("-f") or "-f" not in args:
+        dirs = "/".join(args[args.index("-d") + 1:]) + "/"
+    else:
+        dirs = "/".join(args[args.index("-d") + 1:args.index("-f")]) + "/"
+
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+
+if "-f" in args:
+    with open(dirs + args[args.index("-f") + 1], "a") as file:
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+
+        while True:
+            string = input("Enter content line: ")
+            if string == "stop":
+                file.write("\n")
+                break
+            file.write(string + "\n")
