5534240fe164d5bee2f8c22f89b14c888be91417
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-09-06 17:10:43 +0300

    code refactor and function call

--- app/create_file.py
+++ app/create_file.py
@@ -8,10 +8,10 @@ def create_file() -> None:
     if "-d" in directory_path and "-f" in directory_path:
         directory = directory_path[directory_path.index("-d") + 1:
                                    directory_path.index("-f")]
-        os.makedirs(os.path.join(*directory))
-        with open(
-            f"{directory}/{directory_path[directory_path.index('-f')]}", "a"
-        ) as file:
+        os.makedirs(os.path.join(*directory), exist_ok=True)
+        file_name = directory_path[directory_path.index("-f") + 1]
+        file_path = os.path.join(*directory, file_name)
+        with open(file_path, "a") as file:
             file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
             content = input("Enter content line: ")
             line_number = 1
@@ -19,9 +19,10 @@ def create_file() -> None:
                 file.write(f"{line_number} {content}\n")
                 content = input("Enter content line: ")
                 line_number += 1
+
     elif "-d" in directory_path:
         directory = directory_path[directory_path.index("-d") + 1:]
-        os.makedirs(os.path.join(*directory))
+        os.makedirs(os.path.join(*directory), exist_ok=True)
     elif "-f" in directory_path:
         file_name = directory_path[directory_path.index("-f") + 1]
         with open(file_name, "a") as file:
@@ -32,3 +33,6 @@ def create_file() -> None:
                 file.write(f"{line_number} {content}\n")
                 content = input("Enter content line: ")
                 line_number += 1
+
+
+create_file()
