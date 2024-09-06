88b9384db9b39a7c841c297c65ba0ab82e6f6f0f
Rudge0 <maksim.pariychuk07@gmail.com>
2024-09-06 23:24:16 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,34 @@
-# write your code here
+from datetime import datetime
+import sys
+import os
+
+
+args = sys.argv
+# print(args)
+# print(args.index("-d") + 1)
+
+if "-d" in args:
+
+    for i in range(args.index("-d") + 1, len(args), 1):
+        print(i)
+
+        element = args[i]
+        if element == "-f":
+            break
+
+        os.mkdir(f"{element}")
+        os.chdir(f"{element}")
+
+if "-f" in args:
+
+    with open(f"{args[args.index("-f") + 1]}", "a") as file:
+        formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{formatted_date}\n")
+        num = 1
+        while True:
+            line = input("Enter content line:")
+            if line == "stop":
+                break
+
+            file.write(f"{num} {line}\n")
+            num += 1
