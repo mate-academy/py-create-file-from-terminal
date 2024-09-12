ecc310c4afc1952f27e2a3bddafe3c84841e67e8
ulichka <ulichka7615@gmail.com>
2024-09-12 18:08:44 +0300

    divided the code into functions

--- app/create_file.py
+++ app/create_file.py
@@ -3,25 +3,40 @@ import os
 import datetime
 
 
-for arg in sys.argv:
-    if arg == "-d":
-        if "-f" not in sys.argv:
-            target_path = os.path.join("/".join(sys.argv[2:]))
+def create_path() -> None:
+    command = sys.argv
+    if "-d" in command:
+        if "-f" not in command:
+            path = os.path.join("/".join(command[command.index("-d") + 1:]))
+            create_dir(path)
         else:
-            target_path = os.path.join("/".join(sys.argv[2:-2]))
-        if not os.path.exists(target_path):
-            os.makedirs(target_path, exist_ok=True)
-        os.chdir(target_path)
+            path = os.path.join("/".join(
+                command[command.index("-d") + 1:command.index("-f")]))
+            create_dir(path)
+            os.chdir(path)
+            create_file("file.txt")
+    elif "-f" in command:
+        file_name = command[command.index("-f") + 1]
+        create_file(file_name)
 
-filename = sys.argv[-1] if "-f" in sys.argv else "file.txt"
 
-with open(filename, "a") as file:
-    current_date = datetime.datetime.now()
-    file.write(current_date.strftime("%Y-%d-%m %H:%M:%S") + "\n")
-    while True:
-        content = input("Enter content line: ")
-        count = 1
-        if content == "stop":
-            break
-        file.write(f"{count} " + content + "\n")
-        count += 1
+def create_dir(path: str) -> None:
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+
+
+def create_file(file_path: str) -> None:
+    with open(file_path, "a") as file:
+        current_date = datetime.datetime.now()
+        file.write(current_date.strftime("%Y-%d-%m %H:%M:%S") + "\n")
+        while True:
+            content = input("Enter content line: ")
+            count = 1
+            if content == "stop":
+                break
+            file.write(f"{count} " + content + "\n")
+            count += 1
+
+
+if __name__ == "__main__":
+    create_path()
