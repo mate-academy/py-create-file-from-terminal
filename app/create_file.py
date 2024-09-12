526e1a52e1479b91d6bf1072356e6b9c1855c5ee
ulichka <ulichka7615@gmail.com>
2024-09-12 16:07:46 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,27 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+for arg in sys.argv:
+    if arg == "-d":
+        if "-f" not in sys.argv:
+            target_path = os.path.join("/".join(sys.argv[2:]))
+        else:
+            target_path = os.path.join("/".join(sys.argv[2:-2]))
+        if not os.path.exists(target_path):
+            os.makedirs(target_path, exist_ok=True)
+        os.chdir(target_path)
+
+filename = sys.argv[-1] if "-f" in sys.argv else "file.txt"
+
+with open(filename, "a") as file:
+    current_date = datetime.datetime.now()
+    file.write(current_date.strftime("%Y-%d-%m %H:%M:%S") + "\n")
+    while True:
+        content = input("Enter content line: ")
+        count = 1
+        if content == "stop":
+            break
+        file.write(f"{count} " + content + "\n")
+        count += 1
