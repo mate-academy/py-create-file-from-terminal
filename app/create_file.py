0a1a07e8b37d55bc0314a94be1ea97259ee3908b
Ivan <vanya.bidnenko@gmail.com>
2024-09-13 11:44:52 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -7,30 +7,37 @@ def create_file(file_path: str) -> None:
     mode = "a" if os.path.isfile(file_path) else "w"
     with open(f"{file_path}", mode) as f:
         if mode == "a":
-            f.write("\n")
+            print(mode)
+            f.write("\n\n")
 
-        f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
         line_number = 1
         while True:
             next_line = input("Enter content line: ")
             if next_line == "stop":
                 break
-            f.write(f"{line_number} {next_line}\n")
+            f.write(f"\n{line_number} {next_line}")
             line_number += 1
 
 
 def app_for_create_file() -> None:
     arg = sys.argv
-    if "-d" in arg:
+    directory = os.getcwd()
+    if "-d" in arg and "-f" in arg:
         dir_index = arg.index("-d") + 1
-        file_index = arg.index("-f") if "-f" in arg else len(arg)
+        file_index = arg.index("-f") \
+            if arg.index("-d") < arg.index("-f") else len(arg)
         directory = os.path.join(*arg[dir_index:file_index])
         os.makedirs(directory)
-    else:
-        directory = os.getcwd()
-
-    if "-f" in arg:
+        file_name = arg[arg.index("-f") + 1]
+        create_file(os.path.join(directory, file_name))
+    if "-d" in arg and "-f" not in arg:
+        dir_index = arg.index("-d") + 1
+        file_index = len(arg)
+        directory = os.path.join(*arg[dir_index:file_index])
+        os.makedirs(directory)
+    if "-f" in arg and "-d" not in arg:
         file_name = arg[arg.index("-f") + 1]
         create_file(os.path.join(directory, file_name))
 
