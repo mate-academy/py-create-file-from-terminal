ba7681be85521c4bf7ee398b732af41ff186ef71
Evheniy Yahich <EvheniyY@planit-inc.com>
2024-09-15 19:15:12 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -8,32 +8,36 @@ def create_directory(path: list[str]) -> None:
 
 
 def create_file(name: str) -> None:
+    content = ""
+    count = 0
+    while True:
+        count += 1
+        line = input("Enter content line: ")
+        if line == "stop":
+            break
+        content += "\n" + f"{count} " + line
+    content += "\n\n"
     with open(name, "+a") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        count = 0
-        while True:
-            count += 1
-            line = input("Enter content line: ")
-            if line == "stop":
-                break
-            f.write("\n" + f"{count} " + line)
-        f.write("\n\n")
+        f.write(content)
 
 
 def handle_command() -> None:
     args = sys.argv
+
+    file_flag_index = args.index("-f") if "-f" in args else -1
+    dir_flag_index = args.index("-d") if "-d" in args else -1
+    file_name = args[file_flag_index + 1:][0]
+    dir_path = args[dir_flag_index + 1:file_flag_index]\
+        if file_flag_index > dir_flag_index \
+        else args[dir_flag_index + 1:]
+
     if "-d" in args and "-f" in args:
-        file_name = args[args.index("-f") + 1:][0]
-        args.remove("-f")
-        args.remove(file_name)
-        dir_path = args[args.index("-d") + 1:]
         create_directory(dir_path)
         create_file(os.path.join(*dir_path, file_name))
     if "-d" not in args and "-f" in args:
-        file_name = args[args.index("-f") + 1:][0]
         create_file(file_name)
     if "-d" in args and "-f" not in args:
-        dir_path = args[args.index("-d") + 1:]
         create_directory(dir_path)
 
 
