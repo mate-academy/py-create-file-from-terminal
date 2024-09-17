373af29b5efe1210a9d2d6d7a433d614ff9a9fbb
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-09-18 00:34:42 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -13,14 +13,15 @@ else:
     dir_path = "."
 if "-f" in args:
     file_index = args.index("-f") + 1
-    file_name = args[file_index]
-    file_path = os.path.join(*dir_path, file_name)
-    with open(file_path, "a") as file:
-        file.write(f"{date_today}\n")
-        counter = 1
-        while True:
-            text = input("Enter content line: ")
-            if text.lower() == "stop":
-                break
-            file.write(f"{counter} {text}\n")
-            counter += 1
+    if file_index < len(args):
+        file_name = args[file_index]
+        file_path = os.path.join(dir_path, file_name)
+        with open(file_path, "a") as file:
+            file.write(f"{date_today}\n")
+            counter = 1
+            while True:
+                text = input("Enter content line: ")
+                if text.lower() == "stop":
+                    break
+                file.write(f"{counter} {text}\n")
+                counter += 1
