a7c57445a969240b54b263cdb62ecd09a32617e1
Viktor Panasiuk <Viktor66462@gmail.com>
2024-09-18 11:38:26 +0300

    'Solution_corrected'

--- app/create_file.py
+++ app/create_file.py
@@ -13,7 +13,7 @@ if "-f" in sys.argv and "-d" not in sys.argv:
     formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
     with open("file.txt", "a") as file:
-        file.write(formatted_time + "\n")
+        file.write("\n" + formatted_time + "\n")
         lines = 0
         while True:
             lines += 1
@@ -24,16 +24,25 @@ if "-f" in sys.argv and "-d" not in sys.argv:
 
 
 if "-d" in sys.argv and "-f" in sys.argv:
-    dir1 = sys.argv[2]
-    dir2 = sys.argv[3]
-    path = os.path.join(dir1, dir2)
+    # dir1 = sys.argv[sys.argv.index("-d") + 1]
+    # dir2 = sys.argv[sys.argv.index("-f")]
+    # path = os.path.join(dir1, dir2)
+    # os.makedirs(path, exist_ok=True)
+
+    d_index = sys.argv.index("-d") + 1
+    f_index = sys.argv.index("-f") + 1
+    if d_index < f_index:
+        path = os.path.join(*sys.argv[d_index:f_index - 1])
+
+    else:
+        path = os.path.join(*sys.argv[d_index:])
     os.makedirs(path, exist_ok=True)
 
     file_path = os.path.join(path, "file.txt")
     now = datetime.now()
     formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
     with open(file_path, "a") as file:
-        file.write(formatted_time + "\n")
+        file.write("\n" + formatted_time + "\n")
         lines = 0
         while True:
             lines += 1
