a68fd2ccba9ee859b6498de9f408c4b9434b0ced
Rudge0 <maksim.pariychuk07@gmail.com>
2024-09-21 00:33:12 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -18,7 +18,8 @@ def working_with_file(file_name: str) -> None:
     with open(file_name, "a") as file:
         formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         new_file = os.path.getsize(file_name) == 0
-        data_to_write = f"{formatted_date}\n{data_to_write}" if new_file else f"\n{formatted_date}\n{data_to_write}"
+        data_to_write = (f"{formatted_date}\n{data_to_write}"
+                         if new_file else f"\n{formatted_date}\n{data_to_write}")
         file.write(data_to_write)
 
 
