52f59c7c38a954551ba3ef44cffb7a3df93a6788
Andrii Zubchenko <dissom1987@gmail.com>
2024-09-22 18:25:26 +0300

    fixed wirting to file

--- app/file.py
+++ app/file.py
@@ -4,13 +4,15 @@ import os
 
 def create_file(path: str) -> None:
     mode = "a" if os.path.exists(path) else "w"
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    file_write =f"{timestamp} + \n"
+    line_count = 1
+    while True:
+        content_line = input("Enter content line: ")
+        if content_line.lower() == "stop":
+            break
+        file_write += f"{line_count} {content_line}\n"
+        line_count += 1
     with open(path, mode) as file:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(timestamp + "\n")
-        line_count = 1
-        while True:
-            content_line = str(input("Enter content line: "))
-            if content_line.lower() == "stop":
-                break
-            file.write(f"{line_count} {content_line}\n")
-            line_count += 1
+        file.write(file_write)
+        
