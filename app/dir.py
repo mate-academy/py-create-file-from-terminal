90ac76a5830fb3f25f71910b8fcc8ec9e22b6b04
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-09-22 16:27:55 +0300

    Improved argument handling for path in create_directory.py

--- app/dir.py
+++ app/dir.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 
 def create_directory(path: list[str]) -> Path:
-    dir_path = os.path.join("/".join(path))
+    dir_path = os.path.join(*path)
     os.makedirs(dir_path, exist_ok=True)
     return dir_path
