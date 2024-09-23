7ba1fa794e736e5bfbac0d13dbd3c6fc52e332a8
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-09-23 23:41:57 +0300

    moved the loop outside the with statement

--- app/create_file.py
+++ app/create_file.py
@@ -6,30 +6,39 @@ from sys import argv
 
 def create_file(file_name: str) -> None:
     chek_file = False
+
     if os.path.exists(file_name):
         chek_file = True
-        # with open(file_name, "a") as source_file:
-        #     source_file.write("\n")
+
+    page_number = 1
+    file_content = ""
+
+    while True:
+        line_content = input("Enter content line: ")
+
+        if line_content.lower() == "stop":
+            break
+
+        file_content += f"{page_number} {line_content}\n"
+        page_number += 1
+
     with open(file_name, "a") as source_file:
         if chek_file:
             source_file.write("\n")
+
         time_output = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         source_file.write(f"{time_output}\n")
-        page_number = 1
-        while True:
-            line_content = input("Enter content line: ")
-            if line_content.lower() == "stop":
-                break
-            source_file.write(f"{page_number} {line_content}\n")
-            page_number += 1
+        source_file.write(file_content)
 
 
 def main() -> None:
     char_f = "-f"
     char_d = "-d"
+
     if char_f in argv and char_d in argv:
         index_f = argv.index("-f")
         index_d = argv.index("-d")
+
     if (char_f in argv and char_d in argv
             and index_f > index_d):
         directory_names = os.path.join(*argv[2:index_f:])
@@ -55,4 +64,5 @@ def main() -> None:
         os.makedirs(directory_names, exist_ok=True)
 
 
-main()
+if __name__ == "__main__":
+    main()
