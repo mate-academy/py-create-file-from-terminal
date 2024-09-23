4234fca4ff43f2b202f6858ab809a55cdf452b1f
Andriy <grushevoyandrey@gmail.com>
2024-09-23 19:27:21 +0300

    ерÑthere are errors

--- app/create_file.py
+++ app/create_file.py
@@ -3,25 +3,24 @@ import sys
 from datetime import datetime
 
 
-def create_directory(name_directory: str) -> None:
-    parent_dir = os.getcwd()
-    path = os.path.join(parent_dir, name_directory)
+def create_dir(path: str) -> None:
     try:
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
     except EOFError as error:
         print(f"Error: {error}")
+        sys.exit(1)
 
 
 def create_file(file_path: str) -> None:
     file_exist = False
-    if os.path.exists(file_path):
+    if os.path.exists(file_exist):
         file_exist = True
 
     with open(file_path, "a") as file:
-        date_and_time = datetime.now()
+        current_date_and_time = datetime.now()
         if file_exist:
             file.write("\n")
-        file.write(f"{date_and_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+        file.write(f'{current_date_and_time.strftime("%y-%m-%d %H:%M:%S")}\n')
 
         line_number = 1
         while True:
@@ -33,37 +32,25 @@ def create_file(file_path: str) -> None:
 
 
 def main() -> None:
-    if "-d" in sys.argv and "-f" in sys.argv:
+    if "-d" in sys.argv:
         dir_index = sys.argv.index("-d") + 1
-        file_index = sys.argv.index("-f") + 1
-        directory = os.path.join(*sys.argv[dir_index: file_index - 1])
-        filename = sys.argv[file_index]
-        create_directory(directory)
-        create_file(filename)
-    elif "-d" in sys.argv:
-        dir_index = sys.argv.index("-d") + 1
-        directory = os.path.join(*sys.argv[dir_index:])
-        create_directory(directory)
+        base_dir = os.path.join(os.getcwd(), "app")
+        directory = os.path.join(base_dir, *sys.argv[dir_index:])
+        create_dir(directory)
     elif "-f" in sys.argv:
         file_index = sys.argv.index("-f") + 1
         filename = sys.argv[file_index]
         create_file(filename)
     else:
-        print("Usage:")
-        print(
-            "python create_file.py"
-            " -d <directory_path> - creates directory"
-        )
-        print(
-            "python create_file.py"
-            " -f <filename> - creates file and asks for content"
-        )
-        print(
-            "python create_file.py"
-            " -d <directory_path>"
-            " -f <filename>"
-            " - creates directory and file with content")
+        if "-d" in sys.argv and "-f" in sys.argv:
+            dir_index = sys.argv.index("-d") + 1
+            file_index = sys.argv.index("-f") + 1
+            directory = os.path.join(*sys.argv[dir_index: file_index - 1])
+            filename = sys.argv[file_index]
+            create_dir(directory)
+            file_path = os.path.join(directory, filename)
+            create_file(file_path)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
