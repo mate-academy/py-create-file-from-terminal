18f012e029dd751277ce6502b9825986ef087d97
Max <mxbely98@gmail.com>
2024-10-08 23:54:53 +0300

    created program for create file in the specified directory

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,47 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_path_and_file_name(arguments: list) -> None:
+    flag = True
+    for val in arguments:
+        if val == "-f":
+            flag = False
+        if flag:
+            directory.append(val)
+        else:
+            file_name.append(val)
+    directory.pop(0)
+    file_name.pop(0)
+
+
+def create_dir() -> None:
+    for direc in directory:
+        if not os.path.exists(direc):
+            os.mkdir(direc)
+        os.chdir(direc)
+
+
+def write_information_into_file() -> None:
+    with open("file_name", "w") as file:
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+    with open("file_name", "a") as file:
+        while True:
+            inp = input("Enter content line: ")
+            if inp == "stop":
+                break
+            file.write(inp + "\n")
+
+
+input_arguments = sys.argv
+input_arguments.pop(0)
+os.chdir("app")
+if "-d" not in input_arguments:
+    write_information_into_file()
+else:
+    directory = []
+    file_name = []
+    create_path_and_file_name(input_arguments)
+    create_dir()
+    write_information_into_file()
