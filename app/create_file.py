c6f40a0c61ce3de182d928f165083407762a563f
Max <mxbely98@gmail.com>
2024-10-10 22:57:41 +0300

    fix some disadvantages

--- app/create_file.py
+++ app/create_file.py
@@ -3,46 +3,46 @@ import os
 import datetime
 
 
-def create_path_and_file_name(arguments: list) -> None:
-    flag = True
-    for value in arguments:
-        if value == "-f":
-            flag = False
-        if flag:
-            directory.append(value)
-        else:
-            file_name.append(value)
-    directory.pop(0)
-    file_name.pop(0)
+def main() -> None:
+    def create_path_and_file_name(arguments: list) -> list:
+        file_name = ""
+        directory = []
+        flag = True
+        for value in arguments:
+            if value == "-f":
+                flag = False
+            if flag and value != "-d":
+                directory.append(value)
+            elif value not in ("-d", "-f"):
+                file_name = value
+        return [directory, file_name]
+
+    def create_dir(directory: list) -> None:
+        for direc in directory:
+            if not os.path.exists(direc):
+                os.mkdir(direc)
+            os.chdir(direc)
+
+    def write_information_into_file(file_name: str) -> None:
+        with open(file_name, "w") as file:
+            file.write(datetime.datetime.now().strftime("%Y-%m-%d "
+                                                        "%H:%M:%S") + "\n")
+            while True:
+                content_line = input("Enter content line: ")
+                if content_line == "stop":
+                    break
+                file.write(content_line + "\n")
+
+    input_arguments = sys.argv
+    input_arguments.pop(0)
+    os.chdir("app")
+    path = create_path_and_file_name(input_arguments)
+    if "-d" not in input_arguments:
+        write_information_into_file(path[1])
+    else:
+        create_dir(path[0])
+        write_information_into_file(path[1])
 
 
-def create_dir() -> None:
-    for direc in directory:
-        if not os.path.exists(direc):
-            os.mkdir(direc)
-        os.chdir(direc)
-
-
-def write_information_into_file() -> None:
-    with open("file_name", "w") as file:
-        file.write(datetime.datetime.now().strftime("%Y-%m-%d "
-                                                    "%H:%M:%S") + "\n")
-    with open("file_name", "a") as file:
-        while True:
-            inp = input("Enter content line: ")
-            if inp == "stop":
-                break
-            file.write(inp + "\n")
-
-
-input_arguments = sys.argv
-input_arguments.pop(0)
-os.chdir("app")
-if "-d" not in input_arguments:
-    write_information_into_file()
-else:
-    directory = []
-    file_name = []
-    create_path_and_file_name(input_arguments)
-    create_dir()
-    write_information_into_file()
+if __name__ == "__main__":
+    main()
