ff34e0cc791969c94a5ff2f27d9dccc4492fd6d1
yura <pollo7964132@gmail.com>
2024-10-19 16:23:01 +0300

    implemented file and dir create program

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+import sys
+import datetime
+
+print(sys.argv)
+
+command = sys.argv
+
+if "-d" in command:
+    dirs = command[command.index("-d") + 1: command.index("-f")] \
+        if "-f" in command else command[command.index("-d") + 1:]
+    path_dirs = os.path.join(*dirs)
+    os.makedirs(path_dirs, exist_ok=True)
+
+else:
+    path_dirs = ""
+
+if "-f" in command:
+    file_path = os.path.join(path_dirs, command[command.index("-f") + 1])
+    with open(file_path, "a+") as file:
+        current_time = datetime.datetime.now()
+        file.write(str(current_time.strftime("%Y-%m-%d %H:%M:%S")) + "\n")
+        for num_of_row, file_text in enumerate(iter(input, "stop"), start=1):
+            file.write(f"{num_of_row} {file_text}\n")
