63e20ab7b6e06f819d62d511029ef952c0bdf313
Illia Dubina <armahanz.music@gmail.com>
2024-11-21 18:13:33 +0200

    Update create_file.py

--- app/create_file.py
+++ app/create_file.py
@@ -20,12 +20,12 @@ if "-f" in enter_terminal:
         file_name = os.path.join(new_dir_path, file_name)
     with open(file_name, "a") as f:
         now = datetime.datetime.now()
-        f.write(f"{now.strftime("%Y-%m-%d %X")}\n")
-        num_string = 1
+        f.write(f"{now.strftime('%Y-%m-%d %X')}\n")
+        line_number = 1
         while True:
             content = input("Enter content line: ")
             if content == "stop":
                 f.write("\n")
                 break
-            f.write(f"{num_string} {content}\n")
-            num_string += 1
+            f.write(f"{line_number} {content}\n")
+            line_number += 1
