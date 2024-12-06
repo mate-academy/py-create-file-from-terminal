2ad12aae1f6d9f6da6e2c20d9606e72536072527
01https <Make772@ukr.net>
2024-12-06 14:40:08 +0200

    Update create_file.py

--- app/create_file.py
+++ app/create_file.py
@@ -2,14 +2,13 @@ import sys
 import os
 import datetime
 
-param = sys.argv
-
+param = sys.argv[1:]  # Ігноруємо перший елемент (назву сценарію)
 
 def create_from_terminal_file(arguments: list) -> None:
     if "-d" in arguments and "-f" not in arguments:
         base_path = os.getcwd()
         for argument in arguments:
-            if argument not in ["create_file.py", "-d"]:
+            if argument not in ["-d"]:
                 base_path = os.path.join(base_path, argument)
                 if not os.path.exists(base_path):
                     os.makedirs(base_path)
@@ -17,7 +16,7 @@ def create_from_terminal_file(arguments: list) -> None:
     if "-f" in arguments and "-d" not in arguments:
         current_date = datetime.datetime.now()
         formatted_time = current_date.strftime("%Y-%m-%d %H:%M:%S")
-        with open(arguments[2], "a") as file:
+        with open(arguments[arguments.index("-f") + 1], "a") as file:
             file.write(formatted_time + "\n")
             nums = 0
             while True:
@@ -32,12 +31,12 @@ def create_from_terminal_file(arguments: list) -> None:
     if "-d" in arguments and "-f" in arguments:
         base_path = os.getcwd()
         for argument in arguments:
-            if argument not in ["create_file.py", "-d", "-f", arguments[-1]]:
+            if argument not in ["-d", "-f"]:
                 base_path = os.path.join(base_path, argument)
                 if not os.path.exists(base_path):
                     os.makedirs(base_path)
         os.chdir(base_path)
-        with open(arguments[-1], "a") as file:
+        with open(arguments[arguments.index("-f") + 1], "a") as file:
             current_date = datetime.datetime.now()
             formatted_time = current_date.strftime("%Y-%m-%d %H:%M:%S")
             file.write(formatted_time + "\n")
@@ -50,6 +49,5 @@ def create_from_terminal_file(arguments: list) -> None:
                     file.close()
                     return
                 file.write(f"{nums} {user_input}\n")
-
 
 create_from_terminal_file(param)
