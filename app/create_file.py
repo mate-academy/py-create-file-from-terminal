a51bf985e40d1c2969f1d7834cb8a0779d1c2781
Глеб Дубинин <gleb1395@gmail.com>
2024-12-08 18:47:47 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -20,12 +20,13 @@ def create_file(command: list, file_path: str = os.getcwd()) -> None:
         with open(file_create_path, "a", encoding="utf-8") as file:
             date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             file.write(date + "\n")
-            sequence_number = 0
-            sequence_line = 0
+            sequence_number = 1
+            sequence_line = 1
             while True:
                 user_input = input("Enter content line: ")
                 if user_input.lower() != "stop":
-                    file.write(f"{sequence_number} Line{sequence_line} {user_input}" + "\n")
+                    file.write(f"{sequence_number} "
+                               f"Line{sequence_line} {user_input}" + "\n")
                     sequence_number += 1
                     sequence_line += 1
                 else:
@@ -36,12 +37,12 @@ def create_file(command: list, file_path: str = os.getcwd()) -> None:
 def process_command() -> None:
     commands = sys.argv
     if commands.count("-d") == 1 and commands.count("-f") == 1:
-        index_create_file = commands.index('-f')
+        index_create_file = commands.index("-f")
         commands_before_f = commands[1:index_create_file]
         path = create_directory(commands_before_f)
         commands_after_f = commands[index_create_file:]
         create_file(commands_after_f, path)
-    elif commands[:1] == "-d":
+    elif commands[1] == "-d":
         create_directory(commands)
     elif commands[1] == "-f":
         create_file(commands)
