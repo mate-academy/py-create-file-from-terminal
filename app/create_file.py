f33a47ff8c6279a902873f9c2ce91c16e5502112
Illya Kovaliuk <kovalukilla271@gmail.com>
2024-12-17 20:38:41 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,61 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+"""
+імпорт бібліотек
+"""
+
+def create_file():
+    args = sys.argv[1:] #зріз з командної строки
+    if "-d" in args:
+        dir_index = args.index("-d") + 1
+        directories = args[dir_index:]
+        if "-f" in directories:
+            directories = directories[:directories.index("-f")]
+        dir_path = os.path.join(*directories)
+        os.makedirs(dir_path, exist_ok=True)
+    else:
+        dir_path = ""
+
+    if "-f" in args:
+        file_index = args.index("-f") + 1
+        if file_index >= len(args):
+            print("Error: No file name provided after '-f'.")
+            return
+        file_name = args[file_index]
+        file_path = os.path.join(dir_path, file_name)
+    else:
+        print("Error: Missing '-f' flag for file creation.")
+        return
+
+
+    content_line = []
+    print("Enter content line (type 'stop' to finish):")
+    while True:
+        line = input("Enter content line")
+        if line == "stop":
+            break
+        content_line.append(line)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_ts = f"\n{timestamp}\n"
+    formatted_ts += "\n".join(f"{i + 1} {line}" for i, line in enumerate(content_line))
+
+    if os.path.exists(file_path):
+        with open(file_path, "a") as f:
+            f.write(formatted_ts)
+    else:
+        with open(file_path, "w") as f:
+            f.write(formatted_ts)
+
+    print(f"File created/updated at: {file_path}")
+
+
+
+
+if __name__ == "__main__":
+    create_file("-d dir1 ,dir2 -f file.txt")
+
+
+
+
