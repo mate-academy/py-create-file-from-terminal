facb26613bd31e323b792136928a88f82ec9c574
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-21 13:42:12 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,22 @@
-# write your code here
+import datetime
+import os
+import sys
+
+command = sys.argv[1]
+file_name = sys.argv[2]
+directory_name = f"{sys.argv[2]}/{sys.argv[3]}"
+
+if command == "-f":
+    with open(file_name, "w") as file:
+        file.write(datetime.datetime.now()
+                   .strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        for line in sys.stdin:
+            if "stop" == line.rstrip():
+                break
+            file.write(line)
+            print(f"Enter content line: {line}")
+
+        print("Enter content line: stop")
+
+if command == "-d":
+    os.makedirs(directory_name, exist_ok=True)
