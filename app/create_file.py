b486ae2a219ce150ed4c042adc5135f57c5c7001
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-01-01 15:36:20 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -6,13 +6,12 @@ import datetime
 def append_content_to_file(filename: str) -> datetime:
     file_exists = os.path.exists(filename)
 
-    with open(filename, 'a') as file:
+    with open(filename, "") as file:
         if not file_exists:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(timestamp + "\n")
 
-            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            file.write(timestamp + '\n')
-
-        line_number = sum(1 for _ in open(filename, 'r')) if file_exists else 0
+        line_number = sum(1 for _ in open(filename, "r")) if file_exists else 0
 
         print(
             "Введите строки содержимого для файла."
