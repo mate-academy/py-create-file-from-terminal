b41e7d2a764a8e2155a4fa4983e11fb94e47a20c
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-01-04 15:29:40 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,24 @@
-# write your code here
+import sys
+from datetime import datetime
+import os
+
+
+current_time = datetime.now()
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+file_name_directory = sys.argv[2:]
+file_name_directory = "/".join(file_name_directory)
+
+
+if "-d" in sys.argv:
+    os.makedirs(file_name_directory)
+
+if "f" in sys.argv:
+    with open(sys.argv[2], "a") as file:
+        file.write(f"{formatted_time}\n")
+
+        while True:
+            file_input = input("Enter content line: ")
+            if file_input == "stop":
+                break
+            file.write(f"{file_input}\n")
