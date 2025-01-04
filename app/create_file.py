047bfb9b829ceb920a6dd80d81f21ff21d421f0d
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-01-04 15:52:21 +0100

    Solution2

--- app/create_file.py
+++ app/create_file.py
@@ -7,18 +7,32 @@ current_time = datetime.now()
 formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
 file_name_directory = sys.argv[2:]
-file_name_directory = "/".join(file_name_directory)
+file_name_directory = os.path.join(*file_name_directory)
 
 
 if "-d" in sys.argv:
-    os.makedirs(file_name_directory)
+    d_index = sys.argv.index("-d")
+    dir_path = os.path.join(*sys.argv[d_index + 1:])
+    os.makedirs(dir_path)
 
-if "f" in sys.argv:
-    with open(sys.argv[2], "a") as file:
+if "-f" in sys.argv:
+    f_index = sys.argv.index("-f")
+    file_name = sys.argv[f_index + 1]
+    file_path = file_name
+
+    if "-d" in sys.argv:
+        d_index = sys.argv.index("-d")
+        dir_path = os.path.join(*sys.argv[d_index + 1:f_index])
+        file_path = os.path.join(dir_path, file_name)
+
+    with open(file_path, "a") as file:
+        line_number = 0
         file.write(f"{formatted_time}\n")
 
         while True:
             file_input = input("Enter content line: ")
             if file_input == "stop":
                 break
-            file.write(f"{file_input}\n")
+            line_number += 1
+            file.write(f"Line{line_number} {file_input}\n")
+
