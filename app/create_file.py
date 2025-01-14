3a104f8d2a1755066ab0b9e14f38b386524fe0fa
Valeriy Semenik <valeriy.semenik@gmail.com>
2025-01-14 16:33:51 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,31 @@
-# write your code here
+import os
+from datetime import datetime
+from sys import argv
+
+
+cmd_string = argv
+cur_dir = os.getcwd()
+
+if "-d" in cmd_string:
+    d_indx = cmd_string.index("-d")
+    dir = []
+    d_indx += 1
+    while d_indx < len(cmd_string) and not cmd_string[d_indx].startswith("-"):
+        dir.append(cmd_string[d_indx])
+        d_indx += 1
+    cur_dir = os.path.join(cur_dir, *dir)
+    os.makedirs(cur_dir, exist_ok=True)
+
+if "-f" in cmd_string:
+    f_indx = cmd_string.index("-f")
+    f_name = cmd_string[f_indx + 1]
+    f_path = os.path.join(cur_dir, f_name)
+    with open(f_path, "a") as f:
+        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        line = 1
+        text = input("Enter content line: ")
+        while text.lower() != "stop":
+            f.write(f"{line} {text}\n")
+            line += 1
+            text = input("Enter content line: ")
+        f.write("\n")
