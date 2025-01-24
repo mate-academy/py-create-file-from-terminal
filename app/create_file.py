a5f2ac385af6b779c0643e4a85e3951e378921f4
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-24 13:29:17 +0200

    complete

--- app/create_file.py
+++ app/create_file.py
@@ -10,17 +10,16 @@ def create_directory(directory_path: str) -> None:
     full_path = os.path.join(current_dir, directory_path)
 
     os.makedirs(full_path, exist_ok=True)
-    print(f"Directory structure '{full_path}' created.")
 
 
 def create_file(name: str) -> None:
-    # Ensure the directories exist before creating the file
+
     directory = os.path.dirname(name)
     if directory and not os.path.exists(directory):
         create_directory(directory)
 
     if os.path.exists(name):
-        # Open the file in append mode if it already exists
+
         text_file = open(name, "a")
         current_time = datetime.now()
         formatted_timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -33,7 +32,6 @@ def create_file(name: str) -> None:
             text_file.write(temp + "\n")
         return
 
-    # Create a new file if it does not exist
     text_file = open(name, "w")
     current_time = datetime.now()
     formatted_timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -48,14 +46,28 @@ def create_file(name: str) -> None:
 
 def main() -> None:
     args = sys.argv[1:]
-    if args[0] == "-d":
+
+    if "-d" in args and "-f" in args:
+        d_index = args.index("-d")
+        f_index = args.index("-f")
+
+        dir_path = "/".join(args[d_index + 1:f_index])
+        file_name = args[f_index + 1]
+
+        create_directory(dir_path)
+        create_file(os.path.join(dir_path, file_name))
+        return
+
+    elif args[0] == "-d":
         args.pop(0)
         result_str = "/".join(args)
         create_directory(result_str)
         return
+
     elif args[0] == "-f":
         create_file(args[1])
         return
 
 
-main()
+if __name__ == "__main__":
+    main()
