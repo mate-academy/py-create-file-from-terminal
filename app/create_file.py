21fbf493c9da7551e9887321a39732d7165cbe54
BohdanMT <metelua@gmail.com>
2025-01-26 12:22:20 +0200

    My solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,29 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+directory = sys.argv[1:]
+flags = ["-d", "-f"]
+directory_path = ""
+file_name = ""
+for i in directory:
+    if i not in flags:
+        directory_path += f"{i} "
+    if i == "-f":
+        file_name = directory[-1]
+path = "/".join(directory_path.split())
+dir_path = os.path.dirname(path)
+if dir_path:
+    os.makedirs(dir_path, exist_ok=True)
+
+with open(path, "a") as data:
+    data.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+    line_count = 1
+    while True:
+        content_line = input("Enter content line:")
+        if content_line.lower() == "stop":
+            data.write("\n")
+            break
+        data.write(f"{line_count} {content_line}\n")
+        line_count += 1
