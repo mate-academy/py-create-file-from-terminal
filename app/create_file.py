8a36b20a1ba02a95a09c313ec63120e68f7b534e
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-01-30 21:47:18 +0100

    Tests fail

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,51 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+def create_directory(path):
+    os.makedirs(path, exist_ok=True)
+    print(f"Директорію створено: {path}")
+
+def create_file(file_path):
+    with open(file_path, "a") as f:
+        print(f"Файл створено: {file_path}")
+        line_number = 1
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f"\n{timestamp}\n")
+
+        while True:
+            line = input("Enter content line: ")
+            if line.lower() == "stop":
+                break
+            f.write(f"{line_number} {line}\n")
+            line_number += 1
+
+
+def main():
+    console_arg = sys.argv[1:]
+    directory_parts = []
+    file_name = None
+    creating_directory = False
+
+    for arg in console_arg:
+        if "-d" == arg:
+            creating_directory = True
+        elif "-f" == arg:
+            creating_directory = False
+        else:
+            if creating_directory:
+                directory_parts.append(arg)
+            elif file_name is None:
+                file_name = arg
+
+    if directory_parts:
+        directory_path = os.path.join(os.getcwd(), *directory_parts)
+        create_directory(directory_path)
+
+    if file_name:
+        if directory_parts:
+            file_name = os.path.join(directory_path, file_name)
+
+        file_name = os.path.join(os.getcwd(), file_name)
+        create_file(file_name)
