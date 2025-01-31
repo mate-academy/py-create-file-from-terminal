482c222e166681e9ff61b88dcb5d7c9acbca1f01
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-01-31 18:48:23 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,26 +3,28 @@ import sys
 from datetime import datetime
 
 
-def create_directory(path):
+def create_directory(path: str) -> None:
     os.makedirs(path, exist_ok=True)
     print(f"Директорію створено: {path}")
 
-def create_file(file_path):
+
+def create_file(file_path: str) -> None:
     with open(file_path, "a") as f:
         print(f"Файл створено: {file_path}")
         line_number = 1
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        f.write(f"\n{timestamp}\n")
+        f.write(f"{timestamp}\n")
 
         while True:
             line = input("Enter content line: ")
             if line.lower() == "stop":
+                f.write("\n")
                 break
             f.write(f"{line_number} {line}\n")
             line_number += 1
 
 
-def main():
+def main() -> None:
     console_arg = sys.argv[1:]
     directory_parts = []
     file_name = None
@@ -49,3 +51,6 @@ def main():
 
         file_name = os.path.join(os.getcwd(), file_name)
         create_file(file_name)
+
+
+main()
