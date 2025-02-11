27907b2a8719cb34227236f49cb1b07137827996
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-02-11 01:26:55 +0000

    resolved mistakes

--- app/create_file.py
+++ app/create_file.py
@@ -6,21 +6,21 @@ import datetime
 current_data = datetime.datetime.now()
 
 for item in sys.argv:
-    if "-d" == item:
+    current_dir = os.getcwd()
+    if "-d" == item and "-d" != sys.argv[-1]:
         for elem in sys.argv[sys.argv.index("-d") + 1:]:
-            if elem == "-f":
+            if elem.startswith("-"):
                 break
-            current_dir = os.getcwd()
-            os.mkdir(elem)
             current_dir = os.path.join(current_dir, elem)
+            os.makedirs(current_dir, exist_ok=True)
             os.chdir(current_dir)
 
-
-    if "-f" == item:
+    if "-f" == item and "-f" != sys.argv[-1]:
         current_file = sys.argv[sys.argv.index("-f") + 1]
         if os.path.exists(current_file):
             with open(current_file, "a") as additional_text:
-                additional_text.write(current_data.strftime("\n%I:%M%p %d/%B/%Y\n"))
+                additional_text.write(current_data.strftime(
+                    "\n%Y-%m-%d %H:%M:%S\n"))
                 count = 1
                 while True:
                     user = input("Enter content line: ")
@@ -29,13 +29,13 @@ for item in sys.argv:
                     additional_text.write(f"{count} Another {user}\n")
                     count += 1
             break
-
-        with open(current_file, "a") as f:
-            f.write(current_data.strftime("%I:%M%p %d/%B/%Y\n"))
-            count = 1
-            while True:
-                user = input("Enter content line: ")
-                if user == "stop":
-                    break
-                f.write(f"{count} {user}\n")
-                count += 1
+        else:
+            with open(current_file, "a") as f:
+                f.write(current_data.strftime("%Y-%m-%d %H:%M:%S\n"))
+                count = 1
+                while True:
+                    user = input("Enter content line: ")
+                    if user == "stop":
+                        break
+                    f.write(f"{count} {user}\n")
+                    count += 1
