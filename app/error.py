35978c9096c09682cc4947a3ff2021e5f0c662f1
RuslanTopal <baranotik15@gmail.com>
2025-02-12 16:26:05 +0000

    Solution

---
+++ app/error.py
@@ -0,0 +1,8 @@
+class NotEnoughArguments(Exception):
+    """Raised when there are not enough arguments."""
+    pass
+
+
+class InvalidFlagError(Exception):
+    """Exception raised when an invalid flag is provided."""
+    pass
