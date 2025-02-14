983b482f92ff25d1f52685eb122eb3d354ed4600
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-02-14 13:31:57 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,47 @@
-# write your code here
+from __future__ import annotations
+
+import sys
+from datetime import datetime
+from pathlib import Path
+
+
+def makes_dir(path_file: Path) -> None:
+    path_file.mkdir(parents=True, exist_ok=True)
+
+
+def write_data(file_name: Path) -> None:
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(file_name, "a", encoding="utf-8") as file:
+        file.write(formatted_time + "\n")
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            file.write(line + "\n")
+
+
+def init_data(data: list) -> None:
+    cmd1 = data.pop(0)
+    cmd2 = None
+
+    if "-f" in data:
+        cmd2 = data.pop(data.index("-f"))
+
+    dst_dir = Path(*data).parent
+    dst_file = Path(*data)
+    print(dst_dir)
+
+    match cmd1, cmd2:
+        case ("-d", None):
+            makes_dir(dst_dir)
+        case ("-f", None):
+            write_data(dst_file)
+        case ("-d", "-f"):
+            makes_dir(dst_dir)
+            write_data(dst_file)
+
+
+if __name__ == "__main__":
+    init_data(sys.argv[1:])
