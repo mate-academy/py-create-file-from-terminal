00994d2201899d56112a015868b8ae1d8f78307b
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-02-15 14:15:43 +0100

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -5,12 +5,13 @@ import datetime
 if "-d" in sys.argv:
     d_index = sys.argv.index("-d")
     path_parts = sys.argv[d_index + 1:]
-    os.path.join(*path_parts)
+    str_to_create = os.path.join(*path_parts)
+    os.makedirs(str_to_create)
 if "-f" in sys.argv:
     f_index = sys.argv.index("-f")
     file_name = sys.argv[f_index + 1]
     with open(file_name, "a") as f:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         lines = []
         while True:
             line = input("Enter content line: ")
@@ -18,4 +19,4 @@ if "-f" in sys.argv:
                 break
             lines.append(line)
         for i in range(len(lines)):
-            f.write(f"{i + 1} {lines[i]}")
+            f.write(f"{i + 1} {lines[i]}\n")
