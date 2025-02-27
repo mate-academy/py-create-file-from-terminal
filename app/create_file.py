c9eea0e819de62da97e30af630f5de059191683e
Ruslan Priadko <r.priadko@ukr.net>
2025-02-27 11:49:22 +0200

    sys.argv used instead of argparse

--- app/create_file.py
+++ app/create_file.py
@@ -1,4 +1,5 @@
-import argparse
+# import argparse
+import sys
 import os
 from datetime import datetime
 
@@ -10,17 +11,25 @@ def create_dirs(dir_list: list) -> str:
     return file_path
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", dest="dir_list", nargs="*", default=None)
-parser.add_argument("-f", dest="file_name", nargs="*", default=None)
-args = parser.parse_args()
+dir_list = []
+file_name = []
+args = sys.argv[1:]
+if "-f" in args:
+    file_name = args[args.index("-f") + 1:]
+    if "-d" in args:
+        dir_list = args[args.index("-d") + 1:args.index("-f")]
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-d", dest="dir_list", nargs="*", default=[])
+# parser.add_argument("-f", dest="file_name", nargs=1, default=[])
+# args = parser.parse_args()
 
 created_dir = ""
-if args.dir_list:
-    created_dir = create_dirs(args.dir_list)
+if len(dir_list) > 0:
+    created_dir = create_dirs(dir_list)
 
-if args.file_name:
-    with open(os.path.join(created_dir, args.file_name[0]), "a") as file:
+if len(file_name) > 0:
+    with open(os.path.join(created_dir, file_name[0]), "a") as file:
         file.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
         string_num = 0
         while True:
