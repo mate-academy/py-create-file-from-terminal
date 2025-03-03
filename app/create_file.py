b6c2e8e61d2b36989a1d2db7ba9b8bd4fc3ab5e3
Nikita-pyth <nicksan2002@gmail.com>
2025-03-03 15:16:01 +0200

    Issues addressed

--- app/create_file.py
+++ app/create_file.py
@@ -1,34 +1,62 @@
 import sys
 import os
 from datetime import datetime
+from typing import TextIO
 
 
-arguments = sys.argv
-file_to_create = None
-directories = []
-result_dir = ""
+def parse_arguments() -> tuple[str, list]:
+    arguments = sys.argv
+    file_to_create = None
+    directories = []
 
-if "-f" in arguments and (arguments.index("-f") + 1) < (len(arguments) - 1):
-    file_to_create = arguments[arguments.index("-f") + 1]
+    if "-f" in arguments and (arguments.index("-f") + 1) < len(arguments):
+        file_to_create = arguments[arguments.index("-f") + 1]
 
-if "-d" in arguments:
-    for directory in arguments[arguments.index("-d") + 1:]:
-        if directory == "-f":
+    if "-d" in arguments:
+        directories.extend(
+            directory
+            for directory
+            in arguments[arguments.index("-d") + 1:]
+            if directory != "-f"
+        )
+
+    return file_to_create, directories
+
+
+def create_directories(directories: list[str]) -> str:
+    if directories:
+        result_dir = os.path.join(os.getcwd(), *directories)
+        os.makedirs(result_dir, exist_ok=True)
+        return result_dir
+    return ""
+
+
+def create_file(file_to_create: str, result_dir: str) -> None:
+    if file_to_create:
+        file_path = os.path.join(os.getcwd(), result_dir, file_to_create)
+        with open(file_path, "w") as f:
+            f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            write_content(f)
+
+
+def write_content(file_to_change: TextIO) -> None:
+    line_counter = 0
+    while True:
+        line_counter += 1
+        answer = input("Enter content line: ")
+
+        if answer == "stop":
+            file_to_change.write("\n")
             break
-        directories.append(directory)
 
-if directories:
-    result_dir = os.path.join(os.getcwd(), *directories)
-    os.makedirs(result_dir, exist_ok=True)
+        file_to_change.write(f"\n{line_counter} {answer}")
 
-if file_to_create:
-    file_path = os.path.join(os.getcwd(), result_dir, file_to_create)
-    with open(file_path, "w") as f:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        line_counter = 0
-        while True:
-            line_counter += 1
-            answer = input("Enter content line: ")
-            if answer == "stop":
-                break
-            f.write(f"\n{line_counter} {answer}")
+
+def main() -> None:
+    file_to_create, directories = parse_arguments()
+    result_dir = create_directories(directories)
+    create_file(file_to_create, result_dir)
+
+
+if __name__ == "__main__":
+    main()
