75d619985c9a8e8935cd093097d7480239295fc1
PrimeGlorious <TheComboPunk@gmail.com>
2025-03-05 18:55:01 +0200

    Implement create_file app

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,35 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def write_to_file(path_file: str) -> None:
+    with open(path_file, "a") as file:
+
+        file.write(f"{datetime.datetime.now()
+                   .strftime("%Y-%m-%d %H:%M:%S")}\n")
+        line_count = 0
+
+        while True:
+            new_line = input("Enter content line: ")
+
+            if new_line == "stop":
+                break
+
+            line_count += 1
+            file.write(f"{line_count} {new_line}\n")
+
+
+if sys.argv[1] == "-f":
+    write_to_file(sys.argv[2])
+
+elif sys.argv[1] == "-d":
+
+    if "-f" not in sys.argv[2:]:
+        os.makedirs("/".join(sys.argv[2:]), exist_ok=True)
+
+    else:
+        os.makedirs("/".join(sys.argv[2:-2]), exist_ok=True)
+        index = sys.argv.index("-f")
+        file_path = f"{"/".join(sys.argv[2:-2])}/{sys.argv[index + 1]}"
+        write_to_file(file_path)
