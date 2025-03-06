27ac1da894500b901f6ae9be9cda86cc6f293b2b
Taras Shevchyk <tarashevchik27@gmail.com>
2025-03-06 23:44:51 +0200

    refactor

--- app/create_file.py
+++ app/create_file.py
@@ -4,20 +4,22 @@ from datetime import datetime
 
 
 def create_file() -> None:
-    if '-d' in sys.argv:
-        dir_index = sys.argv.index('-d')
-        dir_path = '/'.join(sys.argv[dir_index + 1:])
+    if "-d" in sys.argv:
+        dir_index = sys.argv.index("-d")
+        dir_path = "/".join(sys.argv[dir_index + 1:])
         os.makedirs(dir_path, exist_ok=True)
-    if '-f' in sys.argv:
-        file_index = sys.argv.index('-f')
+    if "-f" in sys.argv:
+        file_index = sys.argv.index("-f")
         file_name = sys.argv[file_index + 1]
-        file_path = f'{dir_path}/{file_name}' if '-d' in sys.argv else file_name
-        with open(file_path, 'a') as file:
+        file_path = (
+            f"{dir_path}/{file_name}" if "-d" in sys.argv else file_name
+        )
+        with open(file_path, "a") as file:
             file.write(f'\n{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
             line_number = 0
             while True:
-                line = input('Enter content line: ')
-                if line == 'stop':
+                line = input("Enter content line: ")
+                if line == "stop":
                     break
                 line_number += 1
-                file.write(f'\n{line_number} {line}')
+                file.write(f"\n{line_number} {line}")
