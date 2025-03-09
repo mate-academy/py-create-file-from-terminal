ba9844f93f341590f739ea920703f823b0ad3307
Yura Hryn <hrun.yuri@gmail.com>
2025-03-09 19:22:06 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,35 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_file() -> None:
+    command_list = sys.argv
+    current_dir = os.getcwd()
+    if "-d" in command_list:
+        for index in range(command_list.index("-d") + 1, len(command_list)):
+            if command_list[index] == "-f":
+                break
+
+            current_dir = os.path.join(current_dir, command_list[index])
+        if not os.path.exists(current_dir):
+            os.makedirs(current_dir)
+    if "-f" in command_list:
+        current_dir = os.path.join(
+            current_dir,
+            command_list[command_list.index("-f") + 1]
+        )
+    with open(current_dir, "a") as current_file:
+        count = 1
+        current_file.write(
+            f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
+        )
+        content = input("Enter content line: ")
+        while content != "stop":
+            current_file.write(f"{count} {content}\n")
+            content = input("Enter content line: ")
+            count += 1
+        current_file.write("\n")
+
+
+create_file()
