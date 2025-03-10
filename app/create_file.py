c75ce13e2ae2f4594d50497d0d6b7e7dbb808d4f
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-03-10 13:29:48 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,37 @@
-# write your code here
+import argparse
+import os
+import datetime
+
+
+def create_file():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", nargs="*")
+    parser.add_argument("-f", "--filename")
+
+    args = parser.parse_args()
+    full_path = os.getcwd()
+
+    if args.directory:
+        full_path = ""
+        for directory in args.directory:
+            full_path = os.path.join(full_path, directory)
+
+
+    os.makedirs(full_path, exist_ok=True)
+
+    path = os.path.join(full_path, args.filename)
+
+    content = str
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if args.filename:
+        with open(path, "a") as f:
+            f.write(now + "\n")
+            counter_lines = 0
+            while content != "stop":
+                counter_lines += 1
+                content = input("Enter content line: ")
+                f.write(f"{counter_lines} " + content + "\n")
+
+
+if __name__ == "__main__":
+    create_file()
