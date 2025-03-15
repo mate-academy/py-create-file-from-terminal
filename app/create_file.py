080e6c579ba5871c3c60c99728ba5a6018e4ab3d
Volodymyr Vitsenko <your@mail.com>
2025-03-15 21:10:11 +0100

    file_creator

--- app/create_file.py
+++ app/create_file.py
@@ -35,7 +35,7 @@ if len(command_arguments) > 1:
         new_file = os.path.join(path, file_name)
         with open(new_file, "a") as file:
             file.write(
-                f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
+                f"{(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S')}\n"
             )
             line_counter = 1
             while True:
