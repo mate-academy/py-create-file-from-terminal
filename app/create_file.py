88e5e7fbdec0f0de2c9f0cc6dafc82a4281a2c39
Platon Goncharevskyi <platongoncharevskyi@gmail.com>
2025-03-18 16:22:01 +0200

    solution upd1

--- app/create_file.py
+++ app/create_file.py
@@ -1,49 +1,43 @@
 import datetime
-import sys
 import os
+import sys
 
 
-def create_file() -> None:
+def write_content_to_file(file_path, timestamp):
+    line_number = 1
+    with open(file_path, "a") as file:
+        file.write(f"\n{timestamp}\n")
+        while True:
+            content = input("Enter content line: ")
+            if content.lower() == "stop":
+                break
+            file.write(f"{line_number} {content}\n")
+            line_number += 1
+
+
+def create_file():
     path = ""
     filename = ""
     args = sys.argv[1:]
 
     if "-d" in args:
-        d_index = args.index("-d")
-        path = os.path.join(
-            *args[d_index + 1:args.index("-f")]
-            if "-f" in args
-            else args[d_index + 1:]
-        )
+        directory_flag_index = args.index("-d")
+        path = os.path.join(*args[directory_flag_index + 1:args.index("-f")] if "-f" in args else args[directory_flag_index + 1:])
         os.makedirs(path, exist_ok=True)
 
     if "-f" in args:
-        f_index = args.index("-f")
-        filename = args[f_index + 1]
+        file_flag_index = args.index("-f")
+        filename = args[file_flag_index + 1]
 
     file_path = os.path.join(path, filename) if path else filename
-
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     if os.path.exists(file_path):
-        with open(file_path, "a") as file:
-            file.write(f"\n{timestamp}\n")
-            line_number = sum(1 for _ in open(file_path)) + 1
-            while True:
-                content = input("Enter content line: ")
-                if content.lower() == "stop":
-                    break
-                file.write(f"{line_number} {content}\n")
-                line_number += 1
+        write_content_to_file(file_path, timestamp)
     else:
         with open(file_path, "w") as file:
             file.write(f"{timestamp}\n")
-            line_number = 1
-            while True:
-                content = input("Enter content line: ")
-                if content.lower() == "stop":
-                    break
-                file.write(f"{line_number} {content}\n")
-                line_number += 1
+        write_content_to_file(file_path, timestamp)
 
     print(f"File created at: {file_path}")
 
