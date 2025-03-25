aa008f0d2389b26e95b2cd4f828c410fd6e1712b
Roman Kostiv <rkostiv@gmail.com>
2025-03-26 00:21:05 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,14 +1,15 @@
 import sys
 import os
-import datetime
+from datetime import datetime
 
 def create_file():
+
     if len(sys.argv) > 1 and sys.argv[1] == "-d":
-        os.makedirs(os.path.join(os.getcwd(), "/".join(sys.argv[2:])))
+        os.makedirs(os.path.join(os.getcwd(), os.path.join(*sys.argv[2:])), exist_ok= True)
 
     elif len(sys.argv) > 1 and sys.argv[1] == "-f":
         filename = sys.argv[-1]
-        current_time = (datetime.datetime.now().isoformat(" ", "seconds"))
+        current_time = (datetime.now().strftime("%Y-%d-%m %H:%M:%S"))
         try:
             with open(filename, "a") as file:
                 print("Enter content line. Type 'stop' to end")
@@ -17,11 +18,11 @@ def create_file():
 
                     if user_input.lower() == "stop":
                         break
-                    file.writelines([f"{current_time}, \n{user_input}"])
+                    file.writelines([f"{current_time}, \n{user_input} + '\n"])
 
         except Exception as e:
-            print("Error")
+            print(e)
     else:
-        print("no")
+        print("Pass only one argument")
 
-print(create_file())
+create_file()
