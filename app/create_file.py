1342d9bfd0fe3e14fe1389f6b1297eedd0fcd63b
Ostap <ostap.burij@gmail.com>
2025-04-03 22:17:50 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,44 @@
-# write your code here
+import sys
+import os
+import datetime
+
+arg = sys.argv
+
+
+def create_file(directory_path: str) -> None:
+    with open(directory_path, "a") as file:
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{current_time}\n")
+        line_counter = 1
+        while True:
+            new_line = input("Enter content line: ")
+            if new_line == "stop":
+                break
+            file.write(f"{line_counter} {new_line}\n")
+            line_counter += 1
+
+
+if "-d" in arg and "-f" not in arg:
+    direct = arg[2:]
+    path = os.path.join(*direct)
+    os.makedirs(path, exist_ok=True)
+
+if "-f" in arg and "-d" not in arg:
+    file_name = arg[2]
+    create_file(file_name)
+
+if "-d" in arg and "-f" in arg:
+    d_index = arg.index("-d")
+    f_index = arg.index("-f")
+    if d_index > f_index:
+        direct = arg[d_index + 1:]
+        file_name = arg[f_index + 1]
+        path = os.path.join(*direct)
+        os.makedirs(path, exist_ok=True)
+    else:
+        direct = arg[d_index + 1: f_index]
+        file_name = arg[f_index + 1]
+        path = os.path.join(*direct)
+        os.makedirs(path, exist_ok=True)
+
+create_file(os.path.join(path, file_name))
