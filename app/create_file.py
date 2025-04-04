dbe9f7cc711e1d038b80e6f04f6f88f56792d28e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-04 18:03:27 +0300

    create_file.py

--- app/create_file.py
+++ app/create_file.py
@@ -1,5 +1,10 @@
+from sys import argv
+from os import makedirs, path
+from datetime import datetime
+
+terminal = argv[1:]
+
 def write_into_file(our_path: str) -> None:
-    # Extracting filename from the command-line arguments based on the -f flag's position
     filename_index = terminal.index("-f") + 1 if "-f" in terminal else -1
     our_path = path.join(our_path, terminal[filename_index])
     line_number = 1  # Initialize line counter
@@ -9,18 +14,26 @@ def write_into_file(our_path: str) -> None:
             file_text = input("Enter content line: ")
             if file_text == "stop":
                 break
-            # Write each line with a line number
             file.write(f"{line_number} {file_text}\n")
             line_number += 1
 
 def d_and_f_flags() -> None:
-    # Adjusting path construction to exclude the filename
     our_path = path.join(*terminal[1:terminal.index("-f")])
     makedirs(our_path, exist_ok=True)  # Ensure directory is created if not exists
     write_into_file(our_path)
 
+def d_flag() -> None:
+    our_path = path.join(*terminal[1:])
+    makedirs(our_path, exist_ok=True)
+
 def f_flag() -> None:
-    # Handling case when only -f flag is passed
     filename = terminal[1] if len(terminal) > 1 else "default.txt"
     our_path = path.join(".", filename)  # Assume current directory if no path provided
     write_into_file(our_path)
+
+if "-f" in terminal and "-d" in terminal:
+    d_and_f_flags()
+elif "-d" in terminal:
+    d_flag()
+else:
+    f_flag()
