0db3c3d83895de27e3fbb798046c60166432f3d4
Mykola Nakonechnuy <teddykolia@ukr.net>
2025-04-04 11:38:11 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,30 +1,20 @@
-import os
 import sys
-from dir import create_directory
-from file import create_file
+import os
+from file import file_spawn
 
 
-def create_from_prompt() -> None:
-    args = sys.argv
+def file_from_terminal() -> None:
+    argv = sys.argv
+    dir_index = argv.index("-d") if "-d" in argv else None
+    file_index = argv.index("-f") if "-f" in argv else None
+    path = []
 
-    dir_index = args.index("-d") + 1 if "-d" in args else None
-    file_index = args.index("-f") + 1 if "-f" in args else None
-
-    if dir_index and file_index and dir_index < file_index:
-        dir_path = args[dir_index:file_index - 1]
-        file_name = args[file_index]
-        directory = create_directory(dir_path)
-        create_file(os.path.join(directory, file_name))
-    elif dir_index and not file_index:
-        dir_path = args[dir_index:]
-        create_directory(dir_path)
-
-    elif not dir_index and file_index:
-        file_name = args[file_index]
-        create_file(file_name)
-    else:
-        raise TypeError("Provide a directory (-d) or/and file (-f) argument.")
+    if dir_index:
+        path = argv[dir_index + 1: file_index if file_index else None]
+        os.makedirs(os.path.join(*path), exist_ok=True)
+    if file_index:
+        file_path = os.path.join(*path, argv[file_index + 1])
+        file_spawn(file_path)
 
 
-if __name__ == "__main__":
-    create_from_prompt()
+file_from_terminal()
--- app/file.py
+++ app/file.py
@@ -1,17 +1,21 @@
-from datetime import datetime
 import os
+from datetime import datetime
 
 
-def create_file(path: str) -> None:
-    mode = "a" if os.path.exists(path) else "w"
+def file_spawn(file_path: str) -> None:
+    mode = "a" if os.path.exists(file_path) else "w"
+
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    file_write = f"{timestamp} + \n"
-    line_count = 1
+    file_write = f"{timestamp}\n"
+    input_line_num = 1
     while True:
-        content_line = input("Enter content line: ")
-        if content_line.lower() == "stop":
+        input_line = input("Enter content line:")
+        if input_line.lower() == "stop":
+            file_write += "\n"
             break
-        file_write += f"{line_count} {content_line}\n"
-        line_count += 1
-    with open(path, mode) as file:
+        file_write += f"{input_line_num} {input_line}\n"
+        input_line_num += 1
+
+    with open(file_path, mode) as file:
         file.write(file_write)
+
