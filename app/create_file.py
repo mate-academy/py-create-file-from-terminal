bce0dce460edf61e7cd4a94c8cbe2714c5e597bc
Timur Khvat <timurkhvat@gmail.com>
2025-04-08 17:24:20 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,36 @@
-# write your code here
+import os
+import sys
+import datetime
+
+
+def create_directory(directory_path: list) -> str:
+    path = os.path.join(*directory_path)
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+def create_and_write_file(directory_path: list, file_name: str) -> None:
+    path = create_directory(directory_path)
+    full_path = os.path.join(path, file_name)
+    with open(full_path, "a") as file:
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+        line = 1
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                break
+            file.write(f"{line} {content}\n")
+            line += 1
+
+
+def create_file() -> None:
+    if "-d" in sys.argv and "-f" in sys.argv:
+        create_and_write_file(sys.argv[2: -2], sys.argv[-1])
+    elif "-d" in sys.argv:
+        create_directory(sys.argv[2:])
+    elif "-f" in sys.argv:
+        create_and_write_file(["."], sys.argv[-1])
+
+
+if __name__ == "__main__":
+    create_file()
