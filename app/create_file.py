4a86a60297675b3c46dd6afd4c042f6caceb513e
Ekaterina <sudarkova.ekaterina@gmail.com>
2025-04-11 22:16:21 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -2,20 +2,26 @@ import datetime
 import os
 import sys
 
-args = sys.argv
+args = sys.argv[1:]
 date_today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 dir_path = "."
 file_name = None
+
 if "-d" in args:
     dir_index = args.index("-d") + 1
-    directories = args[dir_index:]
     if "-f" in args:
         file_index = args.index("-f")
         directories = args[dir_index:file_index]
+    else:
+        directories = args[dir_index:]
     dir_path = os.path.join(*directories)
     os.makedirs(dir_path, exist_ok=True)
+
 if "-f" in args:
     file_index = args.index("-f") + 1
+    if file_index >= len(args):
+        print("Error: No file name provided after -f")
+        sys.exit(1)
     file_name = args[file_index]
     file_path = os.path.join(dir_path, file_name)
 
