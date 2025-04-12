1e5aff0e68058bf60e17f8a540d6afaf73ac9005
Евгений Наволоков <goustg62@gmail.com>
2025-04-12 15:02:19 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -7,43 +7,46 @@ def create_flags_list(argv: list) -> dict:
     try:
         flags = {"-d": argv.index("-d"), "-f": argv.index("-f")}
     except ValueError as e:
-        if str(e)[1:3] == "-d": flags = {"-f": argv.index("-f")}
-        if str(e)[1:3] == "-f": flags = {"-d": argv.index("-d")}
-            
+        if str(e)[1:3] == "-d":
+            flags = {"-f": argv.index("-f")}
+        if str(e)[1:3] == "-f":
+            flags = {"-d": argv.index("-d")}
+
     return flags
 
 
 def make_dir(*args: list) -> None:
-    os.makedirs("/".join((args)))
-        
-        
+    os.makedirs("/".join(args))
+
+
 def create_file_and_content(filename: str) -> None:
-    contents = []
+    content_lines = []
     while True:
         content = input("Enter content line: ")
         if content.lower().strip() == "stop":
             break
-        contents.append(content)
-        
-    with open(f"{filename}", "a") as fn:
-        fn.write(f"{datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))}\n")
-        fn.writelines(contents)
-        
-if "__main__" == __name__:
+        content_lines.append(content)
+
+    with open(filename, "a") as fn:
+        fn.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        fn.write("\n")
+        fn.writelines(content_lines)
+
+
+if __name__ == "__main__":
     argv = list(sys.argv)
     path = ""
     dictionary = create_flags_list(argv=argv)
+
     if dictionary.get("-d", False):
-        path = argv[dictionary["-d"]+1:dictionary.get("-f", -1)]
+        path = argv[dictionary["-d"] + 1:dictionary.get("-f", -1)]
         make_dir(*path)
-        
+
     if dictionary.get("-f", False):
-        file = argv[dictionary["-f"]+1]
-        if path: 
-            path += "/";
+        file_name = argv[dictionary["-f"] + 1]
+        if path:
+            path += "/"
             path = "/".join(path)
-        filename = f"{path}{file}"
+        filename = f"{path}{file_name}"
         print(filename)
         create_file_and_content(filename=filename)
-        
-    
