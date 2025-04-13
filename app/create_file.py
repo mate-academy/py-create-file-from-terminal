6db04507f54dcfcf1e4f14898f401509d8e9ed0e
Ekaterina <sudarkova.ekaterina@gmail.com>
2025-04-13 20:42:57 +0300

    added functions

--- app/create_file.py
+++ app/create_file.py
@@ -2,32 +2,37 @@ import datetime
 import os
 import sys
 
-args = sys.argv[1:]
-date_today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-dir_path = "."
-file_name = None
 
-if "-d" in args:
-    dir_index = args.index("-d") + 1
-    if "-f" in args:
-        file_index = args.index("-f")
-        if dir_index < file_index:
-            directories = args[dir_index:file_index]
+def parse_arguments(args):
+    dir_path = "."
+    file_name = None
+
+    if "-d" in args:
+        dir_index = args.index("-d") + 1
+        if "-f" in args:
+            file_index = args.index("-f")
+            if dir_index < file_index:
+                directories = args[dir_index:file_index]
+            else:
+                directories = args[dir_index:]
         else:
             directories = args[dir_index:]
-    else:
-        directories = args[dir_index:]
-    dir_path = os.path.join(*directories)
+        dir_path = os.path.join(*directories)
+
+    if "-f" in args:
+        file_index = args.index("-f") + 1
+        if file_index >= len(args):
+            print("Error: No file name provided after -f")
+            sys.exit(1)
+        file_name = args[file_index]
+
+    return dir_path, file_name
+
+def create_directory(dir_path):
     os.makedirs(dir_path, exist_ok=True)
 
-if "-f" in args:
-    file_index = args.index("-f") + 1
-    if file_index >= len(args):
-        print("Error: No file name provided after -f")
-        sys.exit(1)
-    file_name = args[file_index]
-    file_path = os.path.join(dir_path, file_name)
-
+def write_to_file(file_path):
+    date_today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(file_path, "a") as file:
         file.write(f"{date_today}\n")
         counter = 1
@@ -37,3 +42,14 @@ if "-f" in args:
                 break
             file.write(f"{counter} {text}\n")
             counter += 1
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    dir_path, file_name = parse_arguments(args)
+
+    if dir_path != ".":
+        create_directory(dir_path)
+
+    if file_name:
+        file_path = os.join(dir_path, file_name)
+        write_to_file(file_path)
