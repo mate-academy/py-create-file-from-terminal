cadec471ff08981dc1df0a29a796891ea64f87b2
Michal-Naron <michanaron@gmail.com>
2025-04-16 15:43:16 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -6,16 +6,19 @@ import os
 def create_file() -> None:
     params = sys.argv
     if "-f" in params and "-d" in params:
-        path = sys.argv[sys.argv.index("-d") + 1 : sys.argv.index("-f")]
-        path.append(sys.argv[-1])
-        path, og_path = os.path.join(*path), path
-        if os.path.exists(path):
-            with open(path, "a") as file_obj:
+        folder_path = os.path.join(*sys.argv[sys.argv.index("-d") + 1 :
+                                        sys.argv.index(
+            "-f")])
+        file_path = os.path.join(*folder_path.copy().append(sys.argv[-1]))
+
+
+        if os.path.exists(file_path):
+            with open(file_path, "a") as file_obj:
                 file_obj.write("\n")
                 write_to_file(file_obj)
         else:
-            os.makedirs(os.path.join(*og_path[:-1]), exist_ok=True)
-            with open(path, "a") as file_obj:
+            os.makedirs(folder_path)
+            with open(file_path, "a") as file_obj:
                 write_to_file(file_obj)
     elif "-f" in params and os.path.exists(sys.argv[-1]):
         with open(sys.argv[-1], "a") as file_obj:
