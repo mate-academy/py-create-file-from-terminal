0110f8f68057958aae8d61a32efa2c85863ea6a2
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-04-20 23:02:20 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,43 @@
-# write your code here
+import datetime
+import os
+import sys
+
+
+def create_dirs(directories: list[str]) -> os.path:
+    path = os.path.join(*directories)
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+def create_file(file_name: str, dir_path: str = None) -> None:
+    if dir_path:
+        file_name = os.path.join(dir_path, file_name)
+
+    with open(file_name, "a") as file:
+        count = 0
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+        while True:
+            line = input("Enter content line: ")
+            count += 1
+            if line == "stop":
+                file.write("\n")
+                break
+            file.write(f"{count} {line}\n")
+
+
+def main_run() -> None:
+    command_list = sys.argv
+    if "-d" in command_list and "-f" in command_list:
+        directory_path = create_dirs(
+            command_list[command_list.index("-d")
+                         + 1: command_list.index("-f")])
+        create_file(command_list[command_list.index("-f")
+                                 + 1:][0], directory_path)
+    elif "-d" in command_list:
+        create_dirs(command_list[command_list.index("-d") + 1:])
+    else:
+        create_file(command_list[command_list.index("-f") + 1:][0])
+
+
+if __name__ == "__main__":
+    main_run()
