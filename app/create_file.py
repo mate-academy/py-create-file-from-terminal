7ee3152506f122bc688074aad6aa45a32a1e11c1
LassisterV16 <lassisterv16@gmail.com>
2025-05-02 22:15:08 +0200

    fixed minutes

--- app/create_file.py
+++ app/create_file.py
@@ -15,7 +15,7 @@ def create_dir(dir_path: str) -> None:
 
 def create_file(file_name: str) -> None:
     with open(file_name, "a") as source_file:
-        source_file.write(datetime.datetime.now().strftime("%Y-%M-%d %H:%M:%S")
+        source_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                           + "\n")
         i = 1
 
@@ -28,6 +28,9 @@ def create_file(file_name: str) -> None:
 
 
 def create_file_from_terminal() -> None:
+    if len(sys.argv) < 2:
+        return
+
     if sys.argv[1] == "-d" and sys.argv[-2] == "-f":
         dir_path = create_path(sys.argv[2:-2])
         file_path = os.path.join(dir_path, sys.argv[-1])
