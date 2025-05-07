fe75faa30b5fb989f4cf848acc4547ef03f52ee8
Andrii Martyniuk <andrijmartunjyk@gmail.com>
2025-05-07 17:38:17 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -7,7 +7,14 @@ current_path = os.getcwd()
 
 
 def create_dir(input_data: list[str]) -> str:
-    clean_list_dir = input_data[input_data.index("-d") + 1:]
+    try:
+        clean_list_dir = input_data[input_data.index("-d") + 1:]
+        if not clean_list_dir:
+            raise ValueError("No directories provided after '-d'.")
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+
     new_path = current_path
     for my_dir in clean_list_dir:
         new_path = os.path.join(new_path, my_dir)
@@ -22,13 +29,16 @@ def create_dir(input_data: list[str]) -> str:
 
 def create_file(path: str = None) -> None:
     count = 1
-    file_name = next(file for file in main_input_data if file.endswith(".txt"))
+    try:
+        file_name = next(file for file in main_input_data if file.endswith(".txt"))
+    except StopIteration:
+        print("No file name after '-f'.")
+        sys.exit(1)
     if path is not None:
         file_name = os.path.join(path, file_name)
-        print(file_name)
 
     with open(file_name, "a") as file:
-        date_now = datetime.now().strftime("%Y-%d-%m %H:%M:%S")
+        date_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{date_now}\n")
         while True:
             data = input("Enter content line: ")
@@ -46,5 +56,4 @@ if "-f" in main_input_data and "-d" not in main_input_data:
 
 if "-d" in main_input_data and "-f" in main_input_data:
     directories_without_file = main_input_data[:main_input_data.index("-f")]
-    clear_directories = create_dir(directories_without_file)
-    create_file(clear_directories)
+    create_file(create_dir(directories_without_file))
