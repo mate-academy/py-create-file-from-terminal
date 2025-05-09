400dae081fa6abd3c0535b9324ceda3808e7a76e
Bogdan0101 <kononovb777@gmail.com>
2025-05-09 21:51:30 +0300

    Solution#2

--- app/create_file.py
+++ app/create_file.py
@@ -7,7 +7,7 @@ def create_file(name_file: str) -> None:
     if os.path.exists(name_file):
         with open(name_file, "a") as file:
             file.write("\n")
-            file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+            file.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
             number_line = 1
             while True:
                 pyting_line = input("Enter content line: ")
@@ -17,7 +17,7 @@ def create_file(name_file: str) -> None:
                 number_line += 1
         return
     with open(name_file, "w") as file:
-        file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+        file.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
         number_line = 1
         while True:
             pyting_line = input("Enter content line: ")
@@ -27,34 +27,34 @@ def create_file(name_file: str) -> None:
             number_line += 1
 
 
-def create_file_or_dir(command: list) -> str | None:
+def create_file_or_dir(command: list) -> None:
     flag_f_index = command.index("-f") if "-f" in command else None
     flag_d_index = command.index("-d") if "-d" in command else None
     path = ""
-    if not flag_f_index and not flag_d_index:
+    if flag_f_index is None and flag_d_index is None:
         print("Typing command!!!")
-        return None
-    if flag_f_index:
+        return
+    if flag_f_index is not None:
         if not (len(command) > flag_f_index + 1):
             print("Please typing name file with extension!!!")
-            return None
+            return
         if not command[flag_f_index + 1].endswith(".txt"):
             print("Please typing name file with extension!!!")
-            return None
-    if flag_d_index and flag_f_index:
+            return
+    if flag_d_index is not None and flag_f_index is not None:
         path = "/".join(command[flag_d_index + 1:flag_f_index])
-    elif flag_d_index:
+    elif flag_d_index is not None:
         path = "/".join(command[flag_d_index + 1:])
-    if flag_d_index:
+    if flag_d_index is not None:
         if not os.path.exists(path):
             os.makedirs(path)
-        if flag_f_index:
+        if flag_f_index is not None:
             path += f"/{command[flag_f_index + 1]}"
             create_file(path)
-            return path
-    if flag_f_index:
+            return
+    if flag_f_index is not None:
         create_file(command[flag_f_index + 1])
-        return path
+        return
 
 
 if __name__ == "__main__":
