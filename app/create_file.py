48ba54e729b1636735fab86fda3c4153075d5b0a
Andrii Omelchenko <andreyomem@gmail.com>
2025-05-22 16:17:03 +0300

    implement create_file

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,28 @@
-# write your code here
+from os import path
+import sys
+from os import makedirs
+from datetime import datetime
+
+
+arguments = sys.argv
+directories = []
+path_to_file = ""
+for index in range(len(arguments)):
+    if arguments[index] == "-d":
+        for argument in arguments[index + 1:]:
+            if argument == "-f":
+                break
+            directories.append(argument)
+        path_to_file = path.join(*directories)
+        makedirs(path_to_file, exist_ok=True)
+    if arguments[index] == "-f":
+        with open(
+                path.join(*directories, arguments[index + 1])
+                , "w") as output_file:
+            output_file.write(
+                f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+            while True:
+                input_value = input("Enter content line:")
+                if input_value == "exit":
+                    break
+                output_file.write(f"{input_value}\n")
