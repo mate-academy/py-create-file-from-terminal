8d0e52d913eb5eb1d199a6fb132effd372f4211e
Vitalii <vitaliy.honchar@gmail.com>
2025-05-31 22:34:40 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,11 +1,9 @@
 import os
 import sys
-
+import datetime
 
 
 def create_file() -> None:
-    print(sys.argv)
-
     full_path = ""
     file_name = ""
 
@@ -17,24 +15,33 @@ def create_file() -> None:
             is_dir = True
             is_file = False
             continue
-
         if item == "-f":
             is_dir = False
             is_file = True
             continue
-
         if is_dir:
             full_path = os.path.join(full_path, item)
-
         if is_file:
             file_name = item
 
-    input_str = ""
+    if not os.path.isdir(full_path):
+        os.makedirs(full_path)
+
+    if file_name == "":
+        full_path = os.path.join(full_path, "file.txt")
+    else:
+        full_path = os.path.join(full_path, file_name)
+
+    input_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+    count = 0
     while input_str != "stop":
-        input_str = input("Enter content line: ")
-        if input_str == "stop":
+        temp_str = input("Enter content line: ")
+        if temp_str == "stop":
             break
+        count += 1
+        input_str += str(count)
+        input_str += " " + temp_str + "\n"
+    input_str += "\n"
 
-
-
-create_file()
+    with open(full_path, "a") as file:
+        file.write(input_str)
