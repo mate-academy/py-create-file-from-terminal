46d6272cd846bd76908bf1d2f4970e7560a770a0
nkkmkk <nikitmkk@gmail.com>
2025-06-09 14:16:11 +0200

    App writen

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,41 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+flag_d = False
+flag_f = False
+directories = []
+files = []
+
+for arg in sys.argv:
+    if arg == "-d":
+        flag_d = True
+        flag_f = False
+        continue
+    if arg == "-f":
+        flag_f = True
+        flag_d = False
+        continue
+
+    if flag_d:
+        directories.append(arg)
+    if flag_f:
+        files.append(arg)
+
+path = ""
+if directories:
+    path = os.path.join(*directories)
+    os.makedirs(path, exist_ok=True)
+
+for file_name in files:
+    with open(os.path.join(path, file_name), "w") as file:
+        content = str(datetime.now()) + "\n"
+        line = 1
+        while True:
+            user_input = input("Enter content line: ").strip()
+            if user_input.lower() == "stop":
+                break
+            content += f"{line} {user_input}\n"
+            line += 1
+        file.write(content)
