6ba4e1525970ae87c80c19b6940fcec8797f136e
Sasha <kluzodota@gmail.com>
2025-06-11 20:34:16 +0300

    first fix

--- app/create_file.py
+++ app/create_file.py
@@ -3,7 +3,7 @@ from sys import argv
 from os import makedirs, path
 
 
-def datatime_in_format() -> str:
+def datetime_in_format() -> str:
     data_time = datetime.datetime.today()
     return data_time.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -35,13 +35,14 @@ def run_argv() -> None:
     if file_name is not None:
         print(file_name)
         if directory is not None:
-            file_name = directory_join + "\\" + file_name
+            file_name = path.join(directory_join, file_name)
         with open(file_name, "a") as file_a:
-            file_a.write(datatime_in_format() + "\n")
+            file_a.write(datetime_in_format() + "\n")
             count = 1
             input_msg = input("Enter content line: ")
             while input_msg != "stop":
                 file_a.write(str(count) + " " + input_msg + "\n")
+                count += 1
                 input_msg = input("Enter content line: ")
 
 
