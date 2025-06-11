c9644d4b1780f5ea09afae2c96f9f48240b9c18c
Sasha <kluzodota@gmail.com>
2025-06-11 20:23:49 +0300

    problem is solved

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,48 @@
-# write your code here
+import datetime
+from sys import argv
+from os import makedirs, path
+
+
+def datatime_in_format() -> str:
+    data_time = datetime.datetime.today()
+    return data_time.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def run_argv() -> None:
+    data_terminal = argv
+
+    if "-f" in data_terminal:
+        index_f = data_terminal.index("-f")
+        file_name = data_terminal[index_f + 1]
+    else:
+        index_f = len(data_terminal)
+        file_name = None
+
+    if "-d" in data_terminal:
+        index_d = data_terminal.index("-d")
+        if index_d < index_f:
+            directory = data_terminal[index_d + 1:index_f]
+        else:
+            directory = data_terminal[index_d + 1:]
+    else:
+
+        directory = None
+
+    if directory is not None:
+        directory_join = path.join(*directory)
+        makedirs(directory_join, exist_ok=True)
+
+    if file_name is not None:
+        print(file_name)
+        if directory is not None:
+            file_name = directory_join + "\\" + file_name
+        with open(file_name, "a") as file_a:
+            file_a.write(datatime_in_format() + "\n")
+            count = 1
+            input_msg = input("Enter content line: ")
+            while input_msg != "stop":
+                file_a.write(str(count) + " " + input_msg + "\n")
+                input_msg = input("Enter content line: ")
+
+
+run_argv()
