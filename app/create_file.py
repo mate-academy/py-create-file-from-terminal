3249e844429023fdb8f587a9ccb0fc92b382ea23
kbatrakov <kirillbatrakov0819@gmail.com>
2025-06-17 11:31:48 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,56 @@
-# write your code here
+import os
+import sys
+from datetime import datetime
+
+
+def create_directory() -> None:
+    first_dir, second_dir = sys.argv[2:4]
+    path = os.path.join(first_dir, second_dir)
+    os.makedirs(path)
+
+
+def create_file(file_path: str = sys.argv[2]) -> None:
+    current_date_time = datetime.now()
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as file:
+            file.write(current_date_time.strftime("%Y-%m-%d %H:%M:%S") + "\n")
+            line_number = 1
+            while True:
+                file_content = input("Enter content line: ")
+                if file_content.lower().strip() == "stop":
+                    break
+                file.write(f"{line_number} {file_content}\n")
+                line_number += 1
+    else:
+        with open(file_path, "a") as file:
+            file.write("\n")
+            file.write(current_date_time.strftime("%Y-%m-%d %H:%M:%S") + "\n")
+            line_number = 1
+            while True:
+                file_content = input("Enter content line: ")
+                if file_content.lower().strip() == "stop":
+                    break
+                file.write(f"{line_number} {file_content}\n")
+                line_number += 1
+
+
+def create_both() -> None:
+    create_directory()
+    file_name = str(sys.argv[5])
+    folder_1 = str(sys.argv[2])
+    folder_2 = str(sys.argv[3])
+    path = os.path.join(folder_1, folder_2, file_name)
+    create_file(file_path=path)
+
+
+def main() -> None:
+    if len(sys.argv) == 4 and sys.argv[1] == "-d":
+        create_directory()
+
+    if len(sys.argv) == 3 and sys.argv[1] == "-f":
+        create_file()
+
+    create_both()
+
+
+print(main())
