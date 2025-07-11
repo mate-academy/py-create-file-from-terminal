96e767746a35faf43b1ca3481f41d4811c8e451f
Hamster <dtaranenko137@gmail.com>
2025-07-11 19:45:26 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -9,31 +9,33 @@ def create_file() -> Any:
     dir_path = ""
     file_name = ""
 
-    try:
-        if "-d" in arguments:
-            flag_d = arguments.index("-d")
-            next_flag = arguments.index("-f")\
-                if "-f" in arguments\
-                else len(arguments)
-            dir_path = "/".join(arguments[flag_d + 1 : next_flag])
-            os.makedirs(dir_path, exist_ok=True)
+    if "-d" in arguments:
+        flag_d = arguments.index("-d")
+        next_flag = arguments.index("-f")\
+            if "-f" in arguments\
+            else len(arguments)
+        dir_path = "/".join(arguments[flag_d + 1 : next_flag])
+        os.makedirs(dir_path, exist_ok=True)
 
-        if "-f" in arguments:
-            flag_f = arguments.index("-f")
-            file_name = arguments[flag_f + 1]
+    if "-f" in arguments:
+        flag_f = arguments.index("-f")
+        file_name = arguments[flag_f + 1]
 
-        file_path = f"{dir_path}/{file_name}" if dir_path else file_name
+    file_path = f"{dir_path}/{file_name}" if dir_path else file_name
 
-        with open(file_path, "a") as f:
-            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            f.write(f"\n{timestamp}\n")
+    if not file_name:
+        print("Dir crated")
+    else:
+        with open(file_path, "a") as file:
+            print("For stop input please write stop")
+            date_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(f"{date_str}\n")
             while True:
-                line = input("Input ")
-                if line.strip().lower() == "stop":
+                line = input("Print your massege: ")
+                if line.lower() == "stop":
                     break
-                f.write(line + "\n")
-    except Exception as e:
-        print(f"Error: {e}")
+                file.write(f"{line}\n")
+                file.write("\n")
 
 
 create_file()
