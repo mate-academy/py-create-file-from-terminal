443952cdacda2d4727878b219fdd4380400e82f3
MakovM <makovinskiy09@gmail.com>
2025-07-14 16:58:18 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,48 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def file_writer(name: str) -> None:
+    with open(name, "a") as file_open:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file_open.write(current_time + "\n")
+        line = 1
+
+        while True:
+            content = input("Enter content line: ")
+
+            if content.lower() == "stop":
+                break
+            file_open.write(f"{line} {content}/n")
+            line += 1
+        file_open.write("\n")
+
+
+def create_file(args: list) -> None:
+    d_index = args.index("-d")
+    f_index = args.index("-f")
+
+    if "-d" in args and "-f" in args:
+        dir_path = args[d_index + 1 : f_index]
+        file_name = args[f_index + 1]
+    elif "-d" in args:
+        dir_path = args[d_index + 1 : f_index]
+        file_name = None
+    elif "-f" in args:
+        dir_path = []
+        file_name = args[f_index + 1]
+    else:
+        print("Error: No flags provided")
+        return
+    if dir_path:
+        dir_path = os.path.join(*dir_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+    if file_name:
+        file_path = os.path.join(dir_path, file_name)
+        file_writer(file_path)
+
+
+if __name__ == "__main__":
+    create_file(sys.argv[1:])
