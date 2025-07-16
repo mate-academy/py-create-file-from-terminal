748f18da643e32f105a746b110d86d3f4a57205d
yarik <super.racing@ukr.net>
2025-07-16 20:44:43 +0300

    changed paths

--- app/create_file.py
+++ app/create_file.py
@@ -3,13 +3,14 @@ import os
 from datetime import datetime
 
 
-def create_dir(parts: list, num_of_dirs: int) -> None:
+def create_dir(parts: list, num_of_dirs: int) -> str:
     my_path = os.getcwd()
-    os.path.join(my_path, *parts[2:num_of_dirs])
+    my_path = os.path.join(my_path, *parts[2:num_of_dirs])
     os.makedirs(my_path)
+    return my_path
 
 
-def create_file_with_content(parts: list) -> None:
+def create_file_with_content(parts: list, my_path: str) -> None:
     content = []
     while True:
         command = input("Enter content line: ")
@@ -17,8 +18,9 @@ def create_file_with_content(parts: list) -> None:
             break
         content.append(command)
     file_name = parts[parts.index("-f") + 1]
-    with open(file_name, "r") as file:
-        file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+    my_path = os.path.join(my_path, file_name)
+    with open(my_path, "w") as file:
+        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         for line in content:
             file.write(f"{line}\n")
 
@@ -28,7 +30,7 @@ def create_file() -> None:
     if parts[1] == "-d" and "-f" not in parts:
         create_dir(parts, len(parts))
     elif parts[1] == "-f" and "-d" not in parts:
-        create_file_with_content(parts)
+        my_path = os.getcwd()
+        create_file_with_content(parts, my_path)
     elif "-d" in parts and "-f" in parts:
-        create_dir(parts, parts.index("-f"))
-        create_file_with_content(parts)
+        create_file_with_content(parts, create_dir(parts, parts.index("-f")))
