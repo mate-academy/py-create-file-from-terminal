44e666df68ccf8c760ffdd7846d9abb2870b7299
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-07-16 16:39:11 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,49 @@
-# write your code here
+import datetime
+import os
+import sys
+
+
+def main() -> None:
+    if len(sys.argv) < 3 or not sys.argv[1] in ["-f", "-d"]:
+        return
+
+    if sys.argv[1] == "-d" and sys.argv[-2] == "-f":
+        path = sys.argv[2:-2]
+        new_path = create_path(path)
+        filename = new_path + sys.argv[-1]
+
+    elif sys.argv[1] == "-d":
+        path = sys.argv[2:]
+        create_path(path)
+        return
+
+    elif sys.argv[1] == "-f":
+        filename = sys.argv[2]
+
+    else:
+        return
+
+    with open(filename, "a+") as f:
+        while True:
+            counter = 0
+            content = input("Enter content line: ")
+            if not content == "stop":
+                if counter == 0:
+                    timestamp = datetime.datetime.now(
+                    ).strftime("%Y-%m-%d %H:%M:%S")
+                    f.write(f"{timestamp}\n")
+                f.write(f"{content}\n")
+            else:
+                break
+
+
+def create_path(items: list) -> str:
+    curr = os.getcwd()
+    new_path = os.path.join(curr, *items) + os.sep
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    return new_path
+
+
+if __name__ == "__main__":
+    main()
