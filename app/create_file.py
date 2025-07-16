f171bb37dfd8e38decccbb6d88eb877e0d5e868d
Hamster <dtaranenko137@gmail.com>
2025-07-16 18:30:29 +0300

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -2,15 +2,18 @@ import sys
 import datetime
 import os
 
-def parse_arguments():
+
+def parse_arguments() -> tuple[str, str]:
     arguments = sys.argv
     dir_path = ""
     file_name = ""
 
     if "-d" in arguments:
         flag_d = arguments.index("-d")
-        next_flag = arguments.index("-f") if "-f" in arguments else len(arguments)
-        dir_parts = arguments[flag_d + 1 : next_flag]
+        next_flag = arguments.index("-f") \
+            if "-f" in arguments \
+            else len(arguments)
+        dir_parts = arguments[flag_d + 1:next_flag]
         dir_path = os.path.join(*dir_parts)
         os.makedirs(dir_path, exist_ok=True)
 
@@ -20,10 +23,12 @@ def parse_arguments():
 
     return dir_path, file_name
 
-def build_file_path(dir_path, file_name):
+
+def build_file_path(dir_path: str, file_name: str) -> str:
     return os.path.join(dir_path, file_name) if dir_path else file_name
 
-def write_to_file(file_path):
+
+def write_to_file(file_path: str) -> None:
     with open(file_path, "a") as file:
         print("For stop input please write stop")
         date_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -37,13 +42,15 @@ def write_to_file(file_path):
             count += 1
         file.write("\n")
 
-def main():
+
+def main() -> None:
     dir_path, file_name = parse_arguments()
     if not file_name:
         print("Dir created")
     else:
         file_path = build_file_path(dir_path, file_name)
         write_to_file(file_path)
+
 
 if __name__ == "__main__":
     main()
