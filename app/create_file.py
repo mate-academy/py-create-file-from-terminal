3a1219b1f64d56a8b6ae1cad47482863e7135b1b
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-07-24 23:14:54 +0200

    dk

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,40 @@
-# write your code here
+from sys import argv
+from os import makedirs, path
+from datetime import datetime
+
+name = argv
+
+
+def with_d(road: list) -> None:
+    dirs = path.join(*road)
+    makedirs(dirs)
+
+
+def with_f(file_name: str) -> None:
+    with open(file_name, "a") as doc:
+        text = []
+        while True:
+            line = input("Enter content line: ")
+            if line.lower() == "stop":
+                break
+            text.append(line)
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        doc.write(now + "\n")
+        for number in range(len(text)):
+            doc.write(f"{number + 1} {text[number]}\n")
+
+
+if "-d" in name and "-f" in name:
+    road_for_dic = name[name.index("-d") + 1 : name.index("-f")]
+    file_name = name[name.index("-f") + 1]
+    with_d(road_for_dic)
+    file_path = path.join(*road_for_dic, file_name)
+    with_f(file_path)
+
+
+elif "-d" in name:
+    road_for_dic = name[name.index("-d") + 1 :]
+    with_d(road_for_dic)
+elif "-f" in name:
+    file_name = name[name.index("-f") + 1]
+    with_f(file_name)
