3291b470d132afba8370b430beae3e22df32e6f9
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-07-27 19:28:09 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,16 +3,19 @@ import sys
 from datetime import datetime
 
 
-def create_dir(path: list) -> None:
-    for i in range(len(path)):
-        if not os.path.exists("\\".join(path[:i + 1])):
-            os.mkdir("\\".join(path[:i + 1]))
+def create_dir(path: list[str]) -> None:
+    current_path = ""
+    for folder in path:
+        current_path = os.path.join(current_path, folder)
+        if not os.path.exists(current_path):
+            os.mkdir(current_path)
 
 
 def create_file_by_path(path: list, filename: str) -> None:
-    if os.path.exists("\\".join(path) + "\\" + filename):
+    file_path = os.path.join(*path, filename)
+    if os.path.exists(file_path):
         return
-    with open("\\".join(path) + "\\" + filename, "w"):
+    with open(file_path, "w"):
         return
 
 
@@ -23,11 +26,23 @@ def create_file_in_current_dir(filename: str) -> None:
         return
 
 
+def write_to_file(path: str, filename: str) -> None:
+    file_path = os.path.join(path, filename)
+    with open(file_path, "a") as f:
+        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        while True:
+            line = input("Enter content line: ")
+            if line.lower() == "stop":
+                f.write("\n")
+                break
+            f.write(line)
+
+
 def execute_command(command: list) -> None:
     if len(command) < 3:
         return
 
-    path = ""
+    path = "."
     if "-d" in command:
         path = command[command.index("-d") + 1:-2]
         create_dir(path)
@@ -35,17 +50,12 @@ def execute_command(command: list) -> None:
     if "-f" in command:
         if path:
             create_file_by_path(path, command[-1])
-            path = "\\".join(path) + "\\"
+            path = os.path.join(".", *path)
+
         else:
             create_file_in_current_dir(command[-1])
-        with open(path + command[-1], "a") as f:
-            f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            while True:
-                line = input("Enter content line: ")
-                if line.lower() == "stop":
-                    break
-                f.write(line)
 
+        write_to_file(path, command[-1])
 
 if __name__ == "__main__":
-    execute_command(sys.argv)
+    execute_command(sys.argv[1:])
