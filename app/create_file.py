99e73a9857c0d18a65a43f8d59c6dbfd6e3ab666
Anita Gajewska <gajewskaanita0@gmail.com>
2025-07-29 19:07:47 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -17,19 +17,19 @@ if "-d" in args:
         dir_path = os.path.join(*dir_parts)
         os.makedirs(dir_path, exist_ok=True)
 
-    if "-f" in args:
-        f_index = args.index("-f")
-        file_name = args[f_index + 1]
+if "-f" in args:
+    f_index = args.index("-f")
+    file_name = args[f_index + 1]
 
-        full_path = os.path.join(dir_path, file_name) \
-            if dir_path else file_name
+    full_path = os.path.join(dir_path, file_name) \
+        if dir_path else file_name
 
-        print("Enter content line:")
-        while True:
-            line = input()
-            if line.lower() == "stop":
-                break
-            content_lines.append(line)
+    print("Enter content line:")
+    while True:
+        line = input()
+        if line.lower() == "stop":
+            break
+        content_lines.append(line)
 
         with open(full_path, "a", encoding="utf-8") as f:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
