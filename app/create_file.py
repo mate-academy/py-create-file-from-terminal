32fa7bb4fdaa1db3bc4ce2279a23c997de86ae29
Jakub <jakubmichalski89@fgmail.com>
2025-07-30 11:30:45 +0200

    'Solution'

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,23 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def parse_args():
+    args = sys.argv[1:]
+
+    dir_path = ""
+    file_name = ""
+
+    if "-d" in args:
+        d_index = args.index("-d")
+        # zbieramy wszystko aż do następnej flagi lub końca listy
+        next_flag = args[d_index + 1:]
+        f_index = next_flag.index("-f") if "-f" in next_flag else len(next_flag)
+        dir_path = os.path.join(*next_flag[:f_index])
+
+    if "-f" in args:
+        f_index = args.index("-f")
+        file_name = args[f_index + 1]
+
+    return dir_path, file_
