3e891fe1f9f84a3bf4f447e66c0e6ba7b1de834c
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-08-01 22:30:21 +0700

    develop

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,44 @@
-# write your code here
+import sys
+from os import path, makedirs
+from datetime import datetime
+
+
+args = sys.argv[1:]
+
+def parse_args(args: list) -> dict:
+    result = {"-f": [], "-d": []}
+    current_flag = None
+
+    for arg in args:
+        if arg in result:
+            current_flag = arg
+        elif current_flag:
+            result[current_flag].append(arg)
+    if not result["-f"]:
+        result["-f"].append("file.txt")
+    if not result["-d"]:
+        result["-d"].append(".")
+    return result
+
+
+def create_file(args: list) -> None:
+    flagsNargs = parse_args(args)
+    dest = path.join(*flagsNargs["-d"])
+    makedirs(dest, exist_ok=True)
+    dest_file = path.join(dest, *flagsNargs["-f"])
+    prompt = []
+    _timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    while True:
+        user_input = input("Enter content line: ")
+        if user_input == "stop":
+            break
+        prompt.append(user_input + "\n")
+
+    with open(dest_file, "a") as fname:
+        fname.write(_timestamp + "\n")
+        fname.writelines(prompt)
+
+create_file(args)
+
+if __name__ == "main":
+    pass
