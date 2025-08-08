52f06a6c7e6ee69b3938ba6baf3a4fcfd3516e65
Oleksandr Skoromnov <solalex005@gmail.com>
2025-08-08 22:40:35 +0300

    fix2

--- app/create_file.py
+++ app/create_file.py
@@ -2,55 +2,56 @@ from datetime import datetime
 import sys
 import os
 from contextlib import contextmanager
-from typing import Any
+from typing import Iterator, TextIO
+from argparse import ArgumentParser
 
 
 @contextmanager
-def get_file(name: str) -> Any:
-    path = os.path.join(os.getcwd(), name)
-    if not os.path.exists(path):
-        file = open(path, "w")
-    else:
-        file = open(path, "a")
-    file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
-    yield file
-    file.close()
+def get_file(name: str) -> Iterator[TextIO]:
+    source_file = open(name, "a")
+    source_file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+    yield source_file
+    source_file.close()
 
 
 def write_to_file(name: str) -> None:
-    with get_file(name) as f:
+    with get_file(name) as source_file:
         counter = 1
         while True:
             content = input("Enter content line: ")
             if content == "stop":
-                f.write("\n")
+                source_file.write("\n")
                 break
-            f.write(f"{counter} {content}\n")
+            source_file.write(f"{counter} {content}\n")
             counter += 1
 
 
 def create_dirs(names: list[str]) -> None:
     if len(names) == 1:
         os.mkdir(names[0])
-    else:
-        os.makedirs(os.path.join(*names), exist_ok=True)
+    os.makedirs(os.path.join(*names), exist_ok=True)
+
+
+def create_dirs_and_file(filename: str, dirs: list[str]) -> None:
+    create_dirs(dirs)
+    write_to_file(os.path.join(*dirs, filename))
+
+
+def init_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("-d", action="extend", nargs="+", type=str)
+    parser.add_argument("-f", action="store", nargs=1, type=str)
+    return parser
 
 
 if __name__ == "__main__":
-    args = sys.argv
-    if len(args) < 2:
-        raise ValueError("No flag provided. Use -f or -d.")
-    if args[1] not in ["-f", "-d"]:
-        raise ValueError("Wrong flags")
-    if sum(value in ["-f", "-d"] for value in args) == 2:
-        f_index = args.index("-f")
-        create_dirs(args[2:f_index])
-        write_to_file(os.path.join(*args[2:f_index], args[-1]))
-    elif args[1] == "-f":
-        if not args[2]:
-            raise ValueError("Needed file name")
-        write_to_file(args[2])
-    elif args[1] == "-d":
-        if not args[2:]:
-            raise ValueError("Needed dir names")
-        create_dirs(args[2:])
+    parser = init_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    filename = getattr(namespace, "f", None)
+    dirs = getattr(namespace, "d", None)
+    if filename and dirs:
+        create_dirs_and_file(filename[0], dirs)
+    elif filename:
+        write_to_file(filename[0])
+    elif dirs:
+        create_dirs(dirs)
