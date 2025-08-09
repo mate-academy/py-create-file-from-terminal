df50cec760d7ea58924d8c592d26056edb43c5ea
Volodymyr Syvak <volodimirsivak388@gmail.com>
2025-08-09 20:08:42 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,28 +1,27 @@
 import os
 import sys
-from daytime import daytime
+from datetime import datetime
 
 
 command = sys.argv
-directories_to_do = "app/"
-all_content = f"{daytime.daytime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
+directories_to_do = ""
+all_content = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
 line_count = 1
 if command[1] == "-d":
 
     if "-f" in command:
         f_index = command.index("-f")
-        for directory in command[2: f_index]:
-            directories_to_do += f"{directory}/"
+        directories_to_do = os.path.join(command[2: f_index])
         os.makedirs(directories_to_do, exist_ok=True)
 
-        name = command[f_index + 1]
+        name = os.path.join(directories_to_do, command[f_index + 1])
         if os.path.isfile(name):
             content = "\n" + input("Enter content line: ")
             while content != "stop":
                 all_content += f"{line_count} {content}\n"
                 content = input("Enter content line: ")
                 line_count += 1
-            with open(name, "a") as f:
+            with open(f"{directories_to_do}{name}", "a") as f:
                 f.write(all_content)
 
         else:
@@ -31,16 +30,15 @@ if command[1] == "-d":
                 all_content += f"{line_count} {content}\n"
                 content = input("Enter content line: ")
                 line_count += 1
-            with open(name, "w") as f:
+            with open(f"{directories_to_do}{name}", "w") as f:
                 f.write(all_content)
 
     else:
-        for directory in command[2:]:
-            directories_to_do += f"{directory}/"
+        directories_to_do = os.path.join(command[2:])
         os.makedirs(directories_to_do, exist_ok=True)
 
 if command[1] == "-f":
-    name = command[2]
+    name = os.path.join(directories_to_do, command[2])
 
     if os.path.isfile(name):
         content = "\n" + input("Enter content line: ")
@@ -48,7 +46,7 @@ if command[1] == "-f":
             all_content += f"{line_count} {content}\n"
             content = input("Enter content line: ")
             line_count += 1
-        with open(name, "a") as f:
+        with open(f"{directories_to_do}{name}", "a") as f:
             f.write(all_content)
 
     else:
@@ -57,5 +55,5 @@ if command[1] == "-f":
             all_content += f"{line_count} {content}\n"
             content = input("Enter content line: ")
             line_count += 1
-        with open(name, "w") as f:
+        with open(f"{directories_to_do}{name}", "w") as f:
             f.write(all_content)
