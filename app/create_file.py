0a5a5cda30490a4430506935dab0a149d1da1d36
Maksym Pakula <gargantadoto@gmail.com>
2025-08-11 23:44:37 +0300

    added new functions to avoid repeat of code

--- app/create_file.py
+++ app/create_file.py
@@ -1,51 +1,59 @@
 import sys
-import datetime
 import os
+import datetime
 
 
-def create_file() -> None:
-    index = sys.argv.index("-f") + 1
-    file_name = sys.argv[index]
+def parse_args():
+    directory_parts = []
+    file_name = None
+
     if "-d" in sys.argv:
         dir_index = sys.argv.index("-d") + 1
-        dir_parts = sys.argv[dir_index: sys.argv.index("-f")]
-        parent_path = os.getcwd()
-        dir_path = os.path.join(parent_path, *dir_parts)
-        full_file_path = os.path.join(dir_path, file_name)
-    else:
-        full_file_path = os.path.join(os.getcwd(), file_name)
-    with open(full_file_path, "a") as file:
-        file.write(
-            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-        )
-        i = 1
+        if "-f" in sys.argv:
+            directory_parts = sys.argv[dir_index: sys.argv.index("-f")]
+        else:
+            directory_parts = sys.argv[dir_index:]
+
+    if "-f" in sys.argv:
+        file_index = sys.argv.index("-f") + 1
+        file_name = sys.argv[file_index]
+
+    return directory_parts, file_name
+
+
+def create_directory(directory_parts):
+    if not directory_parts:
+        return os.getcwd()
+    directory_path = os.path.join(os.getcwd(), *directory_parts)
+    os.makedirs(directory_path, exist_ok=True)
+    return directory_path
+
+
+def create_file(file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "a", encoding="utf-8") as source_file:
+        source_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        line_number = 1
         while True:
             line = input("Enter content line: ")
             if line == "stop":
                 break
-            file.write(f"{i} " + line + "\n")
-            i += 1
+            source_file.write(f"{line_number} {line}\n")
+            line_number += 1
 
 
-def create_directory() -> None:
-    directory = []
-    parent_path = os.getcwd()
-    for i in range(2, len(sys.argv)):
-        if sys.argv[i] == "-f":
-            break
-        directory.append(sys.argv[i])
-    path = os.path.join(parent_path, *directory)
-    os.makedirs(path, exist_ok=True)
+def main():
+    directory_parts, file_name = parse_args()
+
+    if directory_parts:
+        directory_path = create_directory(directory_parts)
+    else:
+        directory_path = os.getcwd()
+
+    if file_name:
+        file_path = os.path.join(directory_path, file_name)
+        create_file(file_path)
 
 
-if sys.argv[1] == "-f":
-    create_file()
-
-
-if sys.argv[1] == "-d":
-    create_directory()
-
-
-if "-f" in sys.argv and "-d" in sys.argv:
-    create_directory()
-    create_file()
+if __name__ == "__main__":
+    main()
