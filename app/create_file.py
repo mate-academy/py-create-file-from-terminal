8b9ca2c52041f902cb370d6d0a34a8444eeae445
Olexii Babii <babiy0617@gmail.com>
2025-08-29 17:40:40 +0300

    fix_1

--- app/create_file.py
+++ app/create_file.py
@@ -3,44 +3,36 @@ import os
 import datetime
 
 
+def write_text(file) -> None:
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+    index = 1
+    f.write(time)
+    text = input("Enter content line: ")
+    while text != "stop":
+        text = str(index) + " " + text + "\n"
+        index += 1
+        f.write(text)
+        text = input("Enter content line: ")
+    f.write("\n")
+
+
 args = sys.argv
 if "-d" in args and "-f" not in args:
-    os.makedirs(os.path.join(args[2:]))
+    os.makedirs(os.path.join(*args[2:]), exist_ok=True)
 if "-d" not in args and "-f" in args:
     with open(args[2], "a") as f:
-        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-        f.write(time)
-        text = input("Enter content line: ")
-        while text != "stop":
-            text += "\n"
-            f.write(text)
-            text = input("Enter content line: ")
-        f.write("\n")
+        write_text(f)
 
 if "-d" in args and "-f" in args:
     f_index = args.index("-f")
     d_index = args.index("-d")
     if f_index > d_index:
         path = os.path.join(*args[2:f_index])
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
         with open(os.path.join(path, args[f_index + 1]), "a") as f:
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-            f.write(time)
-            text = input("Enter content line: ")
-            while text != "stop":
-                text += "\n"
-                f.write(text)
-                text = input("Enter content line: ")
-            f.write("\n")
+            write_text(f)
     if f_index < d_index:
-        path = os.path.join(args[4:])
-        os.makedirs(path)
+        path = os.path.join(*args[4:])
+        os.makedirs(path, exist_ok=True)
         with open(os.path.join(path, args[f_index + 1]), "a") as f:
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-            f.write(time)
-            text = input("Enter content line: ")
-            while text != "stop":
-                text += "\n"
-                f.write(text)
-                text = input("Enter content line: ")
-            f.write("\n")
+            write_text(f)
