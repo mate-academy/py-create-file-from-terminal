a3f7667b5a07b642e1bd490c497cc4cda7b7fd0a
Olexii Babii <babiy0617@gmail.com>
2025-08-29 18:57:15 +0300

    fix_2

--- app/create_file.py
+++ app/create_file.py
@@ -1,38 +1,54 @@
 import sys
 import os
 import datetime
+from typing import TextIO
 
 
-def write_text(file) -> None:
+def parse_args(args: list) -> list:
+    path = []
+    file_name = ""
+    if ("-f" in args
+        and args.index("-f") != -1
+            and args[args.index("-f") + 1] != "-d"):
+        file_name = args[args.index("-f") + 1]
+    if ("-d" in args
+        and args.index("-d") != -1
+            and args[args.index("-d") + 1] != "-f"):
+        for directory in args[args.index("-d") + 1:]:
+            if directory == "-f":
+                break
+            else:
+                path.append(directory)
+    return [file_name, path]
+
+
+def write_text(opened_file: TextIO) -> None:
     time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
     index = 1
-    f.write(time)
+    opened_file.write(time)
     text = input("Enter content line: ")
     while text != "stop":
         text = str(index) + " " + text + "\n"
         index += 1
-        f.write(text)
+        opened_file.write(text)
         text = input("Enter content line: ")
-    f.write("\n")
+    opened_file.write("\n")
 
 
-args = sys.argv
-if "-d" in args and "-f" not in args:
-    os.makedirs(os.path.join(*args[2:]), exist_ok=True)
-if "-d" not in args and "-f" in args:
-    with open(args[2], "a") as f:
-        write_text(f)
-
-if "-d" in args and "-f" in args:
-    f_index = args.index("-f")
-    d_index = args.index("-d")
-    if f_index > d_index:
-        path = os.path.join(*args[2:f_index])
-        os.makedirs(path, exist_ok=True)
-        with open(os.path.join(path, args[f_index + 1]), "a") as f:
+def create_file() -> None:
+    args = sys.argv
+    filename_path = parse_args(args=args)
+    if "-d" in args and "-f" not in args:
+        os.makedirs(os.path.join(*filename_path[1]), exist_ok=True)
+    if "-d" not in args and "-f" in args:
+        with open(filename_path[0], "a", encoding="utf-8") as f:
             write_text(f)
-    if f_index < d_index:
-        path = os.path.join(*args[4:])
+    if "-d" in args and "-f" in args:
+        path = os.path.join(*filename_path[1])
         os.makedirs(path, exist_ok=True)
-        with open(os.path.join(path, args[f_index + 1]), "a") as f:
+        with open(os.path.join(path, filename_path[0]),
+                  "a", encoding="utf-8") as f:
             write_text(f)
+
+
+create_file()
