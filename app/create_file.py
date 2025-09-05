1631325509a89638a382f97c0000ac350c192c9e
Oleksandr Shkliar <shklyara29@gmail.com>
2025-09-05 12:26:53 +0300

    Corrected bugs in loop while

--- app/create_file.py
+++ app/create_file.py
@@ -16,17 +16,20 @@ def create_file() -> str:
         os.makedirs(os.path.join(*list_arg[flag_d + 1:]), exist_ok=True)
     if "-f" in list_arg and "-d" not in list_arg:
         flag_f = list_arg.index("-f")
-        path = list_arg[flag_f + 1]
+        path = os.path.join(list_arg[flag_f + 1])
         return path
 
 
 def write_content(path: str) -> None:
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(path, "a") as file:
+        if os.path.exists(path) and os.path.getsize(path):
+            file.write("\n")
         file.write(current_time + "\n")
         page_number = 1
-        content = input("Enter content line: ")
-        while content != "stop":
+        while True:
+            content = input("Enter content line: ")
+            if content.lower() == "stop":
+                break
             file.write(f"{page_number} {content}\n")
             page_number += 1
-        file.write("\n")
