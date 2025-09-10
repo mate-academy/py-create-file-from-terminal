7b9051f0501e93624613f12a3b3fcdacecb25064
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-09-10 21:45:22 +0200

    Fix on request

--- app/create_file.py
+++ app/create_file.py
@@ -7,8 +7,15 @@ arguments = sys.argv
 
 
 class NoExpectedAttributes(Exception):
-    def __str__(self) -> str:
-        return "Expected attributes are not provided"
+    pass
+
+
+class NoProvidedFolderNames(Exception):
+    pass
+
+
+class NotProvidedFileName(Exception):
+    pass
 
 
 def write_lines_in_file(file_name: str, blank_line: bool) -> None:
@@ -21,9 +28,10 @@ def write_lines_in_file(file_name: str, blank_line: bool) -> None:
 
         opened_file.write(current_date)
         new_line = input("Enter content line: ")
-        while new_line != "close":
+        while new_line != "stop":
             opened_file.write(str(page_number) + " " + new_line + "\n")
             new_line = input("Enter content line: ")
+            page_number += 1
 
 
 def write_into_file(file_name: str) -> None:
@@ -38,21 +46,44 @@ if "-d" not in arguments and "-f" not in arguments:
     raise NoExpectedAttributes("Wrong arguments, -f and -d expected")
 
 
-if arguments[1] == "-d" and arguments[-2] == "-f":
-    print("FIRST CASE")
-    new_folder_path = os.path.join(os.getcwd(), *arguments[2:-2])
-    file_name = arguments[-1]
+if "-d" in arguments and "-f" in arguments:
+    folder_names = arguments[arguments.index("-d") + 1:arguments.index("-f")]
 
-    os.makedirs(new_folder_path)
-    filepath = os.path.join(*arguments[2:-2], file_name)
+    if len(folder_names) == 0:
+        raise NoProvidedFolderNames
+
+    new_folder_path = os.path.join(
+        os.getcwd(),
+        *folder_names
+    )
+
+    try:
+        file_name = arguments[arguments.index("-f") + 1]
+    except IndexError:
+        raise NotProvidedFileName
+
+    os.makedirs(new_folder_path, exist_ok=True)
+    filepath = os.path.join(
+        *folder_names,
+        file_name
+    )
     write_into_file(filepath)
-elif arguments[1] == "-d":
-    print("SECOND CASE")
-    new_folder_path = os.path.join(os.getcwd(), *arguments[2:])
+elif "-d" in arguments:
+    folder_names = arguments[arguments.index("-d") + 1:]
 
-    os.makedirs(new_folder_path)
+    if len(folder_names) == 0:
+        raise NoProvidedFolderNames
+
+    new_folder_path = os.path.join(
+        os.getcwd(),
+        *folder_names
+    )
+
+    os.makedirs(new_folder_path, exist_ok=True)
 else:
-    print("THIRD CASE")
-    file_name = arguments[2]
+    try:
+        file_name = arguments[arguments.index("-f") + 1]
+    except IndexError:
+        raise NotProvidedFileName
 
     write_into_file(file_name)
