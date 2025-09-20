443c18c5bdd577ac726fc2631bebc5d073bf0c70
Alex <zigzaksash@gmail.com>
2025-09-20 22:55:05 +0200

    Job done:)

--- app/create_file.py
+++ app/create_file.py
@@ -4,43 +4,22 @@ import sys
 
 def create_file(lines: str) -> None:
     user_text = input(f"Enter content line: {lines}")
-    with open("user_text", "w") as file:
-        direct_index = sys.argv.index("-d")
-        file_index = sys.argv.index("-f")
-        start = sys.argv.index("-d")
-        end = sys.argv.index("-f")
 
-        if "-d" and "-f" in sys.argv:
-            for i in range(start, end):
-                if i != "stop":
-                    direct_name = sys.argv[direct_index + i]
-                    if direct_name != file_index:
-                        os.makedirs(direct_name)
-                else:
-                    break
-            for i in range(end, len(sys.argv)):
-                if i != "stop":
-                    file_name = sys.argv[file_index + i]
-                    with open(file_name, "w") as name_file:
-                        name_file.write(user_text)
-                else:
-                    break
+    direct_index = sys.argv.index("-d")
+    file_index = sys.argv.index("-f")
 
-        if "-d" in sys.argv :
-            for i in range(start, end):
-                if i != "stop":
-                    direct_name = sys.argv[direct_index + i]
-                    if direct_name != file_index:
-                        os.makedirs(direct_name)
-                else:
-                    break
+    if "-d" in sys.argv :
+        for i in range(direct_index, file_index):
+            if sys.argv[i] != "stop":
+                if sys.argv[i] != "-f":
+                    os.makedirs(sys.argv[i])
+            else:
+                break
 
-        if "-f" in sys.argv:
-            for i in range(end, len(sys.argv)):
-                if i != "stop":
-                    file_name = sys.argv[file_index + i]
-                    with open(file_name, "w") as name_file:
-                        name_file.write(user_text)
-                else:
-                    break
-    file.write(user_text)
+    if "-f" in sys.argv:
+        for i in range(file_index, len(sys.argv)):
+            if sys.argv[i] != "stop":
+                with open(sys.argv[i], "w") as name_file:
+                    name_file.write(user_text)
+            else:
+                break
