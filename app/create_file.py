20b3b4b03977d1424ddff0f693cdd5c58bd106a1
Kacper Kogut <kogutkacper7@gmail.com>
2025-10-17 19:26:26 +0200

    Solution3

--- app/create_file.py
+++ app/create_file.py
@@ -6,8 +6,9 @@ import datetime
 sys_args = sys.argv
 
 
-def make_file(path: str) -> None:
-    mode, indent = ("a", "\n") if os.path.exists(path) else ("w", "")
+def make_file(path_to_file: str) -> None:
+    mode, indent = ("a", "\n") if os.path.exists(path_to_file) else ("w", "")
+    counter = 1
     with open(path_to_file, mode) as file_append:
         file_append.write(
             f"{indent}"
@@ -16,20 +17,24 @@ def make_file(path: str) -> None:
             message = input("Enter content line: ")
             if message == "stop":
                 break
-            file_append.write(message + "\n")
+            file_append.write(f"{counter} {message}\n")
+            counter += 1
 
 
 if "-f" in sys_args:
     f_index = sys_args.index("-f")
+    file_name = sys_args[f_index + 1]
     if "-d" in sys_args:
         d_index = sys_args.index("-d")
-        path = os.path.join(*sys_args[d_index + 1: f_index])
-        os.makedirs(path, exist_ok=True)
-        path_to_file = os.path.join(path, sys_args[f_index + 1])
+        if f_index < d_index:
+            path_repositories = os.path.join(*sys_args[d_index + 1:])
+        else:
+            path_repositories = os.path.join(*sys_args[d_index + 1: f_index])
+        os.makedirs(path_repositories, exist_ok=True)
+        path_to_file = os.path.join(path_repositories, sys_args[f_index + 1])
         make_file(path_to_file)
     else:
-        path_to_file = os.path.join(*sys_args[f_index + 1:])
-        print(*sys_args[f_index + 1:])
+        path_to_file = os.path.join(sys_args[f_index + 1])
         make_file(path_to_file)
 elif "-d" in sys_args:
     d_index = sys_args.index("-d")
