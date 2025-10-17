e9cafbbd68f08cbb2abcd3f42e6221496f1d67e3
Kacper Kogut <kogutkacper7@gmail.com>
2025-10-17 18:10:11 +0200

    File after Luke's hints

--- app/create_file.py
+++ app/create_file.py
@@ -1,33 +1,38 @@
 import sys
 import os
+import datetime
 
-proba = sys.argv
+
+sys_args = sys.argv
 
 
-if "-f" in proba:
-    f_index = proba.index("-f")
-    if "-d" in proba:
-        d_index = proba.index("-d")
-        path = os.path.join(*proba[d_index + 1: f_index])
+def make_file(path: str) -> None:
+    mode, indent = ("a", "\n") if os.path.exists(path) else ("w", "")
+    with open(path_to_file, mode) as file_append:
+        file_append.write(
+            f"{indent}"
+            f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
+        )
+        while True:
+            message = input("Enter content line: ")
+            if message == "stop":
+                break
+            file_append.write(message + "\n")
+
+
+if "-f" in sys_args:
+    f_index = sys_args.index("-f")
+    if "-d" in sys_args:
+        d_index = sys_args.index("-d")
+        path = os.path.join(*sys_args[d_index + 1: f_index])
         os.makedirs(path, exist_ok=True)
-        path_to_file = path + "\\" + proba[f_index + 1]
-        print(path_to_file)
-        if os.path.exists(path_to_file):
-            with open(path_to_file, "a") as file_append:
-                while True:
-                    message = input("Dopisz cos: ")
-                    if message.lower() == "stop":
-                        break
-                    file_append.write(message + "\n")
-        else:
-            with open(path_to_file, "w") as file_write:
-                while True:
-                    message = input("wpisz cos")
-                    if message.lower() == "stop":
-                        break
-                    file_write.write(message + "\n")
-elif "-d" in proba:
-    d_index = proba.index("-d")
-    path = os.path.join(*proba[d_index + 1:])
-    print(path)
+        path_to_file = os.path.join(path, sys_args[f_index + 1])
+        make_file(path_to_file)
+    else:
+        path_to_file = os.path.join(*sys_args[f_index + 1:])
+        print(*sys_args[f_index + 1:])
+        make_file(path_to_file)
+elif "-d" in sys_args:
+    d_index = sys_args.index("-d")
+    path = os.path.join(*sys_args[d_index + 1:])
     os.makedirs(path, exist_ok=True)
