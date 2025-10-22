129486273c4b65a08845c978c56facbc5a730feb
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-22 14:52:07 +0200

    file 4

--- app/create_file.py
+++ app/create_file.py
@@ -5,23 +5,23 @@ from datetime import datetime
 
 
 def parse(args: list) -> Any:
-    dir_ls = []
+    directory_parts = []
     file_name = None
-    i = 1
-    while i < len(args):
-        if args[i] == "-d":
-            i += 1
-            while i < len(args) and args[i] != "-f":
-                dir_ls.append(args[i])
-                i += 1
-        elif args[i] == "-f":
-            i += 1
-            if i < len(args):
-                file_name = args[i]
-                i += 1
+    index = 1
+    while index < len(args):
+        if args[index] == "-d":
+            index += 1
+            while index < len(args) and args[index] != "-f":
+                directory_parts.append(args[index])
+                index += 1
+        elif args[index] == "-f":
+            index += 1
+            if index < len(args):
+                file_name = args[index]
+                index += 1
         else:
-            i += 1
-    return dir_ls, file_name
+            index += 1
+    return directory_parts, file_name
 
 
 def content_lines() -> Any:
@@ -39,12 +39,12 @@ def content_lines() -> Any:
 def write(file_path: Any, lines: list) -> None:
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     file_exists = os.path.exists(file_path)
-    with open(file_path, "a") as f:
+    with open(file_path, "a") as output_file:
         if file_exists:
-            f.write("\n")
-        f.write(f"{time}\n")
+            output_file.write("\n")
+        output_file.write(f"{time}\n")
         for line in lines:
-            f.write(f"{line}\n")
+            output_file.write(f"{line}\n")
 
 
 def main() -> Any:
