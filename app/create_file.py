99b2e0f52a5525545e1bcd7c38ad687cbecf2ed9
Dawid <kucharzjaw@gmail.com>
2025-10-28 11:20:54 +0100

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -20,28 +20,26 @@ def create_file() -> None:
                 directory.append(arg)
             elif mode == "file" and file_name is None:
                 file_name = arg
+    if file_name:
+        if not directory:
+            full_path = file_name
+        else:
+            folder_path = os.path.join(*directory)
+            full_path = os.path.join(folder_path, file_name)
 
-    if not directory:
-        full_path = file_name
-    else:
-        folder_path = os.path.join(*directory)
-        full_path = os.path.join(folder_path, file_name)
+        if directory:
+            os.makedirs(folder_path, exist_ok=True)
 
-    if directory:
-        os.makedirs(folder_path, exist_ok=True)
+        lines = []
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            lines.append(line)
 
-    lines = []
-    while True:
-        line = input("Enter content line: ")
-        if line == "stop":
-            break
-        lines.append(line)
-
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open(full_path, "a") as f:
-        f.write(timestamp + "\n")
-
-    for i, line in enumerate(lines, start=1):
-        f.write(f"{i}. {line}\n")
-
-    f.write("\n")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(full_path, "a") as f:
+            f.write(timestamp + "\n")
+            for i, line in enumerate(lines, start=1):
+                f.write(f"{i}. {line}\n")
+            f.write("\n")
