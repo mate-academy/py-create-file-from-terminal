408c24694706145f83891c33aae1b1e58588d50e
Olena Pyslar <pislar33@gmail.com>
2025-10-30 23:47:53 +0200

    Fixed Solution 1

--- app/create_file.py
+++ app/create_file.py
@@ -29,18 +29,22 @@ if f_index != -1:
     file_name = args[f_index + 1]
     file_path = os.path.join(dir_path, file_name) if dir_path else file_name
 
-lines = []
-while True:
-    line = input("Enter content line: ")
-    if line == "stop":
-        break
-    lines.append(line)
+    lines = []
+    while True:
+        line = input("Enter content line: ")
+        if line == "stop":
+            break
+        lines.append(line)
 
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-content = f"{timestamp}\n"
-for line_number, line in enumerate(lines, start=1):
-    content += f"{line_number} {line} \n"
+    if os.path.exists(file_path):
+        content = f"\n{timestamp}\n"
+    else:
+        content = f"{timestamp}\n"
 
-with open(file_path, "a", encoding="utf-8") as output_file:
-    output_file.write(content)
+    for line_number, line in enumerate(lines, start=1):
+        content += f"{line_number} {line} \n"
+
+    with open(file_path, "a", encoding="utf-8") as output_file:
+        output_file.write(content)
