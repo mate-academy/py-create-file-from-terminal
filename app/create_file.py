481cc041d8f36462cb025f6ba67503d94331e767
Wojciech Witkowski <witus85@gmail.com>
2025-11-03 23:09:10 +0100

    Refactored to functions and change file from overwrite to append

--- app/create_file.py
+++ app/create_file.py
@@ -3,44 +3,53 @@ from sys import argv
 from datetime import datetime
 
 
-""" Checking flags block """
+""" Check for flags and list path and file name """
 
-dir_arg, file_arg = False, False
-dir_path, file_name = "", ""
-for argument in argv:
-    if argument == "-d" and not dir_path:
-        dir_arg = True
-        file_arg = False
-        continue
-    if argument == "-f":
-        if not file_name:
-            file_arg = True
-        dir_arg = False
-        continue
 
-    if dir_arg:
-        dir_path = path.join(dir_path, argument)
+def flags_interpreter(arguments: list) -> list:
+    dir_arg, file_arg = False, False
+    dir_path, file_name = "", ""
+    dir_parts = []
+    for argument in arguments:
+        if argument == "-d" and not dir_path:
+            dir_arg, file_arg = True, False
+            continue
+        if argument == "-f":
+            if not file_name:
+                file_arg = True
+            dir_arg = False
+            continue
+        if dir_arg:
+            dir_parts.append(argument)
+        if file_arg:
+            file_name = argument
+            file_arg = False
+    if dir_parts:
+        dir_path = create_path(dir_parts)
+    if file_name:
+        file_name = create_path([dir_path, file_name])
+    return [dir_path, file_name]
 
-    if file_arg:
-        ext = ""
-        ext_1 = path.splitext(argument)[-1]
-        if ext_1 != ".txt":
-            ext = ".txt"
-        file_name = argument + ext
-        file_arg = False
 
-""" Creating directory block """
+""" Create directory path """
 
-if dir_path:
+
+def create_path(folders: list) -> str:
+    return path.join(*folders)
+
+
+""" Create folders if not exists """
+
+
+def create_dir(dir_path: str) -> None:
     if not path.exists(dir_path):
         makedirs(dir_path)
 
-""" Creating file block"""
 
-if file_name:
-    if dir_path:
-        file_name = path.join(dir_path, file_name)
+""" Create or update text file """
 
+
+def create_text_file(file_name: str) -> None:
     lines = [datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")]
     line = ""
     line_counter = 0
@@ -49,6 +58,17 @@ if file_name:
         line_counter += 1
         line = input("Enter content line: ")
         lines.append(f"{line_counter} {line}") if line != "stop" else None
-    with open(file_name, "w") as text_file:
+    lines.append("")
+    with open(file_name, "a") as text_file:
         for line in lines:
             text_file.write(f"{line}\n")
+
+
+""" Main code """
+
+directory, text_file = flags_interpreter(argv)
+
+if directory:
+    create_dir(directory)
+if text_file:
+    create_text_file(text_file)
