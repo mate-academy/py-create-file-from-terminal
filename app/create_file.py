3a25786e732592836b36e2222cea4948dbf2a060
viloker <yarmolykvadym@gmail.com>
2025-11-05 23:37:53 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -4,8 +4,10 @@ import datetime
 
 
 def _create_dirs(args: list) -> str:
-    args = args
-    flag_d_index = args.index("-d")
+    try:
+        flag_d_index = args.index("-d")
+    except ValueError:
+        return ""
     dirs = []
     for item in args[flag_d_index + 1:]:
         if item == "-f":
@@ -18,24 +20,33 @@ def _create_dirs(args: list) -> str:
 
 
 def _create_file(args: list, path: str) -> None:
-    flag_f_index = args.index("-f")
+    try:
+        flag_f_index = args.index("-f")
+    except ValueError:
+        return
     file_name = args[flag_f_index + 1]
     file_name = os.path.join(path, file_name)
-    with open(file_name, "a") as file:
-        _fill_file(file)
-        file.write("\n")
+
+    if os.path.isfile(file_name):
+        with open(file_name, "a") as current_file:
+            current_file.write("\n")
+            _fill_file(current_file)
+    else:
+        with open(file_name, "x") as current_file:
+            _fill_file(current_file)
 
 
 def _fill_file(current_file: open) -> None:
-
     time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     current_file.write(time + "\n")
+    number_line = 1
     while True:
         data = input("Enter content line:")
         if data == "stop":
             break
 
-        current_file.write(data + "\n")
+        current_file.write(f"Line{number_line} " + data + "\n")
+        number_line += 1
 
 
 def create_file() -> None:
