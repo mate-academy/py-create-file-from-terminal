89bb757cd982f80bcc315616596ef048ebc8d210
venlivan <venlivandovskyi@gmail.com>
2025-11-10 17:30:16 +0100

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,72 @@
-# write your code here
+import os
+import sys
+import datetime
+
+
+directories = []
+filename = None
+
+
+for i in range(len(sys.argv)):
+    if sys.argv[i] == "-d":
+        next_step = i + 1
+        while next_step < len(sys.argv) and not sys.argv[next_step] == "-f":
+            if not sys.argv[next_step].startswith("-"):
+                directories.append(sys.argv[next_step])
+            else:
+                break
+            next_step += 1
+    if sys.argv[i] == "-f":
+        filename = sys.argv[i + 1] if i + 1 < len(sys.argv) else None
+
+
+def create_directories() -> str:
+    path = os.path.join(".", *directories)
+    abs_path = os.path.abspath(path)
+    if not os.path.exists(abs_path):
+        os.makedirs(abs_path)
+    return abs_path
+
+
+def get_content_lines() -> list:
+    lines = []
+    while True:
+        line = input("Enter content line: ")
+        if line == "stop":
+            break
+        lines.append(line)
+    return lines
+
+
+def write_to_file(file_path: str, content: list[str]) -> None:
+    if not content:
+        return
+
+    has_content = os.path.exists(file_path) and os.path.getsize(file_path) > 0
+    with open(file_path, "a") as file:
+        if has_content:
+            file.write("\n")
+        file.write(f"{datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )}\n")
+        for index, line in enumerate(content, start=1):
+            file.write(f"{index} {line}\n")
+
+
+def create_file(filename: str) -> None:
+    if directories:
+        dir_path = create_directories()
+    else:
+        dir_path = os.getcwd()
+
+    file_path = os.path.join(dir_path, filename)
+    lines = get_content_lines()
+    write_to_file(file_path, lines)
+
+
+if __name__ == "__main__":
+    if filename:
+        create_file(filename)
+    else:
+        print("No filename given")
+        sys.exit(1)
