778142fa05b364054d74b58f43e87117ea9a51b3
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-11-12 20:58:56 +0000

    task 70%

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,23 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+directory = "./"
+if "-d" in sys.argv:
+    index_need = sys.argv.index("-d") + 1
+    directory += "/".join(sys.argv[index_need:])
+    os.makedirs(directory)
+
+if "-f" in sys.argv:
+    index_need = sys.argv.index("-f") + 1
+    with open(f"{directory}/{sys.argv[index_need]}", "a") as f:
+        f.write(f"{datetime.datetime.now()}\n")
+        i = 1
+        while True:
+            text = str(input("Enter content line: "))
+            if text.lower() == "stop":
+                f.write("\n")
+                break
+            f.write(f"{i} {text}\n")
+            i += 1
