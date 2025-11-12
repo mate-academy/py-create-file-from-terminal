43de378da0b04c3e48791093474af003f47f127c
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-11-12 21:06:48 +0000

    fix

--- app/create_file.py
+++ app/create_file.py
@@ -6,18 +6,23 @@ import datetime
 directory = "./"
 if "-d" in sys.argv:
     index_need = sys.argv.index("-d") + 1
-    directory += "/".join(sys.argv[index_need:])
+    dir_parts = []
+    for arg in sys.argv[index_need:]:
+        if arg.startswith("-"):
+            break
+        dir_parts.append(arg)
+    directory = os.path.join(directory, *dir_parts)
     os.makedirs(directory)
 
 if "-f" in sys.argv:
     index_need = sys.argv.index("-f") + 1
-    with open(f"{directory}/{sys.argv[index_need]}", "a") as f:
-        f.write(f"{datetime.datetime.now()}\n")
-        i = 1
+    with open(f"{directory}/{sys.argv[index_need]}", "a") as file:
+        file.write(f"{datetime.datetime.now()}\n")
+        line_number = 1
         while True:
             text = str(input("Enter content line: "))
             if text.lower() == "stop":
-                f.write("\n")
+                file.write("\n")
                 break
-            f.write(f"{i} {text}\n")
-            i += 1
+            file.write(f"{line_number} {text}\n")
+            line_number += 1
