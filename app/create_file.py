ac22d0b62fa302efd0cfc423d26bec0ecec81696
Andrii Yermolenko <vakt159@gmail.com>
2025-11-13 20:18:44 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -13,25 +13,28 @@ def create_directory(dirs: list[str]) -> None:
 def create_file(name: str) -> None:
     datetime_line = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(name, "a+") as file:
-        file.write(datetime_line)
+        file.write(datetime_line + "\n")
         line_counter = 1
         while True:
             line = input("Enter content line:")
             if line == "stop":
                 break
-            file.write(f"\n {line_counter} {line}")
+            file.write(f"{line_counter} {line}\n")
             line_counter += 1
-        file.write("\n\n")
+        file.write("\n")
 
 
 def check_args() -> None:
     if "-d" in arguments and "-f" in arguments:
         dirs = arguments[arguments.index("-d") + 1: arguments.index("-f")]
         create_directory(dirs)
-        path_to_file = os.path.join(*dirs, arguments[-1])
+        path_to_file = os.path.join(
+            *dirs,
+            arguments[arguments.index("-f") + 1]
+        )
         create_file(path_to_file)
     elif "-d" in arguments:
-        dirs = arguments[arguments.index("-d") + 1: arguments.index("-f")]
+        dirs = arguments[arguments.index("-d") + 1:]
         create_directory(dirs)
     elif "-f" in arguments:
         create_file(arguments[-1])
