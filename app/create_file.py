6dce4cac08db6c2ee58ef849e23d78aa89f1b3c8
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-11-13 23:15:18 +0200

    edit output

--- app/create_file.py
+++ app/create_file.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 
 def create_path(directories) -> str:
+    if len(directories) == 0:
+        return "."
     path = os.path.join(*directories)
     return path
 
@@ -18,24 +20,26 @@ def create_directories(directory_path: list["str"]) -> None:
 
     path = create_path(directories)
     os.makedirs(path, exist_ok=True)
+    os.chdir(path)
 
 
 def create_and_fulfill_file(directory_path: list["str"]) -> None:
     filename = directory_path[-1]
     with open(filename, "a") as source_file:
         current_date = datetime.now()
-        source_file.write(current_date.strftime(""))
+        source_file.write(current_date.strftime("%Y-%m-%d %H:%M:%S\n"))
 
         user_input = input("Enter content line: ")
         number_of_line = 1
         while user_input != "stop":
-            source_file.write(f"{number_of_line} {user_input}\n")  # TODO: \n is not necessary?
+            source_file.write(f"{number_of_line} {user_input}\n")
             number_of_line += 1
             user_input = input("Enter content line: ")
+        source_file.write("\n")
 
 
 def create_file_main() -> None:
-
+    result = 1
     directory_path = sys.argv[1:]
     if len(directory_path) != 0:
         if "-d" in directory_path:
@@ -45,7 +49,7 @@ def create_file_main() -> None:
             create_and_fulfill_file(directory_path)
 
 
-#create_file_main()
+create_file_main()
 
 
 
