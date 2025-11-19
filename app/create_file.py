7bed54efc80dcdd07f12728e970becc367af78e0
Maksym Subchak <donjkee2@gmail.com>
2025-11-19 15:24:47 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -2,32 +2,46 @@ import sys
 import os
 import datetime
 
-dirs = []
-file_name = None
 
-if "-d" in sys.argv:
-    idx = sys.argv.index("-d")
-    for arg in sys.argv[idx + 1:]:
-        if arg.startswith("-"):
-            break
-        dirs.append(arg)
+def parse_args() -> tuple[list[str], str | None]:
+    dirs = []
+    file_name = None
 
-if "-f" in sys.argv:
-    idx = sys.argv.index("-f")
-    file_name = (sys.argv[idx + 1])
+    if "-d" in sys.argv:
+        idx = sys.argv.index("-d")
+        for arg in sys.argv[idx + 1:]:
+            if arg.startswith("-"):
+                break
+            dirs.append(arg)
+
+    if "-f" in sys.argv:
+        idx = sys.argv.index("-f")
+        if idx + 1 < len(sys.argv) and not sys.argv[idx + 1].startswith("-"):
+            file_name = (sys.argv[idx + 1])
+
+    return dirs, file_name
+
 
 path = ""
 
-if dirs:
-    path = os.path.join(*dirs)
-    os.makedirs(path, exist_ok=True)
 
-if file_name:
+def make_dir(dirs: list[str]) -> str:
+    if dirs:
+        path = os.path.join(*dirs)
+        os.makedirs(path, exist_ok=True)
+        return path
+    return ""
+
+
+def build_file_path(path: str, file_name: str | None) -> None:
+    if file_name is None:
+        return None
     if path:
-        file_path = os.path.join(path, file_name)
-    else:
-        file_path = f"{file_name}"
+        return os.path.join(path, file_name)
+    return file_name
 
+
+def write_file(file_path: str) -> None:
     lines = []
     while True:
         user = input("Enter content line: ")
@@ -46,3 +60,16 @@ if file_name:
     with open(file_path, "a") as f:
         f.write(block)
         f.write("\n")
+
+
+def main() -> None:
+    dirs, file_name = parse_args()
+    path = make_dir(dirs)
+    file_path = build_file_path(path, file_name)
+
+    if file_path:
+        write_file(file_path)
+
+
+if __name__ == "__main__":
+    main()
