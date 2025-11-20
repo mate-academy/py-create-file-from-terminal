146a3e3b1289fc0369f500b925f65cf683636f62
Jakub Paluch <qbusik@gmail.com>
2025-11-20 09:33:22 +0100

    Solution2

--- app/create_file.py
+++ app/create_file.py
@@ -4,10 +4,7 @@ from datetime import datetime
 
 
 def make_directory_and_open(directory: list[str]) -> None:
-    directory_list = []
-    for name in directory:
-        directory_list.append(name)
-    directory = os.path.join(os.getcwd(), *directory_list)
+    directory = os.path.join(os.getcwd(), *directory)
     os.makedirs(directory, exist_ok=True)
     os.chdir(directory)
 
@@ -18,13 +15,11 @@ def write_file(file_name: str) -> None:
         while True:
             line = input("Enter content line: ")
             if line == "stop":
-                #  add time stamp
                 now = datetime.now()
                 formatted = now.strftime("%Y-%m-%d %H:%M:%S")
-                #  write lines
                 file.write(f"{formatted}\n")
-                for i, line in enumerate(file_lines, 1):
-                    file.write(f"{i} {line}\n")
+                for line_number, line in enumerate(file_lines, 1):
+                    file.write(f"{line_number} {line}\n")
                 file.write("\n")
                 break
             file_lines.append(line)
@@ -32,7 +27,6 @@ def write_file(file_name: str) -> None:
 
 command_line = sys.argv
 if "-d" in command_line and "-f" in command_line:
-    # make directory and go to
     f_position = 0
     for num in range(len(command_line)):
         if command_line[num] == "-f":
