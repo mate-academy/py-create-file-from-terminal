023cc89625334a5b5889537239a215dbda33f4dd
Ievgen German <german.kbrza@gmail.com>
2025-12-02 21:06:16 +0200

    solution 3

--- app/create_file.py
+++ app/create_file.py
@@ -3,28 +3,41 @@ import datetime
 import os
 
 
-dataset = sys.argv[1:]
-path = None
-file_name = None
-if "-d" in dataset:
-    dataset.remove("-d")
-    if "-f" not in dataset:
-        path = os.path.join(*dataset)
-    else:
-        path = os.path.join(*dataset[0: dataset.index("-f")])
-    file_name = dataset[-1] if "-f" in dataset else None
-if path:
-    os.makedirs(path, exist_ok=True)
-if file_name:
-    if path:
-        os.chdir(path)
-    with open(file_name, "a", newline="", encoding="utf-8") as file:
-        file.write(
-            datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S") + "\n")
-        content_line = input("Enter content line:")
-        iteration = 0
-        while content_line != "stop":
-            iteration += 1
-            file.write(f"{iteration} {content_line} \n")
+def receive_data_from_cmd() -> list:
+    return sys.argv[1:]
+
+
+def receive_file_name(list_of_data: list) -> tuple:
+    path_dir = None
+    name_of_file = None
+    if "-f" in list_of_data:
+        name_of_file = list_of_data[-1]
+        list_of_data.pop(-1)
+        list_of_data.pop(-1)
+    if "-d" in list_of_data:
+        list_of_data.pop(0)
+        path_dir = os.path.join(*list_of_data)
+    return path_dir, name_of_file
+
+
+def write_data(file_and_dir_name: tuple) -> None:
+    dir_name, file_name = file_and_dir_name
+    if dir_name:
+        os.makedirs(dir_name, exist_ok=True)
+        os.chdir(dir_name)
+    if file_name:
+        with open(file_name, "a", newline="", encoding="utf-8") as file:
+            file.write(
+                datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S") + "\n")
             content_line = input("Enter content line:")
-        file.write("\n")
+            iteration = 0
+            while content_line != "stop":
+                iteration += 1
+                file.write(f"{iteration} {content_line} \n")
+                content_line = input("Enter content line:")
+            file.write("\n")
+
+
+row_data = receive_data_from_cmd()
+file_and_path_name = receive_file_name(row_data)
+write_data(file_and_path_name)
