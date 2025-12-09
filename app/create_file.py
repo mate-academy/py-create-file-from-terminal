04050e132bd1d99683be99d739e7a41e02a2b7d7
Paweł Dudek <dudek136@gmail.com>
2025-12-09 21:04:19 +0100

    Solution v.2

--- app/create_file.py
+++ app/create_file.py
@@ -1,25 +1,35 @@
 from sys import argv
-from os import makedirs, getcwd
+from os import makedirs, path
 from datetime import datetime
 
+
 args = argv
-program_path = getcwd()
-path = ""
-i = 2
 
 if "-d" in args:
-    while args[i] != "-f":
-        path += args[i] + "\\"
-        i += 1
-        if i == len(args):
-            break
-    makedirs(path, exist_ok=True)
-    path = program_path + "\\" + path
+    d_index = args.index("-d")
+    given_path = argv[(d_index + 1) :]
+
+    if "-f" in args:
+        f_index = args.index("-f")
+        given_path = args[(d_index + 1) : f_index]
+
+    final_path = path.join(*given_path)
+
+    makedirs(final_path, exist_ok=True)
 
 
 if "-f" in args:
-    file_name = args[i + 1]
-    with open(f"{path}/{file_name}.txt", "a") as file:
+    f_index = args.index("-f")
+    file_name = args[f_index + 1]
+    final_path = path.join(*given_path, file_name)
+
+    if path.exists(final_path):
+        file_exist = True
+
+    with open(f"{final_path}", "a") as file:
+        if file_exist is True:
+            file.write("\n")
+
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(str(timestamp) + "\n")
 
