1530232932a07375330fcb7fad58c782c49afddb
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2025-12-16 22:49:33 +0000

    resolved comments

--- app/create_file.py
+++ app/create_file.py
@@ -6,12 +6,12 @@ import sys
 
 def create_app(command: list) -> None:
     directories, file_name = get_files_and_directories(command)
-    destination_path = os.path.join(directories, file_name)
 
     if "-d" in command:
         create_directories(directories)
 
     if "-f" in command:
+        destination_path = os.path.join(directories, file_name)
         create_file(destination_path)
 
 
@@ -36,7 +36,6 @@ def get_files_and_directories(command: list) -> tuple:
 def create_directories(directories: str) -> None:
     destination_path = os.path.join(directories)
     try:
-        os.mkdir(destination_path)
         os.makedirs(destination_path, exist_ok=True)
     except OSError:
         pass
@@ -45,13 +44,22 @@ def create_directories(directories: str) -> None:
 def create_file(destination_path: LiteralString | str | bytes) -> None:
     time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     try:
-        with open(destination_path, "a") as file:
+        with (open(destination_path, "a") as file,
+              open(destination_path, "r") as file_obj):
+
+            if os.path.exists(destination_path) and file_obj.read(1):
+                file.write("\n")
+
             file.write(f"{time_now}\n")
+
+            line_number = 1
+
             while True:
                 user_input = input("Enter content line: ")
                 if user_input == "stop":
                     break
-                file.write(f"{user_input}\n")
+                file.write(f"{line_number} {user_input}\n")
+                line_number += 1
     except OSError:
         raise
 
