caec6372e5ed48087ccda043a0521b462250524d
Bielzin <gabriel.cabecao.lol@gmail.com>
2025-12-22 10:51:16 -0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -16,26 +16,31 @@ def create_directory(path: str, current: str) -> str:
 
 
 def create_file(file_name: str, current: str, string_now: str) -> None:
+    exist = False
     new = os.path.join(current, file_name)
+    if os.path.exists(new):
+        exist = True
     with open(new, "a") as f:
-        f.write(string_now)
+        if exist == True:
+            f.write("\n")
+        f.write(string_now + "\n")
         counter = 0
         while True:
             counter += 1
-            content = input()
+            content = input("Enter content line: ")
             if content == "stop":
                 break
-            f.write(f"{counter} Line{counter} {content}")
+            f.write(f"{counter} {content}\n")
 
 
 args = sys.argv
 if "-d" in args and "-f" in args:
     all_dir = args[2:-2:]
-    path = "/".join(all_dir)
+    path = os.path.join(*all_dir)
     create_file(args[-1], create_directory(path, current), string_now)
 elif "-d" in args:
     all_dir = args[2:]
-    path = "/".join(all_dir)
+    path = os.path.join(*all_dir)
     create_directory(path, current)
 elif "-f" in args:
     create_file(args[-1], current, string_now)
