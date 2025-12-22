c6221fb841c2a8925c32539c6ded8deb95c1c86d
Farlinder <sashakost2006e@gmail.com>
2025-12-22 10:27:21 +0100

    Solving

--- app/create_file.py
+++ app/create_file.py
@@ -2,20 +2,45 @@ import os
 import sys
 import datetime
 
-args = sys.argv
-for i in range(len(args)):
+args = sys.argv[1:]
+
+dir_parts = []
+file_name = None
+
+i = 0
+while i < len(args):
     if args[i] == "-d":
-        print(f"creating directories {args[i + 1]}, {args[i + 2]}")
-        os.mkdir(args[i + 1])
-        os.mkdir(args[i + 2])
+        i += 1
+        while i < len(args) and args[i] not in ("-d", "-f"):
+            dir_parts.append(args[i])
+            i += 1
     elif args[i] == "-f":
-        print(f"creating file {args[i + 1]}")
-        with open(args[i + 1], "a") as f:
-            f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    + "\n")
-            while True:
-                line = input("Enter content line:")
-                if line.lower() == "stop":
-                    f.write("\n")
-                    break
-                f.write(line + "\n")
+        i += 1
+        if i < len(args):
+            file_name = args[i]
+            i += 1
+    else:
+        i += 1
+
+base_path = os.getcwd()
+
+if dir_parts:
+    base_path = os.path.join(base_path, *dir_parts)
+    os.makedirs(base_path, exist_ok=True)
+
+if file_name:
+    file_path = os.path.join(base_path, file_name)
+
+    with open(file_path, "a", encoding="utf-8") as f:
+        if os.path.getsize(file_path) > 0:
+            f.write("\n")
+
+        f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+
+        counter = 1
+        while True:
+            line = input("Enter content line:")
+            if line.lower() == "stop":
+                break
+            f.write(f"{counter} {line}\n")
+            counter += 1
