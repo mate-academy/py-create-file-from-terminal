af0d8350ac185f1696fd7cac50083323ccf5bf5c
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-27 20:16:06 +0100

    fix styling issues

--- app/create_file.py
+++ app/create_file.py
@@ -2,38 +2,47 @@ import sys
 import os
 import datetime
 
-def main():
-    params = parse_arguments()
-    create_file(params["file_name"], params["path"])
 
-def parse_arguments():
+def parse_arguments() -> dict:
     parsed_args = {
         "file_name": "",
         "path": ""
     }
     arguments = sys.argv
-    f_index = arguments.index("-f")
-    d_index = arguments.index("-d")
-    parsed_args["file_name"] = arguments[f_index + 1]
-    *path_list, = arguments[d_index + 1:] if d_index > f_index + 1 else arguments[d_index + 1:f_index]
+    f_flag_index = arguments.index("-f")
+    d_flag_index = arguments.index("-d")
+    parsed_args["file_name"] = arguments[f_flag_index + 1]
+
+    if d_flag_index > f_flag_index + 1:
+        path_list = arguments[d_flag_index + 1:]
+    else:
+        path_list = arguments[d_flag_index + 1:f_flag_index]
+
     parsed_args["path"] = os.path.join(*path_list)
     return parsed_args
+
 
 def create_file(
     file_name: str,
     path: str
-):
+) -> None:
     os.makedirs(path)
     os.chdir(path)
     with (open(file_name, "w") as file):
         creation_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{creation_date}\n")
-        i = 1
+        line_number = 1
         while True:
             content = input("Enter content line: ")
             if content == "stop":
                 break
-            file.write(f"{i} {content}\n")
-            i += 1
+            file.write(f"{line_number} {content}\n")
+            line_number += 1
+
+
+def main() -> None:
+    params = parse_arguments()
+    create_file(params["file_name"], params["path"])
+
 
 main()
