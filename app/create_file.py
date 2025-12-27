fa1f7eeab24bbadc12cf049f58975d5b0b0cf486
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-27 19:53:39 +0100

    implement create_file

--- app/create_file.py
+++ app/create_file.py
@@ -1,8 +1,10 @@
 import sys
 import os
+import datetime
 
 def main():
-    parse_arguments()
+    params = parse_arguments()
+    create_file(params["file_name"], params["path"])
 
 def parse_arguments():
     parsed_args = {
@@ -16,3 +18,18 @@ def parse_arguments():
     *path_list, = arguments[d_index + 1:] if d_index > f_index + 1 else arguments[d_index + 1:f_index]
     parsed_args["path"] = os.path.join(*path_list)
     return parsed_args
+
+def create_file(
+    file_name: str,
+    path: str
+):
+    os.makedirs(path)
+    os.chdir(path)
+    with open(file_name, "w") as file:
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        i = 1
+        content = input("Enter content line: ")
+        while content != "stop":
+            file.write(f"{i} {content}")
+
+main()
