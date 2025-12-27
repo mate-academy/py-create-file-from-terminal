94cfbbd605deea16002127e68f82fb1580f58048
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-27 19:14:30 +0100

    implement parse_arguments function

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,18 @@
-# write your code here
+import sys
+import os
+
+def main():
+    parse_arguments()
+
+def parse_arguments():
+    parsed_args = {
+        "file_name": "",
+        "path": ""
+    }
+    arguments = sys.argv
+    f_index = arguments.index("-f")
+    d_index = arguments.index("-d")
+    parsed_args["file_name"] = arguments[f_index + 1]
+    *path_list, = arguments[d_index + 1:] if d_index > f_index + 1 else arguments[d_index + 1:f_index]
+    parsed_args["path"] = os.path.join(*path_list)
+    return parsed_args
