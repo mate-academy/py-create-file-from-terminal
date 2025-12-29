d9400f5ffb9b0fb3fc3b8fefbc8527c30bda84c9
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-29 13:46:32 +0100

    add additional logic to handle different flags presense in create_file

--- app/create_file.py
+++ app/create_file.py
@@ -30,21 +30,27 @@ def parse_arguments() -> dict:
 
 
 def create_file(
-    file_name: str,
-    path: str
+    file_name: Union[None, str],
+    path: Union[None, PathLike]
 ) -> None:
-    os.makedirs(path)
-    file_path = os.path.join(path, file_name)
-    with open(file_path, "w") as file:
-        creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{creation_date}\n")
-        line_number = 1
-        while True:
-            content = input("Enter content line: ")
-            if content == "stop":
-                break
-            file.write(f"{line_number} {content}\n")
-            line_number += 1
+    current_dir = os.getcwd()
+
+    if path is not None:
+        os.makedirs(path)
+        current_dir = path
+
+    if file_name is not None:
+        file_path = os.path.join(current_dir, file_name)
+        with open(file_path, "a") as file:
+            creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(f"{creation_date}\n")
+            line_number = 1
+            while True:
+                content = input("Enter content line: ")
+                if content == "stop":
+                    break
+                file.write(f"{line_number} {content}\n")
+                line_number += 1
 
 
 def main() -> None:
