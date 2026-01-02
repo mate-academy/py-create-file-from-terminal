c3f079e732cf3c5d35ff1c64945e0ef2468f19a3
ArtemZakhar <artemzakharchuk@gmail.com>
2026-01-02 14:23:56 +0200

    fix

--- app/create_file.py
+++ app/create_file.py
@@ -9,30 +9,38 @@ def create_file(path: str, file_name: str, content: str) -> None:
 
     file_path = os.path.join(path, file_name) if path else file_name
 
-    with open(file_path, "w") as file:
+    with open(file_path, "a") as file:
         file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
 
-    with open(file_path, "a") as file:
         for index, line in enumerate(content.splitlines()):
             file.write(f"{index + 1} {line}\n")
 
 
 def main() -> None:
-    content = []
+    content = ""
     parser = ArgumentParser()
-    parser.add_argument("-d", "--directory", help="Create directory")
+    parser.add_argument(
+        "-d",
+        "--directory",
+        action="store",
+        type=str,
+        nargs="*"
+    )
     parser.add_argument("-f", "--file", help="Create file")
 
-    while True:
-        line = input("Enter content line: ")
-        if line == "stop":
-            break
-        content.append(line)
-    content = "\n".join(content)
     args = parser.parse_args()
 
+    if args.file:
+        user_answers = []
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            user_answers.append(line)
+        content = "\n".join(user_answers)
+
     if args.directory:
-        create_file(args.directory, args.file, content)
+        create_file("/".join(args.directory), args.file, content)
     else:
         create_file("", args.file, content)
 
