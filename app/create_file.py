030d3c07aac6a42f97865148170eba4cf84fb2f5
Mateusz Mroz <mrozu@student.agh.edu.pl>
2026-01-04 17:35:04 +0100

    Refactoring and repairing some functions

--- app/create_file.py
+++ app/create_file.py
@@ -2,61 +2,71 @@ import os
 import sys
 from datetime import datetime
 
-input_string = sys.argv[1:]
 
-dirs = []
-dir_exist = False
-file_name = ""
-file_name_exist = False
+def parse_arguments(input_string: list) -> tuple:
+    dirs = []
+    dir_exist = False
+    file_name = ""
+    file_name_exist = False
 
-for i in input_string:
-    if (i == "-d"):
-        dir_exist = True
-    if (i == "-f"):
-        dir_exist = False
-        file_name_exist = True
-    if (dir_exist and i != "-d"):
-        dirs.append(i)
-    if (file_name_exist and i != "-f"):
-        file_name = i
-        file_name_exist = False
+    for i in input_string:
+        if (i == "-d"):
+            dir_exist = True
+        if (i == "-f"):
+            dir_exist = False
+            file_name_exist = True
+        if (dir_exist and i != "-d"):
+            dirs.append(i)
+        if (file_name_exist and i != "-f"):
+            file_name = i
+            file_name_exist = False
 
-# dirs.remove("-d")
-# print(dirs)
-# print(file_name)
+    return dirs, file_name
 
-actual_directory = os.getcwd()
 
-for director in dirs:
-    actual_directory = os.path.join(actual_directory, director)
+def building_directory(actual_directory: str, dirs: list) -> None:
+    for dir_part in dirs:
+        actual_directory = os.path.join(actual_directory, dir_part)
+    if (not os.path.exists(actual_directory)):
+        os.makedirs(actual_directory)
+    return actual_directory
 
-if (not os.path.exists(actual_directory)):
-    os.makedirs(actual_directory)
 
-if (file_name == ""):
-    print("Please enter a file name")
-    sys.exit()
-else:
-    new_file = os.path.join(actual_directory, file_name)
+def building_file(new_file: str) -> None:
+    lines = []
+    while True:
+        line = input("Enter content line: ")
+        if line == "stop":
+            break
+        lines.append(line)
 
-lines = []
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-while True:
-    line = input("Enter content line: ")
-    if line == "stop":
-        break
-    lines.append(line)
+    number = 1
+    with (open(new_file, "a") as f):
+        if os.path.exists(new_file) and os.path.getsize(new_file) > 0:
+            f.write("\n")
+        f.write(timestamp + "\n")
+        for line in lines:
+            f.write(str(number) + " ")
+            f.write(line + "\n")
+            number += 1
 
-new_data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-# print(new_data)
-number = 1
-with (open(new_file, "a") as f):
-    if os.path.exists(new_file) and os.path.getsize(new_file) > 0:
-        f.write("\n")
-    f.write(new_data + "\n")
-    for line in lines:
-        f.write(str(number) + " ")
-        f.write(line + "\n")
-        number += 1
 
-# print(actual_directory)
+def main() -> None:
+    input_string = sys.argv[1:]
+    dirs, file_name = parse_arguments(input_string)
+
+    actual_directory = os.getcwd()
+    actual_directory = building_directory(actual_directory, dirs)
+
+    if (file_name == ""):
+        return
+    else:
+        new_file = os.path.join(actual_directory, file_name)
+
+    building_file(new_file)
+
+
+if __name__ == "__main__":
+    main()
