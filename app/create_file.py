5381ef395379044820f736b863a9f83ddcdbb526
Oleg <bagrijoleh@gmail.com>
2026-01-16 23:01:40 +0200

    1 try

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,21 @@
-# write your code here
+import datetime
+import os
+import sys
+
+
+args = sys.argv[1:]
+
+if "-d" in sys.argv:
+    way = args[sys.argv.index("-d")+1:] or args[sys.argv.index("-d")+1: sys.argv.index("-f")]
+
+
+
+
+    with open(args[1], "a") as file:
+        file.write(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + "\n")
+        while True:
+            if sys.argv == "stop":
+                break
+        line_number = 1
+        file.write(line_number + sys.argv)
+        line_number += 1
