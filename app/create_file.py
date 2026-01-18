8f2e19082c531e654a0e298b2eb26726649409d0
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-01-19 00:16:40 +0200

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,32 @@
-# write your code here
+import os
+import sys
+import datetime
+
+arguments = sys.argv
+file_name = ""
+current_dir = os.getcwd()
+
+if "-f" in arguments:
+    file_name = arguments[arguments.index("-f") + 1]
+    arguments.pop(arguments.index("-f") + 1)
+    arguments.pop(arguments.index("-f"))
+
+if "-d" in arguments:
+    list_of_dirs = arguments[arguments.index("-d") + 1:]
+    for directory in list_of_dirs:
+        current_dir = os.path.join(current_dir, directory)
+        if not os.path.exists(current_dir):
+            os.mkdir(current_dir)
+
+if file_name:
+    with open(os.path.join(current_dir, file_name), "a") as source_file:
+        source_file.write(
+            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+        )
+        line_number = 1
+        while True:
+            new_line = input("Enter content line: ")
+            if new_line.lower() == "stop":
+                break
+            source_file.write(f"{line_number} {new_line}\n")
+            line_number += 1
