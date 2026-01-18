621c672fd17b2e31422714fd4949ed9e205c8281
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-01-19 00:38:17 +0200

    fixed mistakes

--- app/create_file.py
+++ app/create_file.py
@@ -12,11 +12,10 @@ if "-f" in arguments:
     arguments.pop(arguments.index("-f"))
 
 if "-d" in arguments:
-    list_of_dirs = arguments[arguments.index("-d") + 1:]
-    for directory in list_of_dirs:
+    for directory in arguments[arguments.index("-d") + 1:]:
         current_dir = os.path.join(current_dir, directory)
-        if not os.path.exists(current_dir):
-            os.mkdir(current_dir)
+    if not os.path.exists(current_dir):
+        os.makedirs(current_dir)
 
 if file_name:
     with open(os.path.join(current_dir, file_name), "a") as source_file:
@@ -27,6 +26,8 @@ if file_name:
         while True:
             new_line = input("Enter content line: ")
             if new_line.lower() == "stop":
+                if line_number > 1:
+                    source_file.write("\n")
                 break
             source_file.write(f"{line_number} {new_line}\n")
             line_number += 1
