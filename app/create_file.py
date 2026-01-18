14b57520cb583808a8e9b83b0dd6426140e00b59
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-18 21:29:41 +0200

    Solution2

--- app/create_file.py
+++ app/create_file.py
@@ -4,11 +4,25 @@ import os.path
 from datetime import datetime
 
 
-def read_terminal_args() -> str:
+def write_to_file(file_path: str) -> None:
+    with open(file_path, "a") as source_file:
+        source_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+        count = 1
+        while True:
+            text_input = input("Enter content line: ")
+            if text_input == "stop":
+                source_file.write("\n")
+                break
+            source_file.write(f"{count} {text_input}\n")
+            count += 1
+
+
+def read_terminal_args() -> str | None:
     _, *arguments = sys.argv
-    print(arguments)
+    is_f_flag_exists = False
     try:
         idx_filename = arguments.index("-f") + 1
+        is_f_flag_exists = True
     except ValueError:
         filename = "file.txt"
     else:
@@ -25,24 +39,12 @@ def read_terminal_args() -> str:
             idx_end = len(arguments)
         finally:
             folders = arguments[idx_start:idx_end]
-    return os.path.join(*folders, filename)
-
-
-def write_to_file(file_path: str) -> None:
-    path_to_file = os.path.split(file_path)[0]
-    if not os.path.exists(path_to_file):
-        os.makedirs(path_to_file)
-    with open(file_path, "a") as source_file:
-        source_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
-        count = 1
-        while text_input := input("Enter content line: "):
-            if text_input == "stop":
-                source_file.write("\n")
-                break
-            source_file.write(f"{count} {text_input}\n")
-            count += 1
+    if not is_f_flag_exists:
+        if not os.path.exists(os.path.join(*folders)):
+            os.makedirs(os.path.join(*folders))
+        return
+    return write_to_file(os.path.join(*folders, filename))
 
 
 if __name__ == "__main__":
-    path_to_file = read_terminal_args()
-    write_to_file(path_to_file)
+    read_terminal_args()
