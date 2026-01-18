f6048e29325d96b58fe32ffd413e1bd254057868
viki <v.yavdoschuk@gmail.com>
2026-01-18 15:50:22 +0100

    'solution'

--- app/create_file.py
+++ app/create_file.py
@@ -6,20 +6,36 @@ from itertools import groupby
 
 def create_file(*args) -> None:
 
-    args = args[1:]
-    file_name = ""
-    if "-f" in args:
-        *args, file_name = [list(group) for k, group in
-                            groupby(args, lambda x: x == "-f") if not k]
-        file_name = file_name[0]
+    def parse_args(arguments: tuple) -> [list, list]:
+        arguments = arguments[1:]
+        arguments = [list(group) for k, group in
+                     groupby(arguments, lambda x: x in ("-f", "-d"))]
+        d_args = []
+        f_args = []
 
-    if "-d" in args:
-        *args, directories = [list(group) for k, group in
-                              groupby(args, lambda x: x == "-d") if not k]
-        os.makedirs(os.path.join(*directories))
+        for index in range(len(arguments)):
+            if arguments[index] == ["-d"]:
+                d_args = arguments[index + 1]
+            if arguments[index] == ["-f"]:
+                f_args = arguments[index + 1]
 
-    if file_name != "":
-        with open(file_name, "a") as file:
+        return d_args, f_args
+
+    d_args, f_args = parse_args(args)
+
+    full_file_path = ""
+
+    if d_args != []:
+        print(d_args)
+        directory_path = os.path.join(*d_args)
+        os.makedirs(directory_path)
+
+    if f_args != []:
+        if d_args != []:
+            full_file_path = os.path.join(directory_path, f_args[0])
+        else:
+            full_file_path = f_args[0]
+        with open(full_file_path, "a") as file:
             loop_continues = True
             line_number = 1
             file.write(f"{datetime.now().strftime('%Y_%m_%d %H:%M:%S')}\n")
