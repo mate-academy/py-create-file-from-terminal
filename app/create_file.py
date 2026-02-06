1d90344a2dd00fba27cd312f6d536053603d9e02
Seroga <ilchuk.cergey@gmail.com>
2026-02-06 18:31:33 +0200

    Solution2

--- app/create_file.py
+++ app/create_file.py
@@ -23,12 +23,23 @@ def create_file(file_name: str, text: list[str]) -> None:
         raise FileNotFoundError
 
     with open(file_name, "a") as file:
-        file.writelines(datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "\n")
+        file.write(datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "\n")
         if text:
-            for line in text:
-                file.write(line + "\n")
+            for i, line in enumerate(text, start=1):
+                file.write(f"{i} {line}\n")
             file.write("\n")
 
+
+def text_input_from_user() -> list[str]:
+    text_input = []
+
+    while True:
+        input_data = input("Enter content line: ")
+        if input_data == "stop":
+            break
+        text_input.append(input_data)
+
+    return text_input
 
 def main() -> None:
     if "-d" in sys.argv and "-f" not in sys.argv:
@@ -37,29 +48,17 @@ def main() -> None:
     elif "-f" in sys.argv and "-d" not in sys.argv:
         file_index = sys.argv.index("-f")
         file_path = sys.argv[file_index + 1]
-        text_input = []
-        while True:
-            input_data = input("Enter content line: ")
-            if input_data != "stop":
-                text_input.append(input_data)
-            else:
-                break
-        create_file(file_path, text_input)
+        text_data = text_input_from_user()
+        create_file(file_path, text_data)
     else:
         dir_index = sys.argv.index("-d")
         file_index = sys.argv.index("-f")
         in_dir_path = sys.argv[dir_index + 1:file_index]
         out_dir_path = create_directory(in_dir_path)
-        text_input = []
         file_name = sys.argv[file_index + 1]
         file_path = os.path.join(out_dir_path, file_name)
-        while True:
-            input_data = input("Enter content line: ")
-            if input_data != "stop":
-                text_input.append(input_data)
-            else:
-                break
-        create_file(file_path, text_input)
+        text_data = text_input_from_user()
+        create_file(file_path, text_data)
 
 
 if __name__ == "__main__":
