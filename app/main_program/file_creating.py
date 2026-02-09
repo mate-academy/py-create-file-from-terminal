fc29ae4983f008f2e15eb3d020f42d7819ffe50e
Yurii <pamkivurij92@gmail.com>
2026-02-09 14:39:03 +0100

    Solution 3

--- app/main_program/file_creating.py
+++ app/main_program/file_creating.py
@@ -9,36 +9,45 @@ def file_creation(
         name: str
 ) -> None:
     os.makedirs(path, exist_ok=True)
-    if not name:
-        return
+    assert name,(
+        "File name not found"
+    )
 
     content = []
     while True:
         line = input("Enter content line: ")
-        if line == "stop".lower():
+        if line.lower() == "stop":
             break
         content.append(line)
 
     with open(os.path.join(path, name), "a") as file:
-        file.write(datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S"))
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         for i in range(len(content)):
             file.write(f"\n{i + 1} {content[i]}")
-        else:
-            file.write("\n\n")
+        file.write("\n\n")
 
 
 def separate_argv(args: list) -> None:
-    d_flag = False
+    assert args.count("-d") == 1 and args.count("-f") == 1,(
+        "Too many arguments"
+    )
+
+    d_flag = "None"
     path = os.curdir
-    name = args[args.index("-f") + 1] if args.count("-f") else ""
+    name = ""
 
-    for i in range(len(args)):
-        if args[i - 1] == "-d":
-            d_flag = not d_flag
-        elif args[i] == "-f":
-            d_flag = False
+    for arg in args:
+        if arg == "-d":
+            d_flag = "-d"
+            continue
+        elif arg == "-f":
+            d_flag = "-f"
+            continue
 
-        if d_flag:
-            path = os.path.join(path, args[i])
+        if d_flag == "-d":
+            path = os.path.join(path, arg)
+        if d_flag == "-f":
+            name = arg
+
 
     file_creation(path, name)
