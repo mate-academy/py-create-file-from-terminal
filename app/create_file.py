a0e5f0eb3f8081b2eabbd168b8978d405adf7a32
Hugo Mouto <hugo@mouto.com.br>
2026-02-10 17:52:56 -0300

    feat: solução valdiada com flake8

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,36 @@
-# write your code here
+import argparse
+import os
+from datetime import datetime
+
+
+def create_file() -> None:
+    timestamp_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    parser = argparse.ArgumentParser(description="Cria arquivo "
+                                                 "a partir do diretório "
+                                                 "e nome")
+    parser.add_argument("-d", nargs="+", help="Diretórios "
+                                              "que formam o caminho")
+    parser.add_argument("-f", help="Nome do arquivo a ser criado")
+    args = parser.parse_args()
+
+    file_name = args.f
+    dir_path = os.path.join(*args.d)
+    file_path = os.path.join(dir_path, file_name)
+
+    if not os.path.exists(file_path):
+        os.makedirs(dir_path)
+
+    with open(file_path, "a") as f:
+        f.write(f"{timestamp_str}\n")
+        line_counter = 1
+        while True:
+            user_input = input("Enter content line: ")
+            if user_input.lower() == "stop":
+                break
+            f.write(f"{line_counter} {user_input}\n")
+            line_counter += 1
+
+
+if __name__ == "__main__":
+    create_file()
