149f18da407735e4918abc0ea841760df8b85c09
PlayWithDen4ik <velichkodenis34@gmail.com>
2026-02-16 18:37:12 +0200

    Solution with fix errors logic

--- app/create_file.py
+++ app/create_file.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 def make_dirs(arguments: list) -> str | bytes:
     index_d = arguments.index("-d")
+    if index_d + 1 >= len(arguments):
+        raise ValueError("Missing dirs after '-d' flag.")
     if "-f" not in arguments or arguments.index("-f") < index_d:
         dirs = arguments[index_d + 1 :]
     else:
@@ -16,19 +18,21 @@ def make_dirs(arguments: list) -> str | bytes:
 
 def write_file(arguments: list, path_file: str = "") -> None:
     index_f = arguments.index("-f")
+    if index_f + 1 >= len(arguments):
+        raise ValueError("Missing filename after '-f' flag.")
     file_name = arguments[index_f + 1]
     file_dir = path.join(path_file, file_name)
     with open(file_dir, "a+") as file:
         if file.tell() > 0:
             file.write("\n")
-        file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-        i = 1
+        file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+        line_number = 1
         while True:
             usr_input = input("Enter content line: ")
             if usr_input == "stop":
                 break
-            file.write(f"{i} {usr_input}\n")
-            i += 1
+            file.write(f"{line_number} {usr_input}\n")
+            line_number += 1
 
 
 if __name__ == "__main__":
