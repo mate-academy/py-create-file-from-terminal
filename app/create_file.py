a4dc1d2dc33b7fa98800b65b85ecbe2b446606cd
Oleg <ototskiy@gmail.com>
2026-02-17 22:30:46 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 
-def main() -> None:
+def parse_args() -> None:
     directory_path = []
     f_argument = False
     d_argument = False
@@ -18,36 +18,41 @@ def main() -> None:
 
         if d_argument and argument != "-d":
             directory_path.append(argument)
-            if not os.path.isdir(os.path.join(*directory_path)):
-                os.mkdir(os.path.join(*directory_path))
+            make_dir(directory_path)
 
         if f_argument and argument != "-f":
-            name_of_file = argument
-            text_for_file = ""
-            string_count = 1
-            while True:
-                next_string = input("Enter content line: ")
-                if next_string == "stop":
-                    break
-                text_for_file += f"{string_count} {next_string}\n"
-                string_count += 1
+            write_to_file(
+                os.path.join(*directory_path, argument), collect_content()
+            )
 
-            text_for_file = \
-                (f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-                 f"\n{text_for_file}")
 
-            if os.path.exists(os.path.join(*directory_path, name_of_file)):
-                text_for_file = f"\n{text_for_file}"
-                with open(
-                        os.path.join(*directory_path, name_of_file), "a"
-                ) as file:
-                    file.write(text_for_file)
-            else:
-                with open(
-                        os.path.join(*directory_path, name_of_file
-                                     ), "w") as file:
-                    file.write(text_for_file)
+def make_dir(directory_path: list) -> None:
+    os.makedirs(os.path.join(*directory_path), exist_ok=True)
+
+
+def collect_content() -> str:
+    text_for_file = ""
+    string_count = 1
+    while True:
+        next_string = input("Enter content line: ")
+        if next_string == "stop":
+            break
+        text_for_file += f"{string_count} {next_string}\n"
+        string_count += 1
+
+    date_format = "%Y-%m-%d %H:%M:%S"
+    return f"{datetime.datetime.now().strftime(date_format)}\n{text_for_file}"
+
+
+def write_to_file(file_path: str, text_for_file: str) -> None:
+    if os.path.exists(file_path):
+        text_for_file = f"\n{text_for_file}"
+        with open(file_path, "a") as file:
+            file.write(text_for_file)
+    else:
+        with open(file_path, "w") as file:
+            file.write(text_for_file)
 
 
 if __name__ == "__main__":
-    main()
+    parse_args()
