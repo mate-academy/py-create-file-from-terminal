e2fda94123f3807d35d5747db283a56224cd0a39
noshenko98 <noshenko98@gmail.com>
2026-02-22 16:17:00 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -4,39 +4,42 @@ from datetime import datetime
 
 
 def create_dir(dir_name: list) -> str:
-    dir_name2 = ""
+    current_path = ""
     for dir_ in dir_name:
-        dir_name2 = os.path.join(dir_name2, dir_)
-        if not os.path.exists(dir_name2):
-            os.makedirs(dir_name2)
-    print(dir_name2)
-    return str(dir_name2)
+        current_path = os.path.join(current_path, dir_)
+        if not os.path.exists(current_path):
+            os.makedirs(current_path)
+    return str(current_path)
 
 
 def create_file(file_name: str) -> None:
-    with open(file_name, "a") as f:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S" + "\n"))
+    i_numerating = 1
+    with open(file_name, "a") as result_file:
+        result_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S" + "\n"))
         while True:
             str_input = input("Enter content line: ")
             if str_input == "stop":
-                f.write("\n")
+                result_file.write("\n")
                 break
-            f.write(str_input + "\n")
+            result_file.write(f"{i_numerating} " + str_input + "\n")
+            i_numerating += 1
 
 
 def get_and_executing_command() -> None:
     full_command = sys.argv
     start_dir, end_dir, start_file = 0, 0, 0
-    for i, part_command in enumerate(full_command):
-        if "-d" == part_command:
-            start_dir = i + 1
-            end_dir = len(full_command)
-        if "-f" == part_command:
-            end_dir = i
-            start_file = i + 1
+    if "-d" in full_command:
+        start_dir = full_command.index("-d") + 1
+        end_dir = len(full_command)
+    if "-f" in full_command:
+        start_file = full_command.index("-f") + 1
+        end_dir = full_command.index("-f")
+    if start_dir > start_file :
+        end_dir = len(full_command)
     if start_dir != 0 and end_dir != 0 and start_file != 0:
-        where = create_dir(full_command[start_dir:end_dir])
-        create_file(os.path.join(str(where), full_command[start_file]))
+        directory_path = create_dir(full_command[start_dir:end_dir])
+        create_file(os.path.join(str(directory_path),
+                                 full_command[start_file]))
         return
     if start_dir != 0 and end_dir != 0:
         create_dir(full_command[start_dir:end_dir])
