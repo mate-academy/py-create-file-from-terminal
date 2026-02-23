ed683fdc76364c1b7dd90369eb2dbf24d14bd492
Ambra <trank13f@gmail.com>
2026-02-23 16:40:33 +0700

    add errors check and blanck line

--- app/create_file.py
+++ app/create_file.py
@@ -6,40 +6,63 @@ args = sys.argv[1:]
 has_d = "-d" in args
 has_f = "-f" in args
 
+file_name = None
+path = None
+full_path = None
+
 if has_f:
-    file_name = args[args.index("-f") + 1]
+    f_index = args.index("-f")
+    if f_index + 1 >= len(args):
+        print("Error: -f flag requires a filename.")
+        sys.exit(1)
+    file_name = args[f_index + 1]
 
 if has_d:
+    d_index = args.index("-d")
     dirs = []
 
-    for item in args[args.index("-d") + 1:]:
+    for item in args[d_index + 1:]:
         if item == "-f":
             break
         dirs.append(item)
 
+    if not dirs:
+        print("Error: -d flag requires at least one directory name.")
+        sys.exit(1)
+
     path = os.path.join(*dirs)
     os.makedirs(path, exist_ok=True)
 
-if has_d and has_f:
+if path and file_name:
     full_path = os.path.join(path, file_name)
-elif has_f:
+elif file_name:
     full_path = file_name
-else:
-    full_path = None
 
 if full_path:
-    with open(full_path, "a") as f:
-        lines = []
-        while True:
-            line = input("Enter content line: ")
-            if line == "stop":
-                break
-            lines.append(line)
+    lines = []
 
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+    while True:
+        line = input("Enter content line: ")
+        if line == "stop":
+            break
+        lines.append(line)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(full_path, "a+") as f:
+        f.seek(0)
+        content = f.read()
+
+        if content:
+            f.write("\n\n")
+
+        f.write(timestamp + "\n")
 
         numbered_lines = "\n".join(
             f"{i}. {line}" for i, line in enumerate(lines, start=1)
         )
 
         f.write(numbered_lines + "\n")
+
+else:
+    print("Error: No file specified. Use -f to specify a file.")
