9f9565aec2dfe6f69484eb4a045ff79b3321859b
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-03-15 17:46:37 +0000

    1 save

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,34 @@
-# write your code here
+import sys
+
+import os
+
+import datetime
+
+
+print(sys.argv)
+path = ""
+
+if "-d" in sys.argv:
+    index = sys.argv.index("-d")
+    if "-f" in sys.argv:
+        end_index = sys.argv.index("-f")
+    else:
+        end_index = len(sys.argv)
+    file_path = sys.argv[index + 1: end_index]
+    path = os.path.join(*file_path)
+    os.makedirs(path, exist_ok=True)
+
+if "-f" in sys.argv:
+    index = sys.argv.index("-f")
+    filename = sys.argv[index + 1]
+
+    full_path = os.path.join(path, filename) if path else filename
+    with open(full_path, "a") as f:
+        f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        line_num = 0
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            line_num += 1
+            f.write(str(line_num) + " " + line + "\n")
