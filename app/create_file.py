b2f4c52cb91ceb35e90f9b46b35deca675b4e42d
ramzeth <ramzeth25@gmail.com>
2026-03-15 15:50:30 +0200

    Solution_4

--- app/create_file.py
+++ app/create_file.py
@@ -5,21 +5,15 @@ from datetime import datetime
 from pathlib import Path
 
 
-def file_consist(file_name: str) -> None:
-    with open(file_name, "a") as file:
-        file.write("\n" + current_timestamp + "\n")
-        for counter, line in enumerate(
-                iter(
-                    lambda: input("Enter content line: "),
-                    "stop"
-                ),
-                start=1):
-            file.write(f"{counter} {line}\n")
-
-
-def file_not_consist(file_name: str) -> None:
-    with open(file_name, "w") as file:
-        file.write(current_timestamp + "\n")
+def write_to_file(path_to_file: Path) -> None:
+    write_mode = "w"
+    if path_to_file.is_file():
+        write_mode = "a"
+    with open(file_name, write_mode) as file:
+        if write_mode == "a":
+            file.write("\n" + current_timestamp + "\n")
+        else:
+            file.write(current_timestamp + "\n")
         for counter, line in enumerate(
                 iter(
                     lambda: input("Enter content line: "),
@@ -37,11 +31,8 @@ elif "-f" in sys.argv and "-d" not in sys.argv:
     current_timestamp = (datetime.now().
                          strftime("%Y-%m-%d %H:%M:%S"))  # current timestamp
     file_name = sys.argv[-1]
-    path = Path(os.getcwd(), file_name)
-    if path.is_file():
-        file_consist(file_name)
-    else:
-        file_not_consist(file_name)
+    path = Path(os.getcwd()) / file_name
+    write_to_file(path)
 
 elif "-f" in sys.argv and "-d" in sys.argv:
     current_timestamp = (datetime.now().
@@ -49,6 +40,6 @@ elif "-f" in sys.argv and "-d" in sys.argv:
     parts = sys.argv[sys.argv.index("-d") + 1: sys.argv.index("-f")]
     path = Path(os.getcwd(), *parts)
     path.mkdir(parents=True, exist_ok=True)
-    os.chdir(path)
     file_name = sys.argv[-1]
-    file_not_consist(file_name)
+    file_path = path / file_name
+    write_to_file(file_path)
