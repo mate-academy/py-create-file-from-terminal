d00d9452464c16056314732765cebaae14e93cf8
Kott Q <constantinekuncel@gmail.com>
2026-03-20 14:44:11 +0100

    refactored

--- app/create_file.py
+++ app/create_file.py
@@ -14,19 +14,23 @@ def flags_in_arg() -> bool:
     )
 
 
-f_index = sys.argv.index("-f") if "-f" in sys.argv else None
-d_index = sys.argv.index("-d") if "-d" in sys.argv else None
+# Get flag index
+def get_flag_index(args: list[str]) -> tuple[int | None, int, None]:
+    f_index = args.index("-f") if "-f" in args else None
+    d_index = args.index("-d") if "-d" in args else None
+    return (f_index, d_index)
 
 
 # Get file name
-def get_filename() -> str | None:
+def get_filename(args: list[str], f_index: int | None) -> str:
+
     if f_index is None:
         return None
 
-    if f_index + 1 >= len(sys.argv):
+    if f_index + 1 >= len(args):
         raise ValueError("Filename is missing")
 
-    filename = sys.argv[f_index + 1]
+    filename = args[f_index + 1]
 
     if "." not in filename:
         raise ValueError("Filename must have extension")
@@ -35,43 +39,32 @@ def get_filename() -> str | None:
 
 
 # Get path
-def get_path() -> str | None:
+def get_path(args: list[str], f_index: int, d_index: int) -> str | None:
     if d_index is None:
         return None
 
-    if d_index + 1 >= len(sys.argv):
+    if d_index + 1 >= len(args):
         return None
 
     if sys.argv[d_index + 1] == "-f":
         return None
 
     if f_index is not None:
-        return os.path.join(*sys.argv[d_index + 1 : f_index])
+        return os.path.join(*args[d_index + 1 : f_index])
 
-    return os.path.join(*sys.argv[d_index + 1 :])
+    return os.path.join(*args[d_index + 1 :])
 
 
 # Create directories
-path = get_path()
-
-
-def create_path() -> None:
-    if not flags_in_arg():
-        raise ValueError("Pass -d flag to create path and/or -f flag to create file")
-
+def create_path(path: str | None) -> None:
     if path is not None:
-        return os.makedirs(path, exist_ok=True)
+        os.makedirs(path, exist_ok=True)
 
 
-# Main logic
-def file_editor() -> None:
-    create_path()
-    file_name = get_filename()
-    if file_name is None:
-        return
-
-    file_path = os.path.join(path, file_name) if path else file_name
-
+# Write content
+def write_content(file_path: str) -> None:
+    if not flags_in_arg():
+        raise ValueError("Pass at least 1 flag to arguments")
     file_exists = os.path.exists(file_path)
 
     with open(file_path, "a") as file:
@@ -85,6 +78,22 @@ def file_editor() -> None:
                 break
             file.write(f"{row_number} {content.strip()}\n")
             row_number += 1
+
+
+# Main logic
+def file_editor() -> None:
+    args = sys.argv
+    f_index, d_index = get_flag_index(args)
+    path = get_path(args, f_index, d_index)
+    file_name = get_filename(args, f_index)
+
+    create_path(path)
+
+    if file_name is None:
+        return
+
+    file_path = os.path.join(path, file_name) if path else file_name
+    write_content(file_path)
 
 
 file_editor()
