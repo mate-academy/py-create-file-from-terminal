d3a323627de4bcf91e888ac638125a70f565532d
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2026-03-25 03:35:50 +0100

    created script for creating directories and files

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,36 @@
-# write your code here
+import os
+import sys
+import datetime
+
+
+def create_path() -> str:
+    path = os.path.join(os.getcwd(), args[args.index("-d") + 1],
+                 args[args.index("-d") + 2])
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+def create_file(file_path: str) -> str:
+    filename = args[args.index("-f") + 1]
+    full_path = os.path.join(file_path, filename)
+    with open(full_path, "w") as f:
+        f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        count = 0
+        string = ""
+        while string != "stop":
+            f.write(f"{count} " + string + "\n")
+            count += 1
+            string = input("Enter file name: ")
+    return filename
+
+
+args = sys.argv
+
+if "-d" in args and "-f" in args:
+    path = create_path()
+    create_file(path)
+elif "-d" in args:
+    create_path()
+else:
+    create_file(os.getcwd())
+
