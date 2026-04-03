071e042f45692da27f61ced751ac532a07215c86
LayMiDD <topp8597@gmail.com>
2026-04-04 00:07:47 +0300

    develop v4

--- app/create_file.py
+++ app/create_file.py
@@ -5,32 +5,42 @@ import datetime
 dir_path = ""
 file_name = None
 
-if "-f" in sys.argv:
-    file_index = sys.argv.index("-f") + 1
-    file_name = sys.argv[file_index]
+
 if "-d" in sys.argv:
     dir_index = sys.argv.index("-d") + 1
     if "-f" in sys.argv:
         end_index = sys.argv.index("-f")
     else:
         end_index = len(sys.argv)
+
     folders = sys.argv[dir_index:end_index]
     dir_path = os.path.join(*folders)
+    if dir_path:
+        os.makedirs(dir_path, exist_ok=True)
 
-if dir_path:
-    os.makedirs(dir_path, exist_ok=True)
+if "-f" in sys.argv:
+    file_index = sys.argv.index("-f") + 1
+    file_name = sys.argv[file_index]
 
 if file_name:
-    full_path = os.path.join(dir_path, file_name)
+    if dir_path:
+        full_path = os.path.join(dir_path, file_name)
+    else:
+        full_path = file_name
 
-with open(full_path, "a") as file:
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    file.write(now + "\n")
-    line_number = 1
+    file_exists = os.path.exists(full_path) and os.path.getsize(full_path) > 0
 
-    while True:
-        content = input("Enter content line: ")
-        if content == "stop":
-            break
-        file.write(f"{line_number} {content}\n")
-        line_number += 1
+    with open(full_path, "a") as f:
+        if file_exists:
+            f.write("\n")
+
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write(now + "\n")
+
+        line_num = 1
+        while True:
+            content = input("Enter content line: ")
+            if content == "stop":
+                break
+            f.write(f"{line_num} {content}\n")
+            line_num += 1
