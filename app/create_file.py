bb95be5e667dc21e7304103e38f1660fb2039a7d
Denys Matusevych <denytop3@gmail.com>
2026-04-07 14:19:02 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,37 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+folders = []
+filename = None
+
+for i, arg in enumerate(sys.argv):
+    if arg == "-d":
+        for folder in sys.argv[i + 1:]:
+            if folder.startswith("-f"):
+                break
+            folders.append(folder)
+    if arg == "-f":
+        filename = sys.argv[i + 1]
+
+if folders:
+    path = os.path.join(*folders)
+    os.makedirs(path, exist_ok=True)
+    if filename:
+        filename = os.path.join(path, filename)
+
+lines = []
+while True:
+    content = input("Enter content line: ")
+    if content == "stop":
+        break
+    lines.append(content)
+
+if lines:
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    with open(filename, "a") as file:
+        file.write(f"{timestamp}\n")
+        for i, line in enumerate(lines, 1):
+            file.write(f"{i} {line}\n")
+        file.write("\n")
