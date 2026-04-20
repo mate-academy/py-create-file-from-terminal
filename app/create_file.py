47fe03ba97e1b8efe8f4153bb881f90e1089d47d
Dmytro Herman <dmytroherman@tutamail.com>
2026-04-20 07:23:56 +0000

    Create function get_arguments for parsing arguments

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,34 @@
-# write your code here
+import os
+import sys
+
+
+def get_arguments():
+    arguments = sys.argv
+    directory_names = list()
+    file_name = ""
+
+    if "-d" in arguments:
+        d_index = arguments.index("-d")
+        for arg in arguments[d_index + 1:]:
+            if arg == "-f":
+                break
+            directory_names.append(arg)
+
+    if "-f" in arguments:
+        f_index = arguments.index("-f")
+        if f_index + 1 < len(arguments):
+            file_name = arguments[f_index + 1]
+
+    return directory_names, file_name
+
+
+def main():
+    folder_parts, name = get_arguments()
+
+    if not name:
+        print("No arguments (-f 'filename', -d 'dir1' 'dir2' -> dir1/dir2/filename)")
+        return
+
+
+if __name__ == "__main__":
+    main()
