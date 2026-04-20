e8d3cceada1cf030053652f18ab2e5bcd5b38e9d
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-04-20 22:12:13 +0300

    Created an app "create_file.py" that takes directory path, file name, file content from the terminal and creates file.

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,52 @@
-# write your code here
+import sys
+from datetime import datetime
+from pathlib import Path
+
+
+def main() -> None:
+    args = sys.argv[1:]
+    dirs = []
+    filename = ""
+    current_flag = ""
+
+    for arg in args:
+        if arg == "-d":
+            current_flag = "-d"
+        elif arg == "-f":
+            current_flag = "-f"
+        else:
+            if current_flag == "-d":
+                dirs.append(arg)
+            elif current_flag == "-f":
+                filename = arg
+                current_flag = ""
+
+    full_path = Path(".")
+    if dirs:
+        full_path = Path(*dirs)
+        full_path.mkdir(parents=True, exist_ok=True)
+
+    if filename:
+        full_filepath = full_path / filename
+        lines = []
+
+        while True:
+            user_input = input("Enter content line: ")
+            if user_input.strip().lower() == "stop":
+                break
+            lines.append(user_input)
+
+        if lines:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            content = current_time + "\n"
+            for index, text in enumerate(lines, start=1):
+                content += f"{index} {text}\n"
+
+            with open(full_filepath, "a", encoding="utf-8") as file_object:
+                file_object.write(content)
+
+            print(f"File created/updated at: {full_filepath}")
+
+
+if __name__ == "__main__":
+    main()
