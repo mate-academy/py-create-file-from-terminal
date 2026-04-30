147817bc28d3c51466b88235b0faa0b46f550229
Igor <zalugin.iv2@gmail.com>
2026-04-30 15:29:32 +0200

    everything works but tests doesnt pass

--- app/create_file.py
+++ app/create_file.py
@@ -8,7 +8,7 @@ data = sys.argv
 
 def write_file(filepath: str) -> None:
     with open(filepath, "w") as file:
-        file.write(str(datetime.datetime.now().replace(microsecond=0)) + "\n")
+        file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
         while True:
             text = input("Enter content line:")
             if text == "stop":
@@ -16,18 +16,19 @@ def write_file(filepath: str) -> None:
             file.write(text + "\n")
 
 
-analyzed_data = []
 if "-d" in data:
-    for i in range(len(data)):
-        if data[i] != "-f":
-            analyzed_data.append(data[i])
-        else:
-            path = "/".join(analyzed_data[2:])
-            os.makedirs(path)
-            current_filepath = os.path.join(path, data[i + 1] + ".txt")
-            write_file(current_filepath)
+    analyzed_data = []
+    for item in data:
+        if item == "-f":
             break
-    os.makedirs("/".join(analyzed_data[2:]), exist_ok=True)
+        analyzed_data.append(item)
+    if "-f" in data:
+        path = os.path.join(*analyzed_data[2:])
+        os.makedirs(path)
+        current_filepath = os.path.join(path, data[-1] + ".txt")
+        write_file(str(current_filepath))
+    else:
+        os.makedirs(os.path.join(*analyzed_data[2:]), exist_ok=True)
 
 if "-f" in data and "-d" not in data:
     write_file(data[-1] + ".txt")
