fee429b6458918fb60c521625615c788b7495987
Victor Matveyshin <vic_m@ukr.net>
2026-05-03 15:33:20 +0300

    tests passed

--- app/create_file.py
+++ app/create_file.py
@@ -1,62 +1,73 @@
-from sys import argv, exit
-from os import makedirs, path  # , getcwd
+from sys import argv
+from os import makedirs, path
 from datetime import datetime
+from typing import Any
 
-# argv = ["app/create_file.py", "-f   ", " file.txt"]
 
-if len(argv) <= 2 :
-    print("Usage: -f <filename> or/and -d <directory>")
-    exit(0)
+def parse_arguments(arg_list: list) -> dict:
+    parsed_dict = {
+        "filename": "",
+        "directory": [],
+        "flag_f": False,
+        "flag_d": False,
+        "info": ""
+    }
 
-filename = ""
-directory = []
-stop = False
-content = []
-line_number = 1
+    if len(argv) <= 2:
+        parsed_dict["info"] = "Usage: -f <filename> or/and -d <directory>"
+        return parsed_dict
 
-del argv[0]
+    for index in range(len(arg_list)):
+        if arg_list[index] == "-f":
+            try:
+                parsed_dict["filename"] = arg_list[index + 1]
+                del arg_list[index: index + 2]
+                parsed_dict["flag_f"] = True
+                break
+            except IndexError:
+                parsed_dict["info"] = "No filename"
+                return parsed_dict
 
-for index in range(len(argv)):
-    if argv[index] == "-f":
-        try:
-            filename = argv[index + 1].strip().lower()
-            del argv[index: index + 2]
+    for index in range(len(arg_list)):
+        if arg_list[index] == "-d":
+            if index == len(arg_list) - 1:
+                parsed_dict["info"] = "No directory name(s)"
+                return parsed_dict
+            parsed_dict["directory"] = [name for name in arg_list[index + 1:]]
+            parsed_dict["flag_d"] = True
             break
-        except IndexError:
-            print("Error: correct format -f <filename>")
-            exit(1)
 
-for index in range(len(argv)):
-    if argv[index].strip() == "-d":
-        if index == len(argv) - 1:
-            print("Error: correct format -d <directory name(s)>")
-            exit(1)
-        directory = [name.strip() for name in argv[index + 1:]]
+    return parsed_dict
 
-# print(f"Path: {path.join(*directory, filename)}")
-# print(f"file: {filename}")
 
-if directory != []:
-    makedirs(path.join(*directory), exist_ok=True)
+def create_file() -> Any:
 
-if filename != "":
-    full_path = path.join(*directory, filename)
+    arguments = parse_arguments([arg.strip() for arg in argv[1:]])
+    print(arguments)
+    if arguments["info"] != "":
+        return
 
-    while not stop:
-        line = input("Enter content line: ")
-        if line.lower() == "stop":
-            stop = True
-        else:
-            content.append(f"\n{line_number} {line}")
-            line_number += 1
-    # content.append("\n")
+    if arguments["flag_d"]:
+        makedirs(path.join(*arguments["directory"]), exist_ok=True)
 
-    # current_dir = getcwd()
-    # print(current_dir)
+    if arguments["flag_f"]:
+        full_path = path.join(*arguments["directory"], arguments["filename"])
 
-    with open(full_path, "a") as f:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        for line in content:
-            f.write(line)
+        stop = False
+        line_number = 1
 
-# print("Finish")
+        new_line = "\n\n" if path.isfile(full_path) else ""
+
+        with open(full_path, "a") as f:
+            f.write(new_line + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+            while not stop:
+                line = input("Enter content line: ")
+                if line.lower() == "stop":
+                    stop = True
+                else:
+                    f.write(f"\n{line_number} {line}")
+                    line_number += 1
+
+
+create_file()
