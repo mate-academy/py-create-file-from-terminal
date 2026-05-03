e8e3e750d730badb006d9d5d3d8f3e1d6606f92b
Victor Matveyshin <vic_m@ukr.net>
2026-05-03 12:34:28 +0300

    no 4 test

--- app/create_file.py
+++ app/create_file.py
@@ -1,8 +1,8 @@
 from sys import argv, exit
-from os import makedirs, path
+from os import makedirs, path, getcwd
 from datetime import datetime
 
-#argv = ["app/create_file.py", " file.txt", "-f   ", " file.txt"]
+#argv = ["app/create_file.py", "-f   ", " file.txt"]
 
 if len(argv) <= 2 :
     print("Usage: -f <filename> or/and -d <directory>")
@@ -17,7 +17,8 @@ line_number = 1
 del argv[0]
 
 for index in range(len(argv)):
-    if argv[index].strip() == "-f":
+    n = argv[index]
+    if n.strip() == "-f":
         try:
             filename = argv[index + 1].strip().lower()
             del argv[index: index + 2]
@@ -33,8 +34,8 @@ for index in range(len(argv)):
             exit(1)
         directory = [name.strip() for name in argv[index + 1:]]
 
-print(f"Path: {path.join(*directory, filename)}")
-print(f"file: {filename}")
+# print(f"Path: {path.join(*directory, filename)}")
+# print(f"file: {filename}")
 
 if directory != []:
     makedirs(path.join(*directory), exist_ok=True)
@@ -47,12 +48,16 @@ if filename != "":
         if line.lower() == "stop":
             stop = True
         else:
-            content.append(f"{line_number} {line}\n")
+            content.append(f"\n{line_number} {line}")
             line_number += 1
-    content.append("\n")
+    #content.append("\n")
+
+    # current_dir = getcwd()
+    # print(current_dir)
 
     with open(full_path, "a") as f:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         for line in content:
             f.write(line)
 
+#print("Finish")
